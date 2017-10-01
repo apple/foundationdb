696d432462d8c6d42f443e11fad228dc27e57c6d
Evan Tschannen <ejt@apple.com>
2017-10-01 16:29:32 -0700

    Revert "fix: excluded servers are worst fit for master rather than never assign (so that we can recover if every process has been excluded)"
    This reverts commit 83b2ce68c8e1a29fc1559598cc38d3ef7eb46101.

--- fdbserver/ClusterController.actor.cpp
+++ fdbserver/ClusterController.actor.cpp
@@ -194,10 +194,7 @@ public:
 		int numEquivalent = 1;
 		for( auto& it : id_worker ) {
 			auto fit = it.second.processClass.machineClassFitness( ProcessClass::Master );
-			if(conf.isExcludedServer(it.second.interf.address())) {
-				fit = std::max(fit, ProcessClass::WorstFit);
-			}
-			if( workerAvailable(it.second, checkStable) && fit != ProcessClass::NeverAssign ) {
+			if( workerAvailable(it.second, checkStable) && !conf.isExcludedServer(it.second.interf.address()) && fit != ProcessClass::NeverAssign ) {
 				if( fit < bestFit ) {
 					bestInfo = std::make_pair(it.second.interf, it.second.processClass);
 					bestFit = fit;
@@ -806,6 +803,7 @@ ACTOR Future<Void> clusterWatchDatabase( ClusterControllerData* cluster, Cluster
 				iMaster = newMaster.get();
 
 				db->masterRegistrationCount = 0;
+				db->config = DatabaseConfiguration();
 				db->forceMasterFailure = Promise<Void>();
 
 				auto dbInfo = ServerDBInfo( LiteralStringRef("DB") );
