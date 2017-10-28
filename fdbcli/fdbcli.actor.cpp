e0d33ef8d7a6aa24b280a7fb236030906c35d69b
Alex Miller <alexmiller@apple.com>
2017-10-27 17:20:19 -0700

    Preemptively fix profiler-related valgrind errors/straight out bugs.
    I forgot to initialize some fields in requests.

--- fdbcli/fdbcli.actor.cpp
+++ fdbcli/fdbcli.actor.cpp
@@ -2671,6 +2671,7 @@ ACTOR Future<int> cli(CLIOptions opt, LineNoise* plinenoise) {
 								for (const auto& pair : interfaces) {
 									ProfilerRequest profileRequest;
 									profileRequest.type = ProfilerRequest::Type::FLOW;
+									profileRequest.action = ProfilerRequest::Action::RUN;
 									profileRequest.duration = duration;
 									profileRequest.outputFile = tokens[4];
 									all_profiler_addresses.push_back(pair.first);
@@ -2688,6 +2689,7 @@ ACTOR Future<int> cli(CLIOptions opt, LineNoise* plinenoise) {
 									for (int tokenidx = 5; tokenidx < tokens.size(); tokenidx++) {
 										ProfilerRequest profileRequest;
 										profileRequest.type = ProfilerRequest::Type::FLOW;
+										profileRequest.action = ProfilerRequest::Action::RUN;
 										profileRequest.duration = duration;
 										profileRequest.outputFile = tokens[4];
 										all_profiler_addresses.push_back(tokens[tokenidx]);
