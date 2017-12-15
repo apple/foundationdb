83b21cc57b8646c48b8fb9e9a5e82b7749f29242
A.J. Beamon <ajbeamon@apple.com>
2017-12-15 11:00:29 -0800

    Set the thread name for threads created by our default executor in the Java bindings.

--- bindings/java/src/main/com/apple/foundationdb/FDB.java
+++ bindings/java/src/main/com/apple/foundationdb/FDB.java
@@ -25,6 +25,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * The starting point for accessing FoundationDB.
@@ -63,6 +64,7 @@ public class FDB {
 
 	static class DaemonThreadFactory implements ThreadFactory {
 		private final ThreadFactory factory;
+		private static AtomicInteger threadCount = new AtomicInteger();
 
 		DaemonThreadFactory(ThreadFactory factory) {
 			this.factory = factory;
@@ -71,6 +73,7 @@ public class FDB {
 		@Override
 		public Thread newThread(Runnable r) {
 			Thread t = factory.newThread(r);
+			t.setName("fdb-java-" + threadCount.incrementAndGet());
 			t.setDaemon(true);
 			return t;
 		}
