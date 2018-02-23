a1382895a6bf811ba01165637ee36e44a698226f
Alvin Moore <alvinm@apple.com>
2018-02-23 04:50:23 -0800

    Fixed headers and some whitespace

--- fdbrpc/libb64/encode.h
+++ fdbrpc/libb64/encode.h
@@ -1,23 +1,3 @@
-/*
- * encode.h
- *
- * This source file is part of the FoundationDB open source project
- *
- * Copyright 2013-2018 Apple Inc. and the FoundationDB project authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 // :mode=c++:
 /*
 encode.h - c++ wrapper for a base64 encoding algorithm
@@ -106,4 +86,3 @@ namespace base64
 } // namespace base64
 
 #endif // BASE64_ENCODE_H
-
