5cc8745c3c0826c39b73bf409a509a2f8b83ef76
Alec Grieser <agrieser@apple.com>
2018-02-23 18:17:53 -0800

    use littleEndian16/32 instead of rolling our own

--- fdbclient/RYWIterator.h
+++ fdbclient/RYWIterator.h
@@ -88,14 +88,12 @@ public:
 	static ValueRef getRandomVersionstampValuePos(Arena& arena) {
 		int len = g_random->randomInt(10, 98);
 		std::string value = std::string(len, 'x');
-		int pos = g_random->randomInt(0, len - 9);
+		int32_t pos = g_random->randomInt(0, len - 9);
 		if (g_random->random01() < 0.01) {
 			pos = value.size() - 10;
 		}
-		value += (char)(pos & 0xFF);
-		value += (char)((pos >> 8) & 0xFF);
-		value += (char)((pos >> 16) & 0xFF);
-		value += (char)((pos >> 24) & 0xFF);
+		pos = littleEndian32(pos);
+		value += std::string((const char*)&pos, sizeof(int32_t));
 		return ValueRef(arena, value);
 	}
 
@@ -106,15 +104,15 @@ public:
 			key += '\x00';
 		if (idx % 3 >= 2)
 			key += '\x00';
-		int pos = key.size() - g_random->randomInt(0, 3);
+		int16_t pos = key.size() - g_random->randomInt(0, 3);
 		if (g_random->random01() < 0.01) {
 			pos = 0;
 		}
 		key = key.substr(0, pos);
 		key += "XXXXXXXXYY";
 		key += std::string(g_random->randomInt(0, 3), 'z');
-		key += (char)(pos & 0xFF);
-		key += (char)((pos >> 8) & 0xFF);
+		pos = littleEndian16(pos);
+		key += std::string((const char*)&pos, sizeof(int16_t));
 		return ValueRef(arena, key);
 	}
 
