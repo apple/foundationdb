11df1aeabf2ea4c3efb5dbbd177984ff09ca11df
Balachandar Namasivayam <bnamasivayam@apple.com>
2018-03-02 16:50:30 -0800

    Add new api to get shared tlogs id and address

--- fdbserver/MasterInterface.h
+++ fdbserver/MasterInterface.h
@@ -80,6 +80,8 @@ struct TLogRejoinRequest {
 	DBRecoveryCount recoveryCount;
 	ReplyPromise<bool> reply;   // false means someone else registered, so we should re-register.  true means this master is recovered, so don't send again to the same master.
 
+	TLogRejoinRequest() { }
+	explicit TLogRejoinRequest(const TLogInterface &interf) : myInterface(interf) { }
 	template <class Ar>
 	void serialize(Ar& ar) {
 		ar & myInterface & reply;
