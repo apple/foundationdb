10faffd8ff09020fcbb0ee34cd31809006cf0135
Markus Pilman <markus.pilman@snowflake.net>
2018-05-05 09:38:09 -0700

    Several small compilation fixes for new versions of gcc
    There are several missing includes for cmath in the code, I added those.
    
    Next, Coro returns a reference to a stack variable and this causes a
    warning. As this is probably ok for Coro, I disabled the warning in
    that file for GCC. I want to have this warning in the build system as
    it is generally a very useful warning to have.
    
    Another change is that major and minor are deprecated for a while now.
    I replaced those with gnu_dev_major and gnu_dev_minor.
    
    ErrorOr currently implements operators ==, !=, and <. These do not
    compile because Error does not implement ==. This compiles on older
    versions of gcc and clang because ErrorOr<T>::operator== is not used
    anywhere. It is still wrong though and newer gcc versions complain.
    I simply removed these methods.
    
    The most interesting fix is that TraceEvent::~TraceEvent is currently
    throwing exceptions. This is illegal behavior in C++11 and a idea in
    older versions of C++. For now I simply removed the throw, but this
    might need some more thought.

--- flow/Trace.cpp
+++ flow/Trace.cpp
@@ -931,7 +931,9 @@ TraceEvent::~TraceEvent() {
 	} catch( Error &e ) {
 		TraceEvent(SevError, "TraceEventDestructorError").error(e,true);
 		delete tmpEventMetric;
-		throw;
+		// TODO: it is illegal in C++11 to throw an exception in a destructor
+		// so we need to figure out what to do here
+		//throw;
 	}
 	delete tmpEventMetric;
 }
