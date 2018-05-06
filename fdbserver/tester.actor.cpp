7ed64c821ed06096503ba9d3d2bca00063460ef7
Evan Tschannen <ejt@apple.com>
2018-05-05 17:20:48 -0700

    fix: recruiting a cluster controller takes longer after restarting tests because we wait until files have recovered from disk before starting

--- fdbserver/tester.actor.cpp
+++ fdbserver/tester.actor.cpp
@@ -1093,7 +1093,7 @@ ACTOR Future<Void> runTests( Reference<AsyncVar<Optional<struct ClusterControlle
 		Reference<AsyncVar<Optional<struct ClusterInterface>>> ci, vector<TestSpec> tests, test_location_t at, 
 		int minTestersExpected, StringRef startingConfiguration, LocalityData locality ) {
 	state int flags = (at == TEST_ON_SERVERS ? 0 : GetWorkersRequest::TESTER_CLASS_ONLY) | GetWorkersRequest::NON_EXCLUDED_PROCESSES_ONLY;
-	state Future<Void> testerTimeout = delay(60.0); // wait 60 sec for testers to show up
+	state Future<Void> testerTimeout = delay(600.0); // wait 600 sec for testers to show up
 	state vector<std::pair<WorkerInterface, ProcessClass>> workers;
 
 	loop {
