3fdf367ccc539b5635b782d3a083fa5ddfda06c3
Christophe Chevalier <chevalier@doxense.com>
2018-06-18 22:00:25 +0200

    Convert ActorCompiler project to new style csproj and target netcoreapp2.0

--- flow/actorcompiler/Properties/AssemblyInfo.cs
+++ flow/actorcompiler/Properties/AssemblyInfo.cs
@@ -18,39 +18,8 @@
  * limitations under the License.
  */
 
-using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("actorcompiler")]
-[assembly: AssemblyDescription("Compile Flow code to C++")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Apple Inc")]
-[assembly: AssemblyProduct("actorcompiler")]
-[assembly: AssemblyCopyright("Copyright (c) 2013-2017 Apple Inc.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("fd8d34c8-46ca-43de-bfb3-b437e46943da")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
