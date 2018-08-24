aba62cebb84185064bdc768b45901b4c6d9190ce
Markus Pilman <markus.pilman@snowflake.net>
2018-08-24 16:09:34 -0700

    Reverted earlier changes to actorcompiler

--- flow/actorcompiler.h
+++ flow/actorcompiler.h
@@ -17,11 +17,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#pragma once
 
-#include <boost/preprocessor.hpp>
-#define FLOW_ACOMPILER_STATE 0
-#define FLOW_EMPTY_STR
+#ifndef POST_ACTOR_COMPILER
 
 template<typename T> struct Future;
 struct Never;
@@ -40,28 +37,8 @@ void wait(const Never&);
 template <class T> T waitNext( const FutureStream<T>& );
 #endif
 
-// These are for intellisense to do proper type inferring, etc. They are no
-// included at build time.
-#ifndef NO_INTELLISENSE
-#define ACTOR BOOST_PP_IF(FLOW_ACOMPILER_STATE, FLOW_EMPTY_STR, ACTOR)
-#define state BOOST_PP_IF(FLOW_ACOMPILER_STATE, FLOW_EMPTY_STR, state)
-#define UNCANCELLABLE BOOST_PP_IF(FLOW_ACOMPILER_STATE, FLOW_EMPTY_STR, UNCANCELLABLE)
-#define FLOW_CHOOSE if (1)
-#define FLOW_WHEN(...) for (__VA_ARGS__;;)
-#define choose BOOST_PP_IF(FLOW_ACOMPILER_STATE, FLOW_EMPTY_STR, FLOW_CHOSE)
-#define when(...) BOOST_PP_IF(FLOW_ACOMPILER_STATE, FLOW_WHEN(__VA_ARGS__), when(__VA_ARGS__))
-Void wait(const Never&);
-template <class T>
-typename T::type wait(const T&);
-template <class T>
-typename T::type waitNext(const T&);
-#define COMBINE1(x, y) x##y
-#define COMBINE(x, y) COMBINE1(x, y)
-#define _uvar COMBINE(uniqueVar, __COUNTER__)
-#else
-#define _uvar _
 #endif
 
 #define loop while(true)
 
-#pragma warning(disable : 4355) // 'this' : used in base member initializer list
+#pragma warning( disable: 4355 )	// 'this' : used in base member initializer list
