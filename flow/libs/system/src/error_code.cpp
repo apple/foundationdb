b8fed3d938d09ba13112f3db3a2217d0f32cbc5a
wolfspider <jesse.alexander.bennett@gmail.com>
2019-01-29 02:20:08 -0500

    fix for boost for now

--- flow/libs/system/src/error_code.cpp
+++ flow/libs/system/src/error_code.cpp
@@ -43,6 +43,7 @@ using namespace boost::system::errc;
 #endif
 //----------------------------------------------------------------------------//
 
+#if BOOST_VERSION < 106800
 namespace
 {
 #if defined(__PGI)
@@ -434,3 +435,4 @@ namespace boost
 
   } // namespace system
 } // namespace boost
+#endif
