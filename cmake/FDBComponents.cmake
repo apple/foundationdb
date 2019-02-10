9ad2ffaef63424bf805376a9d31e3b1a08b70ff3
mpilman <markus.pilman@snowflake.com>
2019-02-09 22:08:34 -0800

    flow, python, and go bindings

--- cmake/FDBComponents.cmake
+++ cmake/FDBComponents.cmake
@@ -1,17 +1,4 @@
 ################################################################################
-# Java Bindings
-################################################################################
-
-set(BUILD_JAVA OFF)
-find_package(JNI 1.8 REQUIRED)
-find_package(Java 1.8 COMPONENTS Development)
-if(JNI_FOUND AND Java_FOUND AND Java_Development_FOUND)
-  set(BUILD_JAVA ON)
-  include(UseJava)
-  enable_language(Java)
-endif()
-
-################################################################################
 # LibreSSL
 ################################################################################
 
@@ -30,13 +17,62 @@ else()
   endif()
 endif()
 
+################################################################################
+# Java Bindings
+################################################################################
+
+set(BUILD_JAVA OFF)
+find_package(JNI 1.8 REQUIRED)
+find_package(Java 1.8 COMPONENTS Development)
+if(JNI_FOUND AND Java_FOUND AND Java_Development_FOUND)
+  set(BUILD_JAVA ON)
+  include(UseJava)
+  enable_language(Java)
+endif()
+
+################################################################################
+# Python Bindings
+################################################################################
+
+find_package(Python COMPONENTS Interpreter)
+if(Python_Interpreter_FOUND)
+  set(WITH_PYTHON ON)
+else()
+  set(WITH_PYTHON OFF)
+endif()
+
+################################################################################
+# Pip
+################################################################################
+
+find_package(Virtualenv)
+if (Virtualenv_FOUND)
+  set(BUILD_DOCUMENTATION ON)
+endif()
+
+################################################################################
+# GO
+################################################################################
+
+find_program(GO_EXECUTABLE go)
+if(GO_EXECUTABLE)
+  set(WITH_GO ON)
+else()
+  set(WITH_GO OFF)
+endif()
+
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/packages)
+add_custom_target(packages)
+
 
 function(print_components)
-  message(STATUS "=============================")
+  message(STATUS "=========================================")
   message(STATUS "   Components Build Overview ")
-  message(STATUS "=============================")
-  message(STATUS "Build Python Bindings:    ON")
-  message(STATUS "Build Java Bindings:      ${BUILD_JAVA}")
-  message(STATUS "Build with TLS support:   ${WITH_TLS}")
-  message(STATUS "=============================")
+  message(STATUS "=========================================")
+  message(STATUS "Build Java Bindings:                  ${BUILD_JAoA}")
+  message(STATUS "Build with TLS support:               ${WITH_TLS}")
+  message(STATUS "Build GO bindings:                    ${WITH_GO}")
+  message(STATUS "Build Python sdist (make package):    ${WITH_PYTHON}")
+  message(STATUS "Build Documentation (make html):      ${BUILD_DOCUMENTATION}")
+  message(STATUS "=========================================")
 endfunction()
