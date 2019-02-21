5dcde9efe0a6cb6ce22bd3b90de4507a3e887715
Jingyu Zhou <jingyuzhou@gmail.com>
2019-02-21 15:30:39 -0800

    Fix locality per review comment and a mac compile error

--- fdbrpc/Locality.cpp
+++ fdbrpc/Locality.cpp
@@ -164,49 +164,35 @@ ProcessClass::Fitness ProcessClass::machineClassFitness( ClusterRole role ) cons
 		}
 	case ProcessClass::DataDistributor:
 		switch( _class ) {
-			case ProcessClass::DataDistributorClass:
-				return ProcessClass::BestFit;
-			case ProcessClass::StatelessClass:
-				return ProcessClass::GoodFit;
-			case ProcessClass::MasterClass:
-				return ProcessClass::OkayFit;
-			case ProcessClass::ResolutionClass:
-				return ProcessClass::OkayFit;
-			case ProcessClass::TransactionClass:
-				return ProcessClass::OkayFit;
-			case ProcessClass::ProxyClass:
-				return ProcessClass::OkayFit;
-			case ProcessClass::UnsetClass:
-				return ProcessClass::UnsetFit;
-			case ProcessClass::CoordinatorClass:
-				return ProcessClass::NeverAssign;
-			case ProcessClass::TesterClass:
-				return ProcessClass::NeverAssign;
-			default:
-				return ProcessClass::WorstFit;
+		case ProcessClass::DataDistributorClass:
+			return ProcessClass::BestFit;
+		case ProcessClass::StatelessClass:
+			return ProcessClass::GoodFit;
+		case ProcessClass::MasterClass:
+			return ProcessClass::OkayFit;
+		case ProcessClass::UnsetClass:
+			return ProcessClass::UnsetFit;
+		case ProcessClass::CoordinatorClass:
+		case ProcessClass::TesterClass:
+			return ProcessClass::NeverAssign;
+		default:
+			return ProcessClass::WorstFit;
 		}
 	case ProcessClass::RateKeeper:
 		switch( _class ) {
-			case ProcessClass::RateKeeperClass:
-				return ProcessClass::BestFit;
-			case ProcessClass::StatelessClass:
-				return ProcessClass::GoodFit;
-			case ProcessClass::MasterClass:
-				return ProcessClass::OkayFit;
-			case ProcessClass::ResolutionClass:
-				return ProcessClass::OkayFit;
-			case ProcessClass::TransactionClass:
-				return ProcessClass::OkayFit;
-			case ProcessClass::ProxyClass:
-				return ProcessClass::OkayFit;
-			case ProcessClass::UnsetClass:
-				return ProcessClass::UnsetFit;
-			case ProcessClass::CoordinatorClass:
-				return ProcessClass::NeverAssign;
-			case ProcessClass::TesterClass:
-				return ProcessClass::NeverAssign;
-			default:
-				return ProcessClass::WorstFit;
+		case ProcessClass::RateKeeperClass:
+			return ProcessClass::BestFit;
+		case ProcessClass::StatelessClass:
+			return ProcessClass::GoodFit;
+		case ProcessClass::MasterClass:
+			return ProcessClass::OkayFit;
+		case ProcessClass::UnsetClass:
+			return ProcessClass::UnsetFit;
+		case ProcessClass::CoordinatorClass:
+		case ProcessClass::TesterClass:
+			return ProcessClass::NeverAssign;
+		default:
+			return ProcessClass::WorstFit;
 		}
 	default:
 		return ProcessClass::NeverAssign;
