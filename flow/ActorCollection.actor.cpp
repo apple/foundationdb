c9ee7f54e7462198afe5228e0443d6752e38e357
mpilman <markus.pilman@snowflake.com>
2019-03-15 10:24:26 -0700

    Add compound type to micro-benchmark

--- flow/ActorCollection.actor.cpp
+++ flow/ActorCollection.actor.cpp
@@ -60,24 +60,55 @@ ACTOR Future<Void> actorCollection( FutureStream<Future<Void>> addActor, int* pC
 	}
 }
 
+template<>
+struct Traceable<std::string> : std::true_type {
+	template<class Str>
+	static std::string toString(Str&& s) {
+		return std::forward<Str>(s);
+	}
+};
+
+template<class T, class U>
+struct Traceable<std::pair<T, U>> {
+	static constexpr bool value = Traceable<T>::value && Traceable<U>::value;
+	static std::string toString(const std::pair<T, U>& p) {
+		auto tStr = Traceable<T>::toString(p.first);
+		auto uStr = Traceable<U>::toString(p.second);
+		std::string result(tStr.size() + uStr.size() + 3, 'x');
+		std::copy(tStr.begin(), tStr.end(), result.begin());
+		auto iter = result.begin() + tStr.size();
+		*(iter++) = ' ';
+		*(iter++) = '-';
+		*(iter++) = ' ';
+		std::copy(uStr.begin(), uStr.end(), iter);
+		return result;
+	}
+};
+
+
 TEST_CASE("/flow/TraceEvent") {
 	state unsigned i;
 	state double startTime;
 	state std::vector<std::string> strings;
-	strings.reserve(1000);
+	strings.reserve(10000);
 	for (i = 0; i < 100; ++i) {
 		for (int j = 0; j < 100; ++j) {
 			strings.emplace_back(g_random->randomAlphaNumeric(g_random->randomInt(1, 30)));
 		}
+		wait(delay(0));
 	}
-	wait(delay(0));
+	TraceEvent("pairsfilled")
+		.detail("MemoryUsage", getMemoryUsage());
 	startTime = g_network->now();
 	for (i = 0; i < 100000; ++i) {
 		for (unsigned j = 0; j < 100; ++j) {
+			StringRef key(strings[g_random->randomInt(0, strings.size())]);
+			auto p = std::make_pair(key, g_random->randomInt(-1000, 1000));
 			TraceEvent("TestTraceLineNoDebug")
 				.detail("Num", g_random->randomInt(0, 1000))
 				.detail("Double", g_random->random01())
-				.detail("hello", strings[g_random->randomInt(0, strings.size())]);
+				.detail("str", strings[g_random->randomInt(0, strings.size())])
+				.detail("pair", p);
 		}
 		wait(delay(0));
 	}
@@ -86,10 +117,13 @@ TEST_CASE("/flow/TraceEvent") {
 	startTime = g_network->now();
 	for (i = 0; i < 1000000; ++i) {
 		for (unsigned j = 0; j < 100; ++j) {
-			TraceEvent(SevDebug, "TestTraceLineNoDebug")
+			StringRef key(strings[g_random->randomInt(0, strings.size())]);
+			auto p = std::make_pair(key, g_random->randomInt(-1000, 1000));
+			TraceEvent(SevDebug, "TestTraceLineDebug")
 				.detail("Num", g_random->randomInt(0, 1000))
 				.detail("Double", g_random->random01())
-				.detail("hello", strings[g_random->randomInt(0, strings.size())]);
+				.detail("str", strings[g_random->randomInt(0, strings.size())])
+				.detail("pair", p);
 		}
 		wait(delay(0));
 	}
