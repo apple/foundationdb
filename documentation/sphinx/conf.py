ab96a44a1c7e5f2c5d6c7c1b426187d952c3dcfe
A.J. Beamon <ajbeamon@apple.com>
2019-05-22 13:46:58 -0700

    There are no custom static files, so don't specify a static path.

--- documentation/sphinx/conf.py
+++ documentation/sphinx/conf.py
@@ -143,7 +143,7 @@ html_title = 'FoundationDB ' + version
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [sys.prefix + '/_static']
+#html_static_path = [sys.prefix + '/_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
