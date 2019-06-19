7b12374a87d11ea103e9be89a13766685d5c8590
Alec Grieser <agrieser@apple.com>
2019-06-18 18:36:12 -0700

    Fixes #1690: Server docker image hard-codes 4500 in a few places
    This makes the default public port for starting FDB processes the same as the FDB_PORT. This is probably necessary given #1714, especially for coordinators, though it might not be necessary for other processes in the cluster. This can *almost* be used to start up multiple FDB processes locally and then access them from the same machine, but that (unfortunately) requires both the other processes in the docker compose network and the host machine to agree on what IP to use for the coordinator. But as that machine has different IPs in those networks, they cannot be made to agree.

--- packaging/docker/create_cluster_file.bash
+++ packaging/docker/create_cluster_file.bash
@@ -39,7 +39,7 @@ function create_cluster_file() {
 			echo "Failed to look up coordinator address for $FDB_COORDINATOR" 1>&2
 			exit 1
 		fi
-		echo "docker:docker@$coordinator_ip:4500" > $FDB_CLUSTER_FILE
+		echo "docker:docker@$coordinator_ip:$FDB_COORDINATOR_PORT" > $FDB_CLUSTER_FILE
 	else
 		echo "FDB_COORDINATOR environment variable not defined" 1>&2
 		exit 1
