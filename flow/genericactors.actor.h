4b79d54f8350433c4d3ed7d66bd5f0e3173b2216
sramamoorthy <sramamoorthy@snowflake.net>
2019-06-27 13:53:42 -0700

    timeoutError that takes Future<Error<T>> as input

--- flow/genericactors.actor.h
+++ flow/genericactors.actor.h
@@ -198,7 +198,21 @@ Future<T> timeoutError( Future<T> what, double time, TaskPriority taskID = TaskP
 	}
 }
 
+
 ACTOR template <class T>
+Future<T> timeoutError( Future<ErrorOr<T>> what, double time, TaskPriority taskID = TaskPriority::DefaultDelay ) {
+	Future<Void> end = delay( time, taskID );
+	choose {
+		when( ErrorOr<T> t = wait( what ) ) {
+			if (t.isError()) {
+				throw t.getError();
+			}
+			return t.get();
+		}
+		when( wait( end ) ) { throw timed_out(); }
+	}
+}
+
 Future<T> delayed( Future<T> what, double time = 0.0, TaskPriority taskID = TaskPriority::DefaultDelay  ) {
 	try {
 		state T t = wait( what );
