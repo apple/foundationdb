e7e48a40ceb51061fd38faaa1f38d85b89cfec51
Andrew Noyes <anoyes34@gmail.com>
2019-07-16 13:28:29 -0700

    Fix a few IncludeVersion bugs

--- fdbclient/MonitorLeader.actor.cpp
+++ fdbclient/MonitorLeader.actor.cpp
@@ -246,10 +246,10 @@ TEST_CASE("/flow/FlatBuffers/LeaderInfo") {
 			}
 			in.serializedInfo = rndString;
 		}
-		ObjectWriter writer(Unversioned());
+		ObjectWriter writer(IncludeVersion());
 		writer.serialize(in);
 		Standalone<StringRef> copy = writer.toStringRef();
-		ArenaObjectReader reader(copy.arena(), copy, Unversioned());
+		ArenaObjectReader reader(copy.arena(), copy, IncludeVersion());
 		reader.deserialize(out);
 		ASSERT(in.forward == out.forward);
 		ASSERT(in.changeID == out.changeID);
@@ -268,10 +268,10 @@ TEST_CASE("/flow/FlatBuffers/LeaderInfo") {
 	ErrorOr<EnsureTable<Optional<LeaderInfo>>> objIn(leaderInfo);
 	ErrorOr<EnsureTable<Optional<LeaderInfo>>> objOut;
 	Standalone<StringRef> copy;
-	ObjectWriter writer(Unversioned());
+	ObjectWriter writer(IncludeVersion());
 	writer.serialize(objIn);
 	copy = writer.toStringRef();
-	ArenaObjectReader reader(copy.arena(), copy, Unversioned());
+	ArenaObjectReader reader(copy.arena(), copy, IncludeVersion());
 	reader.deserialize(objOut);
 
 	ASSERT(!objOut.isError());
