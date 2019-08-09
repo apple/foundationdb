b17ad0ad64104f3042b36d32ddd5becd95a2f5d6
Andrew Noyes <anoyes34@gmail.com>
2019-08-09 13:02:41 -0700

    Enable -Wunused-value

--- cmake/ConfigureCompiler.cmake
+++ cmake/ConfigureCompiler.cmake
@@ -163,7 +163,6 @@ else()
       -Wno-unknown-pragmas
       -Wno-delete-non-virtual-dtor
       -Wno-undefined-var-template
-      -Wno-unused-value
       -Wno-tautological-pointer-compare
       -Wno-format)
   endif()
