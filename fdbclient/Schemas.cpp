2d216f7ae54c32873b43b0b116ced73afa90575a
Trevor Clinkenbeard <trevorclinkenbeard@gmail.com>
2019-09-06 14:26:25 -0700

    Added database_lock_state to statusSchema

--- fdbclient/Schemas.cpp
+++ fdbclient/Schemas.cpp
@@ -297,7 +297,10 @@ const KeyRef JSONSchemas::statusSchema = LiteralStringRef(R"statusSchema(
       },
       "degraded_processes":0,
       "database_available":true,
-      "database_locked":false,
+      "database_lock_state": {
+         "locked": true,
+         "lockUID": "00000000000000000000000000000000"
+      },
       "generation":2,
       "latency_probe":{
          "read_seconds":7,
