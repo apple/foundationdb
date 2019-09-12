36fd1ec0e4cc5358fb2c064c4b47c3eb8b3e3593
Andrew Noyes <anoyes34@gmail.com>
2019-09-12 10:53:17 -0700

    Add sealed as a valid class-virt-specifier

--- flow/actorcompiler/ActorParser.cs
+++ flow/actorcompiler/ActorParser.cs
@@ -310,7 +310,7 @@ namespace actorcompiler
                 first = toks.First(NonWhitespace);
             }
             // http://nongnu.org/hcb/#class-virt-specifier-seq
-            toks = toks.SkipWhile(t => Whitespace(t) || t.Value == "final" || t.Value == "explicit");
+            toks = toks.SkipWhile(t => Whitespace(t) || t.Value == "final" || t.Value == "explicit" || t.Value == "sealed");
 
             first = toks.First(NonWhitespace);
             if (first.Value == ":" || first.Value == "{") {
