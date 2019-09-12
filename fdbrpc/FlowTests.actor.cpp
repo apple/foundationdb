ab650c6fe6cb6b42926451e7942dd720df54db82
Andrew Noyes <anoyes34@gmail.com>
2019-09-12 09:21:17 -0700

    Make test resilient to changes elsewhere in file

--- fdbrpc/FlowTests.actor.cpp
+++ fdbrpc/FlowTests.actor.cpp
@@ -31,17 +31,18 @@ void forceLinkFlowTests() {}
 
 using std::vector;
 
+constexpr int firstLine = __LINE__;
 TEST_CASE("/flow/actorcompiler/lineNumbers") {
 	loop {
 		try {
-			ASSERT(__LINE__ == 37);
+			ASSERT(__LINE__ == firstLine + 4);
 			wait(Future<Void>(Void()));
-			ASSERT(__LINE__ == 39);
+			ASSERT(__LINE__ == firstLine + 6);
 			throw success();
 		} catch (Error& e) {
-			ASSERT(__LINE__ == 42);
+			ASSERT(__LINE__ == firstLine + 9);
 			wait(Future<Void>(Void()));
-			ASSERT(__LINE__ == 44);
+			ASSERT(__LINE__ == firstLine + 11);
 		}
 		break;
 	}
