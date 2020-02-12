1248d2b8b41b629043665f42ade7a278d05abcd1
Andrew Noyes <anoyes34@gmail.com>
2020-02-12 10:41:52 -0800

    Remove USE_OBJECT_SERIALIZER knob

--- flow/Knobs.cpp
+++ flow/Knobs.cpp
@@ -69,7 +69,6 @@ FlowKnobs::FlowKnobs(bool randomize, bool isSimulated) {
 	init( RECONNECTION_TIME_GROWTH_RATE,                       1.2 );
 	init( RECONNECTION_RESET_TIME,                             5.0 );
 	init( CONNECTION_ACCEPT_DELAY,                             0.5 );
-	init( USE_OBJECT_SERIALIZER,                                 1 );
 	init( TOO_MANY_CONNECTIONS_CLOSED_RESET_DELAY,             5.0 );
 	init( TOO_MANY_CONNECTIONS_CLOSED_TIMEOUT,                20.0 );
 	init( PEER_UNAVAILABLE_FOR_LONG_TIME_TIMEOUT,           3600.0 );
