aeba8f5cafbe7532a88797157ef9bfe161fa02de
Andrew Noyes <anoyes34@gmail.com>
2020-02-12 21:56:01 -0800

    Fix symbolify.py

--- bindings/c/symbolify.py
+++ bindings/c/symbolify.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     with open(fdb_c_h, 'r') as f:
         symbols = sorted(set('_' + m.group(1) for m in r.finditer(f.read())))
     with open(symbols_file, 'w') as f:
-        print('\n'.join(symbols), file=f)
+        f.write('\n'.join(symbols))
+        f.write('\n')
