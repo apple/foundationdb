3664c6948b2c5c415e7fd8b6e31589c5f0272895
Jingyu Zhou <jingyuzhou@gmail.com>
2020-02-18 13:21:29 -0800

    Consolidate StringRefReader classes
    Fix a compiler error of unused variable too.

--- fdbclient/BackupAgent.actor.h
+++ fdbclient/BackupAgent.actor.h
@@ -880,6 +880,10 @@ struct StringRefReader {
 	const int32_t consumeNetworkInt32() { return (int32_t)bigEndian32((uint32_t)consume<int32_t>()); }
 	const uint32_t consumeNetworkUInt32() { return bigEndian32(consume<uint32_t>()); }
 
+	// Convert big Endian value (e.g., encoded in log file) into a littleEndian uint64_t value.
+	int64_t consumeNetworkInt64() { return (int64_t)bigEndian64((uint32_t)consume<int64_t>()); }
+	uint64_t consumeNetworkUInt64() { return bigEndian64(consume<uint64_t>()); }
+
 	bool eof() { return rptr == end; }
 
 	const uint8_t *rptr, *end;
--- fdbserver/RestoreRoleCommon.actor.h
+++ fdbserver/RestoreRoleCommon.actor.h
@@ -60,53 +60,6 @@ ACTOR Future<Void> handleHeartbeat(RestoreSimpleRequest req, UID id);
 ACTOR Future<Void> handleInitVersionBatchRequest(RestoreVersionBatchRequest req, Reference<RestoreRoleData> self);
 void handleFinishRestoreRequest(const RestoreFinishRequest& req, Reference<RestoreRoleData> self);
 
-// Helper class for reading restore data from a buffer and throwing the right errors.
-// This struct is mostly copied from StringRefReader. We add a sanity check in this struct.
-// We want to decouple code between fast restore and old restore. So we keep this duplicate struct
-struct BackupStringRefReader {
-	BackupStringRefReader(StringRef s = StringRef(), Error e = Error())
-	  : rptr(s.begin()), end(s.end()), failure_error(e), str_size(s.size()) {}
-
-	// Return remainder of data as a StringRef
-	StringRef remainder() { return StringRef(rptr, end - rptr); }
-
-	// Return a pointer to len bytes at the current read position and advance read pos
-	// Consume a little-Endian data. Since we only run on little-Endian machine, the data on storage is little Endian
-	const uint8_t* consume(unsigned int len) {
-		if (rptr == end && len != 0) throw end_of_stream();
-		const uint8_t* p = rptr;
-		rptr += len;
-		if (rptr > end) {
-			printf("[ERROR] BackupStringRefReader throw error! string length:%d\n", str_size);
-			printf("!!!!!!!!!!!![ERROR]!!!!!!!!!!!!!! Worker may die due to the error. Master will stuck when a worker "
-			       "die\n");
-			throw failure_error;
-		}
-		return p;
-	}
-
-	// Return a T from the current read position and advance read pos
-	template <typename T>
-	const T consume() {
-		return *(const T*)consume(sizeof(T));
-	}
-
-	// Functions for consuming big endian (network byte oselfer) integers.
-	// Consumes a big endian number, swaps it to little endian, and returns it.
-	int32_t consumeNetworkInt32() { return (int32_t)bigEndian32((uint32_t)consume<int32_t>()); }
-	uint32_t consumeNetworkUInt32() { return bigEndian32(consume<uint32_t>()); }
-
-	// Convert big Endian value (e.g., encoded in log file) into a littleEndian uint64_t value.
-	int64_t consumeNetworkInt64() { return (int64_t)bigEndian64((uint32_t)consume<int64_t>()); }
-	uint64_t consumeNetworkUInt64() { return bigEndian64(consume<uint64_t>()); }
-
-	bool eof() { return rptr == end; }
-
-	const uint8_t *rptr, *end;
-	const int str_size;
-	Error failure_error;
-};
-
 class RoleVersionBatchState {
 public:
 	static const int INVALID = -1;
