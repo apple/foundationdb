c92d3ef3a6ff9a534b8dfe8c119faf11e861dc8a
chaoguang <13974480+zjuLcg@users.noreply.github.com>
2020-02-24 11:57:53 -0800

    A draft version, which can be refactored

--- fdbclient/PrivateKeySpace.actor.cpp
+++ fdbclient/PrivateKeySpace.actor.cpp
@@ -4,18 +4,166 @@
 ACTOR Future<Optional<Value>> getActor(
     PrivateKeySpace* pks,
     ReadYourWritesTransaction* ryw,
-    KeyRef key,
-    bool snapshot )
+    KeyRef key )
 {
     // use getRange to workaround this
     Standalone<RangeResultRef> result = wait(pks->getRange(ryw, KeySelector( firstGreaterOrEqual(key) ),
-			KeySelector( firstGreaterOrEqual(keyAfter(key)) ), GetRangeLimits(1), snapshot));
+			KeySelector( firstGreaterOrEqual(keyAfter(key)) ), GetRangeLimits()));
+    ASSERT(result.size() <= 1);
     if (result.size()) {
         return Optional<Value>(result[0].value);
     } else {
         return Optional<Value>();
     }
 }
+
+ACTOR Future<Standalone<RangeResultRef>> getRangeAggregationActor(
+    PrivateKeySpace* pks,
+    ReadYourWritesTransaction* ryw,
+    KeySelector begin,
+    KeySelector end,
+    GetRangeLimits limits,
+    bool reverse )
+{   
+    // This function handles ranges lie over more than one underlying keyrane and aggregates all results
+    // GetRangeLimits and reverse are also handled here
+
+    // do parameter validation check stuff 
+    if( limits.isReached() ) {
+		TEST(true); // RYW range read limit 0
+		return Standalone<RangeResultRef>();
+	}
+
+    // TODO: check the reason here
+	// if( !limits.isValid() )
+	// 	return range_limits_invalid();
+    
+    // erase equal here
+    if( begin.orEqual )
+		begin.removeOrEqual(begin.arena());
+
+	if( end.orEqual )
+		end.removeOrEqual(end.arena());
+        
+    if( begin.offset >= end.offset && begin.getKey() >= end.getKey() ) {
+		TEST(true); //range inverted
+		return Standalone<RangeResultRef>();
+	}
+
+    state std::deque<KeyValueRef> resultRef;
+    state Standalone<RangeResultRef> result;
+    // state RangeMap<Key, PrivateKeyRangeBaseImpl*, KeyRangeRef>::Iterator iter;
+    state RangeMap<Key, PrivateKeyRangeBaseImpl*, KeyRangeRef>::Ranges ranges;
+
+
+    // Handle the case where begin offset is zero or negative, which means at least one key before begin.key needs to be read
+    state RangeMap<Key, PrivateKeyRangeBaseImpl*, KeyRangeRef>::Iterator iter = pks->getKeyRangeMap()->rangeContaining(begin.getKey());
+    // state RangeMap<Key, PrivateKeyRangeBaseImpl*, KeyRangeRef>::Iterator prev = curr;
+    // --prev;
+    if (begin.offset <= 0) {
+        state int remains = 1 - begin.offset;
+        while (remains > 0) {
+            if (iter.value() == NULL)  {
+                if (iter == pks->getKeyRangeMap()->ranges().begin())
+                    break;
+                else
+                    --iter;
+                continue;
+            }
+            state Standalone<RangeResultRef> temp = wait(iter->value()->getRange(
+                ryw, 
+                KeySelector(firstGreaterOrEqual(iter.value()->getKeyRange().begin)),
+                KeySelectorRef(firstGreaterOrEqual(begin.getKey())),
+                GetRangeLimits()
+            ));
+            for (int i = temp.size() - 1; i >= 0 && remains > 0; --i) {
+                resultRef.push_front(temp[i]);
+                remains--;
+            }
+            if (iter == pks->getKeyRangeMap()->ranges().begin())
+                break;
+            else
+                --iter;
+        }
+        if (remains > 0) {
+            //throw error here
+        }
+    }
+    // Check limits here
+
+    // (begin.key, range.end)
+
+    // contained range query
+
+    // (range.begin, end.key)
+
+    // end.offset > 0
+    
+    // The interesting range is in (begin.key, end.key)
+    ranges = pks->getKeyRangeMap()->intersectingRanges(KeyRangeRef(begin.getKey(), end.getKey()));
+    for (iter = ranges.begin(); iter != ranges.end(); ++iter) {
+        if (iter->value() == NULL) continue;
+        KeyRangeRef kr = iter->range();
+        Standalone<RangeResultRef> pairs = wait(iter->value()->getRange(ryw,
+            KeySelector( firstGreaterOrEqual(kr.begin) ),
+			KeySelector( firstGreaterOrEqual(kr.end)),
+            GetRangeLimits()
+            ));
+        result.append_deep(result.arena(), pairs.begin(), pairs.size());
+    }
+    if(begin.offset - end.offset <= result.size()){
+        result.pop_front(begin.offset);
+        for (int i =0; i<-end.offset; i++)
+            result.pop_back();
+    } else {
+        return Standalone<RangeResultRef>();
+    }
+    return result;
+}
+
+ACTOR Future<Standalone<RangeResultRef>> getRangeActor(
+    const PrivateKeyRangeSimpleImpl* pkrSimpleImpl,
+    ReadYourWritesTransaction* ryw,
+    KeySelector begin,
+    KeySelector end )
+{
+    // If the start key or end key lies outside this keyrange, it cannot handle the case.
+    // Thus, it is forced for the quired range lies the this keyrange
+    // It assumes the begin of the range is never used as a key
+    KeyRangeRef range = pkrSimpleImpl->getKeyRange();
+    if (begin.orEqual)
+        begin.removeOrEqual(begin.arena());
+    ASSERT(begin.offset > 0 && begin.getKey() >= range.begin);
+    if (end.orEqual)
+        end.removeOrEqual(end.arena());
+    ASSERT(end.offset <= 0 && end.getKey() <= range.end);
+
+    KeyRangeRef kr(begin.getKey(), end.getKey());
+    state Standalone<RangeResultRef> result = wait(pkrSimpleImpl->getRange(ryw, kr));
+    if (begin.offset - end.offset >= result.size()) {
+        // inverted select range
+        return Standalone<RangeResultRef>();
+    } else {
+        // TODO : may need optimization
+        // pop from head
+        result.pop_front(begin.offset);
+        // pop from end
+        for (int i = 0; i < -end.offset; ++i) result.pop_back();
+        // if (limits.reachedBy(result)) {
+        //     int idx;
+        //     for (idx = 0; idx < result.size(); ++ idx) {
+        //         limits.decrement(result[idx]);
+        //         if (limits.isReached()) break;
+        //     }
+        //     while (idx < result.size()) {
+        //         result.pop_back();
+        //         ++idx;
+        //     }
+        // }
+        return result;
+    }
+}
+
 Future<Standalone<RangeResultRef>> PrivateKeyRangeSimpleImpl::getRange(
     ReadYourWritesTransaction* ryw,
     KeySelector begin,
@@ -24,11 +172,9 @@ Future<Standalone<RangeResultRef>> PrivateKeyRangeSimpleImpl::getRange(
     bool snapshot,
     bool reverse ) const
 {
-    // do the easiest stuff here, suppose we have no snapshot and reverse
-    KeyRef startkey = begin.getKey();
-    KeyRef endkey = end.getKey();
-    KeyRange kr = KeyRangeRef(startkey, endkey);
-    return getRange(ryw, kr);
+    // ignore snapshot, which is invalid
+    // ignore reverse and limits, which is handled by PrivateKeySpace when doing aggregation
+    return getRangeActor(this, ryw, begin, end);
 }
 
 Future<Standalone<RangeResultRef>> PrivateKeySpace::getRange(
@@ -37,10 +183,10 @@ Future<Standalone<RangeResultRef>> PrivateKeySpace::getRange(
     KeySelector end,
     GetRangeLimits limits,
     bool snapshot,
-    bool reverse ) const
+    bool reverse )
 {
-    // do stuff here
-    return Standalone<RangeResultRef>();
+    // ignore snapshot, which is not used
+    return getRangeAggregationActor(this, ryw, begin, end, limits, reverse);
 }
 
 Future<Optional<Value>> PrivateKeySpace::get(
@@ -48,5 +194,6 @@ Future<Optional<Value>> PrivateKeySpace::get(
     const Key& key,
     bool snapshot)
 {
-    return getActor(this, ryw, key, snapshot);
+    // ignore snapshot, which is not used
+    return getActor(this, ryw, key);
 }
--- fdbclient/PrivateKeySpace.h
+++ fdbclient/PrivateKeySpace.h
@@ -11,12 +11,18 @@ class ReadYourWritesTransaction;
 class PrivateKeyRangeBaseImpl {
 public:
 	virtual Future<Standalone<RangeResultRef>> getRange(ReadYourWritesTransaction* ryw, KeySelector begin, KeySelector end, GetRangeLimits limits, bool snapshot = false, bool reverse = false) const = 0;
+
+	KeyRangeRef getKeyRange() const {
+		return range;
+	}
+protected:
+	KeyRangeRef range;
 };
 
 // This class 
 class PrivateKeyRangeSimpleImpl : public PrivateKeyRangeBaseImpl {
 public:
-	virtual Future<Standalone<RangeResultRef>> getRange(ReadYourWritesTransaction* ryw, const KeyRange& keys) const = 0;
+	virtual Future<Standalone<RangeResultRef>> getRange(ReadYourWritesTransaction* ryw, KeyRangeRef kr) const = 0;
 	virtual Future<Standalone<RangeResultRef>> getRange(ReadYourWritesTransaction* ryw, KeySelector begin, KeySelector end, GetRangeLimits limits, bool snapshot = false, bool reverse = false) const;
 };
 
@@ -29,24 +35,21 @@ class PrivateKeySpace {
 public:
 	Future<Optional<Value>> get(ReadYourWritesTransaction* ryw, const Key& key, bool snapshot = false);
 
-	Future<Standalone<RangeResultRef>> getRange(ReadYourWritesTransaction* ryw, KeySelector begin, KeySelector end, GetRangeLimits limits, bool snapshot = false, bool reverse = false) const;
+	Future<Standalone<RangeResultRef>> getRange(ReadYourWritesTransaction* ryw, KeySelector begin, KeySelector end, GetRangeLimits limits, bool snapshot = false, bool reverse = false);
 
 	void registerKeyRange(const KeyRange& kr, PrivateKeyRangeBaseImpl* impl) {
 		impls.insert(kr, impl);
 	}
 
-private:
+	RangeMap<Key, PrivateKeyRangeBaseImpl*, KeyRangeRef>::Iterator getIteratorForKey(const Key& key) {
+		return impls.rangeContaining(key);
+	}
 
-	// ACTOR Future<Optional<Value>> getActor(ReadYourWritesTransaction* ryw, const Key& key, bool snapshot) {
-	// 	// use getRange to workaround this
-	// 	Standalone<RangeResultRef> result = wait(getRange(ryw, KeySelector( firstGreaterOrEqual(key), key.arena() ),
-	// 			KeySelector( firstGreaterOrEqual(keyAfter(key)), key.arena() ), GetRangeLimits(1), snapshot));
-	// 	if (result.size()) {
-	// 		return Optional<Value>(result[0].value);
-	// 	} else {
-	// 		return Optional<Value>();
-	// 	}
-	// };
+	KeyRangeMap<PrivateKeyRangeBaseImpl*>* getKeyRangeMap(){
+		return &impls;
+	}
+
+private:
 	KeyRangeMap<PrivateKeyRangeBaseImpl*> impls;
 };
 
