39050308ffb91ec07de56d33fbd0463776275a61
Evan Tschannen <ejt@apple.com>
2020-03-05 18:17:49 -0800

    lower accept batch size just to be conservative with the change

--- flow/Knobs.cpp
+++ flow/Knobs.cpp
@@ -67,7 +67,7 @@ FlowKnobs::FlowKnobs(bool randomize, bool isSimulated) {
 	init( MAX_RECONNECTION_TIME,                               0.5 );
 	init( RECONNECTION_TIME_GROWTH_RATE,                       1.2 );
 	init( RECONNECTION_RESET_TIME,                             5.0 );
-	init( ACCEPT_BATCH_SIZE,                                    20 );
+	init( ACCEPT_BATCH_SIZE,                                    10 );
 	init( USE_OBJECT_SERIALIZER,                                 1 );
 	init( TOO_MANY_CONNECTIONS_CLOSED_RESET_DELAY,             5.0 );
 	init( TOO_MANY_CONNECTIONS_CLOSED_TIMEOUT,                20.0 );
