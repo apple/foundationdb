f0f4e42a4cb8a8ebebd599b0c611c2d7d962b194
Jingyu Zhou <jingyuzhou@gmail.com>
2020-03-23 12:47:42 -0700

    Add removal for backupWorkerCache

--- fdbserver/worker.actor.cpp
+++ fdbserver/worker.actor.cpp
@@ -1175,6 +1175,7 @@ ACTOR Future<Void> workerServer(
 					ReplyPromise<InitializeBackupReply> backupReady = req.reply;
 					backupWorkerCache.set(req.reqId, backupReady.getFuture());
 					Future<Void> backupProcess = backupWorker(recruited, req, dbInfo);
+					backupProcess = storageCache.removeOnReady(req.reqId, backupProcess);
 					errorForwarders.add(forwardError(errors, Role::BACKUP, recruited.id(), backupProcess));
 					TraceEvent("BackupInitRequest", req.reqId).detail("BackupId", recruited.id());
 					InitializeBackupReply reply(recruited, req.backupEpoch);
