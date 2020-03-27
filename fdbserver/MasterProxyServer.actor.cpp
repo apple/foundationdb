f7233bade79a66bb554423f45e7487c11186116a
Meng Xu <meng_xu@apple.com>
2020-03-27 13:08:34 -0700

    Rename ParallelRestoreCorrectnessAtomicOpTinyData.txt by removing TinyData

--- fdbserver/MasterProxyServer.actor.cpp
+++ fdbserver/MasterProxyServer.actor.cpp
@@ -1481,7 +1481,7 @@ ACTOR static Future<Void> readRequestServer( MasterProxyInterface proxy, Promise
 		//WARNING: this code is run at a high priority, so it needs to do as little work as possible
 		if(req.limit != CLIENT_KNOBS->STORAGE_METRICS_SHARD_LIMIT && //Always do data distribution requests
 		   commitData->stats.keyServerLocationIn.getValue() - commitData->stats.keyServerLocationOut.getValue() > SERVER_KNOBS->KEY_LOCATION_MAX_QUEUE_SIZE) {
-			++commitData->stats.keyServerLocationErrors;-
+			++commitData->stats.keyServerLocationErrors;
 			req.reply.sendError(proxy_memory_limit_exceeded());
 			TraceEvent(SevWarnAlways, "ProxyLocationRequestThresholdExceeded").suppressFor(60);
 		} else {
