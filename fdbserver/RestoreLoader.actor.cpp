8a3052633640f593529c5b39ee60a6ff48323c49
Meng Xu <meng_xu@apple.com>
2020-03-28 13:11:32 -0700

    FastRestore:Remove commented assertion

--- fdbserver/RestoreLoader.actor.cpp
+++ fdbserver/RestoreLoader.actor.cpp
@@ -343,7 +343,6 @@ ACTOR Future<Void> handleSendMutationsRequest(RestoreSendMutationsToAppliersRequ
 			TraceEvent(SevInfo, "FastRestoreSendMutationsProcessLogRequest", self->id())
 			    .detail("BatchIndex", req.batchIndex)
 			    .detail("UseRangeFile", req.useRangeFile);
-			// ASSERT(!batchStatus->sendAllRanges.present());
 		} else if (!batchStatus->sendAllLogs.get().isReady()) { // In the process of sending
 			TraceEvent(SevDebug, "FastRestoreSendMutationsWaitDuplicateLogRequest", self->id())
 			    .detail("BatchIndex", req.batchIndex)
@@ -361,7 +360,6 @@ ACTOR Future<Void> handleSendMutationsRequest(RestoreSendMutationsToAppliersRequ
 			TraceEvent(SevInfo, "FastRestoreSendMutationsProcessRangeRequest", self->id())
 			    .detail("BatchIndex", req.batchIndex)
 			    .detail("UseRangeFile", req.useRangeFile);
-			// ASSERT(batchStatus->sendAllLogs.get().isReady());
 		} else if (!batchStatus->sendAllRanges.get().isReady()) {
 			TraceEvent(SevDebug, "FastRestoreSendMutationsWaitDuplicateRangeRequest", self->id())
 			    .detail("BatchIndex", req.batchIndex)
