a7e8bfad82607c3f8745f475b9bdc98747f7f7c7
Xin Dong <xind@apple.com>
2020-03-30 15:24:08 -0700

    Fix the test failure, which was introduced by a typo

--- fdbclient/Schemas.cpp
+++ fdbclient/Schemas.cpp
@@ -536,7 +536,7 @@ const KeyRef JSONSchemas::statusSchema = LiteralStringRef(R"statusSchema(
       "data_distribution_disabled_for_rebalance":true,
       "data_distribution_disabled":true,
       "configuration":{
-         "active_primary_db":"pv",
+         "active_primary_dc":"pv",
          "log_anti_quorum":0,
          "log_replicas":2,
          "log_replication_policy":"(zoneid^3x1)",
@@ -752,6 +752,7 @@ const KeyRef JSONSchemas::clusterConfigurationSchema = LiteralStringRef(R"config
     "$enum":[
         "new"
     ]},
+    "active_primary_dc":"pv",
     "log_anti_quorum":0,
     "log_replicas":2,
     "log_replication_policy":"(zoneid^3x1)",
