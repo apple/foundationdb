b616a6b3b602b7fdfefd7b42ebab78b69a94ef8a
Markus Pilman <mpilman@localhost.localdomain>
2020-04-07 11:33:47 -0700

    generate versions.target with cmake

---
+++ versions.target.cmake
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0"?>
+<Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003">
+  <PropertyGroup>
+    <Version>${CMAKE_PROJECT_VERSION}</Version>
+    <PackageName>${CMAKE_PROJECT_VERSION_MAJOR}.${CMAKE_PROJECT_VERSION_MINOR}</PackageName>
+  </PropertyGroup>
+</Project>
