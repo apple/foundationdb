7d984984235c2d00beb8ecaf1efca9009d2f784b
Alvin Moore <alvinm@apple.com>
2020-04-17 08:56:07 -0700

    Fixed whitespace

--- cmake/AddFdbTest.cmake
+++ cmake/AddFdbTest.cmake
@@ -211,7 +211,7 @@ function(create_test_package)
       COMMENT "Package correctness archive"
       )
     add_custom_target(package_tests ALL DEPENDS ${tar_file})
-		add_custom_target(correctness_pkg ALL DEPENDS ${tar_file})
+    add_custom_target(correctness_pkg ALL DEPENDS ${tar_file})
     # seems make needs this dependency while this does nothing with ninja
     add_dependencies(package_tests strip_only_fdbserver TestHarness)
   endif()
