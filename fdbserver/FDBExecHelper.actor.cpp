697a9422f5605fdb86afc485f90877931121238b
sramamoorthy <sramamoorthy@snowflake.net>
2020-05-07 13:14:51 -0700

    replace boost::process with execv to spawn snapCreate process

--- fdbserver/FDBExecHelper.actor.cpp
+++ fdbserver/FDBExecHelper.actor.cpp
@@ -77,65 +77,44 @@ ACTOR Future<int> spawnProcess(std::string binPath, std::vector<std::string> par
 	return 0;
 }
 #else
-ACTOR Future<int> spawnProcess(std::string binPath, std::vector<std::string> paramList, double maxWaitTime, bool isSync, double maxSimDelayTime)
+ACTOR Future<int> spawnProcess(std::string path, std::vector<std::string> args, double maxWaitTime, bool isSync, double maxSimDelayTime)
 {
-	state std::string argsString;
-	for (auto const& elem : paramList) {
-		argsString += elem + ",";
+	std::vector<char*> paramList;
+	for (int i = 0; i < args.size(); i++) {
+		paramList.push_back(const_cast<char*>(args[i].c_str()));
 	}
-	TraceEvent("SpawnProcess").detail("Cmd", binPath).detail("Args", argsString);
+	paramList.push_back(nullptr);
 
-	state int err = 0;
-	state double runTime = 0;
-	state boost::process::child c(binPath, boost::process::args(paramList),
-								  boost::process::std_err > boost::process::null);
-
-	// for async calls in simulator, always delay by a deterinistic amount of time and do the call
-	// synchronously, otherwise the predictability of the simulator breaks
-	if (!isSync && g_network->isSimulated()) {
-		double snapDelay = std::max(maxSimDelayTime - 1, 0.0);
-		// add some randomness
-		snapDelay += deterministicRandom()->random01();
-		TraceEvent("SnapDelaySpawnProcess")
-			.detail("SnapDelay", snapDelay);
-		wait(delay(snapDelay));
+	auto te = TraceEvent("fdbFork");
+	te.detail("cmd", path);
+	for (int i = 0; i < args.size(); i++) {
+		//te.detail("args", args[i]);
 	}
 
-	if (!isSync && !g_network->isSimulated()) {
-		while (c.running() && runTime <= maxWaitTime) {
-			wait(delay(0.1));
-			runTime += 0.1;
+	pid_t pid = fork();
+	if (pid == -1) {
+		TraceEvent(SevWarnAlways, "Command failed to spawn")
+			.detail("cmd", path);
+		return -1;
+	} else if (pid > 0) {
+		int status;
+		waitpid(pid, &status, 0);
+		if (!(WIFEXITED(status) && WEXITSTATUS(status) == 0)) {
+			TraceEvent(SevWarnAlways, "Command failed")
+				.detail("cmd", path)
+				.detail("errno", WIFEXITED(status) ? WEXITSTATUS(status) : -1);
+			return WIFEXITED(status) ? WEXITSTATUS(status) : -1;
 		}
+		TraceEvent("Command status")
+			.detail("cmd", path)
+			.detail("errno", WIFEXITED(status) ? WEXITSTATUS(status) : 0);
 	} else {
-		if (g_network->isSimulated()) {
-			// to keep the simulator deterministic, wait till the process exits,
-			// hence giving a large wait time
-			c.wait_for(std::chrono::hours(24));
-			ASSERT(!c.running());
-		} else {
-			int maxWaitTimeInt = static_cast<int>(maxWaitTime + 1.0);
-			c.wait_for(std::chrono::seconds(maxWaitTimeInt));
-		}
+		execv(const_cast<char*>(path.c_str()), &paramList[0]);
+		_exit(EXIT_FAILURE);
 	}
+	wait(delay(0.0));
+	return 0;
 
-	if (c.running()) {
-		TraceEvent(SevWarnAlways, "ChildTermination")
-				.detail("Cmd", binPath)
-				.detail("Args", argsString);
-		c.terminate();
-		err = -1;
-		if (!c.wait_for(std::chrono::seconds(1))) {
-			TraceEvent(SevWarnAlways, "SpawnProcessFailedToExit")
-				.detail("Cmd", binPath)
-				.detail("Args", argsString);
-		}
-	} else {
-		err = c.exit_code();
-	}
-	TraceEvent("SpawnProcess")
-		.detail("Cmd", binPath)
-		.detail("Error", err);
-	return err;
 }
 #endif
 
@@ -148,6 +127,7 @@ ACTOR Future<int> execHelper(ExecCmdValueString* execArg, UID snapUID, std::stri
 		// get bin path
 		auto snapBin = execArg->getBinaryPath();
 		std::vector<std::string> paramList;
+		paramList.push_back(snapBin.toString());
 		// get user passed arguments
 		auto listArgs = execArg->getBinaryArgs();
 		for (auto elem : listArgs) {
@@ -174,6 +154,7 @@ ACTOR Future<int> execHelper(ExecCmdValueString* execArg, UID snapUID, std::stri
 		folderTo = folder + "-snap-" + uidStr.toString() + "-" + role;
 		std::vector<std::string> paramList;
 		std::string mkdirBin = "/bin/mkdir";
+		paramList.push_back(mkdirBin);
 		paramList.push_back(folderTo);
 		cmdErr = spawnProcess(mkdirBin, paramList, maxWaitTime, false /*isSync*/, maxSimDelayTime);
 		wait(success(cmdErr));
@@ -181,6 +162,7 @@ ACTOR Future<int> execHelper(ExecCmdValueString* execArg, UID snapUID, std::stri
 		if (err == 0) {
 			std::vector<std::string> paramList;
 			std::string cpBin = "/bin/cp";
+			paramList.push_back(cpBin);
 			paramList.push_back("-a");
 			paramList.push_back(folderFrom);
 			paramList.push_back(folderTo);
