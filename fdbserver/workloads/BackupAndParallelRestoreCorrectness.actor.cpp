4315af25e777f9f8a8f0a07c468ea700f78f106b
Jingyu Zhou <jingyuzhou@gmail.com>
2020-05-19 15:18:45 -0700

    Fix unfinished restore tests
    Because backup worker is disabled before performing the restore, we can't allow
    a new backup of the new type running.

--- fdbserver/workloads/BackupAndParallelRestoreCorrectness.actor.cpp
+++ fdbserver/workloads/BackupAndParallelRestoreCorrectness.actor.cpp
@@ -399,9 +399,11 @@ struct BackupAndParallelRestoreCorrectnessWorkload : TestWorkload {
 			if (!self->locked && BUGGIFY) {
 				TraceEvent("BARW_SubmitBackup2", randomID).detail("Tag", printable(self->backupTag));
 				try {
+					// Note the "partitionedLog" must be false, because we change
+					// the configuration to disable backup workers before restore.
 					extraBackup = backupAgent.submitBackup(
 					    cx, LiteralStringRef("file://simfdb/backups/"), deterministicRandom()->randomInt(0, 100),
-					    self->backupTag.toString(), self->backupRanges, true, self->usePartitionedLogs);
+					    self->backupTag.toString(), self->backupRanges, true, false);
 				} catch (Error& e) {
 					TraceEvent("BARW_SubmitBackup2Exception", randomID)
 					    .error(e)
