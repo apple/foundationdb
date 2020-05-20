a6aff22e655a8e88e2eb5007e645d678d5bdd328
Chaoguang Lin <chaoguang.lin@snowflake.com>
2020-05-19 20:47:52 -0700

    Update the special key prefix for dd-stats to \xff\xff/data_distribution_stats

--- fdbclient/SystemData.cpp
+++ fdbclient/SystemData.cpp
@@ -204,7 +204,7 @@ const KeyRangeRef writeConflictRangeKeysRange =
                 LiteralStringRef("\xff\xff/transaction/write_conflict_range/\xff\xff"));
 
 const KeyRangeRef ddStatsRange =
-    KeyRangeRef(LiteralStringRef("\xff\xff/metrics/dd_stats/"), LiteralStringRef("\xff\xff/metrics/dd_stats/\xff\xff"));
+    KeyRangeRef(LiteralStringRef("\xff\xff/metrics/data_distribution_stats/"), LiteralStringRef("\xff\xff/metrics/data_distribution_stats/\xff\xff"));
 
 //    "\xff/storageCache/[[begin]]" := "[[vector<uint16_t>]]"
 const KeyRangeRef storageCacheKeys( LiteralStringRef("\xff/storageCache/"), LiteralStringRef("\xff/storageCache0") );
