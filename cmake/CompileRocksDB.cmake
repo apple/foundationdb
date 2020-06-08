c54ad12072ff69b00db964e797dfd0e4b37ccd9b
Daniel Smith <daniel.smith@datadoghq.com>
2020-06-08 16:46:42 +0000

    Incremental improvements

--- cmake/CompileRocksDB.cmake
+++ cmake/CompileRocksDB.cmake
@@ -8,7 +8,7 @@ if (RocksDB_FOUND)
   ExternalProject_Add(rocksdb
     SOURCE_DIR "${RocksDB_ROOT}"
     DOWNLOAD_COMMAND ""
-    CMAKE_ARGS -DUSE_RTTI=1 -DPORTABLE=1
+    CMAKE_ARGS -DUSE_RTTI=1 -DPORTABLE=${PORTABLE_ROCKSDB}
                -DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DWITH_GFLAGS=OFF
@@ -27,13 +27,12 @@ if (RocksDB_FOUND)
   ExternalProject_Get_Property(rocksdb BINARY_DIR)
   set(ROCKSDB_LIBRARIES
       ${BINARY_DIR}/librocksdb.a)
-
 else()
   message("She MISSED it!")
   ExternalProject_Add(rocksdb
     URL        https://github.com/facebook/rocksdb/archive/v6.10.1.tar.gz
     URL_HASH   SHA256=d573d2f15cdda883714f7e0bc87b814a8d4a53a82edde558f08f940e905541ee
-    CMAKE_ARGS -DUSE_RTTI=1 -DPORTABLE=1
+    CMAKE_ARGS -DUSE_RTTI=1 -DPORTABLE=${PORTABLE_ROCKSDB}
                -DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
                -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
                -DWITH_GFLAGS=OFF
@@ -54,16 +53,12 @@ else()
       ${BINARY_DIR}/librocksdb.a)
 
   set(ROCKSDB_FOUND TRUE)
-
-  set(ROCKSDB_INCLUDE_DIRS
-      ${ROCKSDB_ROOT_DIR}/include)
 endif()
 
 message(STATUS "Found RocksDB library: ${ROCKSDB_LIBRARIES}")
 message(STATUS "Found RocksDB includes: ${ROCKSDB_INCLUDE_DIRS}")
 
 mark_as_advanced(
-    ROCKSDB_ROOT_DIR
     ROCKSDB_LIBRARIES
-    ROCKSDB_INCLUDE_DIRS
+    ROCKSDB_INCLUDE_DIR
 )
