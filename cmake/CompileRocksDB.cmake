b8ac6ee41e6fecd8cc32e40324d89533aea91735
Daniel Smith <daniel.smith@datadoghq.com>
2020-06-10 20:53:53 +0000

    Get build to work with Ninja

--- cmake/CompileRocksDB.cmake
+++ cmake/CompileRocksDB.cmake
@@ -22,6 +22,7 @@ if (RocksDB_FOUND)
                -DWITH_ZLIB=OFF
                -DWITH_ZSTD=OFF
                -DCMAKE_POSITION_INDEPENDENT_CODE=True
+    BUILD_BYPRODUCTS ${BINARY_DIR}/librocksdb.a
     INSTALL_COMMAND ""
   )
 
@@ -46,6 +47,7 @@ else()
                -DWITH_ZLIB=OFF
                -DWITH_ZSTD=OFF
                -DCMAKE_POSITION_INDEPENDENT_CODE=True
+    BUILD_BYPRODUCTS ${BINARY_DIR}/librocksdb.a
     INSTALL_COMMAND ""
   )
 
