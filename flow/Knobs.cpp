66e2a22803677a11507638821c3a96cac642d2c3
Sam Gwydir <sam.gwydir@snowflake.com>
2020-06-29 13:05:46 -0400

    Add buggify to STORAGE_PAGE_SIZE knob

--- flow/Knobs.cpp
+++ flow/Knobs.cpp
@@ -223,7 +223,7 @@ void FlowKnobs::initialize(bool randomize, bool isSimulated) {
 	init( HEALTH_MONITOR_MARK_FAILED_UNSTABLE_CONNECTIONS,    true );
 	init( HEALTH_MONITOR_CLIENT_REQUEST_INTERVAL_SECS,          30 );
 	init( HEALTH_MONITOR_CONNECTION_MAX_CLOSED,                  5 );
-	init( STORAGE_PAGE_SIZE,                                  4096 );
+	init( STORAGE_PAGE_SIZE,                                  4096 ); if( randomize && BUGGIFY ) STORAGE_PAGE_SIZE = 8192; else if( randomize && BUGGIFY ) STORAGE_PAGE_SIZE = 16384;
 }
 // clang-format on
 
