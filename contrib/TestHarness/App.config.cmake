0f6a01aef490804b374f918a28a9d0dc8ee00f8c
Alvin Moore <alvinm@apple.com>
2020-08-07 10:57:14 -0400

    Added TestHarness application config

---
+++ contrib/TestHarness/App.config.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<configuration>
+    <appSettings>
+        <add key="PROJECT_VERSION" value="${CMAKE_PROJECT_VERSION}" />
+        <add key="PROJECT_VERSION_MAJOR" value="${CMAKE_PROJECT_VERSION_MAJOR}" />
+				<add key="PROJECT_VERSION_MINOR" value="${CMAKE_PROJECT_VERSION_MINOR}" />
+				<add key="GIT_VERSION" value="${CURRENT_GIT_VERSION}" />
+    </appSettings>
+</configuration>
