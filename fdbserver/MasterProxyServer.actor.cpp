d09c6b3edb97786dd207c2ccbf318d1d82bd1970
Xiaoge Su <magichp@gmail.com>
2020-09-02 00:00:13 -0700

    Proxies recognize the split transaction
    Since a group of split transaction should have its own set of
    
        * prevVersion/Version
        * resolution
        * tLog accept/rollback
    
    it should not be batched with other transactions.

--- fdbserver/MasterProxyServer.actor.cpp
+++ fdbserver/MasterProxyServer.actor.cpp
@@ -249,9 +249,46 @@ ACTOR Future<Void> commitBatcher(ProxyCommitData *commitData, PromiseStream<std:
 						batchBytes = 0;
 					}
 
-					batch.push_back(req);
-					batchBytes += bytes;
-					commitData->commitBatchesMemBytesCount += bytes;
+					if (req.splitTransaction.present()) {
+						// The transaction is a split transaction, or part of
+						// a big transaction. In this case, all parts of the
+						// transactions should have the same version. In some
+						// cases. Consider the following schema:
+						//                   /-   TS1 -- Proxy1 (TS1P1)
+						// 	 Transaction 1  ---   TS1 -- Proxy2 (TS1P2)
+						//                   \-   TS1 -- Proxy3 (TS1P3)
+						// Assume TS1P1 is processed with Version TS1V, it is
+						// expected that TS1V will be used for TS1P2 and TS1P3.
+						// Now assume there is another transaction TS2, having
+						// similar distribution TS2P1, TS2P2 and TS2P3, with
+						// version TS2V.
+						// If TS1P2 and TS2P2 are batched now, then the master
+						// will be confused -- should it return TS1V or TS2V?
+						// A race condition is then introduced. The easiest way
+						// is simple -- do *NOT* batch any split transaction.
+						// This is acceptable since for split transaction, each
+						// part is already big enough.
+
+						// NOTE: In fdbclient/MasterProxyInterface.cpp,
+						// prepareSplitTransactions, it is guaranteed that the
+						// split transaction has flag FLAG_FIRST_IN_BATCH.
+
+						// NOTE: Since the bytes from the split request is not
+						// part of the batch, we have to manually add bytes
+						// to total batches count.
+						out.send({ { req }, bytes });
+						commitData->commitBatchesMemBytesCount += bytes;
+
+						lastBatch = now();
+						timeout = delayJittered(commitData->commitBatchInterval, TaskPriority::ProxyCommitBatcher);
+
+						batch.clear();
+						batchBytes = 0;
+					} else {
+						batch.push_back(req);
+						batchBytes += bytes;
+						commitData->commitBatchesMemBytesCount += bytes;
+					}
 				}
 				when(wait(timeout)) {}
 			}
@@ -391,6 +428,8 @@ struct CommitBatchContext {
 
 	Optional<UID> debugID;
 
+	Optional<SplitTransaction> splitTransaction;
+
 	bool forceRecovery = false;
 
 	int64_t localBatchNumber;
@@ -493,6 +532,15 @@ CommitBatchContext::CommitBatchContext(ProxyCommitData* const pProxyCommitData_,
 		);
 	}
 
+	if (trs.size() >= 1 && trs[0].splitTransaction.present()) {
+		ASSERT(trs.size() == 1);
+		splitTransaction = trs[0].splitTransaction.get();
+
+		TraceEvent("SplitTransactionTriggered")
+		    .detail("SplitId", splitTransaction.get().id.toString())
+		    .detail("PartIndex", splitTransaction.get().partIndex)
+		    .detail("TotalParts", splitTransaction.get().totalParts);
+	}
 	// since we are using just the former to limit the number of versions actually in flight!
 	ASSERT(SERVER_KNOBS->MAX_READ_TRANSACTION_LIFE_VERSIONS <= SERVER_KNOBS->MAX_VERSIONS_IN_FLIGHT);
 }
