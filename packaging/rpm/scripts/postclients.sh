2ab889536ad7a3823f427e664f2f5ef33786c707
Oleg Samarin <osamarin@openwaygroup.com>
2020-10-08 19:04:02 +0300

    /etc/foundationdb is not writable for foundationdb user

--- packaging/rpm/scripts/postclients.sh
+++ packaging/rpm/scripts/postclients.sh
@@ -1,3 +1,4 @@
 # older versions of this package failed to correctly clean up their .pyc files, which is "very bad"
 rm -rf /usr/lib64/python2.6/fdb
+if [ -d /etc/foundationdb ]; then chown foundationdb:foundationdb /etc/foundationdb; fi
 exit 0
