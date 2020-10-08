b10ea7677e92cfeead4993d22582e223e5e17a9c
Kevin King <4kevinking@gmail.com>
2020-10-08 15:47:49 -0700

    [fdbbackup] check for credentials inside of setAuthHeaders

--- fdbclient/S3BlobStore.actor.cpp
+++ fdbclient/S3BlobStore.actor.cpp
@@ -661,9 +661,8 @@ ACTOR Future<Reference<HTTP::Response>> doRequest_impl(Reference<S3BlobStoreEndp
 			// Finish/update the request headers (which includes Date header)
 			// This must be done AFTER the connection is ready because if credentials are coming from disk they are
 			// refreshed when a new connection is established and setAuthHeaders() would need the updated secret.
-			if (bstore->credentials.present()) {
-				bstore->setAuthHeaders(bstore->credentials.get(), verb, resource, headers);
-			}
+			bstore->setAuthHeaders(verb, resource, headers);
+
 			remoteAddress = rconn.conn->getPeerAddress();
 			wait(bstore->requestRate->getAllowance(1));
 			Reference<HTTP::Response> _r = wait(timeoutError(HTTP::doRequest(rconn.conn,
@@ -1086,7 +1085,12 @@ std::string S3BlobStoreEndpoint::hmac_sha1(Credentials const &creds, std::string
 	return SHA1::from_string(kopad);
 }
 
-void S3BlobStoreEndpoint::setAuthHeaders(Credentials const &creds, std::string const& verb, std::string const& resource, HTTP::Headers& headers) {
+void S3BlobStoreEndpoint::setAuthHeaders(std::string const& verb, std::string const& resource, HTTP::Headers& headers) {
+	if (!credentials.present()) {
+		return;
+	}
+	Credentials creds = credentials.get();
+
 	std::string& date = headers["Date"];
 
 	char dateBuf[20];
