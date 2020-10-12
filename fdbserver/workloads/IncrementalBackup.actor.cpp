48b9846a6a7ad5e7653ad669de8ebb19ec4bcf38
Jon Fu <jon.fu@snowflake.com>
2020-10-12 17:11:47 -0400

    adjust test spec and manual g_simulator setting

--- fdbserver/workloads/IncrementalBackup.actor.cpp
+++ fdbserver/workloads/IncrementalBackup.actor.cpp
@@ -169,10 +169,6 @@ struct IncrementalBackupWorkload : TestWorkload {
 			wait(success(self->backupAgent.restore(cx, cx, Key(self->tag.toString()), backupURL, true, invalidVersion,
 			                                       true, normalKeys, Key(), Key(), true, true, beginVersion)));
 			TraceEvent("IBackupRestoreSuccess");
-			// SOMEDAY: Remove after backup agents can exist quiescently
-			if (g_simulator.backupAgents == ISimulator::BackupToFile) {
-				g_simulator.backupAgents = ISimulator::NoBackupAgents;
-			}
 		}
 		return Void();
 	}
