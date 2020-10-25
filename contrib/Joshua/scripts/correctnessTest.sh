53778c68b6fcbb439acd1a84036bae378c2a714f
Jingyu Zhou <jingyuzhou@gmail.com>
2020-10-25 11:34:21 -0700

    Fix directory

--- contrib/Joshua/scripts/correctnessTest.sh
+++ contrib/Joshua/scripts/correctnessTest.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 OLDBINDIR="${OLDBINDIR:-/app/deploy/global_data/oldBinaries}"
-FDBVER=`"${SCRIPTDIR}/bin/fdbserver" --version | head -n1 | sed -e 's/.*(v//' -e 's/[-a-zA-Z]*)//'`
+FDBVER=`bin/fdbserver --version | head -n1 | sed -e 's/.*(v//' -e 's/[-a-zA-Z]*)//'`
 mono bin/TestHarness.exe joshua-run "${OLDBINDIR}" "${FDBVER}" false
