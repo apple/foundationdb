84632d63ade1d6006ef1fba1d7e885ac2e43a8be
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2020-11-16 17:54:47 -0800

    Remove stale comment

--- fdbserver/DataDistributionTracker.actor.cpp
+++ fdbserver/DataDistributionTracker.actor.cpp
@@ -657,7 +657,6 @@ ACTOR Future<Void> shardTracker(DataDistributionTracker::SafeAccessor self, KeyR
 			wait( delay(0, TaskPriority::DataDistribution) );
 		}
 	} catch (Error& e) {
-		// If e is broken_promise then self may have already been deleted
 		if (e.code() != error_code_actor_cancelled && e.code() != error_code_dd_tracker_cancelled) {
 			self()->output.sendError(e); // Propagate failure to dataDistributionTracker
 		}
