4d32bc24c3343ebeacaf3d2dd65caa12e06b1da7
Andrew Noyes <andrew.noyes@snowflake.com>
2020-12-17 22:56:55 +0000

    Change ParallelStream::sendError to finish

--- fdbclient/NativeAPI.actor.cpp
+++ fdbclient/NativeAPI.actor.cpp
@@ -2793,7 +2793,7 @@ ACTOR Future<Void> getRangeStreamFragment(ParallelStream<RangeResult>::Fragment*
 						} else {
 							output.arena().dependsOn( range.arena() );
 							output.readThrough = reverse ? range.begin : range.end;
-							results.send(std::move(output));
+							results->send(std::move(output));
 							++shard;
 							break;
 						}
@@ -2882,7 +2882,7 @@ ACTOR Future<Void> getRangeStream(PromiseStream<RangeResult> _results, Database 
 	}
 
 	if (b >= e) {
-		results.sendError(end_of_stream());
+		wait(results.finish());
 		return Void();
 	}
 
@@ -2930,12 +2930,7 @@ ACTOR Future<Void> getRangeStream(PromiseStream<RangeResult> _results, Database 
 			break;
 		}
 	}
-	try {
-		wait(waitForAll(outstandingRequests));
-	} catch (Error& e) {
-		results.sendError(e);
-	}
-	results.sendError(end_of_stream());
+	wait(waitForAll(outstandingRequests) && results.finish());
 	return Void();
 }
 
