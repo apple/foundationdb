f1e8bbc32132298c19582bff3b0ec0f5c3819c4a
Chameleon Cloud User <cc@fdb.novalocal>
2020-12-31 22:39:56 +0000

    an updated version with a faster variation for int

--- fdbrpc/DDSketch.h
+++ fdbrpc/DDSketch.h
@@ -28,19 +28,14 @@
 #include "flow/Error.h"
 #include "flow/UnitTest.h"
 
-#if (!(__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__))
-#error Do not support non-little-endian systems
-#endif
 
-#define FASTLOG 1
-// This significantly reduces the time for log
-
-#if FASTLOG
+// A namespace for fast log() computation.
 namespace fastLogger {
 // Basically, the goal is to compute log(x)/log(r).
-// For double, it is represented as 2^e*(1+s) (0<=s<1), so our goal becomes e*log(2)/log(r)*log(1+s),
-// and we approximate log(1+s) with a cubic function. See more details on Datadog's paper, or
-// CubicallyInterpolatedMapping.java in https://github.com/DataDog/sketches-java/
+// For double, it is represented as 2^e*(1+s) (0<=s<1), so our goal becomes
+// e*log(2)/log(r)*log(1+s), and we approximate log(1+s) with a cubic function.
+// See more details on Datadog's paper, or CubicallyInterpolatedMapping.java in
+// https://github.com/DataDog/sketches-java/
 inline const double correctingFactor = 1.00988652862227438516; // = 7 / (10 * log(2));
 constexpr inline const double A = 6.0 / 35.0, B = -3.0 / 5.0, C = 10.0 / 7.0;
 
@@ -61,36 +56,28 @@ inline double reverseLog(double index) {
 	return ldexp(significandPlusOne / 2, exponent + 1);
 }
 }; // namespace fastLogger
-#endif
 
-// A DDSketch for non-negative numbers
-// (those < 10^-18 are treated as 0, and huge numbers (>>10^18) fail ASSERT)
-template <class T>
-class DDSketch {
+// DDSketch for non-negative numbers (those < EPS = 10^-18 are 
+// treated as 0, and huge numbers (>1/EPS) fail ASSERT). This is the base 
+// class without a concrete log() implementation.
+template <class Impl, class T>
+class DDSketchBase {
 public:
-	explicit DDSketch(double errorGuarantee = 0.1)
-	  : errorGuarantee(errorGuarantee), gamma((1.0 + errorGuarantee) / (1.0 - errorGuarantee)), logGamma(log(gamma)),
-	    multiplier(FASTLOG ? fastLogger::correctingFactor * log(2) / log(1 + 2 * errorGuarantee / (1 - errorGuarantee))
-	                       : 1),
-	    offset((FASTLOG ? fastLogger::fastlog(1 / EPS) * multiplier : log(1 / EPS)) + 5), populationSize(0),
-	    zeroPopulationSize(0), minValue(T()), maxValue(T()), sum(T()) {
-		buckets.resize(2 * offset, 0);
-	}
+	explicit DDSketchBase(double errorGuarantee)
+	  : errorGuarantee(errorGuarantee), populationSize(0), 
+	    zeroPopulationSize(0), minValue(T()), maxValue(T()),
+	    sum(T()) {}
 
-	DDSketch<T>& addSample(T sample) {
+	DDSketchBase<Impl, T>& addSample(T sample) {
 		// Call it addSample for now, while it is not a sample anymore
 		if (!populationSize) minValue = maxValue = sample;
 
 		if (sample <= EPS) {
 			zeroPopulationSize++;
 		} else {
-#if FASTLOG
-			int index = ceil(fastLogger::fastlog(sample) * multiplier);
-#else
-			int index = ceil(log(sample) / logGamma);
-#endif
-			ASSERT(index + offset >= 0 && index + offset < buckets.size());
-			buckets[index + offset]++;
+			int index = static_cast<Impl*>(this)->getIndex(sample);
+			ASSERT(index >= 0 && index < buckets.size());
+			buckets[index]++;
 		}
 
 		populationSize++;
@@ -107,11 +94,12 @@ public:
 
 	T median() { return percentile(0.5); }
 
-	T percentile(double percentile) { // Find the tPP-th (0-indexed) element
+	T percentile(double percentile) { 
 		ASSERT(percentile >= 0 && percentile <= 1);
 
 		if (populationSize == 0) return T();
 		uint64_t targetPercentilePopulation = percentile * (populationSize - 1);
+		// Now find the tPP-th (0-indexed) element
 		if (targetPercentilePopulation < zeroPopulationSize) return T(0);
 
 		int index = -1;
@@ -120,8 +108,9 @@ public:
 			uint64_t count = zeroPopulationSize;
 			for (size_t i = 0; i < buckets.size(); i++) {
 				if (targetPercentilePopulation < count + buckets[i]) {
-					// count + buckets[i] = # of numbers so far (from the rightmost to this
-					// bucket, inclusive), so if target is in this bucket, it should means tPP < cnt + bck[i]
+					// count + buckets[i] = # of numbers so far (from the rightmost to
+					// this bucket, inclusive), so if target is in this bucket, it should
+					// means tPP < cnt + bck[i]
 					found = true;
 					index = i;
 					break;
@@ -130,13 +119,14 @@ public:
 			}
 		} else { // and count down
 			uint64_t count = 0;
-			for (size_t i = buckets.size(); i >= 0; i--) {
+			for (size_t i = buckets.size() - 1; i >= 0; i--) {
 				if (targetPercentilePopulation + count + buckets[i] >= populationSize) {
 					// cnt + bkt[i] is # of numbers to the right of this bucket (incl.),
-					// so if target is not in this bucket (i.e., to the left of this bucket),
-					// it would be as right as the left bucket's rightmost number, so
-					// we would have tPP + cnt + bkt[i] < total population (tPP is 0-indexed),
-					// that means target is in this bucket if this condition is not satisfied.
+					// so if target is not in this bucket (i.e., to the left of this
+					// bucket), it would be as right as the left bucket's rightmost
+					// number, so we would have tPP + cnt + bkt[i] < total population (tPP
+					// is 0-indexed), that means target is in this bucket if this
+					// condition is not satisfied.
 					found = true;
 					index = i;
 					break;
@@ -145,11 +135,7 @@ public:
 			}
 		}
 		ASSERT(found);
-#if FASTLOG
-		return fastLogger::reverseLog((index - offset) / multiplier) * 2.0 / (1 + gamma);
-#else
-		return (T)(2.0 * pow(gamma, (index - offset)) / (gamma + 1));
-#endif
+		return static_cast<Impl*>(this)->getValue(index);
 	}
 
 	T min() const { return minValue; }
@@ -165,11 +151,10 @@ public:
 
 	double getErrorGurantee() const { return errorGuarantee; }
 
-	DDSketch<T>& mergeWith(const DDSketch<T>& anotherSketch) {
-		// Does not work if one use FASTLOG and the other did not
+	DDSketchBase<Impl, T>& mergeWith(const DDSketchBase<Impl, T>& anotherSketch) {
 		// Must have the same guarantee
-		ASSERT(errorGuarantee - anotherSketch.gerrorGuarantee < EPS &&
-		       anotherSketch.gerrorGuarantee - errorGuarantee < EPS && anotherSketch.buckets.size() == buckets.size());
+		ASSERT(fabs(errorGuarantee - anotherSketch.errorGuarantee) < EPS &&
+		       anotherSketch.buckets.size() == buckets.size());
 		for (size_t i = 0; i < anotherSketch.buckets.size(); i++) {
 			buckets[i] += anotherSketch.buckets[i];
 		}
@@ -181,20 +166,105 @@ public:
 		return *this;
 	}
 
-	// Need to implement a ser/deser method before we can send this over network and merge
+	void setBucketSize(int capacity) { buckets.resize(capacity, 0); }
 
+	// Need to implement a ser/deser method before we can send this over network
+	// and merge
+
+	constexpr static double EPS = 1e-18; // smaller numbers are considered as 0
 private:
-	double EPS = 1e-18; // incoming data smaller than this number are considered as 0
-	double errorGuarantee, gamma, logGamma; // As defined in the paper
-	double multiplier; // Calibration for FASTLOG
-	int offset; // For buckets[i] we use buckets[i + offset] instead since i coule be negative
+	double errorGuarantee; // As defined in the paper
 
 	uint64_t populationSize, zeroPopulationSize; // we need to separately count 0s
 	std::vector<uint64_t> buckets;
 	T minValue, maxValue, sum;
 };
 
+// DDSketch with fast log implementation for float numbers
+template <class T>
+class DDSketch : public DDSketchBase<DDSketch<T>, T> {
+public:
+	DDSketch(double errorGuarantee = 0.1)
+	  : DDSketchBase<DDSketch<T>, T>(errorGuarantee), 
+	    gamma((1.0 + errorGuarantee) / (1.0 - errorGuarantee)),
+	    multiplier(fastLogger::correctingFactor * log(2) / log(gamma)) {
+		offset = getIndex(1.0 / DDSketchBase<DDSketch<T>, T>::EPS);
+		this->setBucketSize(2 * offset);
+	}
+
+	int getIndex(T sample) {
+		static_assert(__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__, 
+			"Do not support non-little-endian systems");
+		return ceil(fastLogger::fastlog(sample) * multiplier) + offset;
+	}
+
+	T getValue(int index) { 
+		return fastLogger::reverseLog((index - offset) / multiplier) * 2.0 / (1 + gamma); 
+	}
+
+private:
+	double gamma, multiplier;
+	int offset = 0;
+};
+
+// DDSketch with <cmath> log. Slow and only use this when others doesn't work.
+template <class T>
+class DDSketchSlow : public DDSketchBase<DDSketchSlow<T>, T> {
+public:
+	DDSketchSlow(double errorGuarantee = 0.1)
+	  : DDSketchBase<DDSketchSlow<T>, T>(errorGuarantee), 
+	    gamma((1.0 + errorGuarantee) / (1.0 - errorGuarantee)),
+	    logGamma(log(gamma)) {
+		offset = getIndex(1.0 / DDSketchBase<DDSketch<T>, T>::EPS) + 5;
+		this->setBucketSize(2 * offset);
+	}
+
+	int getIndex(T sample) { return ceil(log(sample) / logGamma) + offset; }
+
+	T getValue(int index) { return (T)(2.0 * pow(gamma, (index - offset)) / (1 + gamma)); }
+
+private:
+	double gamma, logGamma;
+	int offset = 0;
+};
+
+// DDSketch for unsigned int. Faster than the float version. Fixed accuracy.
+class DDSketchFastUnsigned : public DDSketchBase<DDSketchFastUnsigned, unsigned> {
+public:
+	DDSketchFastUnsigned() : DDSketchBase<DDSketchFastUnsigned, unsigned>(errorGuarantee) { this->setBucketSize(129); }
+
+	int getIndex(unsigned sample) {
+		__uint128_t v = sample;
+		v *= v;
+		v *= v; // sample^4
+		uint64_t low = (uint64_t)v, high = (uint64_t)(v >> 64);
+
+		return 128 - (high == 0 ? ((low == 0 ? 64 : __builtin_clzll(low)) + 64) : __builtin_clzll(high));
+	}
+
+	unsigned getValue(int index) {
+		double r = 1, g = gamma;
+		while (index) { // quick power method for power(gamma, index)
+			if (index & 1) r *= g;
+			g *= g;
+			index >>= 1;
+		}
+		// 2.0 * pow(gamma, index) / (1 + gamma) is what we need
+		return (unsigned)(2.0 * r / (1 + gamma) + 0.5); // round to nearest int
+	}
+
+private:
+	constexpr static double errorGuarantee = 0.08642723372; 
+	// getIndex basically calc floor(log_2(x^4)) + 1,
+	// which is almost ceil(log_2(x^4)) as it only matters when x is a power of 2,
+	// and it does not change the error bound. Original sketch asks for
+	// ceil(log_r(x)), so we know r = pow(2, 1/4) = 1.189207115. And r = (1 + eG)
+	// / (1 - eG) so eG = 0.08642723372.
+	constexpr static double gamma = 1.189207115;
+};
+
 #endif
+
 
 TEST_CASE("/fdbrpc/ddsketch/accuracy") {
 
