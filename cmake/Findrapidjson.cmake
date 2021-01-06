3fb056e9fe5329203e3dcae68e3edccc3cc986df
Chaoguang Lin <chaoguang.lin@snowflake.com>
2021-01-05 23:53:42 -0800

    Revert "Let rapidjson as a module in cmake"
    This reverts commit 5ffb280aadc4b5dcba9999a77d05642749d7df66.

--- cmake/Findrapidjson.cmake
+++
@@ -1,8 +0,0 @@
-# Findrapidjson
-
-find_path(RAPIDJSON_INCLUDE_DIR
-  NAMES rapidjson/rapidjson.h
-  PATH_SUFFIXES include)
-
-find_package_handle_standard_args(rapidjson
-  DEFAULT_MSG RAPIDJSON_INCLUDE_DIR)
