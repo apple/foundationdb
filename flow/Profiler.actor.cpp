c3efbe3040770dae65319446b9b3877f29b0ee44
Markus Pilman <markus.pilman@snowflake.com>
2021-01-19 16:52:30 -0700

    fixed minor bug

--- flow/Profiler.actor.cpp
+++ flow/Profiler.actor.cpp
@@ -149,7 +149,7 @@ struct Profiler {
 
 	void signal_handler() {  // async signal safe!
 		static std::atomic<bool> inSigHandler = false;
-		if (!inSigHandler.exchange(true)) { return; }
+		if (inSigHandler.exchange(true)) { return; }
 		if(profilingEnabled) {
 			double t = timer();
 			output_buffer->push(*(void**)&t);
