97c5e130919dfe494ee2be25c75ba59f78c33f93
Markus Pilman <markus.pilman@snowflake.com>
2021-01-26 11:13:14 -0700

    added debug message to figure out why CI is failing

--- cmake/Jemalloc.cmake
+++ cmake/Jemalloc.cmake
@@ -29,6 +29,7 @@ if(NOT WIN32)
       BUILD_IN_SOURCE ON
       BUILD_COMMAND make
       INSTALL_COMMAND make install)
+    message(STATUS "Will link against ${JEMALLOC_DIR}/lib/libjemalloc_pic.a and ${JEMALLOC_DIR}/lib/libjemalloc.a")
     set_target_properties(im_jemalloc_pic PROPERTIES IMPORTED_LOCATION "${JEMALLOC_DIR}/lib/libjemalloc_pic.a")
     set_target_properties(im_jemalloc PROPERTIES IMPORTED_LOCATION "${JEMALLOC_DIR}/lib/libjemalloc.a")
     target_include_directories(jemalloc INTERFACE "${JEMALLOC_DIR}/include")
