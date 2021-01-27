cf6a8e5df73b59b65a845ba326af26292be3a2e6
Evan Tschannen <evan.tschannen@snowflake.com>
2021-01-27 14:36:02 -0800

    waitUntilDiskReady is not an actor

--- fdbrpc/sim2.actor.cpp
+++ fdbrpc/sim2.actor.cpp
@@ -1765,7 +1765,7 @@ ACTOR void doReboot( ISimulator::ProcessInfo *p, ISimulator::KillType kt ) {
 //Simulates delays for performing operations on disk
 Future<Void> waitUntilDiskReady( Reference<DiskParameters> diskParameters, int64_t size, bool sync ) {
 	if(g_simulator.getCurrentProcess()->failedDisk) {
-		wait(Future<Void>(Never()));
+		return Never();
 	}
 	if(g_simulator.connectionFailuresDisableDuration > 1e4)
 		return delay(0.0001);
