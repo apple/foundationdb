e97aef07a8f4036ddcb29e48499880f0fbafcbfc
Markus Pilman <markus@pilman.ch>
2021-01-31 20:21:31 -0700

    wrote build automation script

--- build/build.ps1
+++ build/build.ps1
@@ -1,53 +1,62 @@
 param (
-    [string]$sourcedir = (Resolve-Path "$PSScriptRoot\.."),
-    [string]$imagename = "fdb",
+    [string]$SourceDir = (Resolve-Path "$PSScriptRoot\.."),
+    [string]$ImageName = "fdb",
     # By default we want to leave one CPU core for the OS so the user has some minimal control over the system
-    [string]$cpus = (Get-WmiObject -Class Win32_Processor).NumberOfLogicalProcessors - 1,
+    [string]$Cpus = (Get-WmiObject -Class Win32_Processor).NumberOfLogicalProcessors - 2,
     # We want to leave at least 1GB of memory for the OS
-    [string]$memory = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory - [Math]::Pow(2, 30),
-    [Parameter(Mandatory=$true)][string]$builddir,
-    [switch]$dryrun = $false,
-    [switch]$forceConfigure = $false,
-    [Parameter(Position=0)][string]$target = "installer"
+    [string]$Memory = (Get-WmiObject Win32_ComputerSystem).TotalPhysicalMemory - 2*[Math]::Pow(2, 30),
+    [Parameter(Mandatory=$true)][string]$BuildDir,
+    [switch]$DryRun = $false,
+    [switch]$ForceConfigure = $false,
+    [switch]$SkipDockerBuild = $false,
+    [Parameter(Position=0)][string]$Target = "installer"
 )
 
+$BuildDir = Resolve-Path $BuildDir
+# we don't want a trailing \ in the build dir
+if ($BuildDir.EndsWith("\")) {
+    $BuildDir = $BuildDir.Substring(0, $BuildDir.Length - 1)
+}
 $exponent = 0
-$memory = $memory.ToUpper()
-if ($memory.EndsWith("K")) {
+$Memory = $Memory.ToUpper()
+if ($Memory.EndsWith("K")) {
     $exponent = 10
-} elseif ($memory.EndsWith("M")) {
+} elseif ($Memory.EndsWith("M")) {
     $exponent = 20
-} elseif ($memory.EndsWith("G")) {
+} elseif ($Memory.EndsWith("G")) {
     $exponent = 30
-} elseif ($memory.EndsWith("T")) {
+} elseif ($Memory.EndsWith("T")) {
     $exponent = 40
 }
 if ($exponent -gt 0) {
-    $memory = $memory.Substring(0, $memory.Length - 1) * [Math]::Pow(2, $exponent)
+    $Memory = $Memory.Substring(0, $Memory.Length - 1) * [Math]::Pow(2, $exponent)
 }
 
 $buildCommand = [string]::Format("Get-Content {0}\build\Dockerfile.windows | docker build -t {1} -m {2} -", 
-                                 $sourcedir, $imagename, [Math]::Min(16 * [Math]::Pow(2, 30), $memory))
-if($dryrun) {
+                                 $SourceDir, $ImageName, [Math]::Min(16 * [Math]::Pow(2, 30), $Memory))
+if ($DryRun -and !$SkipDockerBuild) {
     Write-Output $buildCommand
-} else {
+} elseif (!$SkipDockerBuild) {
     Invoke-Expression -Command $buildCommand
 }
 
 # Write build instructions into file
-$batFile = "$builddir\docker_command.cmd"
-"C:\BuildTools\Common7\Tools\VsDevCmd.bat" | Out-File $batFile 
+$cmdFile = "docker_command.ps1"
+$batFile = "$BuildDir\$cmdFile"
+$batFileDocker = "C:\fdbbuild\$cmdFile"
+# "C:\BuildTools\Common7\Tools\VsDevCmd.bat" | Out-File $batFile
 "cd \fdbbuild" | Out-File -Append $batFile
-if ($forceConfigure -or ![System.IO.File]::Exists("$builddir\CMakeCache.txt") -or ($target -eq "configure")) {
+if ($ForceConfigure -or ![System.IO.File]::Exists("$BuildDir\CMakeCache.txt") -or ($Target -eq "configure")) {
     "cmake -G ""Visual Studio 16 2019"" -A x64 -T""ClangCL"" -DCMAKE_BUILD_TYPE=Release C:\foundationdb" | Out-File -Append $batFile
 }
-if ($target -ne "configure") {
-    "msbuild /p:CL_MPCount=$cpus /m /p:UseMultiToolTask=true /p:Configuration=Release foundationdb.sln" | Out-File -Append $batFile
+if ($Target -ne "configure") {
+    "msbuild /p:CL_MPCount=$Cpus /m /p:UseMultiToolTask=true /p:Configuration=Release foundationdb.sln" | Out-File -Append $batFile
 }
 
-$runCommand = [string]::Format("docker run -v {0}:C:\foundationdb -v {1}:C:\fdbbuild --name fdb-build -m {2} --cpus={3} --rm {4} {5}",
-                                $sourcedir, $builddir, $memory, $cpus, $imagename, $batFile);
-if ($dryrun) {
+$dockerCommand = "powershell.exe -NoLogo -ExecutionPolicy Bypass -File $batFileDocker"
+$runCommand = [string]::Format("docker run -v {0}:C:\foundationdb -v {1}:C:\fdbbuild --name fdb-build -m {2} --cpus={3} --rm {4} ""{5}""",
+                                $SourceDir, $BuildDir, $Memory, $Cpus, $ImageName, $dockerCommand);
+if ($DryRun) {
     Write-Output $runCommand
 } else {
     Invoke-Expression $runCommand
