0aafd9d5f0990d27e32ff4ae667ebfeb12050471
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2021-02-01 20:08:47 -0800

    Mark TCServerInfo::isCorrectStoreType const

--- fdbserver/DataDistribution.actor.cpp
+++ fdbserver/DataDistribution.actor.cpp
@@ -100,7 +100,7 @@ struct TCServerInfo : public ReferenceCounted<TCServerInfo> {
 		}
 	}
 
-	bool isCorrectStoreType(KeyValueStoreType configStoreType) {
+	bool isCorrectStoreType(KeyValueStoreType configStoreType) const {
 		// A new storage server's store type may not be set immediately.
 		// If a storage server does not reply its storeType, it will be tracked by failure monitor and removed.
 		return (storeType == configStoreType || storeType == KeyValueStoreType::END);
