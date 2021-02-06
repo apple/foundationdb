5c4863b33ef482eabddff063fb2ac7af0288da6c
A.J. Beamon <aj.beamon@snowflake.com>
2021-02-05 22:07:40 -0800

    Fix unit test that set coordinators to fake processes that the simulator didn't know about

--- fdbclient/ManagementAPI.actor.cpp
+++ fdbclient/ManagementAPI.actor.cpp
@@ -2020,6 +2020,10 @@ TEST_CASE("/ManagementAPI/AutoQuorumChange/checkLocality") {
 		data.locality.set(LiteralStringRef("machineid"), StringRef(machineId));
 		data.address.ip = IPAddress(i);
 
+		g_simulator.newProcess(format("TestProcess%d", i).c_str(), data.address.ip, data.address.port, false, 1,
+		                       data.locality, ProcessClass(ProcessClass::CoordinatorClass, ProcessClass::CommandLineSource),
+		                       "", "");
+
 		workers.push_back(data);
 	}
 
