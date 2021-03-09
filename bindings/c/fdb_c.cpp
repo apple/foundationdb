bdc888ba0af1ddcbf25aafc19612b8e952b6147e
Scott Fines <scottfines@gmail.com>
2021-03-09 11:33:27 -0600

    fixing formatting errors

--- bindings/c/fdb_c.cpp
+++ bindings/c/fdb_c.cpp
@@ -415,10 +415,10 @@ FDBFuture* fdb_transaction_get_range_impl(FDBTransaction* tr,
 	const int mode_bytes_array[] = { CLIENT_KNOBS->BYTE_LIMIT_UNLIMITED, 256, 1000, 4096, 80000 };
 
 	/* The progression used for FDB_STREAMING_MODE_ITERATOR.
-	   Goes  1.5 * previous. */
-	static const int iteration_progression[] = { 
-    4096, 6144, 9216, 13824, 20736, 31104, 46656, 69984, 80000, 120000 
-  };
+	   Goes 1.5 * previous. */
+	static const int iteration_progression[] = {
+		4096, 6144, 9216, 13824, 20736, 31104, 46656, 69984, 80000, 120000 
+	};
 
 	/* length(iteration_progression) */
 	static const int max_iteration = sizeof(iteration_progression) / sizeof(int);
