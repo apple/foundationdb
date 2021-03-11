d91def77430a8d4cd78f3ab287590b932642acba
Vishesh Yadav <vishesh_yadav@apple.com>
2021-03-10 18:56:01 -0800

    Fix windows build

--- fdbserver/CoroFlowCoro.actor.cpp
+++ fdbserver/CoroFlowCoro.actor.cpp
@@ -195,7 +195,7 @@ public:
 	WorkPool() : pool(new Pool) { m_stopOnError = stopOnError(this); }
 
 	Future<Void> getError() const override { return pool->anyError.getResult(); }
-	void addThread(IThreadPoolReceiver* userData) override {
+	void addThread(IThreadPoolReceiver* userData, const char*) override {
 		checkError();
 
 		auto w = new Worker(pool.getPtr(), userData);
