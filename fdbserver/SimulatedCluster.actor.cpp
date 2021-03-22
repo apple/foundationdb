44d4b1c54064518fb5cf2041d62e76cfc5cb9eb9
Jon Fu <jon.fu@snowflake.com>
2021-03-22 16:10:26 -0400

    Use const ref to avoid copying of TestConfig struct

--- fdbserver/SimulatedCluster.actor.cpp
+++ fdbserver/SimulatedCluster.actor.cpp
@@ -840,7 +840,7 @@ ACTOR Future<Void> restartSimulatedSystem(vector<Future<Void>>* systemActors,
 }
 
 struct SimulationConfig {
-	explicit SimulationConfig(TestConfig testConfig);
+	explicit SimulationConfig(const TestConfig& testConfig);
 	int extraDB;
 
 	DatabaseConfiguration db;
@@ -854,10 +854,10 @@ struct SimulationConfig {
 	int coordinators;
 
 private:
-	void generateNormalConfig(TestConfig testConfig);
+	void generateNormalConfig(const TestConfig& testConfig);
 };
 
-SimulationConfig::SimulationConfig(TestConfig testConfig) {
+SimulationConfig::SimulationConfig(const TestConfig& testConfig) {
 	generateNormalConfig(testConfig);
 }
 
@@ -874,7 +874,7 @@ StringRef StringRefOf(const char* s) {
 	return StringRef((uint8_t*)s, strlen(s));
 }
 
-void SimulationConfig::generateNormalConfig(TestConfig testConfig) {
+void SimulationConfig::generateNormalConfig(const TestConfig& testConfig) {
 	set_config("new");
 	const bool simple = false; // Set true to simplify simulation configs for easier debugging
 	// generateMachineTeamTestConfig set up the number of servers per machine and the number of machines such that
