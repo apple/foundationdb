9fd3c559c2d4d41ff6fcd4bf5bbf4ce8139bad74
Jon Fu <jon.fu@snowflake.com>
2021-03-23 17:24:00 -0400

    Added some missing comments for structs

--- fdbserver/SimulatedCluster.actor.cpp
+++ fdbserver/SimulatedCluster.actor.cpp
@@ -839,6 +839,7 @@ ACTOR Future<Void> restartSimulatedSystem(vector<Future<Void>>* systemActors,
 	return Void();
 }
 
+// Configuration details compiled in a structure used when setting up a simulated cluster
 struct SimulationConfig {
 	explicit SimulationConfig(const TestConfig& testConfig);
 	int extraDB;
