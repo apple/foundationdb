cc735e7d6ecb925a8fa4c12bc9dc5d60e5dd9ded
Jingyu Zhou <jingyuzhou@gmail.com>
2021-04-21 21:12:01 -0700

    Test batches to a resolver

--- fdbserver/ptxn/test/TestResolver.actor.cpp
+++ fdbserver/ptxn/test/TestResolver.actor.cpp
@@ -18,31 +18,59 @@
  * limitations under the License.
  */
 
-#include "fdbserver/ptxn/test/Driver.h"
-
+#include <algorithm>
 #include <iostream>
+#include <map>
 #include <memory>
+#include <random>
+#include <stdint.h>
 #include <vector>
 
 #include "fdbclient/FDBTypes.h"
 #include "fdbserver/ptxn/Config.h"
+#include "fdbserver/ptxn/test/Driver.h"
 #include "fdbserver/ptxn/test/FakeResolver.actor.h"
 #include "fdbserver/ResolverInterface.h"
 #include "flow/IRandom.h"
 #include "flow/genericactors.actor.h"
 #include "flow/UnitTest.h"
 
- TEST_CASE("fdbserver/ptxn/test/resolver") {
+namespace {
+
+// Makes a batch of "n" requests starting at "beginVersion", where each batch
+// increases by "increment" amount.
+std::vector<ResolveTransactionBatchRequest> makeTxnBatch(Version prevVersion, Version beginVersion, int n, int64_t increment) {
+	std::vector<ResolveTransactionBatchRequest> batch(n);
+
+	Version current = beginVersion;
+	Version pv = prevVersion;
+	for (auto& r : batch) {
+		r.prevVersion = pv;
+		r.version = current;
+		r.lastReceivedVersion = prevVersion;
+
+		pv = current;
+		current += increment;
+	}
+
+	return batch;
+}
+
+} // anonymous namespace
+
+TEST_CASE("fdbserver/ptxn/test/resolver") {
 	ptxn::TestDriverOptions options(params);
 	std::cout << options << std::endl;
 
+	state const Version lastEpochEnd = 100;
+	state const int totalRequests = 10;
+>>>>>>> Test batches to a resolver
 	state std::vector<Future<Void>> actors;
 	state std::shared_ptr<ptxn::TestDriverContext> context = ptxn::initTestDriverContext();
 
 	startFakeResolver(actors, context);
-	std::cout<<"Started " << context->numResolvers << " Resolvers\n";
+	std::cout << "Started " << context->numResolvers << " Resolvers\n";
 
-	const Version lastEpochEnd = 100;
 	state std::vector<Future<ResolveTransactionBatchReply>> replies;
 	// Imitates resolver initialization from the master server
 	for (auto& r : context->resolverInterfaces) {
@@ -57,10 +85,30 @@
 	}
 
 	wait(waitForAll(replies));
-	for (auto& f : replies) {
+	for (const auto& f : replies) {
 		ASSERT(f.isReady() && f.isValid());
 	}
 	std::cout << "Initialized " << replies.size() << " Resolvers\n";
+
+	// Generate 10 batches, send in a random order, and verify all responses.
+	state Version beginVersion = 200;
+	state int64_t increment = 7;
+
+	std::vector<ResolveTransactionBatchRequest> batch = makeTxnBatch(lastEpochEnd, beginVersion, totalRequests, increment);
+	std::mt19937 g(deterministicRandom()->randomUInt32());
+	std::shuffle(batch.begin(), batch.end(), g);
+
+	replies.clear();
+	for (auto& req : batch) {
+		replies.push_back(brokenPromiseToNever(context->resolverInterfaces[0]->resolve.getReply(req)));
+	}
+
+	// Note even though requests are processed in order at Resolver. The
+	// responses could be received out of order.
+	wait(waitForAll(replies));
+	for (const auto& f : replies) {
+		ASSERT(f.isReady() && f.isValid());
+	}
 
 	return Void();
 }
