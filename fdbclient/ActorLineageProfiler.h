e45faa35342253910645b98e2c68fa5fc135c2d9
Lukas Joswiak <lukas.joswiak@snowflake.com>
2021-04-23 16:38:01 -0700

    Fix a bug where deleting a key invalidated its memory which was later read

--- fdbclient/ActorLineageProfiler.h
+++ fdbclient/ActorLineageProfiler.h
@@ -78,7 +78,7 @@ class SampleCollection_t {
 
 	SampleCollector _collector;
 	mutable std::mutex mutex;
-	std::atomic<double> windowSize = 5.0;
+	std::atomic<double> windowSize = 0.0;
 	std::deque<std::shared_ptr<Sample>> data;
 
 public:
