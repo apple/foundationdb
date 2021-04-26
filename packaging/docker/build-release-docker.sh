83e2a9ff7e440f7fe533076a2ed54f4f4ed2b17a
Russell Sears <russell_sears@apple.com>
2021-04-26 15:20:25 -0700

    improve docker packaging script

--- packaging/docker/build-release-docker.sh
+++ packaging/docker/build-release-docker.sh
@@ -1,35 +1,51 @@
-# Run using . build-release-docker.sh
+#!/bin/bash
+set -euxo pipefail
 
-## This is designed to be run inside an okteto environment.
+## This is designed to be run inside an environment with the following repos checked out under ~/src:
+#
+#     foundationdb
+#     fdb-kubernetes-operator
+#
+# The foundationdb build will write its output to ~/build_output
 
-cmk
+FDB_VERSION=$(grep '  VERSION ' ~/src/foundationdb/CMakeLists.txt | tr -s ' ' ' ' | cut -d ' ' -f 3)
 
-cd ~/src/foundationdb/
+# Options (passed via environment variables)
 
-FDB_VERSION=$(grep '  VERSION ' CMakeLists.txt | tr -s ' ' ' ' | cut -d ' ' -f 3)
-
-# Feel free to customize the image tag:
+# Feel free to customize the image tag.
+# TODO: add a mechanism to set TAG=FDB_VERSION when we're building public releases.
 TAG=${TAG:-${FDB_VERSION}-${OKTETO_NAME}}
-
-export IMAGE=foundationdb/foundationdb:${TAG}
+ECR=${ECR:-112664522426.dkr.ecr.us-west-2.amazonaws.com}
 
 echo Building with tag ${TAG}
 
-WEBSITE_BIN_DIR=website/downloads/$FDB_VERSION/linux/
-TARBALL=${WEBSITE_BIN_DIR}/fdb_$FDB_VERSION.tar.gz
-ECR=112664522426.dkr.ecr.us-west-2.amazonaws.com
+# TODO: This is a copy of the commonly-used 'cmk' function.
+cmake -S ${HOME}/src/foundationdb -B ${HOME}/build_output \
+   -D USE_CCACHE=ON -D USE_WERROR=ON -D RocksDB_ROOT=/opt/rocksdb-6.10.1 -D RUN_JUNIT_TESTS=ON -D RUN_JAVA_INTEGRATION_TESTS=ON \
+   -G Ninja \
+      && ninja -C ${HOME}/build_output -j 84
 
-cd ~/src/foundationdb/packaging/docker
+
+# derived variables
+IMAGE=foundationdb/foundationdb:${TAG}
+SIDECAR_IMAGE=foundationdb/foundationdb-kubernetes-sidecar:${TAG}-1
+
+WEBSITE_BIN_DIR=website/downloads/${FDB_VERSION}/linux/
+TARBALL=${WEBSITE_BIN_DIR}/fdb_${FDB_VERSION}.tar.gz
+
+# copy packaging scripts from operator repo into fdb build_output directory
+cp -an ~/src/fdb-kubernetes-operator/foundationdb-kubernetes-sidecar/* ~/build_output/packages/docker/
+
+cd ~/build_output/packages/docker
 
 mkdir -p ${WEBSITE_BIN_DIR}
 tar -C ~/build_output/packages/ -zcvf ${TARBALL} bin lib
+cp ~/build_output/packages/lib/libfdb_c.so ${WEBSITE_BIN_DIR}/libfdb_c_${FDB_VERSION}.so
 
-# XXX
-make -C ~/src/fdb-kubernetes-tests/tests/ ecr-login
-
-yes| cp ~/build_output/packages/lib/libfdb_c.so ${WEBSITE_BIN_DIR}/libfdb_c_${FDB_VERSION}.so
 docker pull ${ECR}/ubuntu:18.04
 docker tag ${ECR}/ubuntu:18.04 ubuntu:18.04
+docker pull ${ECR}/python:3.9-slim
+docker tag ${ECR}/python:3.9-slim python:3.9-slim
 
 docker build -t ${IMAGE} \
    --build-arg FDB_WEBSITE=file:///mnt/website \
@@ -38,21 +54,6 @@ docker build -t ${IMAGE} \
    -f release/Dockerfile .
 
 docker tag ${IMAGE} ${ECR}/${IMAGE}
-docker push ${ECR}/${IMAGE}
-
-cd ~/src/fdb-kubernetes-operator/foundationdb-kubernetes-sidecar
-echo
-pwd
-echo
-
-mkdir -p ${WEBSITE_BIN_DIR}
-tar -C ~/build_output/packages/ -zcvf ${TARBALL} bin lib
-yes| cp ~/build_output/packages/lib/libfdb_c.so ${WEBSITE_BIN_DIR}/libfdb_c_${FDB_VERSION}.so
-
-SIDECAR_IMAGE=foundationdb/foundationdb-kubernetes-sidecar:${TAG}-1
-
-docker pull ${ECR}/python:3.9-slim
-docker tag ${ECR}/python:3.9-slim python:3.9-slim
 
 docker build -t ${SIDECAR_IMAGE} \
    --build-arg FDB_WEBSITE=file:///mnt/website \
@@ -60,7 +61,11 @@ docker build -t ${SIDECAR_IMAGE} \
    --build-arg FDB_LIBRARY_VERSIONS=$FDB_VERSION \
    -f Dockerfile .
 
-docker tag ${IMAGE} ${ECR}/${SIDECAR_IMAGE}
-docker push ${ECR}/${SIDECAR_IMAGE}
+docker tag ${SIDECAR_IMAGE} ${ECR}/${SIDECAR_IMAGE}
 
-#docker build -f release/Dockerfile -t foundationdb/foundationdb:6.2.29 . --build-arg FDB_VERSION=6.2.29
+# Login to ECR
+# TODO: Move this to a common place instead of repeatedly copy-pasting it.
+aws ecr get-login-password | docker login --username AWS --password-stdin ${ECR}
+
+docker push ${ECR}/${IMAGE}
+docker push ${ECR}/${SIDECAR_IMAGE}
