bb250284e51bec672a1eeb0346c14707f4dd33c5
Andrew Noyes <andrew.noyes@snowflake.com>
2021-05-13 00:21:23 +0000

    Basic tests for rpm and deb packages

---
+++ contrib/pkg_tester/README.md
@@ -0,0 +1,42 @@
+# pkg_tester
+
+This is a test suite that can be used to validate properties of generated package files.
+
+To use it, first build the package files as described in the main [README](https://github.com/apple/foundationdb#linux)
+
+Then setup a virtualenv:
+
+```
+$ python3 -m venv .venv
+$ source .venv/bin/activate
+$ pip install -r requirements.txt
+```
+
+Then you can run the tests with pytest:
+
+```
+$ BUILDDIR=<BUILDDIR> python -m pytest -s -v
+```
+
+These are snapshot tests, so you may need to update the snapshots with
+
+```
+$ BUILDDIR=<BUILDDIR> python -m pytest -s -v --snapshot-update
+```
+
+Use discretion about whether or not the behavior change is acceptable.
+
+A helpful tip for debugging: if you run pytest with `--pdb`, then it will pause
+the tests at the first error which gives you a chance to run some `docker exec`
+commands to try and see what's wrong.
+
+# Requirements
+
+docker, python
+
+# Future work?
+
+- [x] Test rpms
+- [x] Test debs
+- [ ] Test versioned packages
+- [ ] Test that upgrades preserve data/config
---
+++ contrib/pkg_tester/test_fdb_pkgs.py
@@ -0,0 +1,188 @@
+# test_fdb_pkgs.py
+#
+# This source file is part of the FoundationDB open source project
+#
+# Copyright 2013-2021 Apple Inc. and the FoundationDB project authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import glob
+import os
+import pathlib
+import pytest
+import shlex
+import subprocess
+
+from typing import List, Union
+
+
+def pytest_generate_tests(metafunc):
+    if "linux_container" in metafunc.fixturenames:
+        metafunc.parametrize("linux_container", ["ubuntu", "centos"], indirect=True)
+
+
+def run(args: List[str]) -> str:
+    print("$ {}".format(" ".join(map(shlex.quote, args))))
+    result = subprocess.check_output(args).decode("utf-8")
+    print(result)
+    return result
+
+
+class Image:
+    def __init__(self, uid: str):
+        self.uid = uid
+
+    def dispose(self):
+        run(["docker", "image", "rm", self.uid])
+
+
+class Container:
+    def __init__(self, image: Union[str, Image], initd=False):
+        if isinstance(image, Image):
+            name = image.uid
+        else:
+            assert isinstance(image, str)
+            name = image
+
+        # minimal privilege required to run systemd
+        # https://github.com/docker/for-linux/issues/106#issuecomment-330518243
+        extra_privilege = []
+        if initd:
+            extra_privilege = "--cap-add=SYS_ADMIN -e container=docker -v /sys/fs/cgroup:/sys/fs/cgroup".split()
+
+        self.uid = run(
+            ["docker", "run"]
+            + ["-t", "-d"]
+            + extra_privilege
+            + [name]
+            + ["/usr/sbin/init" for _ in range(1) if initd]
+        ).rstrip()
+
+    def run(self, args: List[str]) -> bytes:
+        return run(["docker", "exec", self.uid] + args)
+
+    def copy_to(self, src_path: str, dst_path: str) -> None:
+        run(["docker", "cp", src_path, "{}:{}".format(self.uid, dst_path)])
+
+    def commit(self) -> Image:
+        output = run(["docker", "commit", self.uid])
+        uid = output.split(":")[1].rstrip()
+        return Image(uid)
+
+    def dispose(self):
+        run(["docker", "rm", "-f", self.uid])
+
+
+@pytest.fixture(scope="session")
+def ubuntu_image_with_fdb() -> Image:
+    """
+    Return an image which has just the fdb deb packages installed.
+    """
+    debs = [
+        deb
+        for deb in glob.glob(
+            os.path.join(os.environ.get("BUILDDIR"), "packages", "*.deb")
+        )
+        if "versioned" not in deb
+    ]
+    if not debs:
+        yield None
+        return
+
+    container = Container("ubuntu")
+    for deb in debs:
+        container.copy_to(deb, "/tmp")
+    container.run(["bash", "-c", "dpkg -i /tmp/*.deb"])
+    container.run(["bash", "-c", "rm /tmp/*.deb"])
+    image = container.commit()
+    yield image
+    container.dispose()
+    image.dispose()
+
+
+@pytest.fixture(scope="session")
+def centos_image_with_fdb() -> Image:
+    """
+    Return an image which has just the fdb rpm packages installed.
+    """
+    rpms = [
+        rpm
+        for rpm in glob.glob(
+            os.path.join(os.environ.get("BUILDDIR"), "packages", "*.rpm")
+        )
+        if "versioned" not in rpm
+    ]
+    if not rpms:
+        yield None
+        return
+
+    container = Container("centos", initd=True)
+    for rpm in rpms:
+        container.copy_to(rpm, "/tmp")
+    container.run(["bash", "-c", "yum install -y /tmp/*.rpm"])
+    container.run(["bash", "-c", "rm /tmp/*.rpm"])
+    image = container.commit()
+    yield image
+    container.dispose()
+    image.dispose()
+
+
+@pytest.fixture()
+def linux_container(request, ubuntu_image_with_fdb, centos_image_with_fdb) -> Container:
+    if request.param == "ubuntu":
+        if ubuntu_image_with_fdb is None:
+            pytest.skip("No debian packages available to test")
+        container = Container(ubuntu_image_with_fdb)
+        container.run(
+            ["service", "foundationdb", "start"]
+        )  # outside docker this shouldn't be necessary
+    elif request.param == "centos":
+        if centos_image_with_fdb is None:
+            pytest.skip("No rpm packages available to test")
+        container = Container(centos_image_with_fdb, initd=True)
+    else:
+        assert False
+    yield container
+    container.dispose()
+
+
+#################### BEGIN ACTUAL TESTS ####################
+
+
+def test_db_available(linux_container: Container):
+    linux_container.run(["fdbcli", "--exec", "status"])
+
+
+def test_write(linux_container: Container, snapshot):
+    linux_container.run(["fdbcli", "--exec", "writemode on; set x y"])
+    assert snapshot == linux_container.run(["fdbcli", "--exec", "get x"])
+
+
+def test_fdbcli_help_text(linux_container: Container, snapshot):
+    assert snapshot == linux_container.run(["fdbcli", "--help"])
+
+
+def test_backup_restore(linux_container: Container, snapshot, tmp_path: pathlib.Path):
+    linux_container.run(["fdbcli", "--exec", "writemode on; set x y"])
+    assert snapshot == linux_container.run(
+        ["fdbbackup", "start", "-d", "file:///tmp/fdb_backup", "-w"]
+    )
+    linux_container.run(["fdbcli", "--exec", "writemode on; clear x"])
+    linux_container.run(
+        [
+            "bash",
+            "-c",
+            "fdbrestore start -r file://$(echo /tmp/fdb_backup/*) -w --dest_cluster_file /etc/foundationdb/fdb.cluster",
+        ]
+    )
+    assert snapshot == linux_container.run(["fdbcli", "--exec", "get x"])
