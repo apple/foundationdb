dcbbc9a3157433d3b2877b579674376f2ce0db66
Daniel Smith <daniel_b_smith@apple.com>
2021-06-09 16:15:38 -0400

    Merge pull request #4922 from Daniel-B-Smith/proxies-status-json
    Add a backwards-compatible 'proxies' field to status json

--- fdbclient/Schemas.cpp
+++ fdbclient/Schemas.cpp
@@ -741,7 +741,8 @@ const KeyRef JSONSchemas::statusSchema = LiteralStringRef(R"statusSchema(
          "auto_logs":3,
          "commit_proxies":5,
          "grv_proxies":1,
-         "backup_worker_enabled":1
+         "backup_worker_enabled":1,
+         "proxies":6
       },
       "data":{
          "least_operating_space_bytes_log_server":0,
