ab4cc1da2fe64cf0e92411af095a8bc593a39871
Daniel Smith <daniel_b_smith@apple.com>
2021-06-10 14:04:28 -0400

    revert controversial printf

--- fdbserver/fdbserver.actor.cpp
+++ fdbserver/fdbserver.actor.cpp
@@ -1593,11 +1593,6 @@ private:
 } // namespace
 
 int main(int argc, char* argv[]) {
-	for (int i = 0; i < argc; ++i) {
-		std::cout << argv[i] << " ";
-	}
-	std::cout << std::endl;
-
 	try {
 		platformInit();
 
