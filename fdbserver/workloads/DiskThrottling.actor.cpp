145705a767bc18e46ec7c0a6e7de2e0a7bddd9c0
negoyal <neelam.goyal@gmail.com>
2021-07-01 15:00:19 -0700

    Adding the Disk throttle workload file that I forgot earlier.

--- fdbserver/workloads/DiskThrottling.actor.cpp
+++ fdbserver/workloads/DiskThrottling.actor.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * DiskThrottling.actor.cpp
  *
@@ -18,6 +19,8 @@
  * limitations under the License.
  */
 
+=======
+>>>>>>> 96bde8919... Adding the Disk throttle workload file that I forgot earlier.
 #include "fdbclient/NativeAPI.actor.h"
 #include "fdbserver/TesterInterface.actor.h"
 #include "fdbserver/workloads/workloads.actor.h"
@@ -28,6 +31,7 @@
 #include "flow/actorcompiler.h" // This must be the last #include.
 
 struct DiskThrottlingWorkload : TestWorkload {
+<<<<<<< HEAD
 	bool enabled;
 	double testDuration;
 	double startDelay;
@@ -253,5 +257,128 @@ struct DiskThrottlingWorkload : TestWorkload {
 			wait(chaosGetStatus(self));
 		}
 	}
+=======
+    bool enabled;
+    double testDuration;
+    double throttleFor;
+    DiskThrottlingWorkload(WorkloadContext const& wcx) : TestWorkload(wcx) {
+        enabled = !clientId; // only do this on the "first" client
+        testDuration = getOption(options, LiteralStringRef("testDuration"), 10.0);
+        throttleFor = getOption(options, LiteralStringRef("throttleDelay"), 2.0);
+        TraceEvent("DiskThrottlingWorkload").detail("TestDuration", testDuration).detail("For", throttleFor);
+    }
+
+    std::string description() const override {
+        if (&g_simulator == g_network)
+            return "DiskThrottling";
+        else
+            return "NoSimDiskThrolling";
+    }
+
+    Future<Void> setup(Database const& cx) override { return Void(); }
+
+    Future<Void> start(Database const& cx) override {
+        if (&g_simulator == g_network && enabled) {
+            TraceEvent("DiskThrottlingStart").detail("For", throttleFor);
+            return timeout(reportErrors(throttleDiskClient<ISimulator::ProcessInfo*>(cx, this), "DiskThrottlingError"),
+                           testDuration,
+                           Void());
+        } else if (enabled) {
+            return timeout(reportErrors(throttleDiskClient<WorkerInterface>(cx, this), "DiskThrottlingError"),
+                           testDuration,
+                           Void());
+        } else
+            return Void();
+    }
+
+    Future<bool> check(Database const& cx) override { return true; }
+
+    void getMetrics(vector<PerfMetric>& m) override {}
+
+    ACTOR void doThrottle(ISimulator::ProcessInfo* machine, double t, double delay = 0.0) {
+        wait(::delay(delay));
+        TraceEvent("ThrottleDisk").detail("For", t);
+        g_simulator.throttleDisk(machine, t);
+        TraceEvent("ThrottleDiskSet").detail("For", t);
+    }
+
+    static void checkDiskThrottleResult(Future<Void> res, WorkerInterface worker) {
+        if (res.isError()) {
+            auto err = res.getError();
+            if (err.code() == error_code_client_invalid_operation) {
+                TraceEvent(SevError, "ChaosDisabled")
+                    .detail("OnEndpoint", worker.waitFailure.getEndpoint().addresses.address.toString());
+            } else {
+                TraceEvent(SevError, "DiskThrottlingFailed")
+                    .detail("OnEndpoint", worker.waitFailure.getEndpoint().addresses.address.toString())
+                    .error(err);
+            }
+        }
+    }
+
+    ACTOR void doThrottle(WorkerInterface worker, double t, double delay = 0.0) {
+        state Future<Void> res;
+        wait(::delay(delay));
+        SetFailureInjection::ThrottleDiskCommand throttleDisk;
+        throttleDisk.time = t;
+        SetFailureInjection req;
+        req.throttleDisk = throttleDisk;
+        TraceEvent("ThrottleDisk").detail("For", t);
+        res = worker.clientInterface.setFailureInjection.getReply(req);
+        wait(ready(res));
+        checkDiskThrottleResult(res, worker);
+    }
+
+    static Future<Void> getAllWorkers(DiskThrottlingWorkload* self, std::vector<ISimulator::ProcessInfo*>* result) {
+        result->clear();
+        *result = g_simulator.getAllProcesses();
+        return Void();
+    }
+
+    static Future<Void> getAllStorageWorkers(Database cx, DiskThrottlingWorkload* self, std::vector<ISimulator::ProcessInfo*>* result) {
+        vector<ISimulator::ProcessInfo*> all = g_simulator.getAllProcesses();
+        for (int i = 0; i < all.size(); i++)
+            if (!all[i]->failed &&
+                all[i]->name == std::string("Server") &&
+                ((all[i]->startingClass == ProcessClass::StorageClass) ||
+                 (all[i]->startingClass == ProcessClass::UnsetClass)))
+                result->emplace_back(all[i]);
+        return Void();
+    }
+
+    ACTOR static Future<Void> getAllWorkers(DiskThrottlingWorkload* self, std::vector<WorkerInterface>* result) {
+        result->clear();
+        std::vector<WorkerDetails> res =
+            wait(self->dbInfo->get().clusterInterface.getWorkers.getReply(GetWorkersRequest{}));
+        for (auto& worker : res) {
+            result->emplace_back(worker.interf);
+        }
+        return Void();
+    }
+
+    ACTOR static Future<Void> getAllStorageWorkers(Database cx, DiskThrottlingWorkload* self, std::vector<WorkerInterface>* result) {
+        result->clear();
+        state std::vector<WorkerInterface> res = wait(getStorageWorkers(cx, self->dbInfo, false));
+        for (auto& worker : res) {
+            result->emplace_back(worker);
+        }
+        return Void();
+    }
+
+    ACTOR template <class W>
+    Future<Void> throttleDiskClient(Database cx, DiskThrottlingWorkload* self) {
+        state double lastTime = now();
+        state double workloadEnd = now() + self->testDuration;
+        state std::vector<W> machines;
+        loop {
+            wait(poisson(&lastTime, 1));
+            wait(DiskThrottlingWorkload::getAllStorageWorkers(cx, self, &machines));
+            //wait(DiskThrottlingWorkload::getAllWorkers(self, &machines));
+            auto machine = deterministicRandom()->randomChoice(machines);
+            TraceEvent("DoThrottleDisk").detail("For", self->throttleFor);
+            self->doThrottle(machine, self->throttleFor);
+        }
+    }
+>>>>>>> 96bde8919... Adding the Disk throttle workload file that I forgot earlier.
 };
 WorkloadFactory<DiskThrottlingWorkload> DiskThrottlingWorkloadFactory("DiskThrottling");
