94fa100def82eadfe47a3703513a09071eb263be
Chaoguang Lin <chaoguang.lin@snowflake.com>
2021-08-06 23:36:18 +0000

    Remove TODO comments

--- bindings/python/tests/fdbcli_tests.py
+++ bindings/python/tests/fdbcli_tests.py
@@ -441,7 +441,6 @@ if __name__ == '__main__':
     # assertions will fail if fdbcli does not work as expected
     process_number = int(sys.argv[3])
     if process_number == 1:
-        # TODO: disable for now, the change can cause the database unavailable
         advanceversion()
         cache_range()
         consistencycheck()
