8ed041132f3231ad4ffc11b86aa1da1699a83ba1
Chaoguang Lin <chaoguang.lin@snowflake.com>
2021-08-20 13:42:50 -0700

    Rewrite cache_range related functions to template code; Remove duplicate code; fix the existing bug in cache_range;

--- fdbcli/CacheRangeCommand.actor.cpp
+++ fdbcli/CacheRangeCommand.actor.cpp
@@ -22,75 +22,13 @@
 
 #include "fdbclient/FDBOptions.g.h"
 #include "fdbclient/IClientApi.h"
+#include "fdbclient/ManagementAPI.actor.h"
 #include "fdbclient/SystemData.h"
 
 #include "flow/Arena.h"
 #include "flow/FastRef.h"
 #include "flow/ThreadHelper.actor.h"
 #include "flow/actorcompiler.h" // This must be the last #include.
-
-namespace {
-
-ACTOR Future<Void> changeCachedRange(Reference<IDatabase> db, KeyRangeRef range, bool add) {
-	state Reference<ITransaction> tr = db->createTransaction();
-	state KeyRange sysRange = KeyRangeRef(storageCacheKey(range.begin), storageCacheKey(range.end));
-	state KeyRange sysRangeClear = KeyRangeRef(storageCacheKey(range.begin), keyAfter(storageCacheKey(range.end)));
-	state KeyRange privateRange = KeyRangeRef(cacheKeysKey(0, range.begin), cacheKeysKey(0, range.end));
-	state Value trueValue = storageCacheValue(std::vector<uint16_t>{ 0 });
-	state Value falseValue = storageCacheValue(std::vector<uint16_t>{});
-	loop {
-		tr->setOption(FDBTransactionOptions::LOCK_AWARE);
-		tr->setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
-		try {
-			tr->clear(sysRangeClear);
-			tr->clear(privateRange);
-			tr->addReadConflictRange(privateRange);
-			// hold the returned standalone object's memory
-			state ThreadFuture<RangeResult> previousFuture =
-			    tr->getRange(KeyRangeRef(storageCachePrefix, sysRange.begin), 1, false, true);
-			RangeResult previous = wait(safeThreadFutureToFuture(previousFuture));
-			bool prevIsCached = false;
-			if (!previous.empty()) {
-				std::vector<uint16_t> prevVal;
-				decodeStorageCacheValue(previous[0].value, prevVal);
-				prevIsCached = !prevVal.empty();
-			}
-			if (prevIsCached && !add) {
-				// we need to uncache from here
-				tr->set(sysRange.begin, falseValue);
-				tr->set(privateRange.begin, serverKeysFalse);
-			} else if (!prevIsCached && add) {
-				// we need to cache, starting from here
-				tr->set(sysRange.begin, trueValue);
-				tr->set(privateRange.begin, serverKeysTrue);
-			}
-			// hold the returned standalone object's memory
-			state ThreadFuture<RangeResult> afterFuture =
-			    tr->getRange(KeyRangeRef(sysRange.end, storageCacheKeys.end), 1, false, false);
-			RangeResult after = wait(safeThreadFutureToFuture(afterFuture));
-			bool afterIsCached = false;
-			if (!after.empty()) {
-				std::vector<uint16_t> afterVal;
-				decodeStorageCacheValue(after[0].value, afterVal);
-				afterIsCached = afterVal.empty();
-			}
-			if (afterIsCached && !add) {
-				tr->set(sysRange.end, trueValue);
-				tr->set(privateRange.end, serverKeysTrue);
-			} else if (!afterIsCached && add) {
-				tr->set(sysRange.end, falseValue);
-				tr->set(privateRange.end, serverKeysFalse);
-			}
-			wait(safeThreadFutureToFuture(tr->commit()));
-			return Void();
-		} catch (Error& e) {
-			TraceEvent(SevDebug, "ChangeCachedRangeError").error(e);
-			wait(safeThreadFutureToFuture(tr->onError(e)));
-		}
-	}
-}
-
-} // namespace
 
 namespace fdb_cli {
 
@@ -101,9 +39,9 @@ ACTOR Future<bool> cacheRangeCommandActor(Reference<IDatabase> db, std::vector<S
 	} else {
 		state KeyRangeRef cacheRange(tokens[2], tokens[3]);
 		if (tokencmp(tokens[1], "set")) {
-			wait(changeCachedRange(db, cacheRange, true));
+			wait(ManagementAPI::addCachedRange(db, cacheRange));
 		} else if (tokencmp(tokens[1], "clear")) {
-			wait(changeCachedRange(db, cacheRange, false));
+			wait(ManagementAPI::removeCachedRange(db, cacheRange));
 		} else {
 			printUsage(tokens[0]);
 			return false;
