5f59ba17b1b82d5b066d94f66ef212c07d7277b7
Jingyu Zhou <jingyuzhou@gmail.com>
2021-08-25 10:21:56 -0700

    Fix LogSet and TLogSet construction
    The corresponding fields are not copied correctly.

--- fdbclient/ServerKnobs.cpp
+++ fdbclient/ServerKnobs.cpp
@@ -104,7 +104,7 @@ void ServerKnobs::initialize(Randomize _randomize, ClientKnobs* clientKnobs, IsS
 	init( PUSH_STATS_SLOW_RATIO,                                 0.5 );
 	init( TLOG_POP_BATCH_SIZE,                                  1000 ); if ( randomize && BUGGIFY ) TLOG_POP_BATCH_SIZE = 10;
 	init( TLOG_SERVER_TEAM_PARTITIONED,                        false );
-	init( TLOG_NEW_INTERFACE,                        		   false );
+	init( TLOG_NEW_INTERFACE,                                  false );
 
 	// disk snapshot max timeout, to be put in TLog, storage and coordinator nodes
 	init( MAX_FORKED_PROCESS_OUTPUT,                            1024 );
