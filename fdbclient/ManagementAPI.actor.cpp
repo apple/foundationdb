50cd4b0a78385929e20277eccf9e35e46db193a7
Xiaoxi Wang <xiaoxi.wang@snowflake.com>
2021-09-17 10:52:02 -0700

    fdbcli

--- fdbclient/ManagementAPI.actor.cpp
+++ fdbclient/ManagementAPI.actor.cpp
@@ -634,11 +634,9 @@ ACTOR Future<ConfigurationResult> changeConfig(Database cx, std::map<std::string
 
 					if (newConfig.storageServerStoreType != oldConfig.storageServerStoreType &&
 					    newConfig.storageMigrationType == StorageMigrationType::DISABLED) {
-						warnChangeStorageNoMigrate = true;
-					} else if ((newConfig.storageMigrationType == StorageMigrationType::GRADUAL &&
-					            newConfig.perpetualStorageWiggleSpeed == 0) ||
-					           (newConfig.perpetualStorageWiggleSpeed > 0 &&
-					            newConfig.storageMigrationType == StorageMigrationType::DISABLED)) {
+						return ConfigurationResult::STORAGE_MIGRATION_DISABLED;
+					} else if (newConfig.storageMigrationType == StorageMigrationType::GRADUAL &&
+					           newConfig.perpetualStorageWiggleSpeed == 0) {
 						warnPPWGradual = true;
 					}
 				}
@@ -699,8 +697,6 @@ ACTOR Future<ConfigurationResult> changeConfig(Database cx, std::map<std::string
 
 	if (warnPPWGradual) {
 		return ConfigurationResult::SUCCESS_WARN_PPW_GRADUAL;
-	} else if (warnChangeStorageNoMigrate) {
-		return ConfigurationResult::SUCCESS_WARN_CHANGE_STORAGE_NOMIGRATE;
 	} else {
 		return ConfigurationResult::SUCCESS;
 	}
