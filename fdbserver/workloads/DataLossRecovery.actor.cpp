f2154ff31ec8a86ba9bf63a207fa6ab6449ec5fe
He Liu <heliu023@gmail.com>
2021-09-17 20:39:58 -0700

    DD crashes after excluding the last server

--- fdbserver/workloads/DataLossRecovery.actor.cpp
+++ fdbserver/workloads/DataLossRecovery.actor.cpp
@@ -34,58 +34,97 @@
 struct DataLossRecoveryWorkload : TestWorkload {
 	FlowLock startMoveKeysParallelismLock;
 	FlowLock finishMoveKeysParallelismLock;
+	const bool enabled;
 
 	DataLossRecoveryWorkload(WorkloadContext const& wcx)
-	  : TestWorkload(wcx), startMoveKeysParallelismLock(1), finishMoveKeysParallelismLock(1) {}
+	  : TestWorkload(wcx), startMoveKeysParallelismLock(1), finishMoveKeysParallelismLock(1), enabled(!clientId) {}
 
 	std::string description() const override { return "DataLossRecovery"; }
 
 	Future<Void> setup(Database const& cx) override { return Void(); }
 
 	Future<Void> start(Database const& cx) override {
-		std::string key = "TestKey";
-		Future<Void> f = init(cx, key);
-		Future<std::vector<UID>> excluded = exclude(cx, key, f);
-		return moveRange(this, cx, KeyRangeRef(KeyRef(key), LiteralStringRef("TestKey0")), excluded);
+		std::cout << clientId << " started" << std::endl;
+		if (!enabled) {
+			std::cout << clientId << " skipping" << std::endl;
+			return Void();
+		}
+		return _start(this, cx);
 	}
 
-	ACTOR Future<Void> init(Database cx, std::string key) {
+	ACTOR Future<Void> _start(DataLossRecoveryWorkload* self, Database cx) {
+		state std::string key = "TestKey";
+		state std::string oldValue = "TestValue";
+		state std::string newValue = "TestNewValue";
+
+		wait(self->readWriteKey(cx, key, "", oldValue));
+		wait(self->moveRange(self, cx, KeyRangeRef(KeyRef(key), LiteralStringRef("TestKey0"))));
+		wait(self->exclude(cx, key));
+		wait(self->readWriteKey(cx, key, oldValue, newValue));
+
+		return Void();
+	}
+
+	ACTOR Future<Void> readWriteKey(Database cx, std::string key, std::string oldValue, std::string newValue) {
+		std::cout << "r" << std::endl;
+		std::cout << "d" << std::endl;
 		state Transaction tr(cx);
 		state StringRef k(key);
+		state StringRef v(newValue);
+
+		try {
+			Optional<Value> res = wait(tr.get(k));
+			if (res.present()) {
+				std::cout << "old value: " << res.get().toString();
+			}
+		} catch (Error& e) {
+			wait(tr.onError(e));
+		}
+
 		loop {
 			tr.reset();
 			try {
-				tr.set(k, LiteralStringRef("Value"));
+				tr.set(k, v);
 				wait(tr.commit());
 				break;
 			} catch (Error& e) {
 				wait(tr.onError(e));
 			}
 		}
+		std::cout << "Write Done" << std::endl;
 
+		loop {
+			tr.reset();
+			try {
+				Optional<Value> res = wait(tr.get(k));
+				if (res.present()) {
+					assert(res.get() == v);
+					break;
+				}
+			} catch (Error& e) {
+				wait(tr.onError(e));
+			}
+		}
 		return Void();
 	}
 
-	ACTOR Future<std::vector<UID>> exclude(Database cx, std::string key, Future<Void> init) {
-		wait(init);
+	ACTOR Future<Void> exclude(Database cx, std::string key) {
+		int ignore = wait(setDDMode(cx, 1));
 		state Transaction tr(cx);
 		state StringRef k(key);
 		state std::vector<AddressExclusion> servers;
-		state std::unordered_set<std::string> addressSet;
-		state std::vector<UID> dest;
 		loop {
+			std::cout << "Start exluding..." << std::endl;
 			tr.reset();
 			servers.clear();
-			addressSet.clear();
 			try {
 				Standalone<VectorRef<const char*>> addresses = wait(tr.getAddressesForKey(k));
 				for (int i = 0; i < addresses.size(); ++i) {
-					std::cout << addresses[i] << std::endl;
+					std::cout << "Exclude address: " << addresses[i] << std::endl;
 					servers.push_back(AddressExclusion::parse(StringRef(std::string(addresses[i]))));
-					addressSet.insert(std::string(addresses[i]));
 				}
 				std::cout << std::endl;
-				excludeServers(tr, servers);
+				excludeServers(tr, servers, true);
 				wait(tr.commit());
 				break;
 			} catch (Error& e) {
@@ -94,36 +133,31 @@ struct DataLossRecoveryWorkload : TestWorkload {
 			}
 		}
 
+		std::cout << "Waiting for exclude to complete..." << std::endl;
 		// Wait until all data are moved out of servers.
 		std::set<NetworkAddress> inProgress = wait(checkForExcludingServers(cx, servers, true));
 		assert(inProgress.empty());
 
-		wait(includeServers(cx, servers, false));
-
-		// Fetch server ids.
-		while (dest.empty()) {
-			state std::vector<StorageServerInterface> interfs = wait(getStorageServers(cx));
-			for (auto& ssi : interfs) {
-				std::cout << "SS: " << ssi.address().toString() << std::endl;
-				if (addressSet.count(ssi.address().toString()) > 0) {
-					dest.push_back(ssi.uniqueID);
-				}
-			}
-			wait(delay(1));
-			std::cout << std::endl;
-		}
-		std::cout << "dest: " << describe(dest) << std::endl;
-		int ignore = wait(setDDMode(cx, 0));
-
-		return dest;
+		std::cout << "Exclude done." << std::endl;
+		// Wait until all data are moved out of servers.
+		return Void();
 	}
 
-	ACTOR Future<Void> moveRange(DataLossRecoveryWorkload* self,
-	                             Database cx,
-	                             KeyRange keys,
-	                             Future<std::vector<UID>> excluded) {
-		std::vector<UID> dest = wait(excluded);
-		std::cout << "Recived dest: " << describe(dest) << std::endl;
+	ACTOR Future<Void> moveRange(DataLossRecoveryWorkload* self, Database cx, KeyRange keys) {
+		int ignore = wait(setDDMode(cx, 0));
+		state std::string addr;
+		state std::vector<UID> dest;
+
+		while (dest.empty()) {
+			std::vector<StorageServerInterface> interfs = wait(getStorageServers(cx));
+			if (!interfs.empty()) {
+				int idx = random() % interfs.size();
+				dest.push_back(interfs[idx].uniqueID);
+				addr = interfs[idx].address().toString();
+			}
+		}
+		std::cout << "dest: " << describe(dest) << ", address: " << addr << std::endl;
+
 		state DDEnabledState ddEnabledState;
 		wait(moveKeys(cx,
 		              keys,
@@ -136,6 +170,14 @@ struct DataLossRecoveryWorkload : TestWorkload {
 		              false,
 		              UID(), // for logging only
 		              &ddEnabledState));
+
+		Transaction tr(cx);
+		Standalone<VectorRef<const char*>> addresses = wait(tr.getAddressesForKey(keys.begin));
+		assert(addresses.size() == 1);
+		assert(std::string(addresses[0]) == address);
+
+		std::cout << "Moved" << std::endl;
+
 		return Void();
 	}
 
