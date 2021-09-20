9dc6daab8e7467b4ae28067e3f76dff53ac2f96f
Chaoguang Lin <chaoguang.lin@snowflake.com>
2021-09-20 15:40:53 -0700

    Refactor fileconfigure command

--- fdbcli/fdbcli.actor.cpp
+++ fdbcli/fdbcli.actor.cpp
@@ -469,13 +469,6 @@ void initHelp() {
 	    "clear a range of keys from the database",
 	    "All keys between BEGINKEY (inclusive) and ENDKEY (exclusive) are cleared from the database. This command will "
 	    "succeed even if the specified range is empty, but may fail because of conflicts." ESCAPINGK);
-	helpMap["fileconfigure"] = CommandHelp(
-	    "fileconfigure [new] <FILENAME>",
-	    "change the database configuration from a file",
-	    "The `new' option, if present, initializes a new database with the given configuration rather than changing "
-	    "the configuration of an existing one. Load a JSON document from the provided file, and change the database "
-	    "configuration to match the contents of the JSON document. The format should be the same as the value of the "
-	    "\"configuration\" entry in status JSON without \"excluded_servers\" or \"coordinators_count\".");
 	helpMap["exit"] = CommandHelp("exit", "exit the CLI", "");
 	helpMap["quit"] = CommandHelp();
 	helpMap["waitconnected"] = CommandHelp();
@@ -636,141 +629,6 @@ ACTOR Future<Void> commitTransaction(Reference<ITransaction> tr) {
 	else
 		printf("Nothing to commit\n");
 	return Void();
-}
-
-ACTOR Future<bool> fileConfigure(Database db, std::string filePath, bool isNewDatabase, bool force) {
-	std::string contents(readFileBytes(filePath, 100000));
-	json_spirit::mValue config;
-	if (!json_spirit::read_string(contents, config)) {
-		fprintf(stderr, "ERROR: Invalid JSON\n");
-		return true;
-	}
-	if (config.type() != json_spirit::obj_type) {
-		fprintf(stderr, "ERROR: Configuration file must contain a JSON object\n");
-		return true;
-	}
-	StatusObject configJSON = config.get_obj();
-
-	json_spirit::mValue schema;
-	if (!json_spirit::read_string(JSONSchemas::clusterConfigurationSchema.toString(), schema)) {
-		ASSERT(false);
-	}
-
-	std::string errorStr;
-	if (!schemaMatch(schema.get_obj(), configJSON, errorStr)) {
-		printf("%s", errorStr.c_str());
-		return true;
-	}
-
-	std::string configString;
-	if (isNewDatabase) {
-		configString = "new";
-	}
-
-	for (const auto& [name, value] : configJSON) {
-		if (!configString.empty()) {
-			configString += " ";
-		}
-		if (value.type() == json_spirit::int_type) {
-			configString += name + ":=" + format("%d", value.get_int());
-		} else if (value.type() == json_spirit::str_type) {
-			configString += value.get_str();
-		} else if (value.type() == json_spirit::array_type) {
-			configString +=
-			    name + "=" +
-			    json_spirit::write_string(json_spirit::mValue(value.get_array()), json_spirit::Output_options::none);
-		} else {
-			printUsage(LiteralStringRef("fileconfigure"));
-			return true;
-		}
-	}
-	ConfigurationResult result =
-	    wait(makeInterruptable(ManagementAPI::changeConfig(db.getReference(), configString, force)));
-	// Real errors get thrown from makeInterruptable and printed by the catch block in cli(), but
-	// there are various results specific to changeConfig() that we need to report:
-	bool ret;
-	switch (result) {
-	case ConfigurationResult::NO_OPTIONS_PROVIDED:
-		fprintf(stderr, "ERROR: No options provided\n");
-		ret = true;
-		break;
-	case ConfigurationResult::CONFLICTING_OPTIONS:
-		fprintf(stderr, "ERROR: Conflicting options\n");
-		ret = true;
-		break;
-	case ConfigurationResult::UNKNOWN_OPTION:
-		fprintf(stderr, "ERROR: Unknown option\n"); // This should not be possible because of schema match
-		ret = true;
-		break;
-	case ConfigurationResult::INCOMPLETE_CONFIGURATION:
-		fprintf(stderr,
-		        "ERROR: Must specify both a replication level and a storage engine when creating a new database\n");
-		ret = true;
-		break;
-	case ConfigurationResult::INVALID_CONFIGURATION:
-		fprintf(stderr, "ERROR: These changes would make the configuration invalid\n");
-		ret = true;
-		break;
-	case ConfigurationResult::DATABASE_ALREADY_CREATED:
-		fprintf(stderr, "ERROR: Database already exists! To change configuration, don't say `new'\n");
-		ret = true;
-		break;
-	case ConfigurationResult::DATABASE_CREATED:
-		printf("Database created\n");
-		ret = false;
-		break;
-	case ConfigurationResult::DATABASE_UNAVAILABLE:
-		fprintf(stderr, "ERROR: The database is unavailable\n");
-		printf("Type `fileconfigure FORCE <FILENAME>' to configure without this check\n");
-		ret = true;
-		break;
-	case ConfigurationResult::STORAGE_IN_UNKNOWN_DCID:
-		fprintf(stderr, "ERROR: All storage servers must be in one of the known regions\n");
-		printf("Type `fileconfigure FORCE <FILENAME>' to configure without this check\n");
-		ret = true;
-		break;
-	case ConfigurationResult::REGION_NOT_FULLY_REPLICATED:
-		fprintf(stderr,
-		        "ERROR: When usable_regions > 1, All regions with priority >= 0 must be fully replicated "
-		        "before changing the configuration\n");
-		printf("Type `fileconfigure FORCE <FILENAME>' to configure without this check\n");
-		ret = true;
-		break;
-	case ConfigurationResult::MULTIPLE_ACTIVE_REGIONS:
-		fprintf(stderr, "ERROR: When changing usable_regions, only one region can have priority >= 0\n");
-		printf("Type `fileconfigure FORCE <FILENAME>' to configure without this check\n");
-		ret = true;
-		break;
-	case ConfigurationResult::REGIONS_CHANGED:
-		fprintf(stderr,
-		        "ERROR: The region configuration cannot be changed while simultaneously changing usable_regions\n");
-		printf("Type `fileconfigure FORCE <FILENAME>' to configure without this check\n");
-		ret = true;
-		break;
-	case ConfigurationResult::NOT_ENOUGH_WORKERS:
-		fprintf(stderr, "ERROR: Not enough processes exist to support the specified configuration\n");
-		printf("Type `fileconfigure FORCE <FILENAME>' to configure without this check\n");
-		ret = true;
-		break;
-	case ConfigurationResult::REGION_REPLICATION_MISMATCH:
-		fprintf(stderr, "ERROR: `three_datacenter' replication is incompatible with region configuration\n");
-		printf("Type `fileconfigure FORCE <TOKEN...>' to configure without this check\n");
-		ret = true;
-		break;
-	case ConfigurationResult::DCID_MISSING:
-		fprintf(stderr, "ERROR: `No storage servers in one of the specified regions\n");
-		printf("Type `fileconfigure FORCE <TOKEN...>' to configure without this check\n");
-		ret = true;
-		break;
-	case ConfigurationResult::SUCCESS:
-		printf("Configuration changed\n");
-		ret = false;
-		break;
-	default:
-		ASSERT(false);
-		ret = true;
-	};
-	return ret;
 }
 
 // FIXME: Factor address parsing from coordinators, include, exclude
@@ -1946,11 +1804,11 @@ ACTOR Future<int> cli(CLIOptions opt, LineNoise* plinenoise) {
 				if (tokencmp(tokens[0], "fileconfigure")) {
 					if (tokens.size() == 2 || (tokens.size() == 3 && (tokens[1] == LiteralStringRef("new") ||
 					                                                  tokens[1] == LiteralStringRef("FORCE")))) {
-						bool err = wait(fileConfigure(localDb,
-						                              tokens.back().toString(),
-						                              tokens[1] == LiteralStringRef("new"),
-						                              tokens[1] == LiteralStringRef("FORCE")));
-						if (err)
+						bool _result = wait(fileConfigureCommandActor(db,
+						                                              tokens.back().toString(),
+						                                              tokens[1] == LiteralStringRef("new"),
+						                                              tokens[1] == LiteralStringRef("FORCE")));
+						if (!_result)
 							is_error = true;
 					} else {
 						printUsage(tokens[0]);
