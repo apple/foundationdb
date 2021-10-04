c1d7b03087ac47b35ea8d324ed87bc44756fbf42
Jingyu Zhou <jingyuzhou@gmail.com>
2021-10-03 21:04:04 -0700

    CommitProxy uses Resolver's private mutations
    Note the commit proxy still runs applyMetadataMutations for setting up memory
    states, and no longer generates private mutations.

--- fdbserver/LogSystem.h
+++ fdbserver/LogSystem.h
@@ -22,6 +22,7 @@
 #define FDBSERVER_LOGSYSTEM_H
 
 #include <set>
+#include <stdint.h>
 #include <vector>
 
 #include "fdbserver/SpanContextMessage.h"
@@ -804,6 +805,13 @@ struct LogPushData : NonCopyable {
 	// Returns the ratio of empty messages in this version batch.
 	// MUST be called after getMessages() and recordEmptyMessage().
 	float getEmptyMessageRatio() const;
+
+	// Returns the total number of mutations.
+	uint32_t getMutationCount() const { return subsequence; }
+
+	// Sets mutations for all internal writers. "mutations" is the output from
+	// getAllMessages() and is used before writing any other mutations.
+	void setMutations(uint32_t totalMutations, VectorRef<StringRef> mutations);
 
 private:
 	Reference<ILogSystem> logSystem;
