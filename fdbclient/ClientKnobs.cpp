dff95dc143e1eb145894db1d3769a4b82de88b38
Vaidas Gasiunas <vaidas.gasiunas@snowflake.com>
2021-10-22 09:57:56 +0200

    MVC2.0: Fix whitespace alignment
    Co-authored-by: A.J. Beamon <aj.beamon@snowflake.com>

--- fdbclient/ClientKnobs.cpp
+++ fdbclient/ClientKnobs.cpp
@@ -253,7 +253,7 @@ void ClientKnobs::initialize(Randomize randomize) {
 
 	// multi-version client control
 	init( MVC_CLIENTLIB_CHUNK_SIZE,              8*1024 );
-	init( MVC_CLIENTLIB_CHUNKS_PER_TRANSACTION,  32 );
+	init( MVC_CLIENTLIB_CHUNKS_PER_TRANSACTION,      32 );
 
 	// clang-format on
 }
