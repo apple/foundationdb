1fa70681fe6e2ca3cd04e193d8733975169f292b
Yao Xiao <yxiao6@apple.com>
2021-11-15 22:54:14 -0800

    Throttle the number of concurrent reads to RocksDB

--- fdbserver/Knobs.cpp
+++ fdbserver/Knobs.cpp
@@ -347,6 +347,14 @@ void ServerKnobs::initialize(bool randomize, ClientKnobs* clientKnobs, bool isSi
 	init( ROCKSDB_PREFIX_LEN,                                      0 );
 	init( ROCKSDB_BLOCK_CACHE_SIZE,                                0 );
 	init( ROCKSDB_METRICS_DELAY,                                60.0 );
+	init( ROCKSDB_READ_VALUE_TIMEOUT,                            5.0 );
+	init( ROCKSDB_READ_VALUE_PREFIX_TIMEOUT,                     5.0 );
+	init( ROCKSDB_READ_RANGE_TIMEOUT,                            5.0 );
+	init( ROCKSDB_READ_QUEUE_WAIT,                               1.0 );
+	init( ROCKSDB_READ_QUEUE_HARD_MAX,                          1000 );
+	init( ROCKSDB_READ_QUEUE_SOFT_MAX,                           500 );
+	init( ROCKSDB_FETCH_QUEUE_HARD_MAX,                          100 );
+	init( ROCKSDB_FETCH_QUEUE_SOFT_MAX,                           50 );
 
 	// Leader election
 	bool longLeaderElection = randomize && BUGGIFY;
