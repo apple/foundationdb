f8cc50734f9aae923f2daf1d7951bb019a098bc9
Xiaoge Su <magichp@gmail.com>
2021-11-30 15:56:59 -0800

    Update FakeTLog to support epoch
    Also add fake latencies to simulate network latencies.
    
    It is known that TestTLogPeek is broken due to the change. The failures
    are addressed in future commits

--- fdbserver/ptxn/test/FakeTLog.actor.h
+++ fdbserver/ptxn/test/FakeTLog.actor.h
@@ -24,6 +24,7 @@
 #elif !defined(FDBSERVER_PTXN_TEST_FAKETLOG_ACTOR_H)
 #define FDBSERVER_PTXN_TEST_FAKETLOG_ACTOR_H
 
+#include <limits>
 #include <memory>
 #include <unordered_map>
 
@@ -31,6 +32,7 @@
 #include "fdbserver/ptxn/MessageSerializer.h"
 #include "fdbserver/ptxn/MessageTypes.h"
 #include "fdbserver/ptxn/StorageServerInterface.h"
+#include "fdbserver/ptxn/test/Delay.h"
 #include "fdbserver/ptxn/TLogInterface.h"
 #include "flow/flow.h"
 
@@ -43,10 +45,16 @@ namespace ptxn::test {
 struct TestDriverContext;
 
 struct FakeTLogContext {
+	// FIXME FakeTLogContext should *not* depend on TestDriverContext, should use TestEnvironment
 	std::shared_ptr<TestDriverContext> pTestDriverContext;
 	std::shared_ptr<TLogInterfaceBase> pTLogInterface;
 
+	// Maximum bytes a peek would return
+	// The actual bytes might be different since
 	size_t maxBytesPerPeek = 1024 * 1024;
+
+	// Maximum versions a peek would return
+	int maxVersionsPerPeek = std::numeric_limits<int>::max();
 
 	// Team IDs
 	std::vector<StorageTeamID> storageTeamIDs;
@@ -58,8 +66,20 @@ struct FakeTLogContext {
 	// keep its own version of data, to simulate the "persistence" behavior.
 	Arena persistenceArena;
 
-	// Deseriqlized messages grouped by teams
-	std::unordered_map<StorageTeamID, VectorRef<VersionSubsequenceMessage>> storageTeamMessages;
+	using StorageTeamMessages = std::unordered_map<StorageTeamID, VectorRef<VersionSubsequenceMessage>>;
+	// Stores the messages per commit version, in format
+	// Commit version -- ( Storage Team ID -- (Storage Team Version, Subsequence, Message) )
+	//                   ( Storage Team ID -- (Storage Team Version, Subsequence, Message) )
+	//                   ...
+	// Commit version -- ( Storage Team ID -- (Storage Team Version, Subsequence, Message) )
+	//                   ...
+	std::map<Version, StorageTeamMessages> epochVersionMessages;
+
+	// Stores the version range per epoch
+	std::vector<std::pair<Version, Version>> epochVersionRange;
+
+	// Delay to simulate network latency
+	RandomDelay latency = RandomDelay(0.01, 0.02);
 };
 
 ACTOR Future<Void> fakeTLog_ActivelyPush(std::shared_ptr<FakeTLogContext> pFakeTLogContext);
