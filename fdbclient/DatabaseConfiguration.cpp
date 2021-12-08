6eb5a6bfdc98269296a6a3f4c5725f92bb3e90f9
Suraj Gupta <suraj.gupta@snowflake.com>
2021-12-08 14:15:10 -0500

    Default to blob granules disabled.

--- fdbclient/DatabaseConfiguration.cpp
+++ fdbclient/DatabaseConfiguration.cpp
@@ -50,7 +50,7 @@ void DatabaseConfiguration::resetInternal() {
 	perpetualStorageWiggleSpeed = 0;
 	perpetualStorageWiggleLocality = "0";
 	storageMigrationType = StorageMigrationType::DEFAULT;
-	blobGranulesEnabled = true; // TODO: default to false if merging to master
+	blobGranulesEnabled = false;
 }
 
 int toInt(ValueRef const& v) {
