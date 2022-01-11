2856777636acc309c73cd717b4c57d8329bd311f
Hao Fu <77984096+hfu5@users.noreply.github.com>
2022-01-10 22:31:26 -0800

    Refactor TLog test (#6230)
    * Refactor TLog test
    
    Remove duplicate code

--- fdbserver/ptxn/test/Driver.h
+++ fdbserver/ptxn/test/Driver.h
@@ -108,6 +108,9 @@ struct TestDriverContext {
 	// For a given storageTeamID, returns the pair(prevVersion, currentVersion).
 	std::pair<Version, Version> getCommitVersionPair(const TLogGroupID& tLogGroupID, const Version& currentVersion);
 
+	std::unordered_map<ptxn::TLogGroupID, IKeyValueStore*> kvStores;
+	std::unordered_map<ptxn::TLogGroupID, IDiskQueue*> diskQueues;
+
 	// Storage Server
 	bool useFakeStorageServer;
 	int numStorageServers;
