eae3dab04d46b4555ec99923478c6222a28b34be
Yi Wu <yi.wu@snowflake.com>
2022-02-03 16:30:50 -0800

    use DiskQueueVersion::V2 for KeyValueStoreMemory

--- fdbserver/KeyValueStoreMemory.actor.cpp
+++ fdbserver/KeyValueStoreMemory.actor.cpp
@@ -887,8 +887,7 @@ IKeyValueStore* keyValueStoreMemory(std::string const& basename,
 	    .detail("MemoryLimit", memoryLimit)
 	    .detail("StoreType", storeType);
 
-	// SOMEDAY: Maybe update to use DiskQueueVersion::V2 with xxhash3 checksum.
-	IDiskQueue* log = openDiskQueue(basename, ext, logID, DiskQueueVersion::V1);
+	IDiskQueue* log = openDiskQueue(basename, ext, logID, DiskQueueVersion::V2);
 	if (storeType == KeyValueStoreType::MEMORY_RADIXTREE) {
 		return new KeyValueStoreMemory<radix_tree>(log, logID, memoryLimit, storeType, false, false, false);
 	} else {
