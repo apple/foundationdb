36c39d3eb51d2582d78bb81fb750b821459055ef
sfc-gh-zyang <zhenfeng.yang@snowflake.com>
2022-02-16 17:38:48 -0800

    set database config via argument

--- contrib/generate_profile.sh
+++ contrib/generate_profile.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-if [ $# -ne 1 ]
+if [ $# -eq 0 ] || [ $# -gt 2 ]
   then
-    echo "Usage: generate_profile.sh Path_Of_Foundation_Build_Directory"
+    echo "Usage: generate_profile.sh Path_Of_Foundation_Build_Directory Database_Config"
     exit 1
 fi
 fdbdir=$1
+db_config='configure new ssd single'
+if [ $# -eq 2 ]
+  then
+    db_config='configure new '$2
+fi
+
 export LD_LIBRARY_PATH=$fdbdir/lib:$LD_LIBRARY_PATH
 export FDB_CLUSTER_FILE=$fdbdir/fdb.cluster
 export LLVM_PROFILE_FILE=$fdbdir/sandbox/fdb-%p.profraw
 $fdbdir/bin/fdbmonitor --conffile $fdbdir/sandbox/foundationdb.conf --lockfile $fdbdir/sandbox/fdbmonitor.pid &
 # This profile will be ignored
 export LLVM_PROFILE_FILE=$fdbdir/sandbox/cli-%m.profraw
-$fdbdir/bin/fdbcli -C $fdbdir/fdb.cluster --exec 'configure new ssd single'
+$fdbdir/bin/fdbcli -C $fdbdir/fdb.cluster --exec "$db_config"
 export LLVM_PROFILE_FILE=$fdbdir/sandbox/mako-build-%m.profraw
 $fdbdir/bin/mako -p 64 -t 1 --keylen 32 --vallen 16 --mode build --rows 10000  --trace  --trace_format json
 export LLVM_PROFILE_FILE=$fdbdir/sandbox/mako-run-%m.profraw
