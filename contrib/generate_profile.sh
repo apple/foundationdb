51dca9c50be78a9639f5717fd02949bd5e9eff89
sfc-gh-zyang <zhenfeng.yang@snowflake.com>
2022-02-17 14:54:35 -0800

    rename storage_engine

--- contrib/generate_profile.sh
+++ contrib/generate_profile.sh
@@ -5,10 +5,10 @@ if [ $# -eq 0 ] || [ $# -gt 2 ]
     exit 1
 fi
 fdbdir=$1
-db_config='ssd'
+storage_engine='ssd'
 if [ $# -eq 2 ]
   then
-    db_config=$2
+    storage_engine=$2
 fi
 
 export LD_LIBRARY_PATH=$fdbdir/lib:$LD_LIBRARY_PATH
@@ -17,7 +17,7 @@ export LLVM_PROFILE_FILE=$fdbdir/sandbox/fdb-%p.profraw
 $fdbdir/bin/fdbmonitor --conffile $fdbdir/sandbox/foundationdb.conf --lockfile $fdbdir/sandbox/fdbmonitor.pid &
 # This profile will be ignored
 export LLVM_PROFILE_FILE=$fdbdir/sandbox/cli-%m.profraw
-$fdbdir/bin/fdbcli -C $fdbdir/fdb.cluster --exec "configure new $db_config single"
+$fdbdir/bin/fdbcli -C $fdbdir/fdb.cluster --exec "configure new $storage_engine single"
 export LLVM_PROFILE_FILE=$fdbdir/sandbox/mako-build-%m.profraw
 $fdbdir/bin/mako -p 64 -t 1 --keylen 32 --vallen 16 --mode build --rows 10000  --trace  --trace_format json
 export LLVM_PROFILE_FILE=$fdbdir/sandbox/mako-run-%m.profraw
