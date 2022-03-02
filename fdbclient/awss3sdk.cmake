4cd4d04a0309d933737af6e4d3ccaa265421cc20
Josh Slocum <josh.slocum@snowflake.com>
2022-03-01 21:16:53 -0600

    installs but weirdly!

--- fdbclient/awss3sdk.cmake
+++ fdbclient/awss3sdk.cmake
@@ -1,15 +1,11 @@
-project(awss3sdk-download)
+project(awss3sdk-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(awss3sdk
   GIT_REPOSITORY    https://github.com/aws/aws-sdk-cpp.git
-  GIT_TAG           502b3999084911185bc52c6ab1f6687f0ea9a7d5 # v1.9.205
+  GIT_TAG           1.9.200
   GIT_CONFIG        advice.detachedHead=false
-  SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/awss3sdk-src"
-  BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/awss3sdk-build"
- CMAKE_ARGS        -DBUILD_ONLY=s3 -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTING=OFF
-  BUILD_COMMAND     ""
-  INSTALL_COMMAND   ""
+  CMAKE_ARGS        -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTING=OFF -DBUILD_ONLY=core -DSIMPLE_INSTALL=ON -DCMAKE_INSTALL_PREFIX="${CMAKE_CURRENT_BINARY_DIR}/pleasegohere/"
   TEST_COMMAND      ""
   BUILD_BYPRODUCTS  "${CMAKE_CURRENT_BINARY_DIR}/libaws-s3-sdk.a"
 )
