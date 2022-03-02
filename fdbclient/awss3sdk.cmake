317bcb4faf2eee4e4ae1c9c580f810807fc881fc
Josh Slocum <josh.slocum@snowflake.com>
2022-03-02 08:27:02 -0600

    unjanked install

--- fdbclient/awss3sdk.cmake
+++ fdbclient/awss3sdk.cmake
@@ -5,7 +5,7 @@ ExternalProject_Add(awss3sdk
   GIT_REPOSITORY    https://github.com/aws/aws-sdk-cpp.git
   GIT_TAG           1.9.200
   GIT_CONFIG        advice.detachedHead=false
-  CMAKE_ARGS        -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTING=OFF -DBUILD_ONLY=core -DSIMPLE_INSTALL=ON -DCMAKE_INSTALL_PREFIX="${CMAKE_CURRENT_BINARY_DIR}/pleasegohere/"
+  CMAKE_ARGS        -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTING=OFF -DBUILD_ONLY=core -DCMAKE_INSTALL_PREFIX=pleasegohere
   TEST_COMMAND      ""
   BUILD_BYPRODUCTS  "${CMAKE_CURRENT_BINARY_DIR}/libaws-s3-sdk.a"
 )
