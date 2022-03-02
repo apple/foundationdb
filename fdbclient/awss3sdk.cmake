da58b8e761df1ecb6670eee2a94749fc06530b30
Josh Slocum <josh.slocum@snowflake.com>
2022-03-01 20:10:48 -0600

    just messing around

--- fdbclient/awss3sdk.cmake
+++ fdbclient/awss3sdk.cmake
@@ -4,14 +4,12 @@ include(ExternalProject)
 ExternalProject_Add(awss3sdk
   GIT_REPOSITORY    https://github.com/aws/aws-sdk-cpp.git
   GIT_TAG           502b3999084911185bc52c6ab1f6687f0ea9a7d5 # v1.9.205
-  SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/awss3sdk-src"
   GIT_CONFIG        advice.detachedHead=false
+  SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/awss3sdk-src"
   BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/awss3sdk-build"
-  CMAKE_ARGS        "-DBUILD_ONLY=s3 -DCMAKE_BUILD_TYPE=RelWithDebInfo"
-  CONFIGURE_COMMAND ""
+ CMAKE_ARGS        -DBUILD_ONLY=s3 -DBUILD_SHARED_LIBS=OFF -DENABLE_TESTING=OFF
   BUILD_COMMAND     ""
   INSTALL_COMMAND   ""
-  UPDATE_COMMAND    git submodule update --init --recursive aws-cpp-sdk-s3
   TEST_COMMAND      ""
   BUILD_BYPRODUCTS  "${CMAKE_CURRENT_BINARY_DIR}/libaws-s3-sdk.a"
 )
