e61c26758c637498630b72f0cd35ab0229c372c7
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2022-03-02 17:16:57 -0800

    Move TransactionTagCounter implementation into .cpp file

--- fdbserver/TransactionTagCounter.h
+++ fdbserver/TransactionTagCounter.h
@@ -20,6 +20,9 @@
 
 #pragma once
 
+#include "fdbclient/TagThrottle.actor.h"
+#include "fdbserver/Knobs.h"
+
 struct TransactionTagCounter {
 	struct TagInfo {
 		TransactionTag tag;
@@ -42,52 +45,13 @@ struct TransactionTagCounter {
 
 	Reference<EventCacheHolder> busiestReadTagEventHolder;
 
-	TransactionTagCounter(UID thisServerID)
-	  : thisServerID(thisServerID),
-	    busiestReadTagEventHolder(makeReference<EventCacheHolder>(thisServerID.toString() + "/BusiestReadTag")) {}
+	TransactionTagCounter(UID thisServerID);
 
-	int64_t costFunction(int64_t bytes) const { return bytes / SERVER_KNOBS->READ_COST_BYTE_FACTOR + 1; }
+	static int64_t costFunction(int64_t bytes) { return bytes / SERVER_KNOBS->READ_COST_BYTE_FACTOR + 1; }
 
-	void addRequest(Optional<TagSet> const& tags, int64_t bytes) {
-		if (tags.present()) {
-			TEST(true); // Tracking tag on storage server
-			double cost = costFunction(bytes);
-			for (auto& tag : tags.get()) {
-				int64_t& count = intervalCounts[TransactionTag(tag, tags.get().getArena())];
-				count += cost;
-				if (count > busiestTagCount) {
-					busiestTagCount = count;
-					busiestTag = tag;
-				}
-			}
+	void addRequest(Optional<TagSet> const& tags, int64_t bytes);
 
-			intervalTotalSampledCount += cost;
-		}
-	}
-
-	void startNewInterval() {
-		double elapsed = now() - intervalStart;
-		previousBusiestTag.reset();
-		if (intervalStart > 0 && CLIENT_KNOBS->READ_TAG_SAMPLE_RATE > 0 && elapsed > 0) {
-			double rate = busiestTagCount / CLIENT_KNOBS->READ_TAG_SAMPLE_RATE / elapsed;
-			if (rate > SERVER_KNOBS->MIN_TAG_READ_PAGES_RATE) {
-				previousBusiestTag = TagInfo(busiestTag, rate, (double)busiestTagCount / intervalTotalSampledCount);
-			}
-
-			TraceEvent("BusiestReadTag", thisServerID)
-			    .detail("Elapsed", elapsed)
-			    .detail("Tag", printable(busiestTag))
-			    .detail("TagCost", busiestTagCount)
-			    .detail("TotalSampledCost", intervalTotalSampledCount)
-			    .detail("Reported", previousBusiestTag.present())
-			    .trackLatest(busiestReadTagEventHolder->trackingKey);
-		}
-
-		intervalCounts.clear();
-		intervalTotalSampledCount = 0;
-		busiestTagCount = 0;
-		intervalStart = now();
-	}
+	void startNewInterval();
 
 	Optional<TagInfo> getBusiestTag() const { return previousBusiestTag; }
 };
