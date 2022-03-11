78d3d2d2e904979d74343a829fdfb152e07864ff
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2022-03-10 20:13:51 -0400

    Fix file names in copyright headers

--- fdbserver/ProxyCommitData.actor.h
+++ fdbserver/ProxyCommitData.actor.h
@@ -1,5 +1,5 @@
 /*
- * ProxyCommitData.h
+ * ProxyCommitData.actor.h
  *
  * This source file is part of the FoundationDB open source project
  *
--- fdbserver/RestoreController.actor.h
+++ fdbserver/RestoreController.actor.h
@@ -1,5 +1,5 @@
 /*
- * RestoreController.h
+ * RestoreController.actor.h
  *
  * This source file is part of the FoundationDB open source project
  *
