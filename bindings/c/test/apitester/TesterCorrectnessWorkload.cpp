344198793d7e2bdc16a5abb96ccb7237f82d6c28
Jon Fu <jon.fu@snowflake.com>
2022-03-23 16:50:55 -0400

    remove TODO comment

--- bindings/c/test/apitester/TesterCorrectnessWorkload.cpp
+++ bindings/c/test/apitester/TesterCorrectnessWorkload.cpp
@@ -83,7 +83,6 @@ private:
 			    auto results = std::make_shared<std::vector<std::optional<std::string>>>();
 			    execTransaction(
 			        [kvPairs, results](auto ctx) {
-				        // TODO: Enable after merging with GRV caching
 				        ctx->tx()->setOption(FDB_TR_OPTION_USE_GRV_CACHE);
 				        auto futures = std::make_shared<std::vector<Future>>();
 				        for (const auto& kv : *kvPairs) {
