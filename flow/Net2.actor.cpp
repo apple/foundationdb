ebe928e7e1a16a81f227bbbeeba77fa0e72c3b66
Renxuan Wang <renxuan@apple.com>
2022-04-01 23:00:39 -0700

    Throw lookup_failed() when hostname resolving fails.

--- flow/Net2.actor.cpp
+++ flow/Net2.actor.cpp
@@ -1892,21 +1892,24 @@ Future<std::vector<NetworkAddress>> Net2::resolveTCPEndpoint(const std::string& 
 
 std::vector<NetworkAddress> Net2::resolveTCPEndpointBlocking(const std::string& host, const std::string& service) {
 	tcp::resolver tcpResolver(reactor.ios);
-	tcp::resolver::query query(host, service);
-	auto iter = tcpResolver.resolve(query);
-	decltype(iter) end;
-	std::vector<NetworkAddress> addrs;
-	while (iter != end) {
-		auto endpoint = iter->endpoint();
-		auto addr = endpoint.address();
-		if (addr.is_v6()) {
-			addrs.emplace_back(IPAddress(addr.to_v6().to_bytes()), endpoint.port());
-		} else {
-			addrs.emplace_back(addr.to_v4().to_ulong(), endpoint.port());
+	try {
+		auto iter = tcpResolver.resolve(host, service);
+		decltype(iter) end;
+		std::vector<NetworkAddress> addrs;
+		while (iter != end) {
+			auto endpoint = iter->endpoint();
+			auto addr = endpoint.address();
+			if (addr.is_v6()) {
+				addrs.emplace_back(IPAddress(addr.to_v6().to_bytes()), endpoint.port());
+			} else {
+				addrs.emplace_back(addr.to_v4().to_ulong(), endpoint.port());
+			}
+			++iter;
 		}
-		++iter;
+		return addrs;
+	} catch (...) {
+		throw lookup_failed();
 	}
-	return addrs;
 }
 
 bool Net2::isAddressOnThisHost(NetworkAddress const& addr) const {
