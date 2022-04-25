3f130c38a6e49d57c2d803ac46f77945156a6172
Xiaoxi Wang <xiaoxi.wang@snowflake.com>
2022-04-24 22:53:06 -0700

    add store type to status json

--- fdbserver/Status.actor.cpp
+++ fdbserver/Status.actor.cpp
@@ -584,13 +584,12 @@ struct RolesInfo {
 				}
 			}
 
-			if (!iface.isTss()) { // only storage server has Metadata field
-				TraceEventFields const& metadata = metrics.at("Metadata");
-				JsonBuilderObject metadataObj;
-				metadataObj["created_time_datetime"] = metadata.getValue("CreatedTimeDatetime");
-				metadataObj["created_time_timestamp"] = metadata.getDouble("CreatedTimeTimestamp");
-				obj["storage_metadata"] = metadataObj;
-			}
+			TraceEventFields const& metadata = metrics.at("Metadata");
+			JsonBuilderObject metadataObj;
+			metadataObj["created_time_datetime"] = metadata.getValue("CreatedTimeDatetime");
+			metadataObj["created_time_timestamp"] = metadata.getUint64("CreatedTimeTimestamp");
+			metadataObj["storage_engine"] = metadata.getValue("StoreType");
+			obj["storage_metadata"] = metadataObj;
 
 		} catch (Error& e) {
 			if (e.code() != error_code_attribute_not_found)
@@ -1940,11 +1939,13 @@ ACTOR static Future<std::vector<std::pair<StorageServerInterface, EventMap>>> ge
 			TraceEventFields metadataField;
 			metadataField.addField("CreatedTimeTimestamp", std::to_string(metadata[i].get().createdTime));
 			metadataField.addField("CreatedTimeDatetime", epochsToGMTString(metadata[i].get().createdTime));
+			metadataField.addField("StoreType", metadata[i].get().storeType.toString());
 			results[i].second.emplace("Metadata", metadataField);
 		} else if (!servers[i].isTss()) {
 			TraceEventFields metadataField;
 			metadataField.addField("CreatedTimeTimestamp", "0");
 			metadataField.addField("CreatedTimeDatetime", "[removed]");
+			metadataField.addField("StoreType", "[unknown]");
 			results[i].second.emplace("Metadata", metadataField);
 		}
 	}
