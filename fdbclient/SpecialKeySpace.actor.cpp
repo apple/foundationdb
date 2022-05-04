dc446c5259107b46f89a34aae735703f2a17c92b
Vishesh Yadav <vishesh_yadav@apple.com>
2022-05-03 17:14:38 -0700

    Create GlobalConfig object for each database instance
    Currently, GlobalConfig is a singleton that means for each process there is only
    one GlobalConfig object. This is bug from clients perspective as a client can
    keep connections to several databases. This patch tracks GlobalConfig for each
    database using an unordered_map in flowGlobals.
    
    We discovered this bug while testing multi-version client, where the client got
    stuck. This was lucky, as normally it'd just write down config to the wrong
    database.

--- fdbclient/SpecialKeySpace.actor.cpp
+++ fdbclient/SpecialKeySpace.actor.cpp
@@ -1454,7 +1454,7 @@ Future<Optional<std::string>> ConsistencyCheckImpl::commit(ReadYourWritesTransac
 	return Optional<std::string>();
 }
 
-GlobalConfigImpl::GlobalConfigImpl(KeyRangeRef kr) : SpecialKeyRangeRWImpl(kr) {}
+GlobalConfigImpl::GlobalConfigImpl(UID dbId, KeyRangeRef kr) : dbId(dbId), SpecialKeyRangeRWImpl(kr) {}
 
 // Returns key-value pairs for each value stored in the global configuration
 // framework within the range specified. The special-key-space getrange
@@ -1465,7 +1465,7 @@ Future<RangeResult> GlobalConfigImpl::getRange(ReadYourWritesTransaction* ryw,
                                                GetRangeLimits limitsHint) const {
 	RangeResult result;
 
-	auto& globalConfig = GlobalConfig::globalConfig();
+	auto& globalConfig = GlobalConfig::globalConfig(dbId);
 	KeyRangeRef modified =
 	    KeyRangeRef(kr.begin.removePrefix(getKeyRange().begin), kr.end.removePrefix(getKeyRange().begin));
 	std::map<KeyRef, Reference<ConfigValue>> values = globalConfig.get(modified);
