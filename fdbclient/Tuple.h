437d7b1716ce7009d0c4df6fa2777c65f00c7653
hao fu <hfu5@apple.com>
2022-05-06 13:12:00 -0700

    Fix comments

--- fdbclient/Tuple.h
+++ fdbclient/Tuple.h
@@ -36,6 +36,8 @@ struct Tuple {
 	static Tuple unpack(StringRef const& str, bool exclude_incomplete = false);
 
 	Tuple& append(Tuple const& tuple);
+
+	// the str needs to be a Tuple encoded string.
 	Tuple& appendRaw(StringRef const& str);
 	Tuple& append(StringRef const& str, bool utf8 = false);
 	Tuple& append(int64_t);
@@ -62,6 +64,7 @@ struct Tuple {
 		data.clear();
 		offsets.clear();
 	}
+	// Return a Tuple encoded raw string.
 	StringRef subTupleRawString(size_t index) const;
 	ElementType getType(size_t index) const;
 	Standalone<StringRef> getString(size_t index) const;
