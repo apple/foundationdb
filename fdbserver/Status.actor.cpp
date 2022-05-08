9f56d6ec38a8edee6bd4c63ef0c8bc3543092ed2
Xiaoxi Wang <xiaoxi.wang@snowflake.com>
2022-05-07 22:25:16 -0700

    store future

--- fdbserver/Status.actor.cpp
+++ fdbserver/Status.actor.cpp
@@ -1947,12 +1947,7 @@ static Future<std::vector<StorageServerStatusInfo>> readStorageInterfaceAndMetad
 			state std::vector<Future<Void>> futures(servers.size());
 			for (int i = 0; i < servers.size(); ++i) {
 				auto& info = servers[i];
-				futures[i] = fmap(
-				    [&info](Optional<StorageMetadataType> meta) -> Void {
-					    info.metadata = meta;
-					    return Void();
-				    },
-				    metadataMap.get(tr, servers[i].id()));
+				futures[i] = store(servers[i].metadata, metadataMap.get(tr, servers[i].id()));
 				// TraceEvent(SevDebug, "MetadataAppear", servers[i].id()).detail("Present", metadata.present());
 			}
 			wait(waitForAll(futures));
