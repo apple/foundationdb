9e9b9914e518990ae34e3ca220d7ff875cdb1089
Jingyu Zhou <jingyuzhou@gmail.com>
2022-05-20 17:34:58 -0700

    Revert "Enable MATCHED and UNMATCHED mode for index prefetch (#7162)"
    This reverts commit 20cab33954f912a5e141e9a8476c4d06d08a3986.

--- fdbclient/MultiVersionTransaction.h
+++ fdbclient/MultiVersionTransaction.h
@@ -80,7 +80,6 @@ struct FdbCApi : public ThreadSafeReferenceCounted<FdbCApi> {
 		 * and take the shortcut. */
 		FDBGetRangeReqAndResult getRange;
 		unsigned char buffer[32];
-		bool boundaryAndExist;
 	} FDBMappedKeyValue;
 
 #pragma pack(push, 4)
