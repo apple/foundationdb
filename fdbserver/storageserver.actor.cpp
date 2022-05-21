042616aaf006f36cd0da8f61a4ce9ab63737b440
Steve Atherton <steve.atherton@snowflake.com>
2022-05-20 17:26:52 -0700

    Explicitly cancel byte sample recovery on failure of a recovered storage server to prevent it from trying to read from the IKeyValueStore after shutdown.

--- fdbserver/storageserver.actor.cpp
+++ fdbserver/storageserver.actor.cpp
@@ -9231,6 +9231,10 @@ ACTOR Future<Void> storageServer(IKeyValueStore* persistentData,
 
 		throw internal_error();
 	} catch (Error& e) {
+		if (self.byteSampleRecovery.isValid()) {
+			self.byteSampleRecovery.cancel();
+		}
+
 		if (recovered.canBeSet())
 			recovered.send(Void());
 
