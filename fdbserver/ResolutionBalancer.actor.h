77d20006725c91d0ca33cec5675e4ddb7d3d735e
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2022-06-07 10:51:04 -0700

    Add missing actorcompiler.h includes to actor header files

--- fdbserver/ResolutionBalancer.actor.h
+++ fdbserver/ResolutionBalancer.actor.h
@@ -33,6 +33,7 @@
 #include "flow/Arena.h"
 #include "flow/IRandom.h"
 #include "flow/genericactors.actor.h"
+#include "flow/actorcompiler.h" // must be last include
 
 struct ResolutionBalancer {
 	AsyncVar<Standalone<VectorRef<ResolverMoveRef>>> resolverChanges;
