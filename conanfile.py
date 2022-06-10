3c7b40eefa5f6084ad6cf345583d4e4bf32e4cfa
Markus Pilman <markus.pilman@snowflake.com>
2022-06-10 10:40:50 -0600

    Conan working on macOS

--- conanfile.py
+++ conanfile.py
@@ -7,26 +7,26 @@ class FoundationDB(ConanFile):
     requires = "boost/1.78.0", "openssl/1.1.1n", "toml11/3.7.1", "jemalloc/5.2.1", "aws-sdk-cpp/1.9.234", "benchmark/1.6.1", "msgpack/3.3.0", "fmt/8.1.1"
     generators = "cmake_find_package", "cmake_paths"
     options = {
-        "java": [True, False],
         "liburing": [True, False]
     }
     default_options = {
-        "java": True,
         "liburing": False,
         "jemalloc:enable_cxx": False,
         "jemalloc:shared": False,
-        "aws-sdk-cpp:s3": True
+        "aws-sdk-cpp:s3": True,
+        "aws-sdk-cpp:access-management": False,
+        "aws-sdk-cpp:identity-management": False,
+        "aws-sdk-cpp:monitoring": False,
+        "aws-sdk-cpp:queues": False,
+        "aws-sdk-cpp:text-to-speech": False,
+        "aws-sdk-cpp:transfer": False,
     }
 
     def configure(self):
-        if self.settings.os == 'Macos' and self.settings.arch == 'armv8':
-            self.options.java = False
         if self.settings.os == 'Linux':
             self.options.liburing = True
 
     def requirements(self):
-        if self.options.java:
-            self.requires("openjdk/16.0.1")
         if self.options.liburing:
             self.requires("liburing/2.1")
 
