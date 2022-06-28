7040cb968841ddcc034efe5e76241ef935578d94
Markus Pilman <markus.pilman@snowflake.com>
2022-06-27 19:00:40 -0600

    fixed link errors

--- fdbclient/libb64/cencode.c
+++ fdbclient/libb64/cencode.c
@@ -5,7 +5,7 @@ This is part of the libb64 project, and has been placed in the public domain.
 For details, see http://sourceforge.net/projects/libb64
 */
 
-#include "cencode.h"
+#include "fdbclient/libb64/cencode.h"
 
 const int CHARS_PER_LINE = 72;
 
