7d03052d50244d70beadb7fe999503530fd7d158
Marian Dvorsky <marian.dvorsky@snowflake.com>
2022-07-04 22:13:05 +0200

    Handle errors

--- bindings/c/test/apitester/TesterWatchAndWaitWorkload.cpp
+++ bindings/c/test/apitester/TesterWatchAndWaitWorkload.cpp
@@ -31,45 +31,75 @@ public:
 
 private:
 	void randomOperation(TTaskFct cont) override {
-		// The test will set key to initialVal, then set up a watch on that key, change the key to newVal and verify
-		// that watch was called.
+		// This test sets a key to an initial value, sets up a watch and waits for it to be triggered.
 		Key key(randomKeyName());
 		Value initialVal = randomValue();
-		Value newVal = randomValue();
-		// Ensure that newVal is different from initialVal, otherwise watch may not trigger.
-		while (initialVal == newVal) {
-			newVal = randomValue();
-		}
 		execTransaction(
 		    // 1. Set the key to initialVal.
 		    [key, initialVal](auto ctx) {
 			    ctx->tx().set(key, initialVal);
 			    ctx->commit();
 		    },
-		    [this, key, newVal, cont]() {
-			    auto watch_f = std::make_shared<fdb::TypedFuture<fdb::future_var::None>>();
+		    [this, key, initialVal, cont]() { watch(key, initialVal, cont); });
+	}
 
+	void watch(Key key, Value initialVal, TTaskFct cont) {
+		// Assuming key starts with a value initialVal, set up a watch for that key, change its value to a different
+		// value newVal, and ensure that the watch was triggered. Upon retryable watch failures retries the whole
+		// process again.
+		Value newVal = randomValue();
+		// Ensure that newVal is different from initialVal, otherwise watch may not trigger.
+		while (initialVal == newVal) {
+			newVal = randomValue();
+		}
+
+		auto watch_f = std::make_shared<fdb::TypedFuture<fdb::future_var::None>>();
+		execTransaction(
+		    // 2. Create a watch for key.
+		    [key, watch_f](auto ctx) {
+			    *watch_f = ctx->tx().watch(key);
+			    ctx->commit();
+		    },
+		    [this, key, newVal, watch_f, cont]() {
 			    execTransaction(
-			        // 2. Create a watch for key.
-			        [key, watch_f](auto ctx) {
-				        *watch_f = ctx->tx().watch(key);
+			        // 3. Set the key to a newVal which is guaranteed to be different from initialVal, i.e.,
+			        // must trigger the watch.
+			        [key, newVal](auto ctx) {
+				        ctx->tx().set(key, newVal);
 				        ctx->commit();
 			        },
 			        [this, key, newVal, watch_f, cont]() {
+				        auto retry = std::make_shared<bool>(false);
 				        execTransaction(
-				            // 3. Set the key to a newVal which is guaranteed to be different from initialVal, i.e.,
-				            // must trigger the watch.
-				            [key, newVal](auto ctx) {
-					            ctx->tx().set(key, newVal);
-					            ctx->commit();
-				            },
-				            [this, watch_f, cont]() {
-					            execTransaction(
-					                [watch_f](auto ctx) {
-						                // 4. Wait for the watch future to become ready.
-						                ctx->continueAfter(*watch_f, [ctx] { ctx->done(); });
+				            [this, key, watch_f, retry](auto ctx) {
+					            // 4. Wait for the watch future to become ready.
+					            // Note that we do this in the context of a dummy transaction. Ideally we would do this
+					            // in the context of the transaction which sets up the watch in the first place, but
+					            ctx->continueAfter(
+					                *watch_f,
+					                [this, ctx, watch_f, retry] {
+						                // If the watch future finished with a failure, see if it can be retried.
+						                if (watch_f->error()) {
+							                auto onErrorFuture = ctx->tx().onError(watch_f->error());
+							                fdb::Error blockError = onErrorFuture.blockUntilReady();
+							                if (blockError) {
+								                error(fmt::format("Block error: {}", blockError.what()));
+							                } else if (onErrorFuture.error()) {
+								                error(fmt::format("Watch error: {}", onErrorFuture.error().what()));
+							                } else {
+								                *retry = true;
+							                }
+						                }
+						                ctx->done();
 					                },
-					                [this, cont]() { schedule(cont); });
+					                false);
+				            },
+				            [this, retry, key, newVal, cont]() {
+					            if (*retry) {
+						            watch(key, newVal, cont);
+					            } else {
+						            schedule(cont);
+					            }
 				            });
 			        });
 		    });
