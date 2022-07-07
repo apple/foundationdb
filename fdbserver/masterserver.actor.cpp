b599f00ee80ca3c001e7bceecaabd38d00fe59ba
Sreenath Bodagala <82616783+sbodagala@users.noreply.github.com>
2022-07-07 14:00:10 +0530

    Merge pull request #7529 from sbodagala/main
    Make sequencer aware of the locality of the primary region

--- fdbserver/masterserver.actor.cpp
+++ fdbserver/masterserver.actor.cpp
@@ -118,9 +118,7 @@ struct MasterData : NonCopyable, ReferenceCounted<MasterData> {
 			forceRecovery = false;
 		}
 		balancer = resolutionBalancer.resolutionBalancing();
-		locality = (SERVER_KNOBS->ENABLE_VERSION_VECTOR_HA_OPTIMIZATION && myInterface.locality.dcId().present())
-		               ? std::stoi(myInterface.locality.dcId().get().toString())
-		               : tagLocalityInvalid;
+		locality = tagLocalityInvalid;
 	}
 	~MasterData() = default;
 };
@@ -314,7 +312,8 @@ ACTOR Future<Void> updateRecoveryData(Reference<MasterData> self) {
 		    .detail("CurrentRecoveryTxnVersion", self->recoveryTransactionVersion)
 		    .detail("CurrentLastEpochEnd", self->lastEpochEnd)
 		    .detail("NumCommitProxies", req.commitProxies.size())
-		    .detail("VersionEpoch", req.versionEpoch);
+		    .detail("VersionEpoch", req.versionEpoch)
+		    .detail("PrimaryLocality", req.primaryLocality);
 
 		self->recoveryTransactionVersion = req.recoveryTransactionVersion;
 		self->lastEpochEnd = req.lastEpochEnd;
@@ -339,6 +338,8 @@ ACTOR Future<Void> updateRecoveryData(Reference<MasterData> self) {
 
 		self->resolutionBalancer.setCommitProxies(req.commitProxies);
 		self->resolutionBalancer.setResolvers(req.resolvers);
+
+		self->locality = req.primaryLocality;
 
 		req.reply.send(Void());
 	}
