7d101b23fea37557cadcadbf4295b684038383c7
Renxuan Wang <renxuan@apple.com>
2022-07-08 16:02:17 -0700

    Get network interfaces ready for https proxy.

--- fdbrpc/sim2.actor.cpp
+++ fdbrpc/sim2.actor.cpp
@@ -315,6 +315,8 @@ struct Sim2Conn final : IConnection, ReferenceCounted<Sim2Conn> {
 	NetworkAddress getPeerAddress() const override { return peerEndpoint; }
 	UID getDebugID() const override { return dbgid; }
 
+	boost::asio::ip::tcp::socket& getSocket() override { throw operation_failed(); }
+
 	bool opened, closedByCaller, stableConnection;
 
 private:
@@ -939,8 +941,7 @@ public:
 	TaskPriority getCurrentTask() const override { return currentTaskID; }
 	void setCurrentTask(TaskPriority taskID) override { currentTaskID = taskID; }
 	// Sets the taskID/priority of the current task, without yielding
-	Future<Reference<IConnection>> connect(NetworkAddress toAddr, const std::string& host) override {
-		ASSERT(host.empty());
+	Future<Reference<IConnection>> connect(NetworkAddress toAddr) override {
 		if (!addressMap.count(toAddr)) {
 			return waitForProcessAndConnect(toAddr, this);
 		}
@@ -966,7 +967,7 @@ public:
 		return onConnect(::delay(0.5 * deterministicRandom()->random01()), myc);
 	}
 
-	Future<Reference<IConnection>> connectExternal(NetworkAddress toAddr, const std::string& host) override {
+	Future<Reference<IConnection>> connectExternal(NetworkAddress toAddr) override {
 		return SimExternalConnection::connect(toAddr);
 	}
 
