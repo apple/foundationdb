b9437e9a8d148ecfcd45bb226494d2bfb45eae43
Josh Slocum <josh.slocum@snowflake.com>
2022-07-13 10:38:39 -0500

    BW re-sending merge candidate if manager changes

--- fdbserver/BlobWorker.actor.cpp
+++ fdbserver/BlobWorker.actor.cpp
@@ -1086,7 +1086,13 @@ ACTOR Future<Void> granuleCheckMergeCandidate(Reference<BlobWorkerData> bwData,
 				                                                                 true,
 				                                                                 metadata->originalEpoch,
 				                                                                 metadata->originalSeqno));
-				return Void();
+				// if a new manager appears, also tell it about this granule being mergeable
+				state int64_t lastSendEpoch = bwData->currentManagerEpoch;
+				while (lastSendEpoch == bwData->currentManagerEpoch) {
+					wait(bwData->currentManagerStatusStream.onChange());
+					wait(delay(0));
+				}
+				TEST(true); // Blob worker re-sending merge candidate to new manager
 			} catch (Error& e) {
 				if (e.code() == error_code_operation_cancelled) {
 					throw e;
