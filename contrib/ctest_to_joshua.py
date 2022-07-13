836cf0147769f94b470ba52ce1a89a152b4ba09f
Marian Dvorsky <marian.dvorsky@snowflake.com>
2022-07-13 20:55:19 +0200

    Package dirs in ctest_to_joshua

--- contrib/ctest_to_joshua.py
+++ contrib/ctest_to_joshua.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from argparse import ArgumentParser
 import glob
 import io
@@ -31,11 +33,15 @@ class JoshuaBuilder:
         if os.path.exists(arg):
             if not os.path.relpath(arg, self.build_dir).startswith(".."):
                 relpath = "build/" + os.path.relpath(arg, self.build_dir)
-                self.files[arg] = relpath
+                # Avoid packaging the full build directory.
+                if relpath != "build/.":
+                    self.files[arg] = relpath
                 return relpath
             elif not os.path.relpath(arg, self.src_dir).startswith(".."):
                 relpath = "src/" + os.path.relpath(arg, self.src_dir)
-                self.files[arg] = relpath
+                # Avoid packaging the full source directory.
+                if relpath != "src/.":
+                    self.files[arg] = relpath
                 return relpath
             elif os.access(arg, os.X_OK):
                 # Hope it's on the path
@@ -61,8 +67,7 @@ class JoshuaBuilder:
     def write_tarball(self, output, joshua_test):
         with tarfile.open(output, "w:gz") as tar:
             for file, arcfile in self.files.items():
-                if not os.path.isdir(file):
-                    self._add_file(tar, file, arcfile)
+                self._add_file(tar, file, arcfile)
             tarinfo = tarfile.TarInfo("joshua_test")
             tarinfo.mode = 0o755
             joshua_bytes = joshua_test.encode("utf-8")
