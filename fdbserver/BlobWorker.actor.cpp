1da88d5d8c3b2a8d80e40febaaf54fd76051ff86
Ata E Husain Bohra <ata.husain@snowflake.com>
2022-07-14 18:16:45 -0700

    BlobFile Encryption and compression support
    Fix formatting issues
    
    Description
    
    Testing

--- fdbserver/BlobWorker.actor.cpp
+++ fdbserver/BlobWorker.actor.cpp
@@ -1104,13 +1104,9 @@ ACTOR Future<BlobFileIndex> checkSplitAndReSnapshot(Reference<BlobWorkerData> bw
 				// wait for manager stream to become ready, and send a message
 				loop {
 					choose {
-						when(wait(bwData->currentManagerStatusStream.get().onReady())) {
-							break;
-						}
+						when(wait(bwData->currentManagerStatusStream.get().onReady())) { break; }
 						when(wait(bwData->currentManagerStatusStream.onChange())) {}
-						when(wait(metadata->resumeSnapshot.getFuture())) {
-							break;
-						}
+						when(wait(metadata->resumeSnapshot.getFuture())) { break; }
 					}
 				}
 				if (metadata->resumeSnapshot.isSet()) {
@@ -1149,9 +1145,7 @@ ACTOR Future<BlobFileIndex> checkSplitAndReSnapshot(Reference<BlobWorkerData> bw
 		// manager change/no response
 		choose {
 			when(wait(bwData->currentManagerStatusStream.onChange())) {}
-			when(wait(metadata->resumeSnapshot.getFuture())) {
-				break;
-			}
+			when(wait(metadata->resumeSnapshot.getFuture())) { break; }
 			when(wait(delay(1.0))) {}
 		}
 
@@ -1225,12 +1219,8 @@ ACTOR Future<Void> granuleCheckMergeCandidate(Reference<BlobWorkerData> bwData,
 				// wait for manager stream to become ready, and send a message
 				loop {
 					choose {
-						when(wait(delay(std::max(0.0, sendTimeGiveUp - now())))) {
-							break;
-						}
-						when(wait(bwData->currentManagerStatusStream.get().onReady())) {
-							break;
-						}
+						when(wait(delay(std::max(0.0, sendTimeGiveUp - now())))) { break; }
+						when(wait(bwData->currentManagerStatusStream.get().onReady())) { break; }
 						when(wait(bwData->currentManagerStatusStream.onChange())) {}
 					}
 				}
@@ -1514,9 +1504,7 @@ ACTOR Future<Void> waitOnCFVersion(Reference<GranuleMetadata> metadata, Version 
 			                                 ? metadata->activeCFData.get()->whenAtLeast(waitVersion)
 			                                 : Never();
 			choose {
-				when(wait(atLeast)) {
-					break;
-				}
+				when(wait(atLeast)) { break; }
 				when(wait(metadata->activeCFData.onChange())) {}
 			}
 		} catch (Error& e) {
@@ -3045,9 +3033,7 @@ ACTOR Future<Void> doBlobGranuleFileRequest(Reference<BlobWorkerData> bwData, Bl
 
 			choose {
 				when(wait(metadata->readable.getFuture())) {}
-				when(wait(metadata->cancelled.getFuture())) {
-					throw wrong_shard_server();
-				}
+				when(wait(metadata->cancelled.getFuture())) { throw wrong_shard_server(); }
 			}
 
 			// in case both readable and cancelled are ready, check cancelled
@@ -3063,9 +3049,7 @@ ACTOR Future<Void> doBlobGranuleFileRequest(Reference<BlobWorkerData> bwData, Bl
 				if (metadata->historyLoaded.canBeSet()) {
 					choose {
 						when(wait(metadata->historyLoaded.getFuture())) {}
-						when(wait(metadata->cancelled.getFuture())) {
-							throw wrong_shard_server();
-						}
+						when(wait(metadata->cancelled.getFuture())) { throw wrong_shard_server(); }
 					}
 				}
 
@@ -3077,9 +3061,7 @@ ACTOR Future<Void> doBlobGranuleFileRequest(Reference<BlobWorkerData> bwData, Bl
 						when(GranuleFiles f = wait(finalChunks[chunkIdx].second)) {
 							rangeGranulePair.push_back(std::pair(finalChunks[chunkIdx].first, f));
 						}
-						when(wait(metadata->cancelled.getFuture())) {
-							throw wrong_shard_server();
-						}
+						when(wait(metadata->cancelled.getFuture())) { throw wrong_shard_server(); }
 					}
 
 					if (rangeGranulePair.back().second.snapshotFiles.empty()) {
@@ -3116,13 +3098,9 @@ ACTOR Future<Void> doBlobGranuleFileRequest(Reference<BlobWorkerData> bwData, Bl
 					// version on rollback
 					try {
 						choose {
-							when(wait(waitForVersionFuture)) {
-								break;
-							}
+							when(wait(waitForVersionFuture)) { break; }
 							when(wait(metadata->activeCFData.onChange())) {}
-							when(wait(metadata->cancelled.getFuture())) {
-								throw wrong_shard_server();
-							}
+							when(wait(metadata->cancelled.getFuture())) { throw wrong_shard_server(); }
 						}
 					} catch (Error& e) {
 						// We can get change feed cancelled from whenAtLeast. This means the change feed may
