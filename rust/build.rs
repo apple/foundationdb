24677fb7de19cef7a55221524466019d777c4100
Russell Sears <russell_sears@apple.com>
2022-07-20 16:31:08 +0000

    Bi-directional C++ <-> Rust works, with hacks

--- rust/build.rs
+++ rust/build.rs
@@ -2,6 +2,7 @@ fn main() {
     let _build = cxx_build::bridge("src/lib.rs")
         .flag_if_supported("-std=c++17")
         .flag_if_supported("-fPIC")
+        .flag_if_supported("-I/root/build_output/flow/include/")
         .compile("fdb_rust");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ffi::CStr;
+
 #[cxx::bridge]
 mod ffi {
     #[namespace = "shared"]
@@ -5,6 +7,11 @@ mod ffi {
         i: u32,
     }
 
+    unsafe extern "C++" {
+        include!("flow/SourceVersion.h");
+        //const char* getSourceVersion()
+        fn getSourceVersion() -> * const c_char;
+    }
     #[namespace = "fdb_rust"]
     extern "Rust" {
         fn new_foo() -> Foo;
@@ -15,6 +22,11 @@ mod ffi {
 pub fn new_foo() -> ffi::Foo {
     ffi::Foo { i: 42 }
 }
+
 pub fn print_foo(foo: &ffi::Foo) {
-    println!("Foo: {}", foo.i);
+    let ver_char_ptr = ffi::getSourceVersion();
+    let ver_c_str = unsafe { CStr::from_ptr(ver_char_ptr) };
+    let ver_str = ver_c_str.to_str().unwrap();
+    println!("FDB git SHA: {}", ver_str);
+    println!("Foo: {};", foo.i);
 }
