cfd86aa09313140942f6dbd507fc5700901e7ea0
Xiaoge Su <magichp@gmail.com>
2022-07-23 17:48:42 -0700

    fixup! Format

--- fdbserver/storageserver.actor.cpp
+++ fdbserver/storageserver.actor.cpp
@@ -1525,9 +1525,7 @@ ACTOR Future<Version> waitForVersionNoTooOld(StorageServer* data, Version versio
 	if (version <= data->version.get())
 		return version;
 	choose {
-		when(wait(data->version.whenAtLeast(version))) {
-			return version;
-		}
+		when(wait(data->version.whenAtLeast(version))) { return version; }
 		when(wait(delay(SERVER_KNOBS->FUTURE_VERSION_DELAY))) {
 			if (deterministicRandom()->random01() < 0.001)
 				TraceEvent(SevWarn, "ShardServerFutureVersion1000x", data->thisServerID)
