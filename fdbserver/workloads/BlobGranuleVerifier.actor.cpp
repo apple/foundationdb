c7f1123f532f64d884aff1bd053cc8045269aff1
Josh Slocum <josh.slocum@snowflake.com>
2022-07-26 09:57:02 -0500

    better check for granule-ification

--- fdbserver/workloads/BlobGranuleVerifier.actor.cpp
+++ fdbserver/workloads/BlobGranuleVerifier.actor.cpp
@@ -49,8 +49,6 @@
  */
 struct BlobGranuleVerifierWorkload : TestWorkload {
 	bool doSetup;
-	double minDelay;
-	double maxDelay;
 	double testDuration;
 	double timeTravelLimit;
 	uint64_t timeTravelBufferSize;
@@ -65,6 +63,7 @@ struct BlobGranuleVerifierWorkload : TestWorkload {
 	int64_t purges = 0;
 	std::vector<Future<Void>> clients;
 	bool enablePurging;
+	bool initAtEnd;
 
 	DatabaseConfiguration config;
 
@@ -73,14 +72,17 @@ struct BlobGranuleVerifierWorkload : TestWorkload {
 
 	BlobGranuleVerifierWorkload(WorkloadContext const& wcx) : TestWorkload(wcx) {
 		doSetup = !clientId; // only do this on the "first" client
-		// FIXME: don't do the delay in setup, as that delays the start of all workloads
-		minDelay = getOption(options, LiteralStringRef("minDelay"), 0.0);
-		maxDelay = getOption(options, LiteralStringRef("maxDelay"), 0.0);
 		testDuration = getOption(options, LiteralStringRef("testDuration"), 120.0);
 		timeTravelLimit = getOption(options, LiteralStringRef("timeTravelLimit"), testDuration);
 		timeTravelBufferSize = getOption(options, LiteralStringRef("timeTravelBufferSize"), 100000000);
 		threads = getOption(options, LiteralStringRef("threads"), 1);
 		enablePurging = getOption(options, LiteralStringRef("enablePurging"), false /*sharedRandomNumber % 2 == 0*/);
+		sharedRandomNumber /= 2;
+		// randomly some tests write data first and then turn on blob granules later, to test conversion of existing DB
+		// TODO CHANGE to infrequent!
+		initAtEnd = !enablePurging;
+		// initAtEnd = !enablePurging && sharedRandomNumber % 10 == 0;
+		sharedRandomNumber /= 10;
 		ASSERT(threads >= 1);
 
 		if (BGV_DEBUG) {
@@ -104,9 +106,8 @@ struct BlobGranuleVerifierWorkload : TestWorkload {
 
 	// FIXME: run the actual FDBCLI command instead of copy/pasting its implementation
 	// Sets the whole user keyspace to be blobified
-	ACTOR Future<Void> setUpBlobRange(Database cx, Future<Void> waitForStart) {
+	ACTOR Future<Void> setUpBlobRange(Database cx) {
 		state Reference<ReadYourWritesTransaction> tr = makeReference<ReadYourWritesTransaction>(cx);
-		wait(waitForStart);
 		loop {
 			try {
 				tr->setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
@@ -136,11 +137,9 @@ struct BlobGranuleVerifierWorkload : TestWorkload {
 
 		wait(success(ManagementAPI::changeConfig(cx.getReference(), "blob_granules_enabled=1", true)));
 
-		double initialDelay = deterministicRandom()->random01() * (self->maxDelay - self->minDelay) + self->minDelay;
-		if (BGV_DEBUG) {
-			printf("BGW setup initial delay of %.3f\n", initialDelay);
+		if (!self->initAtEnd) {
+			wait(self->setUpBlobRange(cx));
 		}
-		wait(self->setUpBlobRange(cx, delay(initialDelay)));
 		return Void();
 	}
 
@@ -368,6 +367,11 @@ struct BlobGranuleVerifierWorkload : TestWorkload {
 	ACTOR Future<bool> _check(Database cx, BlobGranuleVerifierWorkload* self) {
 		// check error counts, and do an availability check at the end
 
+		if (self->doSetup && self->initAtEnd) {
+			// FIXME: this doesn't check the data contents post-conversion, just that it finishes successfully
+			wait(self->setUpBlobRange(cx));
+		}
+
 		state Transaction tr(cx);
 		state Version readVersion = wait(self->doGrv(&tr));
 		state Version startReadVersion = readVersion;
@@ -384,8 +388,21 @@ struct BlobGranuleVerifierWorkload : TestWorkload {
 			state Future<Void> rangeFetcher = self->findGranules(cx, self);
 			loop {
 				wait(self->granuleRanges.onChange());
+				// wait until entire keyspace has granules
 				if (!self->granuleRanges.get().empty()) {
-					break;
+					bool haveAll = true;
+					if (self->granuleRanges.get().front().begin != normalKeys.begin ||
+					    self->granuleRanges.get().back().end != normalKeys.end) {
+						haveAll = false;
+					}
+					for (int i = 0; haveAll && i < self->granuleRanges.get().size() - 1; i++) {
+						if (self->granuleRanges.get()[i].end != self->granuleRanges.get()[i + 1].begin) {
+							haveAll = false;
+						}
+					}
+					if (haveAll) {
+						break;
+					}
 				}
 			}
 			rangeFetcher.cancel();
