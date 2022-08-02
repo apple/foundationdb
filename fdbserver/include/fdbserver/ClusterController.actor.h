c36320b696a1913c01c86d18a4a46cee52f7976d
Nim Wijetunga <nim.wijetunga@snowflake.com>
2022-08-01 21:59:15 -0700

    fix issues

--- fdbserver/include/fdbserver/ClusterController.actor.h
+++ fdbserver/include/fdbserver/ClusterController.actor.h
@@ -3312,6 +3312,7 @@ public:
 		serverInfo.masterLifetime.ccID = id;
 		serverInfo.clusterInterface = ccInterface;
 		serverInfo.myLocality = locality;
+		serverInfo.client.isEncryptionEnabled = SERVER_KNOBS->ENABLE_ENCRYPTION;
 		db.serverInfo->set(serverInfo);
 		cx = openDBOnServer(db.serverInfo, TaskPriority::DefaultEndpoint, LockAware::True);
 
