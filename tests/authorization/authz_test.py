041e030e929b87a2777723d264c783e99567e015
Junhyun Shim <junhyun.shim@snowflake.com>
2022-08-24 15:40:01 +0200

    Add testcases bad_token and system_and_special_key_range_disallowed

--- tests/authorization/authz_test.py
+++ tests/authorization/authz_test.py
@@ -23,11 +23,23 @@ import argparse
 import authlib
 import fdb
 import pytest
+import random
 import sys
 import time
 from multiprocessing import Process, Pipe
 from typing import Union
-from util import public_keyset_from_keys, random_alphanum_str, random_alphanum_bytes, to_str, to_bytes
+from util import alg_from_kty, public_keyset_from_keys, random_alphanum_str, random_alphanum_bytes, to_str, to_bytes
+
+special_key_ranges = [
+    ("transaction description", b"/description", b"/description\x00"),
+    ("global knobs", b"/globalKnobs", b"/globalKnobs\x00"),
+    ("knobs", b"/knobs0", b"/knobs0\x00"),
+    ("conflicting keys", b"/transaction/conflicting_keys/", b"/transaction/conflicting_keys/\xff\xff"),
+    ("read conflict range", b"/transaction/read_conflict_range/", b"/transaction/read_conflict_range/\xff\xff"),
+    ("conflicting keys", b"/transaction/write_conflict_range/", b"/transaction/write_conflict_range/\xff\xff"),
+    ("data distribution stats", b"/metrics/data_distribution_stats/", b"/metrics/data_distribution_stats/\xff\xff"),
+    ("kill storage", b"/globals/killStorage", b"/globals/killStorage\x00"),
+]
 
 def token_claim_1h(tenant_name):
     now = time.time()
@@ -67,9 +79,9 @@ def test_cross_tenant_access_disallowed(private_key, default_tenant, token_gen, 
     # test that read transaction fails
     try:
         value = tr_second[b"abc"].value
-        assert False, "expected permission denied, but read transaction went through, value: {}".format(value)
+        assert False, f"expected permission denied, but read transaction went through, value: {value}"
     except fdb.FDBError as e:
-        assert e.code == 6000, "expected permission_denied, got {} instead".format(e)
+        assert e.code == 6000, f"expected permission_denied, got {e} instead"
     # test that write transaction fails
     tr_second = tenant_tr_gen(second_tenant)
     tr_second.options.set_authorization_token(token_default)
@@ -78,7 +90,61 @@ def test_cross_tenant_access_disallowed(private_key, default_tenant, token_gen, 
         tr_second.commit().wait()
         assert False, "expected permission denied, but write transaction went through"
     except fdb.FDBError as e:
-        assert e.code == 6000, "expected permission_denied, got {} instead".format(e)
+        assert e.code == 6000, f"expected permission_denied, got {e} instead"
+
+def test_system_and_special_key_range_disallowed(db, tenant_tr_gen, token_gen):
+    second_tenant = random_alphanum_bytes(12)
+    try:
+        fdb.tenant_management.create_tenant(db, second_tenant)
+        assert False, "disallowed create_tenant has succeeded"
+    except fdb.FDBError as e:
+        assert e.code == 6000, f"expected permission_denied, got {e} instead"
+
+    try:
+        tr = db.create_transaction()
+        tr.options.set_access_system_keys()
+        kvs = tr.get_range(b"\xff", b"\xff\xff", limit=1).to_list()
+        assert False, f"disallowed system keyspace read has succeeded. found item: {kvs}"
+    except fdb.FDBError as e:
+        assert e.code == 6000, f"expected permission_denied, got {e} instead"
+
+    for range_name, special_range_begin, special_range_end in special_key_ranges:
+        tr = db.create_transaction()
+        tr.options.set_access_system_keys()
+        tr.options.set_special_key_space_relaxed()
+        try:
+            kvs = tr.get_range(special_range_begin, special_range_end, limit=1).to_list()
+            assert False, f"disallowed special keyspace read for range {range_name} has succeeded. found item {kvs}"
+        except fdb.FDBError as e:
+            assert e.code == 6000, f"expected permission_denied from attempted read to range {range_name}, got {e} instead"
+
+    try:
+        tr = db.create_transaction()
+        tr.options.set_access_system_keys()
+        del tr[b"\xff":b"\xff\xff"]
+        tr.commit().wait()
+        assert False, f"disallowed system keyspace write has succeeded"
+    except fdb.FDBError as e:
+        assert e.code == 6000, f"expected permission_denied, got {e} instead"
+
+    for range_name, special_range_begin, special_range_end in special_key_ranges:
+        tr = db.create_transaction()
+        tr.options.set_access_system_keys()
+        tr.options.set_special_key_space_relaxed()
+        try:
+            del tr[special_range_begin:special_range_end]
+            tr.commit().wait()
+            assert False, f"write to disallowed special keyspace range {range_name} has succeeded"
+        except fdb.FDBError as e:
+            assert e.code == 6000, f"expected permission_denied from attempted write to range {range_name}, got {e} instead"
+
+    try:
+        tr = db.create_transaction()
+        tr.options.set_access_system_keys()
+        kvs = tr.get_range(b"", b"\xff", limit=1).to_list()
+        assert False, f"disallowed normal keyspace read has succeeded. found item {kvs}"
+    except fdb.FDBError as e:
+        assert e.code == 6000, f"expected permission_denied, got {e} instead"
 
 def test_public_key_set_rollover(
         kty, private_key_gen, private_key, public_key_refresh_interval,
@@ -94,7 +160,7 @@ def test_public_key_set_rollover(
 
     interim_set = public_keyset_from_keys([new_key, private_key])
 
-    print("interim key set: {}".format(interim_set))
+    print(f"interim keyset: {interim_set}")
     old_key_json = None
     with open(cluster.public_key_json_file, "r") as keyfile:
         old_key_json = keyfile.read()
@@ -104,7 +170,7 @@ def test_public_key_set_rollover(
     try:
         with open(cluster.public_key_json_file, "w") as keyfile:
             keyfile.write(interim_set)
-        print("sleeping {} seconds for interim key file to take effect".format(delay))
+        print(f"sleeping {delay} seconds for interim key file to take effect")
         time.sleep(delay)
 
         tr_default = tenant_tr_gen(default_tenant)
@@ -118,11 +184,11 @@ def test_public_key_set_rollover(
         tr_second.commit().wait()
 
         final_set = public_keyset_from_keys([new_key])
-        print("final key set: {}".format(final_set))
+        print(f"final keyset: {final_set}")
         with open(cluster.public_key_json_file, "w") as keyfile:
             keyfile.write(final_set)
 
-        print("sleeping {} seconds for final key file to take effect".format(delay))
+        print(f"sleeping {delay} seconds for final key file to take effect")
         time.sleep(delay)
 
         # token_default is still cached and with active TTL. transaction should work
@@ -138,21 +204,86 @@ def test_public_key_set_rollover(
             value = tr_default[b"abc"].value
             assert False, "expected permission_denied, but read transaction went through"
         except fdb.FDBError as ferr:
-            assert ferr.code == 6000, "expected permission_denied, got {} instead".format(ferr)
+            assert ferr.code == 6000, f"expected permission_denied, got {ferr} instead"
         try:
             tr_default[b"abc"] = b"ghi"
             tr_default.commit().wait()
         except fdb.FDBError as ferr:
-            assert ferr.code == 6000, "expected permission_denied, got {} instead".format(ferr)
+            assert ferr.code == 6000, f"expected permission_denied, got {ferr} instead"
 
         tr_second = tenant_tr_gen(second_tenant)
         tr_second.options.set_authorization_token(token_second)
         assert tr_second[b"ghi"] == b"jkl", "earlier write transaction for tenant {} not visible".format(second_tenant)
     except Exception as e:
-        print("unexpected exception {}, reverting key file to default".format(e))
+        print(f"unexpected exception {e}, reverting key file to default")
         raise e
     finally:
         with open(cluster.public_key_json_file, "w") as keyfile:
             keyfile.write(old_key_json)
-        print("key file reverted. waiting {} seconds for the update to take effect...".format(delay))
+        print(f"key file reverted. waiting {delay} seconds for the update to take effect...")
         time.sleep(delay)
+
+def test_bad_token(private_key, token_gen, default_tenant, tenant_tr_gen):
+    def del_attr(d, attr):
+        del d[attr]
+        return d
+
+    def set_attr(d, attr, value):
+        d[attr] = value
+        return d
+
+    claim_mutations = [
+        ("no nbf", lambda claim: del_attr(claim, "nbf")),
+        ("no exp", lambda claim: del_attr(claim, "exp")),
+        ("no iat", lambda claim: del_attr(claim, "iat")),
+        ("too early", lambda claim: set_attr(claim, "nbf", time.time() + 30)),
+        ("too late", lambda claim: set_attr(claim, "exp", time.time() - 10)),
+        ("no tenants", lambda claim: del_attr(claim, "tenants")),
+        ("empty tenants", lambda claim: set_attr(claim, "tenants", [])),
+    ]
+    for case_name, mutation in claim_mutations:
+        tr = tenant_tr_gen(default_tenant)
+        tr.options.set_authorization_token(token_gen(private_key, mutation(token_claim_1h(default_tenant))))
+        try:
+            value = tr[b"abc"].value
+            assert False, f"expected permission_denied for case {case_name}, but read transaction went through"
+        except fdb.FDBError as e:
+            assert e.code == 6000, f"expected permission_denied for case {case_name}, got {e} instead"
+        tr = tenant_tr_gen(default_tenant)
+        tr.options.set_authorization_token(token_gen(private_key, mutation(token_claim_1h(default_tenant))))
+        tr[b"abc"] = b"def"
+        try:
+            tr.commit().wait()
+            assert False, f"expected permission_denied for case {case_name}, but write transaction went through"
+        except fdb.FDBError as e:
+            assert e.code == 6000, f"expected permission_denied for case {case_name}, got {e} instead"
+
+    # unknown key case: override "kid" field in header
+    # first, update only the kid field of key with export-update-import
+    key_dict = private_key.as_dict(is_private=True)
+    key_dict["kid"] = random_alphanum_str(10)
+    renamed_key = authlib.jose.JsonWebKey.import_key(key_dict)
+    unknown_key_token = token_gen(
+                renamed_key,
+                token_claim_1h(default_tenant),
+                headers={
+                    "typ": "JWT",
+                    "kty": renamed_key.kty,
+                    "alg": alg_from_kty(renamed_key.kty),
+                    "kid": renamed_key.kid,
+                })
+    tr = tenant_tr_gen(default_tenant)
+    tr.options.set_authorization_token(unknown_key_token)
+    try:
+        value = tr[b"abc"].value
+        assert False, f"expected permission_denied for 'unknown key' case, but read transaction went through"
+    except fdb.FDBError as e:
+        assert e.code == 6000, f"expected permission_denied for 'unknown key' case, got {e} instead"
+    tr = tenant_tr_gen(default_tenant)
+    tr.options.set_authorization_token(unknown_key_token)
+    tr[b"abc"] = b"def"
+    try:
+        tr.commit().wait()
+        assert False, f"expected permission_denied for 'unknown key' case, but write transaction went through"
+    except fdb.FDBError as e:
+        assert e.code == 6000, f"expected permission_denied for 'unknown key' case, got {e} instead"
