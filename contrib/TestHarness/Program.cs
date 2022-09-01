92797867925badd68ca38cd59e88d7a6c56cae3e
Josh Slocum <josh.slocum@snowflake.com>
2022-09-01 16:28:05 -0500

    Revert "Revert "Add snowflake-specific binaries folder for restart tests""
    This reverts commit eb0833204b3e9a4f1ba048dab881879ca01a4666.

--- contrib/TestHarness/Program.cs
+++ contrib/TestHarness/Program.cs
@@ -324,8 +324,10 @@ namespace SummarizeTest
                         oldBinaryVersionLowerBound = "0.0.0";
                     }
                     string[] currentBinary = { fdbserverName };
-                    IEnumerable<string> oldBinaries = Array.FindAll(
-                                                         Directory.GetFiles(oldBinaryFolder),
+                    var snowflakePath = Path.Combine("/app", "deploy", "global_data", "snowflakeBinaries");
+                    string[] snowflakeBinaries = Directory.Exists(snowflakePath) ? Directory.GetFiles(snowflakePath) : Array.Empty<string>();
+
+                    IEnumerable<string> oldBinaries = Array.FindAll(Directory.GetFiles(oldBinaryFolder).Concat(snowflakeBinaries).ToArray(),
                                                          x => versionGreaterThanOrEqual(Path.GetFileName(x).Split('-').Last(), oldBinaryVersionLowerBound)
                                                            && versionLessThan(Path.GetFileName(x).Split('-').Last(), oldBinaryVersionUpperBound));
                     if (!lastFolderName.Contains("until_")) {
