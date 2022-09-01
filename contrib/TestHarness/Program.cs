77462b734c5f4067cf9212e4884d347c2bd34c95
Josh Slocum <josh.slocum@snowflake.com>
2022-09-01 16:28:56 -0500

    only use snowflake binary directory folder for restarting test

--- contrib/TestHarness/Program.cs
+++ contrib/TestHarness/Program.cs
@@ -327,7 +327,7 @@ namespace SummarizeTest
                     var snowflakePath = Path.Combine("/app", "deploy", "global_data", "snowflakeBinaries");
                     string[] snowflakeBinaries = Directory.Exists(snowflakePath) ? Directory.GetFiles(snowflakePath) : Array.Empty<string>();
 
-                    IEnumerable<string> oldBinaries = Array.FindAll(Directory.GetFiles(oldBinaryFolder).Concat(snowflakeBinaries).ToArray(),
+                    IEnumerable<string> oldBinaries = Array.FindAll(snowflakeBinaries,
                                                          x => versionGreaterThanOrEqual(Path.GetFileName(x).Split('-').Last(), oldBinaryVersionLowerBound)
                                                            && versionLessThan(Path.GetFileName(x).Split('-').Last(), oldBinaryVersionUpperBound));
                     if (!lastFolderName.Contains("until_")) {
