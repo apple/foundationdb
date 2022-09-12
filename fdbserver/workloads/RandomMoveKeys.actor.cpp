949b1c1af95fdc069b048b41ec8f5fa6994300cd
Xiaoxi Wang <xiaoxi.wang@snowflake.com>
2022-09-12 15:40:18 -0700

    Add MoveKeysParams struct; use txnProcessor->moveKeys()

--- fdbserver/workloads/RandomMoveKeys.actor.cpp
+++ fdbserver/workloads/RandomMoveKeys.actor.cpp
@@ -143,18 +143,18 @@ struct MoveKeysWorkload : TestWorkload {
 			state Promise<Void> signal;
 			state DDEnabledState ddEnabledState;
 			wait(moveKeys(cx,
-			              deterministicRandom()->randomUniqueID(),
-			              keys,
-			              destinationTeamIDs,
-			              destinationTeamIDs,
-			              lock,
-			              signal,
-			              &fl1,
-			              &fl2,
-			              false,
-			              relocateShardInterval.pairID,
-			              &ddEnabledState,
-			              CancelConflictingDataMoves::True));
+			              MoveKeysParams{ deterministicRandom()->randomUniqueID(),
+			                              keys,
+			                              destinationTeamIDs,
+			                              destinationTeamIDs,
+			                              lock,
+			                              signal,
+			                              &fl1,
+			                              &fl2,
+			                              false,
+			                              relocateShardInterval.pairID,
+			                              &ddEnabledState,
+			                              CancelConflictingDataMoves::True }));
 			TraceEvent(relocateShardInterval.end()).detail("Result", "Success");
 			return Void();
 		} catch (Error& e) {
