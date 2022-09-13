155443d91c1850f06da9d2e8ea53d19474376482
Alex Lorenz <arphaman@gmail.com>
2022-09-13 11:11:47 -0700

    move generate modulemap function into its own cmake module

---
+++ cmake/GenerateModulemap.cmake
@@ -0,0 +1,16 @@
+# Generate flow modulemap
+
+function(generate_modulemap out module target)
+  cmake_parse_arguments(ARG "" "" "OMIT" ${ARGN})
+
+  set(MODULE_NAME "${module}")
+  get_target_property(MODULE_HEADERS ${target} HEADER_FILES)
+  foreach(header ${MODULE_HEADERS})
+    get_filename_component(fname ${header} NAME)
+    if(NOT ${fname} IN_LIST ARG_OMIT)
+      set(header_list "${header_list}    header \"${header}\"\n")
+    endif()
+  endforeach()
+  set(MODULE_HEADERS "${header_list}")
+  configure_file("${CMAKE_CURRENT_SOURCE_DIR}/empty.modulemap" "${out}/module.modulemap" @ONLY)
+endfunction()
