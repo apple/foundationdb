baff7402da19a7d8ef07ad2811011ade97483ea0
Alex Lorenz <arphaman@gmail.com>
2022-09-13 16:17:54 -0700

    initial Flow and FDBServer adoption in Swift sources in FDBServer

--- fdbserver/SwiftUtils.swift
+++ fdbserver/SwiftUtils.swift
@@ -1,5 +1,21 @@
+import Flow
+import FDBServer
 
 @_cdecl("testSwiftInFDB")
 public func testSwiftInFDB() {
     print("[swift] fdbserver")
+    print("a")
+    installGlobalSwiftConcurrencyHooks()
+
+    // capture the main thread ID
+    let mainTID = _tid()
+    func assertOnNet2EventLoop() {
+        precondition(_tid() == mainTID) // we're on the main thread, which the Net2 runloop runs on
+    }
+
+    print("[swift][tid:\(_tid())] run network @ thread:\(_tid())")
+    globalNetworkRun()
+}
+
+func testFDBServerImport(_ p: ResolutionBalancer) {
 }
