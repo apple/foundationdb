11a4f8a67203a6eaea16c1a14a185f13c7824fc8
A.J. Beamon <aj.beamon@snowflake.com>
2022-09-20 14:49:22 -0700

    Fix the computation of the maximum array bound for the decoding array

--- contrib/libb64/cdecode.c
+++ contrib/libb64/cdecode.c
@@ -12,9 +12,9 @@ int base64_decode_value(int value_in) {
 		                            -1, -1, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, 15, 16, 17,
 		                            18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31,
 		                            32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51 };
-	static const int decoding_size = sizeof(decoding);
+	static const int decoding_size = sizeof(decoding) / sizeof(decoding[0]);
 	value_in -= 43;
-	if (value_in < 0 || value_in > decoding_size)
+	if (value_in < 0 || value_in >= decoding_size)
 		return -1;
 	return decoding[value_in];
 }
