b607ca68effe6ef82066992dc9e6de1687e46c89
Steve Atherton <steve.atherton@snowflake.com>
2022-09-21 00:09:02 -0700

    Added BenchPriorityMultiLock benchmark.

--- flowbench/BenchPriorityMultiLock.actor.cpp
+++ flowbench/BenchPriorityMultiLock.actor.cpp
@@ -23,10 +23,54 @@
 #include "flow/flow.h"
 #include "flow/ThreadHelper.actor.h"
 #include "flow/PriorityMultiLock.actor.h"
-
+#include <deque>
 #include "flow/actorcompiler.h" // This must be the last #include.
 
 ACTOR static Future<Void> benchPriorityMultiLock(benchmark::State* benchState) {
+	state std::vector<int> priorities;
+
+	// Set up priority list with limits 10, 20, 30, ...
+	while (priorities.size() < benchState->range(0)) {
+		priorities.push_back(10 * (priorities.size() + 1));
+	}
+
+	state int concurrency = priorities.size() * 10;
+	state PriorityMultiLock* pml = new PriorityMultiLock(concurrency, priorities);
+	state std::vector<int> counts;
+	counts.resize(priorities.size(), 0);
+
+	// Clog the lock buy taking concurrency locks
+	state std::deque<Future<PriorityMultiLock::Lock>> lockFutures;
+	for (int j = 0; j < concurrency; ++j) {
+		lockFutures.push_back(pml->lock(j % priorities.size()));
+	}
+
+	// Wait for all of the initial locks to be taken
+	wait(waitForAll(std::vector<Future<PriorityMultiLock::Lock>>(lockFutures.begin(), lockFutures.end())));
+
+	state int p = 0;
+	state int i = 0;
+	while (benchState->KeepRunning()) {
+		// Get and replace the i'th lock future with a new lock waiter
+		Future<PriorityMultiLock::Lock> f = lockFutures[i];
+		lockFutures[i] = pml->lock(p);
+
+		PriorityMultiLock::Lock lock = wait(f);
+
+		// Rotate to another priority
+		if (++p == priorities.size()) {
+			p = 0;
+		}
+
+		// Rotate to next lock index
+		if (++i == lockFutures.size()) {
+			i = 0;
+		}
+	}
+
+	benchState->SetItemsProcessed(static_cast<long>(benchState->iterations()));
+
+	delete pml;
 	return Void();
 }
 
