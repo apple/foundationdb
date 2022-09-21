891cf87811201436744f467f1654d7dc6c0d84a1
Steve Atherton <steve.atherton@snowflake.com>
2022-09-21 00:55:35 -0700

    Added comments to PriorityMultiLock benchmark.

--- flowbench/BenchPriorityMultiLock.actor.cpp
+++ flowbench/BenchPriorityMultiLock.actor.cpp
@@ -46,8 +46,14 @@ ACTOR static Future<Void> benchPriorityMultiLock(benchmark::State* benchState) {
 	}
 
 	// Wait for all of the initial locks to be taken
+	// This will work regardless of their priorities as there are only n = concurrency of them
 	wait(waitForAll(std::vector<Future<PriorityMultiLock::Lock>>(lockFutures.begin(), lockFutures.end())));
 
+	// For each iteration of the loop, one new lock user is created, for a total of
+	// concurrency + 1 users.  The new user replaces an old one, which is then waited
+	// on.  This will succeed regardless of the lock priorities used because prior to
+	// new user there were only n = concurrency users so they will all be served before
+	// the new user.
 	state int p = 0;
 	state int i = 0;
 	while (benchState->KeepRunning()) {
