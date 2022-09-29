63b8d775a3fa24aebff914ceccdde30a01a68f37
He Liu <86634338+liquid-helium@users.noreply.github.com>
2022-09-29 15:40:29 -0700

    Knob for RocksDb behaviors (#8360)
    * Added knob for rocksdb suggest compact.
    
    * Added ROCKSDB_LEVEL_COMPACTION_DYNAMIC_LEVEL_BYTES knob.
    
    Co-authored-by: He Liu <heliu@apple.com>

--- fdbserver/KeyValueStoreShardedRocksDB.actor.cpp
+++ fdbserver/KeyValueStoreShardedRocksDB.actor.cpp
@@ -230,7 +230,7 @@ Error statusToError(const rocksdb::Status& s) {
 
 rocksdb::ColumnFamilyOptions getCFOptions() {
 	rocksdb::ColumnFamilyOptions options;
-	options.level_compaction_dynamic_level_bytes = true;
+	options.level_compaction_dynamic_level_bytes = SERVER_KNOBS->ROCKSDB_LEVEL_COMPACTION_DYNAMIC_LEVEL_BYTES;
 	options.OptimizeLevelStyleCompaction(SERVER_KNOBS->ROCKSDB_MEMTABLE_BYTES);
 	if (SERVER_KNOBS->ROCKSDB_PERIODIC_COMPACTION_SECONDS > 0) {
 		options.periodic_compaction_seconds = SERVER_KNOBS->ROCKSDB_PERIODIC_COMPACTION_SECONDS;
@@ -1873,7 +1873,9 @@ struct ShardedRocksDBKeyValueStore : IKeyValueStore {
 				ASSERT(cf != columnFamilyMap->end());
 				auto begin = toSlice(range.begin);
 				auto end = toSlice(range.end);
-				ASSERT(a.db->SuggestCompactRange(cf->second, &begin, &end).ok());
+				if (SERVER_KNOBS->ROCKSDB_SUGGEST_COMPACT_CLEAR_RANGE) {
+					ASSERT(a.db->SuggestCompactRange(cf->second, &begin, &end).ok());
+				}
 			}
 
 			if (a.getHistograms) {
