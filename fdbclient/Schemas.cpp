3ce168f604e7d2b73254b094a82bec1963f30eab
Jon Fu <jon.fu@snowflake.com>
2022-09-30 11:58:41 -0700

    add metacluster to status schema

--- fdbclient/Schemas.cpp
+++ fdbclient/Schemas.cpp
@@ -964,6 +964,9 @@ const KeyRef JSONSchemas::statusSchema = LiteralStringRef(R"statusSchema(
       },
       "tenants":{
          "num_tenants":0
+      },
+      "metacluster" : {
+         "cluster_type" : "standalone"
       }
    },
    "client":{
