999809e79b30d4a5b5f04f30c71caadc84314ff8
Ankita Kejriwal <ankita.kejriwal@snowflake.com>
2022-09-29 19:21:44 -0700

    Increase the data in the workload and add ASSERTs for expected values.

--- fdbserver/workloads/GetEstimatedRangeSize.actor.cpp
+++ fdbserver/workloads/GetEstimatedRangeSize.actor.cpp
@@ -24,6 +24,7 @@
 #include "fdbserver/workloads/workloads.actor.h"
 #include "fdbserver/workloads/BulkSetup.actor.h"
 
+#include "flow/Error.h"
 #include "flow/actorcompiler.h" // This must be the last #include.
 
 struct GetEstimatedRangeSizeWorkload : TestWorkload {
@@ -83,7 +84,19 @@ struct GetEstimatedRangeSizeWorkload : TestWorkload {
 
 	ACTOR static Future<Void> checkSize(GetEstimatedRangeSizeWorkload* self, Database cx) {
 		state int64_t size = wait(getSize(self, cx));
-		ASSERT_GT(size, 0);
+		// The following expected values are hard coded based on expected size for the tenants.
+		// We use a wide range to avoid flakiness because the underlying function (being tested)
+		// is making an estimation.
+		if (!self->hasTenant) {
+			ASSERT_GT(size, 9393000 / 5);
+			ASSERT_LT(size, 9393000 * 5);
+		} else if (self->tenant == "First"_sr) {
+			ASSERT_GT(size, 8525000 / 5);
+			ASSERT_LT(size, 8525000 * 5);
+		} else if (self->tenant == "Second"_sr) {
+			ASSERT_GT(size, 93000 / 5);
+			ASSERT_LT(size, 93000 * 5);
+		}
 		return Void();
 	}
 
