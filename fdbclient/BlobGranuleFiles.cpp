dea6da142df7856502ba76fe4a09fde853d1952f
Hui Liu <hui.liu@snowflake.com>
2022-09-29 21:00:12 -0700

    fix unittest benchFromFiles

--- fdbclient/BlobGranuleFiles.cpp
+++ fdbclient/BlobGranuleFiles.cpp
@@ -2907,6 +2907,7 @@ TEST_CASE("!/blobgranule/files/benchFromFiles") {
 	std::vector<bool> chunkModes = { false, true };
 	std::vector<bool> encryptionModes = { false, true };
 	std::vector<Optional<CompressionFilter>> compressionModes;
+	compressionModes.push_back({});
 	compressionModes.insert(
 	    compressionModes.end(), CompressionUtils::supportedFilters.begin(), CompressionUtils::supportedFilters.end());
 
@@ -2938,6 +2939,10 @@ TEST_CASE("!/blobgranule/files/benchFromFiles") {
 				if (!chunk && compressionFilter.present()) {
 					continue;
 				}
+				if (compressionFilter.present() && CompressionFilter::NONE == compressionFilter.get()) {
+					continue;
+				}
+
 				std::string name;
 				if (!chunk) {
 					name = "old";
@@ -3010,9 +3015,13 @@ TEST_CASE("!/blobgranule/files/benchFromFiles") {
 			if (!chunk && encrypt) {
 				continue;
 			}
+
 			Optional<BlobGranuleCipherKeysCtx> keys = encrypt ? cipherKeys : Optional<BlobGranuleCipherKeysCtx>();
 			for (auto& compressionFilter : compressionModes) {
 				if (!chunk && compressionFilter.present()) {
+					continue;
+				}
+				if (compressionFilter.present() && CompressionFilter::NONE == compressionFilter.get()) {
 					continue;
 				}
 				std::string name;
