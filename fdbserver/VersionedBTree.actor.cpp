f75f6e54a479961dd461409bc0eafc5fccc5f3cf
Yi Wu <yi.wu@snowflake.com>
2022-10-04 10:03:49 -0700

    revert buggify xor encoding

--- fdbserver/VersionedBTree.actor.cpp
+++ fdbserver/VersionedBTree.actor.cpp
@@ -6233,8 +6233,8 @@ private:
 		// size.
 		//
 		// There's another case. When encryption domain is enabled, we want to make sure the root node is encrypted
-		// using the default encryption domain. An indication that's not true is when the first record is not
-		// is using dbBegin as key.
+		// using the default encryption domain. An indication that's not true is when the first record is not using
+		// dbBegin as key.
 		while (records.size() > 1 ||
 		       records.front().getChildPage().size() > (BUGGIFY ? 1 : BTreeCommitHeader::maxRootPointerSize) ||
 		       records[0].key != dbBegin.key) {
@@ -8104,7 +8104,7 @@ public:
 			ASSERT(encryptionKeyProvider->expectedEncodingType() == EncodingType::AESEncryptionV1);
 			encodingType = EncodingType::AESEncryptionV1;
 			m_keyProvider = encryptionKeyProvider;
-		} else if (BUGGIFY) {
+		} else if (g_network->isSimulated() && logID.hash() % 2 == 0) {
 			// Simulation only. Deterministically enable encryption based on uid
 			encodingType = EncodingType::XOREncryption_TestOnly;
 			m_keyProvider = makeReference<XOREncryptionKeyProvider_TestOnly>(filename);
