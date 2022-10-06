a2747fbc18b794fcebb2f633ac7a358e54c03255
Kevin Hoxha <kevin.hoxha@snowflake.com>
2022-10-06 12:36:01 -0700

    metrics: Add simulation testing and fix incorrect TraceEvent names - Added a background actor that listens on METRICS_EMISSION_UDP_PORT for incoming metrics (and verifies they are in the correct format) - TraceEvent details have certain requirements for naming. This commit makes a seperate name for Counter/LatencySample and its underlying IMetric to avoid those issues

--- fdbrpc/include/fdbrpc/simulator.h
+++ fdbrpc/include/fdbrpc/simulator.h
@@ -97,6 +97,7 @@ public:
 		LocalityData locality;
 		ProcessClass startingClass;
 		TDMetricCollection tdmetrics;
+		MetricCollection metrics;
 		ChaosMetrics chaosMetrics;
 		HistogramRegistry histograms;
 		std::map<NetworkAddress, Reference<IListener>> listenerMap;
