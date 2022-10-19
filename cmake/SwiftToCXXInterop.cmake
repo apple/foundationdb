3d6fa2b512c1e0ea2a189c55f3bb88a8feaa2f96
Alex Lorenz <arphaman@gmail.com>
2022-10-19 11:16:56 -0700

    [interop] require a new toolchain (>= Oct 19th) to build

--- cmake/SwiftToCXXInterop.cmake
+++ cmake/SwiftToCXXInterop.cmake
@@ -10,17 +10,11 @@ function(generate_cxx_compat_header target headerName)
   if (EXISTS ${SwiftInteropVersionFile})
     file(READ ${SwiftInteropVersionFile} SwiftInteropVersion)
     message(STATUS "Swift: Experimental C++ interop version is ${SwiftInteropVersion}")
-    if (${SwiftInteropVersion} VERSION_LESS 2)
-      message(FATAL_ERROR "Swift: reverse interop support is too old.")
+    if (${SwiftInteropVersion} VERSION_LESS 4)
+      message(FATAL_ERROR "Swift: reverse interop support is too old. Update your toolchain.")
     endif()
-    set(LegacySwiftToolchain NO) # FIXME: remove
   else()
-    # FIXME: Once toolchain upgrades to Sept 30th, Always fail here.
-    check_swift_source_compiles("@_expose(Cxx) public func test() {}" REVERSE_INTEROP_SUPPORTED)
-     if (NOT ${REVERSE_INTEROP_SUPPORTED})
-       message(FATAL_ERROR "Swift: reverse interop is required, but not supported.  Update your toolcain.")
-     endif()
-    set(LegacySwiftToolchain YES)
+    message(FATAL_ERROR "Swift: reverse interop is required, but not supported. Update your toolcain.")
   endif()
 
   set(destpath ${CMAKE_CURRENT_BINARY_DIR}/include/SwiftModules)
@@ -29,20 +23,8 @@ function(generate_cxx_compat_header target headerName)
   # Generate a directory into which the C++ headers will go.
   file(MAKE_DIRECTORY ${destpath})
  
-  if (LegacySwiftToolchain)
-    # FIXME: Remove.
-    # Copy the shim file for C++ to find it.
-    file(MAKE_DIRECTORY ${destpath}/../shims)
-    swift_get_resource_path(SWIFT_RESOURCE_PATH)
-    add_custom_command(TARGET ${target} PRE_BUILD
-                       COMMAND ${CMAKE_COMMAND} -E
-                         copy ${SWIFT_RESOURCE_PATH}/shims/_SwiftCxxInteroperability.h ${destpath}/../shims/_SwiftCxxInteroperability.h)
-  endif()
-
   # Generate the C++ compatibility header for the Swift module.
   target_compile_options(${target} PRIVATE "$<$<COMPILE_LANGUAGE:Swift>:SHELL: -Xfrontend -emit-clang-header-path -Xfrontend ${destpath}/${headerName}>")
-  if (NOT LegacySwiftToolchain)
-    # Note: do not generate Stdlib bindings yet (C++20 only).
-    target_compile_options(${target} PRIVATE "$<$<COMPILE_LANGUAGE:Swift>:SHELL: -Xfrontend -clang-header-expose-decls=has-expose-attr>")
-  endif()
+  # Note: do not generate Stdlib bindings yet (C++20 only).
+  target_compile_options(${target} PRIVATE "$<$<COMPILE_LANGUAGE:Swift>:SHELL: -Xfrontend -clang-header-expose-decls=has-expose-attr>")
 endfunction()
