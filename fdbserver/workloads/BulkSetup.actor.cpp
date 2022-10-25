0870adecfc3e2e351145305df6e45da11a1c7089
Nim Wijetunga <nim.wijetunga@snowflake.com>
2022-10-25 13:30:21 -0700

    bulk setup  workload improvements

--- fdbserver/workloads/BulkSetup.actor.cpp
+++ fdbserver/workloads/BulkSetup.actor.cpp
@@ -18,29 +18,31 @@
  * limitations under the License.
  */
 
+#include "fdbclient/TenantManagement.actor.h"
 #include "fdbrpc/ContinuousSample.h"
-#include "fdbclient/NativeAPI.actor.h"
+#include "fdbserver/Knobs.h"
 #include "fdbserver/TesterInterface.actor.h"
 #include "fdbserver/workloads/workloads.actor.h"
 #include "fdbserver/workloads/BulkSetup.actor.h"
+#include "flow/genericactors.actor.h"
 #include "flow/actorcompiler.h" // This must be the last #include.
 
 struct BulkSetupWorkload : TestWorkload {
 	static constexpr auto NAME = "BulkSetup";
 
-	std::vector<TenantName> tenantNames;
 	int nodeCount;
 	double transactionsPerSecond;
 	Key keyPrefix;
+	double maxNumTenants;
+	double minNumTenants;
 
 	BulkSetupWorkload(WorkloadContext const& wcx) : TestWorkload(wcx) {
 		transactionsPerSecond = getOption(options, "transactionsPerSecond"_sr, 5000.0) / clientCount;
 		nodeCount = getOption(options, "nodeCount"_sr, transactionsPerSecond * clientCount);
 		keyPrefix = unprintable(getOption(options, "keyPrefix"_sr, ""_sr).toString());
-		std::vector<std::string> tenants = getOption(options, "tenants"_sr, std::vector<std::string>());
-		for (std::string tenant : tenants) {
-			tenantNames.push_back(TenantName(tenant));
-		}
+		maxNumTenants = getOption(options, "maxNumTenants"_sr, 0);
+		minNumTenants = getOption(options, "minNumTenants"_sr, 0);
+		ASSERT(minNumTenants <= maxNumTenants);
 	}
 
 	void getMetrics(std::vector<PerfMetric>& m) override {}
@@ -51,21 +53,47 @@ struct BulkSetupWorkload : TestWorkload {
 
 	Standalone<KeyValueRef> operator()(int n) { return KeyValueRef(key(n), value((n + 1) % nodeCount)); }
 
-	Future<Void> start(Database const& cx) override {
-		return bulkSetup(cx,
-		                 this,
-		                 nodeCount,
-		                 Promise<double>(),
-		                 false,
-		                 0.0,
-		                 1e12,
-		                 std::vector<uint64_t>(),
-		                 Promise<std::vector<std::pair<uint64_t, double>>>(),
-		                 0,
-		                 0.1,
-		                 0,
-		                 0,
-		                 this->tenantNames);
+	ACTOR static Future<Void> _setup(BulkSetupWorkload* workload, Database cx) {
+		// create a bunch of tenants (between min and max tenants)
+		state int numTenantsToCreate =
+		    deterministicRandom()->randomInt(workload->minNumTenants, workload->maxNumTenants + 1);
+		TraceEvent("BulkSetupTenantCreation").detail("NumTenants", numTenantsToCreate);
+		std::vector<Future<Void>> tenantFutures;
+		state std::vector<TenantName> tenantNames;
+		for (int i = 0; i < numTenantsToCreate; i++) {
+			TenantMapEntry entry;
+			entry.encrypted = SERVER_KNOBS->ENABLE_ENCRYPTION;
+			tenantNames.push_back(TenantName(format("BulkSetupTenant%04d", i)));
+			TraceEvent("CreatingTenant").detail("Tenant", tenantNames.back()).detail("TenantGroup", entry.tenantGroup);
+			tenantFutures.push_back(success(TenantAPI::createTenant(cx.getReference(), tenantNames.back())));
+		}
+		wait(waitForAll(tenantFutures));
+
+		// bulk load data into the tenants created above
+		wait(bulkSetup(cx,
+		               workload,
+		               workload->nodeCount,
+		               Promise<double>(),
+		               false,
+		               0.0,
+		               1e12,
+		               std::vector<uint64_t>(),
+		               Promise<std::vector<std::pair<uint64_t, double>>>(),
+		               0,
+		               0.1,
+		               0,
+		               0,
+		               tenantNames));
+		return Void();
+	}
+
+	Future<Void> start(Database const& cx) override { return Void(); }
+
+	Future<Void> setup(Database const& cx) override {
+		if (clientId == 0) {
+			return _setup(this, cx);
+		}
+		return Void();
 	}
 
 	Future<bool> check(Database const& cx) override { return true; }
