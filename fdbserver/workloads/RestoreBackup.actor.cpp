b565523061cd9c9bf0bdb4be8c664fa17f2f0198
Yi Wu <yi.wu@snowflake.com>
2022-11-01 10:44:29 -0700

    set ACCESS_SYSTEM_KEYS inside loop for retry

--- fdbserver/workloads/RestoreBackup.actor.cpp
+++ fdbserver/workloads/RestoreBackup.actor.cpp
@@ -96,9 +96,9 @@ struct RestoreBackupWorkload : TestWorkload {
 
 	ACTOR static Future<Void> clearDatabase(Database cx) {
 		state Transaction tr(cx);
-		tr.setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
 		loop {
 			try {
+				tr.setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
 				tr.clear(normalKeys);
 				for (auto& r : getSystemBackupRanges()) {
 					tr.clear(r);
