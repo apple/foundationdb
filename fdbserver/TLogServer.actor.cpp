938e521d7d34b21a4a0b20639df47c7d86ba150e
Zhe Wu <halfprice@users.noreply.github.com>
2022-11-02 10:13:16 -0700

    Change ASSERT_WE_THINK to ASSERT when checking that peek reply start version must be greater than latest pop version

--- fdbserver/TLogServer.actor.cpp
+++ fdbserver/TLogServer.actor.cpp
@@ -1874,7 +1874,7 @@ Future<Void> tLogPeekMessages(PromiseType replyPromise,
 			return Void();
 		}
 
-		ASSERT_WE_THINK(reqBegin >= poppedVersion(logData, reqTag));
+		ASSERT(reqBegin >= poppedVersion(logData, reqTag));
 
 		endVersion = logData->version.get() + 1;
 		onlySpilled = false;
