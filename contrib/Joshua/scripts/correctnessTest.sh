4318130d81da77de9666b3aa3ee72f976ff15f52
Dennis Zhou <dennis.zhou@snowflake.com>
2022-11-09 13:49:39 -0800

    snowflake/testharness2: use snowflakeBinaries
    Snowflake binaries are uploaded via the jenkins job in 1 and made
    available in the joshua image, example below:
    
    // ls /app/deploy/global_data/snowflakeBinaries/
    fdbserver-7.1.3  fdbserver-71.2.0  fdbserver-71.2.1
    
    The publish job will put new binaries in:
     s3://sfc-jenkins/foundationdb/builds/oldBinaries/
    
    The jenkins image build job will search this s3 bucket for the binaries
    to pull into that image.

--- contrib/Joshua/scripts/correctnessTest.sh
+++ contrib/Joshua/scripts/correctnessTest.sh
@@ -3,7 +3,7 @@
 # Simulation currently has memory leaks. We need to investigate before we can enable leak detection in joshua.
 export ASAN_OPTIONS="detect_leaks=0"
 
-OLDBINDIR="${OLDBINDIR:-/app/deploy/global_data/oldBinaries}"
+OLDBINDIR="${OLDBINDIR:-/app/deploy/global_data/snowflakeBinaries}"
 #mono bin/TestHarness.exe joshua-run "${OLDBINDIR}" false
 
 python3 -m test_harness.app -s ${JOSHUA_SEED} --old-binaries-path ${OLDBINDIR}
