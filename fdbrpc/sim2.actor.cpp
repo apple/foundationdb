f9f6f207891965a79ab528dc99436474e5175b3f
Markus Pilman <markus.pilman@snowflake.com>
2022-11-09 10:14:13 -0700

    Fix gcc problem with typenames

--- fdbrpc/sim2.actor.cpp
+++ fdbrpc/sim2.actor.cpp
@@ -784,13 +784,14 @@ private:
 			std::string sourceFilename = self->filename + ".part";
 
 			if (machineCache.count(sourceFilename)) {
+				// it seems gcc has some trouble with these types. Aliasing with typename is ugly, but seems to work.
+				using block_value_type = typename decltype(g_simulator->corruptedBlocks)::key_type::second_type;
 				TraceEvent("SimpleFileRename")
 				    .detail("From", sourceFilename)
 				    .detail("To", self->filename)
 				    .detail("SourceCount", machineCache.count(sourceFilename))
 				    .detail("FileCount", machineCache.count(self->filename));
-				auto maxBlockValue =
-				    std::numeric_limits<decltype(g_simulator->corruptedBlocks)::key_type::second_type>::max();
+				auto maxBlockValue = std::numeric_limits<block_value_type>::max();
 				g_simulator->corruptedBlocks.erase(
 				    g_simulator->corruptedBlocks.lower_bound(std::make_pair(sourceFilename, 0u)),
 				    g_simulator->corruptedBlocks.upper_bound(std::make_pair(self->filename, maxBlockValue)));
@@ -2752,7 +2753,9 @@ ACTOR Future<Void> renameFileImpl(std::string from, std::string to) {
 	// rename all keys in the corrupted list
 	// first we have to delete all corruption of the destination, since this file will be unlinked if it exists
 	TraceEvent("RenamingFile").detail("From", from).detail("To", to).log();
-	auto maxBlockValue = std::numeric_limits<decltype(g_simulator->corruptedBlocks)::key_type::second_type>::max();
+	// it seems gcc has some trouble with these types. Aliasing with typename is ugly, but seems to work.
+	using block_value_type = typename decltype(g_simulator->corruptedBlocks)::key_type::second_type;
+	auto maxBlockValue = std::numeric_limits<block_value_type>::max();
 	g_simulator->corruptedBlocks.erase(g_simulator->corruptedBlocks.lower_bound(std::make_pair(to, 0u)),
 	                                   g_simulator->corruptedBlocks.upper_bound(std::make_pair(to, maxBlockValue)));
 	// next we need to rename all files. In practice, the number of corruptions for a given file should be very small
