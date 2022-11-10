81fc8e9d2ff6a0fda381610ac44c6b7f63e24c6b
Zhe Wu <halfprice@users.noreply.github.com>
2022-11-09 22:41:22 -0800

    Using a knob to guard updateClusterSharedStateMap

--- fdbclient/ClientKnobs.cpp
+++ fdbclient/ClientKnobs.cpp
@@ -294,6 +294,7 @@ void ClientKnobs::initialize(Randomize randomize) {
 	init( METACLUSTER_ASSIGNMENT_FIRST_CHOICE_DELAY, 1.0 ); if ( randomize && BUGGIFY ) METACLUSTER_ASSIGNMENT_FIRST_CHOICE_DELAY = deterministicRandom()->random01() * 60;
 	init( METACLUSTER_ASSIGNMENT_AVAILABILITY_TIMEOUT, 10.0 ); if ( randomize && BUGGIFY ) METACLUSTER_ASSIGNMENT_AVAILABILITY_TIMEOUT = 1 + deterministicRandom()->random01() * 59;
 	init( TENANT_ENTRY_CACHE_LIST_REFRESH_INTERVAL,  2 ); if( randomize && BUGGIFY ) TENANT_ENTRY_CACHE_LIST_REFRESH_INTERVAL = deterministicRandom()->randomInt(1, 10);
+	init( ENABLE_CLUSTER_SHARED_STATE_MAP,         false); if( randomize && BUGGIFY ) ENABLE_CLUSTER_SHARED_STATE_MAP = true;
 
 	init( ENABLE_ENCRYPTION_CPU_TIME_LOGGING,     false );
 	// clang-format on
