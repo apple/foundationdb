2d42e625ece879014e5782efeed78cc4111c2f53
Vaidas Gasiunas <vaidas.gasiunas@snowflake.com>
2022-11-15 16:35:08 +0100

    Use file locking to avoid port collisions in ctests using local cluster

--- tests/TestRunner/upgrade_test.py
+++ tests/TestRunner/upgrade_test.py
@@ -169,6 +169,7 @@ class UpgradeTest:
 
     def __exit__(self, xc_type, exc_value, traceback):
         self.cluster.stop_cluster()
+        self.cluster.release_ports()
         if CLEANUP_ON_EXIT:
             shutil.rmtree(self.tmp_dir)
 
