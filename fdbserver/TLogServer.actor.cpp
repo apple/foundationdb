214db4d17ee8ec38bb7b4b865dc29d8c2a7b2ddb
Sam Gwydir <sam.gwydir@snowflake.com>
2022-11-15 13:38:55 -0800

    formatting

--- fdbserver/TLogServer.actor.cpp
+++ fdbserver/TLogServer.actor.cpp
@@ -1445,9 +1445,7 @@ ACTOR Future<Void> updateStorage(TLogData* self) {
 ACTOR Future<Void> updateStorageLoop(TLogData* self) {
 	wait(delay(0, TaskPriority::UpdateStorage));
 
-	loop {
-		wait(updateStorage(self));
-	}
+	loop { wait(updateStorage(self)); }
 }
 
 void commitMessages(TLogData* self,
@@ -1608,9 +1606,7 @@ ACTOR Future<Void> waitForMessagesForTag(Reference<LogData> self, Tag reqTag, Ve
 			// we want the caller to finish first, otherwise the data structure it is building might not be complete
 			wait(delay(0.0));
 		}
-		when(wait(delay(timeout))) {
-			self->blockingPeekTimeouts += 1;
-		}
+		when(wait(delay(timeout))) { self->blockingPeekTimeouts += 1; }
 	}
 	return Void();
 }
@@ -2795,9 +2791,7 @@ ACTOR Future<Void> pullAsyncData(TLogData* self,
 	while (!endVersion.present() || logData->version.get() < endVersion.get()) {
 		loop {
 			choose {
-				when(wait(r ? r->getMore(TaskPriority::TLogCommit) : Never())) {
-					break;
-				}
+				when(wait(r ? r->getMore(TaskPriority::TLogCommit) : Never())) { break; }
 				when(wait(dbInfoChange)) {
 					if (logData->logSystem->get()) {
 						r = logData->logSystem->get()->peek(logData->logId, tagAt, endVersion, tags, true);
@@ -3278,9 +3272,7 @@ ACTOR Future<Void> restorePersistentState(TLogData* self,
 
 								choose {
 									when(wait(updateStorage(self))) {}
-									when(wait(allRemoved)) {
-										throw worker_removed();
-									}
+									when(wait(allRemoved)) { throw worker_removed(); }
 								}
 							}
 						} else {
@@ -3291,9 +3283,7 @@ ACTOR Future<Void> restorePersistentState(TLogData* self,
 						}
 					}
 				}
-				when(wait(allRemoved)) {
-					throw worker_removed();
-				}
+				when(wait(allRemoved)) { throw worker_removed(); }
 			}
 		}
 	} catch (Error& e) {
@@ -3639,9 +3629,7 @@ ACTOR Future<Void> tLog(IKeyValueStore* persistentData,
 						forwardPromise(req.reply, self.tlogCache.get(req.recruitmentID));
 					}
 				}
-				when(wait(error)) {
-					throw internal_error();
-				}
+				when(wait(error)) { throw internal_error(); }
 				when(wait(activeSharedChange)) {
 					if (activeSharedTLog->get() == tlogId) {
 						TraceEvent("SharedTLogNowActive", self.dbgid).detail("NowActive", activeSharedTLog->get());
