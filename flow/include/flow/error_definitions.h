14b3f59697e5f423ccb61bd5be3a22c31f7add4b
He Liu <heliu@apple.com>
2022-11-29 21:11:48 -0800

    Fixed error description.

--- flow/include/flow/error_definitions.h
+++ flow/include/flow/error_definitions.h
@@ -210,8 +210,8 @@ ERROR( value_not_tuple, 2042, "The value cannot be parsed as a tuple" );
 ERROR( mapper_not_tuple, 2043, "The mapper cannot be parsed as a tuple" );
 ERROR( invalid_checkpoint_format, 2044, "Invalid checkpoint format" )
 ERROR( invalid_throttle_quota_value, 2045, "Invalid quota value. Note that reserved_throughput cannot exceed total_throughput" )
-ERROR( failed_to_create_checkpoint, 2046, "Invalid quota value. Note that reserved_throughput cannot exceed total_throughput" )
-ERROR( failed_to_restore_checkpoint, 2047, "Invalid quota value. Note that reserved_throughput cannot exceed total_throughput" )
+ERROR( failed_to_create_checkpoint, 2046, "Failed to create a checkpoint" )
+ERROR( failed_to_restore_checkpoint, 2047, "Failed to restore a checkpoint" )
 
 ERROR( incompatible_protocol_version, 2100, "Incompatible protocol version" )
 ERROR( transaction_too_large, 2101, "Transaction exceeds byte limit" )
