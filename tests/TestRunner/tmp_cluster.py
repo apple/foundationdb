4818891c91c442cb4fec666d813db1fedcbb791d
hao fu <hfu5@apple.com>
2022-12-15 10:17:49 -0800

    change tmp_cluster.py

--- tests/TestRunner/tmp_cluster.py
+++ tests/TestRunner/tmp_cluster.py
@@ -200,6 +200,9 @@ if __name__ == "__main__":
                 cmd = cmd.replace(placeholder, value)
             cmd_args.append(cmd)
         env = dict(**os.environ)
+        env["FDB_CLUSTER_FILE"] = env.get(
+            "FDB_CLUSTER_FILE", cluster.cluster_file
+        )
         env["FDB_CLUSTERS"] = env.get(
             "FDB_CLUSTERS", cluster.cluster_file
         )
