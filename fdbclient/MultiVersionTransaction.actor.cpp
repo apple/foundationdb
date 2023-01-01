a9ebb59372307c6e86f49b66e76c944773af5a1c
Xiaoge Su <magichp@gmail.com>
2022-12-31 21:09:30 -0800

    fixup! Move the include to the head of the file
    Wall time 1150.0, clang time 16044.0

--- fdbclient/MultiVersionTransaction.actor.cpp
+++ fdbclient/MultiVersionTransaction.actor.cpp
@@ -18,6 +18,10 @@
  * limitations under the License.
  */
 
+#ifdef __unixish__
+#include <fcntl.h>
+#endif
+
 #include "fdbclient/IClientApi.h"
 #include "fdbclient/json_spirit/json_spirit_reader_template.h"
 #include "fdbclient/json_spirit/json_spirit_writer_template.h"
@@ -42,6 +46,10 @@
 #include "flow/Platform.h"
 #include "flow/ProtocolVersion.h"
 #include "flow/UnitTest.h"
+
+#ifdef __unixish__
+#include <fcntl.h>
+#endif // __unixish__
 
 #include "flow/actorcompiler.h" // This must be the last #include.
 
@@ -2660,8 +2668,6 @@ void MultiVersionApi::addExternalLibraryDirectory(std::string path) {
 	}
 }
 #if defined(__unixish__)
-#include <fcntl.h>
-
 std::vector<std::pair<std::string, bool>> MultiVersionApi::copyExternalLibraryPerThread(std::string path) {
 	ASSERT_GE(threadCount, 1);
 	// Copy library for each thread configured per version
@@ -2729,7 +2735,7 @@ std::vector<std::pair<std::string, bool>> MultiVersionApi::copyExternalLibraryPe
 
 	return paths;
 }
-#else
+#else // if defined (__unixish__)
 std::vector<std::pair<std::string, bool>> MultiVersionApi::copyExternalLibraryPerThread(std::string path) {
 	if (threadCount > 1) {
 		TraceEvent(SevError, "MultipleClientThreadsUnsupportedOnWindows").log();
@@ -2739,7 +2745,7 @@ std::vector<std::pair<std::string, bool>> MultiVersionApi::copyExternalLibraryPe
 	paths.push_back({ path, false });
 	return paths;
 }
-#endif
+#endif // if defined (__unixish__)
 
 void MultiVersionApi::disableLocalClient() {
 	MutexHolder holder(lock);
