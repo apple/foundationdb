fe4f3320f65976e011cd403edfd9aff87905d454
Nim Wijetunga <nim.wijetunga@snowflake.com>
2023-01-04 10:06:06 -0500

    add comment

--- fdbserver/ClusterRecovery.actor.cpp
+++ fdbserver/ClusterRecovery.actor.cpp
@@ -1079,6 +1079,8 @@ ACTOR Future<Void> readTransactionSystemState(Reference<ClusterRecoveryData> sel
 	// Sets self->configuration to the configuration (FF/conf/ keys) at self->lastEpochEnd
 
 	// Recover transaction state store
+	// If it's the first recovery the encrypt mode is not yet avilable so create the txn state store with encryption
+	// disabled. This is fine since we will not write any data to disk using this txn store.
 	state EncryptionAtRestMode encryptModeForTxnStateStore = EncryptionAtRestMode::DISABLED;
 	if (self->controllerData && self->controllerData->encryptionAtRestMode.isValid() &&
 	    self->controllerData->encryptionAtRestMode.getFuture().isValid() &&
