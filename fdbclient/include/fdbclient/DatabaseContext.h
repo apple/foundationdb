5b027553966358b6f76dc3fbca34db0d6bf13b3f
Vaidas Gasiunas <vaidas.gasiunas@snowflake.com>
2023-01-16 15:07:43 +0100

    get_client_status: document the schema of the returned json

--- fdbclient/include/fdbclient/DatabaseContext.h
+++ fdbclient/include/fdbclient/DatabaseContext.h
@@ -720,7 +720,36 @@ public:
 	std::unique_ptr<GlobalConfig> globalConfig;
 	EventCacheHolder connectToDatabaseEventCacheHolder;
 
-	// Get client-side status information
+	// Get client-side status information as a JSON string with the following schema:
+	// { "Healthy" : <overall health status: true or false>,
+	//    "ClusterID" : <UUID>,
+	//    "Coordinators" : [ <address>, ...  ],
+	//    "CurrentCoordinator" : <address>
+	//    "GrvProxies" : [ <address>, ...  ],
+	//    "CommitProxies" : [ <address>", ... ],
+	//    "StorageServers" : [ { "Address" : <address>, "SSID" : <Storage Server ID> }, ... ],
+	//    "Connections" : [
+	//      { "Address" : "<address>",
+	//        "Status" : <failed|connected|connecting|disconnected>,
+	//        "Compatible" : <is protocol version compatible with the client>,
+	//        "ConnectFailedCount" : <number of failed connection attempts>,
+	//        "LastConnectTime" : <elapsed time in seconds since the last connection attempt>,
+	//        "PingCount" : <total ping count>,
+	//        "PingTimeoutCount" : <number of ping timeouts>,
+	//        "BytesSampleTime" : <elapsed time of the reported the bytes received and sent values>,
+	//        "BytesReceived" : <bytes received>,
+	//        "BytesSent" : <bytes sent>,
+	//        "ProtocolVersion" : <protocol version of the server, present if known>
+	//      },
+	//      ...
+	//    ]
+	// }
+	//
+	// The addresses in the Connections array match the addresses of Coordinators, GrvProxies,
+	// CommitProxies and StorageServers
+	//
+	// If the database context is initialized with an error, the JSON contains just the error code
+	// { "InitializationError" : <error code> }
 	Standalone<StringRef> getClientStatus();
 
 private:
