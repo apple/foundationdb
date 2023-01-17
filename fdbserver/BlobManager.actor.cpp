dd7f9d522c791d2caa96fa4db18a649854a62f12
Josh Slocum <josh.slocum@snowflake.com>
2023-01-17 10:36:55 -0600

    removing extra unecessary conflict checking in BM lock check

--- fdbserver/BlobManager.actor.cpp
+++ fdbserver/BlobManager.actor.cpp
@@ -1156,8 +1156,6 @@ ACTOR Future<Void> checkManagerLock(Transaction* tr, Reference<BlobManagerData> 
 
 		throw blob_manager_replaced();
 	}
-	tr->addReadConflictRange(singleKeyRange(blobManagerEpochKey));
-	tr->addWriteConflictRange(singleKeyRange(blobManagerEpochKey));
 
 	return Void();
 }
