c145db8ab5a6d32346fd2a2a5e14e1e825de0b5a
FoundationDB CI <foundationdb_ci@apple.com>
2023-01-20 03:09:02 +0000

    Updated FoundationDB Git Docs to version: 7.2.3

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.2.0',
+    VERSION: '7.2.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
