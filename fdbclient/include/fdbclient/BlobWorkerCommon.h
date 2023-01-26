0881c0e4e2bdc291daa9b7490158de9ccefd824c
Josh Slocum <josh.slocum@snowflake.com>
2023-01-26 16:56:45 -0600

    Bg perf 2 (#9052)
    * added dynamic write amp calculations for blob granule compaction
    
    * changing blob worker parallelism counts to bytes budget to handle less uniform operation sizes
    
    * more snapshotting parallelism for behind feeds
    
    * add a bit of observability when this happens
    
    * adding knobs
    
    * typo
    
    * adjusting some knobs up with buggified granule size
    
    * fixing bugs in dynamic write amp
    
    * fixing formatting
    
    * fixing bug in knob buggification
    
    * fix formatting

--- fdbclient/include/fdbclient/BlobWorkerCommon.h
+++ fdbclient/include/fdbclient/BlobWorkerCommon.h
@@ -46,10 +46,12 @@ struct BlobWorkerStats {
 	Counter fullRejections;
 	Counter forceFlushCleanups;
 	Counter readDrivenCompactions;
+	Counter oldFeedSnapshots;
 
 	int numRangesAssigned;
 	int mutationBytesBuffered;
 	int activeReadRequests;
+	// TODO: add gauge for granules blocking on old snapshots, once this guage is fixed
 	int granulesPendingSplitCheck;
 	Version minimumCFVersion;
 	Version cfVersionLag;
@@ -63,8 +65,8 @@ struct BlobWorkerStats {
 	LatencySample readLatencySample;
 
 	Reference<FlowLock> initialSnapshotLock;
-	Reference<FlowLock> resnapshotLock;
-	Reference<FlowLock> deltaWritesLock;
+	Reference<FlowLock> resnapshotBudget;
+	Reference<FlowLock> deltaWritesBudget;
 
 	Future<Void> logger;
 
@@ -72,8 +74,8 @@ struct BlobWorkerStats {
 	explicit BlobWorkerStats(UID id,
 	                         double interval,
 	                         Reference<FlowLock> initialSnapshotLock,
-	                         Reference<FlowLock> resnapshotLock,
-	                         Reference<FlowLock> deltaWritesLock,
+	                         Reference<FlowLock> resnapshotBudget,
+	                         Reference<FlowLock> deltaWritesBudget,
 	                         double sampleLoggingInterval,
 	                         double fileOpLatencySketchAccuracy,
 	                         double requestLatencySketchAccuracy)
@@ -93,17 +95,17 @@ struct BlobWorkerStats {
 	    flushGranuleReqs("FlushGranuleReqs", cc), compressionBytesRaw("CompressionBytesRaw", cc),
 	    compressionBytesFinal("CompressionBytesFinal", cc), fullRejections("FullRejections", cc),
 	    forceFlushCleanups("ForceFlushCleanups", cc), readDrivenCompactions("ReadDrivenCompactions", cc),
-	    numRangesAssigned(0), mutationBytesBuffered(0), activeReadRequests(0), granulesPendingSplitCheck(0),
-	    minimumCFVersion(0), cfVersionLag(0), notAtLatestChangeFeeds(0), lastResidentMemory(0),
-	    snapshotBlobWriteLatencySample("SnapshotBlobWriteMetrics",
-	                                   id,
-	                                   sampleLoggingInterval,
-	                                   fileOpLatencySketchAccuracy),
+	    oldFeedSnapshots("OldFeedSnapshots", cc), numRangesAssigned(0), mutationBytesBuffered(0), activeReadRequests(0),
+	    granulesPendingSplitCheck(0), minimumCFVersion(0), cfVersionLag(0), notAtLatestChangeFeeds(0),
+	    lastResidentMemory(0), snapshotBlobWriteLatencySample("SnapshotBlobWriteMetrics",
+	                                                          id,
+	                                                          sampleLoggingInterval,
+	                                                          fileOpLatencySketchAccuracy),
 	    deltaBlobWriteLatencySample("DeltaBlobWriteMetrics", id, sampleLoggingInterval, fileOpLatencySketchAccuracy),
 	    reSnapshotLatencySample("GranuleResnapshotMetrics", id, sampleLoggingInterval, fileOpLatencySketchAccuracy),
 	    readLatencySample("GranuleReadLatencyMetrics", id, sampleLoggingInterval, requestLatencySketchAccuracy),
-	    estimatedMaxResidentMemory(0), initialSnapshotLock(initialSnapshotLock), resnapshotLock(resnapshotLock),
-	    deltaWritesLock(deltaWritesLock) {
+	    estimatedMaxResidentMemory(0), initialSnapshotLock(initialSnapshotLock), resnapshotBudget(resnapshotBudget),
+	    deltaWritesBudget(deltaWritesBudget) {
 		specialCounter(cc, "NumRangesAssigned", [this]() { return this->numRangesAssigned; });
 		specialCounter(cc, "MutationBytesBuffered", [this]() { return this->mutationBytesBuffered; });
 		specialCounter(cc, "ActiveReadRequests", [this]() { return this->activeReadRequests; });
@@ -115,10 +117,10 @@ struct BlobWorkerStats {
 		specialCounter(cc, "EstimatedMaxResidentMemory", [this]() { return this->estimatedMaxResidentMemory; });
 		specialCounter(cc, "InitialSnapshotsActive", [this]() { return this->initialSnapshotLock->activePermits(); });
 		specialCounter(cc, "InitialSnapshotsWaiting", [this]() { return this->initialSnapshotLock->waiters(); });
-		specialCounter(cc, "ReSnapshotsActive", [this]() { return this->resnapshotLock->activePermits(); });
-		specialCounter(cc, "ReSnapshotsWaiting", [this]() { return this->resnapshotLock->waiters(); });
-		specialCounter(cc, "DeltaFileWritesActive", [this]() { return this->deltaWritesLock->activePermits(); });
-		specialCounter(cc, "DeltaFileWritesWaiting", [this]() { return this->deltaWritesLock->waiters(); });
+		specialCounter(cc, "ReSnapshotBytesActive", [this]() { return this->resnapshotBudget->activePermits(); });
+		specialCounter(cc, "ReSnapshotBytesWaiting", [this]() { return this->resnapshotBudget->waiters(); });
+		specialCounter(cc, "DeltaFileWriteBytesActive", [this]() { return this->deltaWritesBudget->activePermits(); });
+		specialCounter(cc, "DeltaFileWriteBytesWaiting", [this]() { return this->deltaWritesBudget->waiters(); });
 
 		logger = cc.traceCounters("BlobWorkerMetrics", id, interval, "BlobWorkerMetrics");
 	}
