c3849ce81bddc392319b079832783ef59a5d05ba
Marian Dvorsky <marian.dvorsky@snowflake.com>
2023-01-27 19:12:49 +0100

    Fix C++ style comment

--- bindings/c/foundationdb/fdb_c.h
+++ bindings/c/foundationdb/fdb_c.h
@@ -227,7 +227,7 @@ typedef enum { FDB_BG_MUTATION_TYPE_SET_VALUE = 0, FDB_BG_MUTATION_TYPE_CLEAR_RA
 
 #pragma pack(push, 4)
 typedef struct bgmutation {
-	uint8_t type; // FDBBGMutationType
+  /* FDBBGMutationType */ uint8_t type;
 	int64_t version;
 	const uint8_t* param1_ptr;
 	int param1_length;
