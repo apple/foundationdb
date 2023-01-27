c959bcf1f323bfa73396768fa726e2750a39270a
Xiaoge Su <magichp@gmail.com>
2023-01-26 21:31:05 -0800

    fixup! Fix a bunch of bugs

--- contrib/Joshua/scripts/binding_test_start.sh
+++ contrib/Joshua/scripts/binding_test_start.sh
@@ -1,3 +1,7 @@
 #! /usr/bin/env bash
 
-python3 ./binding_test.py --debug --stop-at-failure 10 --fdbserver-path $(pwd)/fdbserver --fdbcli-path $(pwd)/fdbcli --libfdb-path $(pwd) --num-ops 1000 --num-hca-ops 100 --concurrency 5 --test-timeout 600
+set -e
+set -o pipefail
+
+# It is necessary to tee to output.log in case timeout happens
+python3 ./binding_test.py --stop-at-failure 10 --fdbserver-path $(pwd)/fdbserver --fdbcli-path $(pwd)/fdbcli --libfdb-path $(pwd) --num-ops 1000 --num-hca-ops 100 --concurrency 5 --test-timeout 60 --random | tee output.log
