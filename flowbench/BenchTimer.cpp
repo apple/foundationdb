be70a341f28a661c89634b451bb39ab392b02f88
Dan Adkins <dan.adkins@snowflake.com>
2023-02-02 10:30:31 -0800

    Revert "Use real clock source for trace events in real fdbserver, but now() in simulation. (#9270)"
    This reverts commit 02f78500b50a4e29fae00965fc7509b6966c8353.
    
    That changed the default behavior of the clock used for trace logs, which
    might break some tooling or workflows.

--- flowbench/BenchTimer.cpp
+++ flowbench/BenchTimer.cpp
@@ -36,13 +36,5 @@ static void bench_timer_monotonic(benchmark::State& state) {
 	state.SetItemsProcessed(static_cast<long>(state.iterations()));
 }
 
-static void bench_timestamp_counter(benchmark::State& state) {
-	for (auto _ : state) {
-		benchmark::DoNotOptimize(timestampCounter());
-	}
-	state.SetItemsProcessed(static_cast<long>(state.iterations()));
-}
-
 BENCHMARK(bench_timer)->ReportAggregatesOnly(true);
 BENCHMARK(bench_timer_monotonic)->ReportAggregatesOnly(true);
-BENCHMARK(bench_timestamp_counter)->ReportAggregatesOnly(true);
