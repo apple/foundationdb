9aa15b459c169db222af19137eff82d581669273
Jingyu Zhou <jingyuzhou@gmail.com>
2023-02-09 08:57:04 -0800

    Clean up trace events

--- fdbserver/DDTeamCollection.actor.cpp
+++ fdbserver/DDTeamCollection.actor.cpp
@@ -176,9 +176,6 @@ public:
 	ACTOR static Future<Void> getTeam(DDTeamCollection* self, GetTeamRequest req) {
 		try {
 			wait(self->checkBuildTeams());
-			TraceEvent("GetTeam", self->distributorId)
-			    .detail("Primary", self->primary)
-			    .detail("ZeroHealthy", self->zeroHealthyTeams->get());
 			// report the median available space
 			if (now() - self->lastMedianAvailableSpaceUpdate > SERVER_KNOBS->AVAILABLE_SPACE_UPDATE_DELAY) {
 				self->lastMedianAvailableSpaceUpdate = now();
@@ -228,9 +225,6 @@ public:
 			// The situation happens rarely. We may want to eliminate this situation someday
 			if (!self->teams.size()) {
 				req.reply.send(std::make_pair(Optional<Reference<IDataDistributionTeam>>(), foundSrc));
-				TraceEvent("GetTeam2", self->distributorId)
-				    .detail("Primary", self->primary)
-				    .detail("ZeroHealthy", self->zeroHealthyTeams->get());
 				return Void();
 			}
 
@@ -259,10 +253,6 @@ public:
 						if (found && teamList[j]->isHealthy()) {
 							bestOption = teamList[j];
 							req.reply.send(std::make_pair(bestOption, foundSrc));
-							TraceEvent("GetTeam3", self->distributorId)
-							    .detail("Primary", self->primary)
-							    .detail("Team", teamList[j]->getServerIDsStr())
-							    .detail("ZeroHealthy", self->zeroHealthyTeams->get());
 							return Void();
 						}
 					}
@@ -393,19 +383,15 @@ public:
 			// }
 
 			if (!self->readyToStart.isReady()) {
-				TraceEvent("GetTeam4", self->distributorId)
-				    .detail("Primary", self->primary)
-				    .detail("ZeroHealthy", self->zeroHealthyTeams->get());
+				// When remote DC is not ready, DD shouldn't reply with a team because
+				// a data movement to that team can't be completed and such a move
+				// may block the primary DC from reaching "storage_recovered".
+				TraceEvent("GetTeamNotReady", self->distributorId).detail("Primary", self->primary);
 				req.reply.send(std::make_pair(Optional<Reference<IDataDistributionTeam>>(), foundSrc));
 				return Void();
 			}
 
 			req.reply.send(std::make_pair(bestOption, foundSrc));
-			TraceEvent("GetTeam5", self->distributorId)
-			    .detail("Primary", self->primary)
-			    .detail("Team", bestOption.present() ? describe(bestOption.get()->getServerIDs()) : "")
-			    .detail("ZeroHealthy", self->zeroHealthyTeams->get());
-
 			return Void();
 		} catch (Error& e) {
 			if (e.code() != error_code_actor_cancelled)
@@ -1160,10 +1146,6 @@ public:
 				NetworkAddress a = server->getLastKnownInterface().address();
 				AddressExclusion worstAddr(a.ip, a.port);
 				DDTeamCollection::Status worstStatus = self->excludedServers.get(worstAddr);
-				TraceEvent("DDD", self->distributorId)
-				    .detail("Address", worstAddr.toString())
-				    .detail("ServerId", server->getId())
-				    .detail("Status", (int)worstStatus);
 
 				if (worstStatus == DDTeamCollection::Status::WIGGLING && invalidWiggleServer(worstAddr, self, server)) {
 					TraceEvent(SevInfo, "InvalidWiggleServer", self->distributorId)
@@ -1421,6 +1403,7 @@ public:
 			state Error err = e;
 			TraceEvent("StorageServerTrackerCancelled", self->distributorId)
 			    .errorUnsuppressed(e)
+			    .suppressFor(1.0)
 			    .detail("Primary", self->primary)
 			    .detail("Server", server->getId());
 			if (e.code() != error_code_actor_cancelled && errorOut.canBeSet()) {
@@ -1877,7 +1860,6 @@ public:
 		// Fetch the list of excluded servers
 		state ReadYourWritesTransaction tr(self->dbContext());
 		loop {
-			TraceEvent("DDExcludedServersChanged000", self->distributorId).detail("Primary", self->isPrimary());
 			try {
 				tr.setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
 				tr.setOption(FDBTransactionOptions::PRIORITY_SYSTEM_IMMEDIATE);
@@ -1916,10 +1898,8 @@ public:
 						failed.insert(addr);
 					}
 				}
-				TraceEvent("DDExcludedServersChangedBefore", self->distributorId).detail("Primary", self->isPrimary());
 
 				wait(success(fworkers));
-				TraceEvent("DDExcludedServersChangedAfter", self->distributorId).detail("Primary", self->isPrimary());
 				std::vector<ProcessData> workers = fworkers.get();
 				for (const auto& r : excludedLocalityResults) {
 					std::string locality = decodeExcludedLocalityKey(r.key);
@@ -1944,10 +1924,6 @@ public:
 					}
 				}
 				for (const auto& n : excluded) {
-					TraceEvent("DDExcludedServersChanged0", self->distributorId)
-					    .detail("N", n.toString())
-					    .detail("Primary", self->isPrimary())
-					    .detail("Failed", failed.count(n));
 					if (!failed.count(n)) {
 						self->excludedServers.set(n, DDTeamCollection::Status::EXCLUDED);
 					}
