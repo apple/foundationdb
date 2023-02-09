6c4a9b5f2388c70f60ac8416e8f6b1630d3222a9
Jingyu Zhou <jingyuzhou@gmail.com>
2023-02-08 23:15:09 -0800

    Fix DD stuck when remote DC is dead
    When remote DC is down, the remote team collection of DD can initializing
    waiting for the remote to recover (all_tlog_recruited state). However, the
    getTeam request can already be served by the remote team collection. So, for
    a RelocateShard (data movement such as split, move), it will get a team for
    the remote DC. But the data movement can't make progress on the remote team
    because the remote DC hasn't recovered yet. Because of the stuck of data
    movement, the primary cannot reach the "storage_recovered" state and stay in
    accepting_commit state.
    
    The specifc test failure: slow/ApiCorrectness.toml -s 339026305 -b on
    at commit:  0edd899d65
    
    In this test, primary DC has 1 SS killed, remote DC has 2 TLog and 2 SS killed.
    So the remote is dead, the remaining 2 SSes can't make progress because of the
    loss of 2 TLogs. The repairDeadDatacenter() can't reach the "storage_recovered"
    state due to DD's failure of moving shards away from the killed SS in the
    primary.
    
    The fix is to exclude all remote in repairDeadDatacenter() so that tells DD to
    mark all SSes in the remote as unhealthy. Another fix is to return empty
    results for getTeam request if the remote team collection is not ready. This
    will allow the data movement to continue, essentially remote team is not changed
    for the data movement.

--- fdbserver/QuietDatabase.actor.cpp
+++ fdbserver/QuietDatabase.actor.cpp
@@ -19,6 +19,7 @@
  */
 
 #include <cinttypes>
+#include <unordered_map>
 #include <vector>
 #include <type_traits>
 
@@ -715,7 +716,7 @@ ACTOR Future<Void> repairDeadDatacenter(Database cx,
                                         Reference<AsyncVar<ServerDBInfo> const> dbInfo,
                                         std::string context) {
 	if (g_network->isSimulated() && g_simulator->usableRegions > 1 && !g_simulator->quiesced) {
-		bool primaryDead = g_simulator->datacenterDead(g_simulator->primaryDcId);
+		state bool primaryDead = g_simulator->datacenterDead(g_simulator->primaryDcId);
 		bool remoteDead = g_simulator->datacenterDead(g_simulator->remoteDcId);
 
 		// FIXME: the primary and remote can both be considered dead because excludes are not handled properly by the
@@ -731,6 +732,15 @@ ACTOR Future<Void> repairDeadDatacenter(Database cx,
 			    .detail("RemoteDead", remoteDead)
 			    .detail("PrimaryDead", primaryDead);
 			g_simulator->usableRegions = 1;
+
+			state std::vector<AddressExclusion> servers =
+			    g_simulator->getExcludeDCAddresses(primaryDead ? g_simulator->primaryDcId : g_simulator->remoteDcId);
+			wait(excludeServers(cx, servers, false));
+			TraceEvent(SevWarnAlways, "DisablingFearlessConfiguration")
+			    .detail("Location", context)
+			    .detail("Stage", "LocalityExcluded")
+			    .detail("Localities", describe(servers));
+
 			wait(success(ManagementAPI::changeConfig(
 			    cx.getReference(),
 			    (primaryDead ? g_simulator->disablePrimary : g_simulator->disableRemote) + " repopulate_anti_quorum=1",
