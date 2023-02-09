9c649d78809f10a8a23c16a3e34b7a6e45a8f407
Ata E Husain Bohra <ata.husain@snowflake.com>
2023-02-08 22:51:05 -0800

    EaR: Configurable encryption framework (#9271)
    * EaR: Configurable encryption framework
    
    Description
    
    EaR implementation only supports fixed size on-disk encryption header format.
    One drawback of the scheme is, introducing a newer encryption scheme as well
    as updating header format in future may incur data migration restrictions.
    Major changes proposed in the patch includes:
    1. Flexible Encryption header format allowing the following:
     1.1. Header flags (metadata) can evolve separately from the encryption algorithm
     1.2. Specific encryption algorithm header to allow future extensions.
    2. Update the BlobCipher encryption/decryption util classes to work with newer
    encryption header format.
    3. Continue supporting multiple encryption authentication schemes such as:
    HMAC-SHA and AES-CMAC; also, supports no encryption-authentication schemes.
    4. Refactor BlobCipher unit test to enable testing of new format.
    5. Configuration knobs to control encryption header flags and algorithm
    versions.
    
    Note: 
    The on-disk header storage footprint savings due to the newer scheme is as follows:
    1. No encryption authentication: 54% smaller compared to existing implementation.
    3. AES-CMAC: 16% smaller compared to existing implementation.
    3. HMAC-SHA encryption authentication: almost same size.
    
    
    Testing
    
    BlobCipherTest
    EncryptionOpsTest

--- fdbclient/include/fdbclient/ClientKnobs.h
+++ fdbclient/include/fdbclient/ClientKnobs.h
@@ -301,6 +301,11 @@ public:
 	// key_not_found errors for. If TenantInfo::INVALID_TENANT is contained within the list then no tenants will be
 	// dropped. This Knob should ONLY be used in simulation for testing purposes
 	std::string SIMULATION_EKP_TENANT_IDS_TO_DROP;
+	bool ENABLE_CONFIGURABLE_ENCRYPTION;
+	int ENCRYPT_HEADER_FLAGS_VERSION;
+	int ENCRYPT_HEADER_AES_CTR_NO_AUTH_VERSION;
+	int ENCRYPT_HEADER_AES_CTR_AES_CMAC_AUTH_VERSION;
+	int ENCRYPT_HEADER_AES_CTR_HMAC_SHA_AUTH_VERSION;
 
 	ClientKnobs(Randomize randomize);
 	void initialize(Randomize randomize);
--- flow/EncryptUtils.cpp
+++ flow/EncryptUtils.cpp
@@ -116,10 +116,8 @@ EncryptAuthTokenAlgo getAuthTokenAlgoFromMode(const EncryptAuthTokenMode mode) {
 }
 
 EncryptAuthTokenMode getRandomAuthTokenMode() {
-	std::vector<EncryptAuthTokenMode> modes = { EncryptAuthTokenMode::ENCRYPT_HEADER_AUTH_TOKEN_MODE_NONE,
-		                                        EncryptAuthTokenMode::ENCRYPT_HEADER_AUTH_TOKEN_MODE_SINGLE };
-	int idx = deterministicRandom()->randomInt(0, modes.size());
-	return modes[idx];
+	return deterministicRandom()->coinflip() ? EncryptAuthTokenMode::ENCRYPT_HEADER_AUTH_TOKEN_MODE_NONE
+	                                         : EncryptAuthTokenMode::ENCRYPT_HEADER_AUTH_TOKEN_MODE_SINGLE;
 }
 
 EncryptAuthTokenAlgo getRandomAuthTokenAlgo() {
