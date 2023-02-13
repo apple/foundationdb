a1576a890c95646559f5ea1fa22d0b1ed60925f2
Evan Tschannen <evan.tschannen@snowflake.com>
2023-02-13 15:57:16 -0800

    fix formatting

--- fdbserver/include/fdbserver/WorkerInterface.actor.h
+++ fdbserver/include/fdbserver/WorkerInterface.actor.h
@@ -1318,7 +1318,9 @@ Future<T> ioTimeoutError(Future<T> what, double time, const char* context = null
 	}
 	Future<Void> end = lowPriorityDelay(time);
 	choose {
-		when(T t = wait(what)) { return t; }
+		when(T t = wait(what)) {
+			return t;
+		}
 		when(wait(end)) {
 			Error err = io_timeout();
 			if (!isSimulatorProcessReliable()) {
@@ -1352,7 +1354,9 @@ Future<T> ioDegradedOrTimeoutError(Future<T> what,
 	if (degradedTime < errTime) {
 		Future<Void> degradedEnd = lowPriorityDelay(degradedTime);
 		choose {
-			when(T t = wait(what)) { return t; }
+			when(T t = wait(what)) {
+				return t;
+			}
 			when(wait(degradedEnd)) {
 				CODE_PROBE(true, "TLog degraded", probe::func::deduplicate);
 				TraceEvent(SevWarnAlways, "IoDegraded").log();
@@ -1363,7 +1367,9 @@ Future<T> ioDegradedOrTimeoutError(Future<T> what,
 
 	Future<Void> end = lowPriorityDelay(errTime - degradedTime);
 	choose {
-		when(T t = wait(what)) { return t; }
+		when(T t = wait(what)) {
+			return t;
+		}
 		when(wait(end)) {
 			Error err = io_timeout();
 			if (!isSimulatorProcessReliable()) {
