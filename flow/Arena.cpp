7edec547845aad43de0f1915c0140385248bddf9
Junhyun Shim <junhyun.shim@snowflake.com>
2023-02-17 23:26:32 +0100

    Disable client memory wipe test for all XSAN builds

--- flow/Arena.cpp
+++ flow/Arena.cpp
@@ -22,6 +22,8 @@
 
 #include "flow/UnitTest.h"
 
+#include "flow/config.h"
+
 // We don't align memory properly, and we need to tell lsan about that.
 extern "C" const char* __lsan_default_options(void) {
 	return "use_unaligned=1";
@@ -998,7 +1000,7 @@ TEST_CASE("/flow/Arena/OptionalMap") {
 }
 
 TEST_CASE("/flow/Arena/Secure") {
-#ifndef ADDRESS_SANITIZER
+#ifndef USE_SANITIZER
 	// Note: Assumptions underlying this unit test are speculative.
 	//       Disable for a build configuration or entirely if deemed flaky.
 	//       As of writing, below equivalency of (buf == newBuf) holds except for ASAN builds.
@@ -1051,6 +1053,6 @@ TEST_CASE("/flow/Arena/Secure") {
 		}
 	}
 	fmt::print("Total iterations: {}, # of times check passed: {}\n", totalIters, samePtrCount);
-#endif // ADDRESS_SANITIZER
+#endif // USE_SANITIZER
 	return Void();
 }
