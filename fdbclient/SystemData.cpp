b0d6e520e4e6f021e3088493d46cb6372b7cb0f1
Chaoguang Lin <chaoguang.lin@snowflake.com>
2023-02-21 14:56:12 -0800

    Add system keys for storage parameters

--- fdbclient/SystemData.cpp
+++ fdbclient/SystemData.cpp
@@ -956,6 +956,8 @@ std::string encodeFailedLocalityKey(std::string const& locality) {
 }
 
 const KeyRef storageEngineParamsPrefix = "\xff/conf/storage_engine_params/"_sr;
+const KeyRef storageEngineParamsVersionKey = "\xff/conf/storage_engine_params"_sr;
+const KeyRangeRef storageEngineParamsKeys("\xff/conf/storage_engine_params/"_sr, "\xff/conf/storage_engine_params0"_sr);
 
 // const KeyRangeRef globalConfigKeys( "\xff/globalConfig/"_sr, "\xff/globalConfig0"_sr );
 // const KeyRef globalConfigPrefix = globalConfigKeys.begin;
