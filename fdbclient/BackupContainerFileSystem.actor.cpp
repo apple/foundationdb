ffa3467098ff53e95bd220b33790e01330abbf07
Zhe Wu <halfprice@users.noreply.github.com>
2023-02-26 12:17:07 -0800

    Explicitly using min and max restorable version from backup description in query command in stead of going throw snapshots

--- fdbclient/BackupContainerFileSystem.actor.cpp
+++ fdbclient/BackupContainerFileSystem.actor.cpp
@@ -914,6 +914,7 @@ public:
 
 		if (logsOnly) {
 			state RestorableFileSet restorableSet;
+			restorableSet.targetVersion = targetVersion;
 			state std::vector<LogFile> logFiles;
 			Version begin = beginVersion == invalidVersion ? 0 : beginVersion;
 			wait(store(logFiles, bc->listLogFiles(begin, targetVersion, false)));
@@ -927,9 +928,6 @@ public:
 		// Find the most recent keyrange snapshot through which we can restore filtered key ranges into targetVersion.
 		state std::vector<KeyspaceSnapshotFile> snapshots = wait(bc->listKeyspaceSnapshots());
 		state int i = snapshots.size() - 1;
-		if (targetVersion == earliestVersion) {
-			i = 0;
-		}
 		for (; i >= 0; i--) {
 			// The smallest version of filtered range files >= snapshot beginVersion > targetVersion
 			if (targetVersion >= 0 && snapshots[i].beginVersion > targetVersion) {
@@ -970,7 +968,7 @@ public:
 				}
 			}
 			// 'latestVersion' represents using the minimum restorable version in a snapshot.
-			restorable.targetVersion = targetVersion == earliestVersion ? maxKeyRangeVersion : targetVersion;
+			restorable.targetVersion = targetVersion == latestVersion ? maxKeyRangeVersion : targetVersion;
 			// Any version < maxKeyRangeVersion is not restorable.
 			if (restorable.targetVersion < maxKeyRangeVersion)
 				continue;
