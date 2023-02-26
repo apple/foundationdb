4fd3a9073298060f9d6b510dc428e3731652ea0a
Zhe Wu <halfprice@users.noreply.github.com>
2023-02-26 12:38:52 -0800

    fix clang build error

--- fdbbackup/backup.actor.cpp
+++ fdbbackup/backup.actor.cpp
@@ -2769,6 +2769,10 @@ ACTOR Future<Void> queryBackup(const char* name,
 		restoreVersion = v;
 	}
 
+	state int64_t totalRangeFilesSize = 0;
+	state int64_t totalLogFilesSize = 0;
+	state JsonBuilderArray rangeFilesJson;
+	state JsonBuilderArray logFilesJson;
 	try {
 		state Reference<IBackupContainer> bc = openBackupContainer(name, destinationContainer, proxy, {});
 		BackupDescription desc = wait(bc->describeBackup());
@@ -2793,10 +2797,6 @@ ACTOR Future<Void> queryBackup(const char* name,
 			return Void();
 		}
 
-		state int64_t totalRangeFilesSize = 0;
-		state int64_t totalLogFilesSize = 0;
-		state JsonBuilderArray rangeFilesJson;
-		state JsonBuilderArray logFilesJson;
 		if (snapshotVersion != invalidVersion) {
 			Optional<RestorableFileSet> fileSet = wait(bc->getRestoreSet(snapshotVersion, keyRangesFilter));
 			if (fileSet.present()) {
