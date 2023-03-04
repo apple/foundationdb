50d567b5a5f5660f6173224a28929a4435d79a7c
Steve Atherton <steve.atherton@snowflake.com>
2023-03-04 03:07:34 -0800

    Refactored some parts of database configuration to support log_engine=<name> and storage_engine=<name> and generate these when converting a DatabaseConfig JSON object to a `configure` command.  Refactored `fileconfigure` and simulation setup to use the same JSON -> configure function as the same code was copy/pasted to both places but only one has been kept up to date with new features.   Renamed Redwood to `ssd-redwood-1` canonically but the experimental name is still supported for backward compatibility.

--- fdbclient/include/fdbclient/FDBTypes.h
+++ fdbclient/include/fdbclient/FDBTypes.h
@@ -933,7 +933,7 @@ struct KeyValueStoreType {
 		case SSD_BTREE_V2:
 			return "ssd-2";
 		case SSD_REDWOOD_V1:
-			return "ssd-redwood-1-experimental";
+			return "ssd-redwood-1";
 		case SSD_ROCKSDB_V1:
 			return "ssd-rocksdb-v1";
 		case SSD_SHARDED_ROCKSDB:
@@ -946,6 +946,27 @@ struct KeyValueStoreType {
 			return "unknown";
 		}
 	}
+
+	// Convert a string to a KeyValueStoreType
+	// This is a many-to-one mapping as there are aliases for some storage engines
+	static KeyValueStoreType fromString(const std::string& str) {
+		static std::map<std::string, StoreType> names = { { "ssd-1", SSD_BTREE_V1 },
+			                                              { "ssd-2", SSD_BTREE_V2 },
+			                                              { "ssd", SSD_BTREE_V2 },
+			                                              { "redwood", SSD_REDWOOD_V1 },
+			                                              { "ssd-redwood-1", SSD_REDWOOD_V1 },
+			                                              { "ssd-redwood-1-experimental", SSD_REDWOOD_V1 },
+			                                              { "ssd-rocksdb-v1", SSD_ROCKSDB_V1 },
+			                                              { "ssd-sharded-rocksdb", SSD_SHARDED_ROCKSDB },
+			                                              { "memory", MEMORY },
+			                                              { "memory-radixtree-beta", MEMORY_RADIXTREE } };
+		auto it = names.find(str);
+		if (it == names.end()) {
+			throw unknown_storage_engine();
+		}
+		return it->second;
+	}
+
 	std::string toString() const { return getStoreTypeStr((StoreType)type); }
 
 private:
