121f526317b7b2e36b93ee90631e70c709a52d88
Jingyu Zhou <jingyuzhou@gmail.com>
2023-03-07 17:03:07 -0800

    Use changeConfig that takes a string

--- fdbserver/workloads/ClogTlog.actor.cpp
+++ fdbserver/workloads/ClogTlog.actor.cpp
@@ -130,17 +130,14 @@ struct ClogTlogWorkload : TestWorkload {
 			}
 			when(wait(timeout)) {
 				// recovery state hasn't changed in 30s, exclude the failed tlog
-				std::vector<StringRef> tokens;
 				state Optional<ConfigureAutoResult> conf;
 
 				CODE_PROBE(true, "Exclude failed tlog");
 				TraceEvent("ExcludeFailedLog")
 				    .detail("TLog", self->tlog.get())
 				    .detail("RecoveryState", self->dbInfo->get().recoveryState);
-				state std::string addr = "exclude=" + formatIpPort(self->tlog.get().ip, self->tlog.get().port);
-				tokens.push_back(StringRef(addr));
-				ConfigurationResult r =
-				    wait(ManagementAPI::changeConfig(cx.getReference(), tokens, conf, /*force=*/true));
+				std::string modes = "exclude=" + formatIpPort(self->tlog.get().ip, self->tlog.get().port);
+				ConfigurationResult r = wait(ManagementAPI::changeConfig(cx.getReference(), modes, /*force=*/true));
 				TraceEvent("ExcludeFailedLog")
 				    .detail("Result", r)
 				    .detail("ConfigIsValid", conf.present() && conf.get().isValid());
