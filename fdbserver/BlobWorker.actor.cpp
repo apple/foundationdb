dd032d7a160b2a5623a7f9977e904bae7615f323
Josh Slocum <josh.slocum@snowflake.com>
2023-03-09 09:43:27 -0600

    fixing bytesInNewDeltaFiles calculation when a snapshot file is rolled back (#9609)

--- fdbserver/BlobWorker.actor.cpp
+++ fdbserver/BlobWorker.actor.cpp
@@ -1032,6 +1032,14 @@ ACTOR Future<BlobFileIndex> writeEmptyDeltaFile(Reference<BlobWorkerData> bwData
 			if (!dfValue.present()) {
 				// Only check if not seen yet. If we get commit unknown result and then retry, we'd see our own delete
 				wait(store(dfValue, tr->get(oldDFKey)));
+				if (!dfValue.present()) {
+					TraceEvent("MissingFileEmptyWrite", bwData->id)
+					    .detail("Granule", keyRange)
+					    .detail("PrevVersion", previousVersion)
+					    .detail("CurrentVersion", currentDeltaVersion)
+					    .detail("PrevKey", oldDFKey)
+					    .detail("NewKey", newDFKey);
+				}
 				ASSERT(dfValue.present());
 			} else {
 				tr->addReadConflictRange(singleKeyRange(oldDFKey));
@@ -1966,7 +1974,9 @@ Version doGranuleRollback(Reference<GranuleMetadata> metadata,
 		cfRollbackVersion = metadata->durableDeltaVersion.get();
 		metadata->pendingSnapshotVersion = metadata->durableSnapshotVersion.get();
 		int toPop = 0;
-		bool pendingSnapshot = false;
+		// keep bytes in delta files pending here, then add back already durable delta files at end
+		metadata->bytesInNewDeltaFiles = 0;
+
 		for (auto& f : inFlightFiles) {
 			if (f.snapshot) {
 				if (f.version > rollbackVersion) {
@@ -1982,14 +1992,10 @@ Version doGranuleRollback(Reference<GranuleMetadata> metadata,
 				} else {
 					metadata->pendingSnapshotVersion = f.version;
 					metadata->bytesInNewDeltaFiles = 0;
-					pendingSnapshot = true;
 				}
 			} else {
 				if (f.version > rollbackVersion) {
 					f.future.cancel();
-					if (!pendingSnapshot) {
-						metadata->bytesInNewDeltaFiles -= f.bytes;
-					}
 					toPop++;
 					CODE_PROBE(true, "Granule rollback cancelling delta file");
 					if (BW_DEBUG) {
@@ -2001,9 +2007,7 @@ Version doGranuleRollback(Reference<GranuleMetadata> metadata,
 				} else {
 					ASSERT(f.version > cfRollbackVersion);
 					cfRollbackVersion = f.version;
-					if (pendingSnapshot) {
-						metadata->bytesInNewDeltaFiles += f.bytes;
-					}
+					metadata->bytesInNewDeltaFiles += f.bytes;
 				}
 			}
 		}
@@ -2029,6 +2033,14 @@ Version doGranuleRollback(Reference<GranuleMetadata> metadata,
 		metadata->currentDeltas = Standalone<GranuleDeltas>();
 		metadata->bufferedDeltaBytes = 0;
 		metadata->bufferedDeltaVersion = cfRollbackVersion;
+
+		// calculate number of bytes in durable delta files after last snapshot
+		// FIXME: this assumes delta file serialized size ~= logical size, which is false with compression
+		for (int i = metadata->files.deltaFiles.size() - 1;
+		     i >= 0 && metadata->files.deltaFiles[i].version > metadata->pendingSnapshotVersion;
+		     i--) {
+			metadata->bytesInNewDeltaFiles += metadata->files.deltaFiles[i].length;
+		}
 
 		// Track that this rollback happened, since we have to re-read mutations up to the rollback
 		// Add this rollback to in progress, and put all completed ones back in progress
@@ -2363,6 +2375,8 @@ ACTOR Future<Void> blobGranuleUpdateFiles(Reference<BlobWorkerData> bwData,
 				Version snapshotVersion = files.snapshotFiles.back().version;
 				for (int i = files.deltaFiles.size() - 1; i >= 0; i--) {
 					if (files.deltaFiles[i].version > snapshotVersion) {
+						// FIXME: this assumes delta file serialized size ~= logical size, which is false with
+						// compression
 						metadata->bytesInNewDeltaFiles += files.deltaFiles[i].length;
 					}
 				}
