00dfc57cc38b4d131cc17757132985b2d4a995e7
zhongxing.zhang <zhongxing.zhang@snowflake.com>
2023-03-10 13:14:21 -0800

    simplify codes

--- fdbserver/include/fdbserver/DDMovingAverageRate.h
+++ fdbserver/include/fdbserver/DDMovingAverageRate.h
@@ -27,9 +27,10 @@
 #include "flow/Deque.h"
 #include "fdbserver/Knobs.h"
 
-// Perfomed as the rolling window to calculate average moving bytes rate by DD.
+// Perfomed as the rolling window to calculate average change rates in the past <interval>
+// e.g., we may use it in "MovingData" Trace to show average moving bytes rate by DD.
 template <class T>
-struct MovingAverageRate {
+class MovingAverageRate {
 private:
 	T previous;
 	T total;
@@ -37,11 +38,10 @@ private:
 	// Actually, Deque has its own Deque::max_size = 1 << 30, We may narrow it down here.
 	// = 100MB / sizeof(pair<double, int64_t>) = 100MB / 16B ~ 2^16 ~ SHRT_MAX
 	int maxSize;
-	// True if the length of updates > maxSize
-	bool isFull;
-	Deque<std::pair<double, T>> updates; // pair{time, bytes}
+	Deque<std::pair<double, T>> updates; // pair{time, numeric}
 	double interval;
-	double startTS; // timestamp of creation
+	// Updated when initlization Or pop() due to full Deque
+	double previousPopTime;
 
 	void pop() {
 		previous += updates.front().second;
@@ -50,22 +50,18 @@ private:
 
 public:
 	MovingAverageRate()
-	  : previous(0), total(0), maxSize(SHRT_MAX), isFull(false),
-	    interval(SERVER_KNOBS->DD_TRACE_MOVE_BYTES_AVERAGE_INTERVAL), startTS(now()) {}
+	  : previous(0), total(0), maxSize(SHRT_MAX), interval(SERVER_KNOBS->DD_TRACE_MOVE_BYTES_AVERAGE_INTERVAL),
+	    previousPopTime(now()) {}
 
 	T getTotal() const { return total; }
 
 	double getAverage() {
 		while (!updates.empty() && updates.front().first < now() - interval) {
 			pop();
-			isFull = false;
 		}
 
-		if (isFull) {
-			ASSERT(updates.back().first != updates.front().first);
-			return (total - previous) / (updates.back().first - updates.front().first);
-		} else if (now() - startTS < interval) { // DD has just initialized
-			return (total - previous) / (now() - startTS);
+		if (now() - interval <= previousPopTime) { // DD just initialized Or just pop() due to full
+			return (total - previous) / (now() - previousPopTime);
 		} else {
 			return (total - previous) / interval;
 		}
@@ -77,7 +73,7 @@ public:
 		// If so, we would pop the front element from the Deque.
 		while (updates.size() > maxSize) {
 			pop();
-			isFull = true;
+			previousPopTime = now();
 		}
 	}
 };
