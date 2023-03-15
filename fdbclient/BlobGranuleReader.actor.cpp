e0b1b3bc1846f04f23394c08fdc89b9498d1b8ba
Hui Liu <hui.liu@snowflake.com>
2023-03-15 12:16:14 -0700

    Fix KeyRangeRef::isCovered() (#9675) (#9697)
    * fix KeyRangeRef::isCovered()
    
    * reproduce bug
    
    * more unit test added
    
    * fmt
    
    Co-authored-by: Zhe Wang <zhe.wang@wustl.edu>

--- fdbclient/BlobGranuleReader.actor.cpp
+++ fdbclient/BlobGranuleReader.actor.cpp
@@ -193,5 +193,46 @@ TEST_CASE("/fdbserver/blobgranule/isRangeCoveredByBlob") {
 		testAddChunkRange("key_b1"_sr, "key_b9"_sr, continuedChunks);
 		ASSERT(isRangeFullyCovered(KeyRangeRef("key_a1"_sr, "key_b9"_sr), continuedChunks));
 	}
+
+	// check functionality of isCovered()
+	{
+		std::vector<KeyRangeRef> ranges;
+		ranges.push_back(KeyRangeRef("key_a"_sr, "key_b"_sr));
+		ranges.push_back(KeyRangeRef("key_x"_sr, "key_y"_sr));
+		ASSERT(KeyRangeRef("key_x"_sr, "key_y"_sr).isCovered(ranges));
+
+		ranges.clear();
+		ranges.push_back(KeyRangeRef("key_a"_sr, "key_b"_sr));
+		ranges.push_back(KeyRangeRef("key_v"_sr, "key_y"_sr));
+		ASSERT(KeyRangeRef("key_x"_sr, "key_y"_sr).isCovered(ranges));
+
+		ranges.clear();
+		ranges.push_back(KeyRangeRef("key_a"_sr, "key_b"_sr));
+		ranges.push_back(KeyRangeRef("key_x"_sr, "key_xa"_sr));
+		ranges.push_back(KeyRangeRef("key_xa"_sr, "key_ya"_sr));
+		ASSERT(KeyRangeRef("key_x"_sr, "key_y"_sr).isCovered(ranges));
+
+		ranges.clear();
+		ranges.push_back(KeyRangeRef("key_a"_sr, "key_b"_sr));
+		ranges.push_back(KeyRangeRef("key_x"_sr, "key_xa"_sr));
+		ranges.push_back(KeyRangeRef("key_xa"_sr, "key_xb"_sr));
+		ASSERT(!KeyRangeRef("key_x"_sr, "key_y"_sr).isCovered(ranges));
+
+		ranges.clear();
+		ranges.push_back(KeyRangeRef("key_a"_sr, "key_b"_sr));
+		ranges.push_back(KeyRangeRef("key_x"_sr, "key_xa"_sr));
+		ASSERT(!KeyRangeRef("key_x"_sr, "key_y"_sr).isCovered(ranges));
+
+		ranges.clear();
+		ranges.push_back(KeyRangeRef("key_a"_sr, "key_b"_sr));
+		ranges.push_back(KeyRangeRef("key_xa"_sr, "key_y"_sr));
+		ASSERT(!KeyRangeRef("key_x"_sr, "key_y"_sr).isCovered(ranges));
+
+		ranges.clear();
+		ranges.push_back(KeyRangeRef("key_a"_sr, "key_b"_sr));
+		ranges.push_back(KeyRangeRef("key_x"_sr, "key_y"_sr));
+		ASSERT(!KeyRangeRef("key_a"_sr, "key_y"_sr).isCovered(ranges));
+	}
+
 	return Void();
 }
