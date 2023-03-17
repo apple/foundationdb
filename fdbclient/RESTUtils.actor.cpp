32e01e74fc3122b2eb656dd6f6d45cf8ab942ba3
Ata E Husain Bohra <ata.husain@snowflake.com>
2023-03-16 22:52:51 -0700

    EaR: Avoid appending `tls` to the URL (#9734)
    Description
    
    Patch proposes two changes:
    
    1. Avoid appending tls as part of URI for secure connections
    2. RefreshEKs recurring task can be skipped if there are no keys to be refreshed
    
    Testing
    
    EncryptionOps.toml
    EncryptKeyProxyTest.toml
    devRunCorrectness
    devRunCorrectnessFiltered 'Encrypt*'
    
    (cherry picked from commit c492f83bf4754faaf146c5100a2970b9651e3bba)

--- fdbclient/RESTUtils.actor.cpp
+++ fdbclient/RESTUtils.actor.cpp
@@ -143,8 +143,7 @@ ACTOR Future<RESTConnectionPool::ReusableConnection> connect_impl(Reference<REST
 	ASSERT(poolItr == connectionPool->connectionPoolMap.end() || poolItr->second.empty());
 
 	// No valid connection exists, create a new one
-	state Reference<IConnection> conn =
-	    wait(INetworkConnections::net()->connect(connectKey.first, connectKey.second, isSecure));
+	state Reference<IConnection> conn = wait(INetworkConnections::net()->connect(connectKey.first, connectKey.second));
 	wait(conn->connectHandshake());
 
 	TraceEvent("RESTTUilCreateNewConn")
