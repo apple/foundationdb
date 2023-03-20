a4438d45429d101c8caa928945c80c2e509f1867
Steve Atherton <steve.atherton@snowflake.com>
2023-03-20 02:10:16 -0700

    Refactored and simplified KeyBackedTypes to base types KeyBackedProperty, KeyBackedMap, and KeyBackedSet.  Object and BinaryValue variations are now redefined as customizations of the base types.  Added WatchableTrigger for using a key to track a last updated version, supported by all KeyBackedTypes.   KeyBackedStruct is renamed to KeyBackedClass contains a WatchableTrigger to pass to contained KeyBackTypes.

--- fdbserver/DDTeamCollection.actor.cpp
+++ fdbserver/DDTeamCollection.actor.cpp
@@ -2899,8 +2899,7 @@ public:
 		loop {
 			try {
 				tr->setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
-				auto property = metadataMap.getProperty(server->getId());
-				Optional<StorageMetadataType> metadata = wait(property.get(tr));
+				Optional<StorageMetadataType> metadata = wait(metadataMap.get(tr, server->getId()));
 				// NOTE: in upgrade testing, there may not be any metadata
 				if (metadata.present()) {
 					data.createdTime = metadata.get().createdTime;
