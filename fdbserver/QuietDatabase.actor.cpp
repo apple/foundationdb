965c47929bf86ce2ae37b1e870ca7ecc4299f705
A.J. Beamon <aj.beamon@snowflake.com>
2023-03-21 14:58:19 -0700

    Once we start repairing a dead datacenter, don't allow it to be canceled

--- fdbserver/QuietDatabase.actor.cpp
+++ fdbserver/QuietDatabase.actor.cpp
@@ -791,7 +791,7 @@ ACTOR Future<Void> reconfigureAfter(Database cx,
                                     Reference<AsyncVar<ServerDBInfo> const> dbInfo,
                                     std::string context) {
 	wait(delay(time));
-	wait(repairDeadDatacenter(cx, dbInfo, context));
+	wait(uncancellable(repairDeadDatacenter(cx, dbInfo, context)));
 	return Void();
 }
 
