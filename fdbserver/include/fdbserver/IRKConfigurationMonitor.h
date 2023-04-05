1e973eba5741b9380535249e5fba7cf7bd2ccd56
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-04-04 20:29:41 -0700

    Add comments to IRKConfigurationMonitor

--- fdbserver/include/fdbserver/IRKConfigurationMonitor.h
+++ fdbserver/include/fdbserver/IRKConfigurationMonitor.h
@@ -8,11 +8,16 @@
 #include "fdbclient/NativeAPI.actor.h"
 #include "flow/flow.h"
 
+// Responsible for monitoring the throttling-relevant
+// components of the database configuration
 class IRKConfigurationMonitor {
 public:
 	virtual ~IRKConfigurationMonitor() = default;
 	virtual bool areBlobGranulesEnabled() const = 0;
 	virtual int getStorageTeamSize() const = 0;
+
+	// Run actors to periodically refresh throttling-relevant statistics
+	// Returned Future should never be ready, but can be used to propagate errors
 	virtual Future<Void> run() = 0;
 };
 
