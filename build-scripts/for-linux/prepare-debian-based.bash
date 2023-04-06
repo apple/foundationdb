3129f01aa99a20b4ee0e328ed27678135cf53c1e
Oleg Samarin <osamarin@openintegration.inc>
2023-04-06 15:08:56 +0300

    Fixed the build environment

--- build-scripts/for-linux/prepare-debian-based.bash
+++ build-scripts/for-linux/prepare-debian-based.bash
@@ -6,7 +6,7 @@ BASE_DIR=`dirname $0`
 
 sudo apt update
 
-$BASE_DIR/prepare-debian-based-cmake.bash
+$BASE_DIR/prepare-debian-based-cmake.bash 3.24
 $BASE_DIR/prepare-debian-based-clang.sh
 $BASE_DIR/prepare-debian-based-jdk.bash
 $BASE_DIR/prepare-debian-based-libstdc.sh
@@ -20,6 +20,7 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
   ninja-build \
   pigz \
   python3 \
+  python3-pip \
   python3-sphinx \
   python3-venv \
   rpm
@@ -33,3 +34,5 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
   libmono-system-data-datasetextensions4.0-cil \
   libmono-system-runtime-serialization4.0-cil \
   libmono-system-xml-linq4.0-cil
+
+sudo pip install sphinx-bootstrap-theme
