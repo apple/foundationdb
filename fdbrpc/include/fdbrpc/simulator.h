089583b907b91e2586cf79e1aa66f52770bd7bb9
Josh Slocum <josh.slocum@snowflake.com>
2023-04-07 19:28:16 -0500

    Adding BlobFailureInjection workload (Cherry-Pick #9833 to snowflake/release-71.3) (#9919)

--- fdbrpc/include/fdbrpc/simulator.h
+++ fdbrpc/include/fdbrpc/simulator.h
@@ -126,6 +126,8 @@ public:
 	                          KillType* ktFinal = nullptr) = 0;
 	virtual bool killAll(KillType kt, bool forceKill = false, KillType* ktFinal = nullptr) = 0;
 	// virtual KillType getMachineKillState( UID zoneID ) = 0;
+	virtual void processInjectBlobFault(ProcessInfo* machine, double failureRate) = 0;
+	virtual void processStopInjectBlobFault(ProcessInfo* machine) = 0;
 	virtual bool canKillProcesses(std::vector<ProcessInfo*> const& availableProcesses,
 	                              std::vector<ProcessInfo*> const& deadProcesses,
 	                              KillType kt,
