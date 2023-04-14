4027b77e02a6928e1c4a1be96e1d460ad684b013
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-04-14 09:01:00 -0700

    Add comments to IRKThroughputQuotaCache.h

--- fdbserver/include/fdbserver/IRKThroughputQuotaCache.h
+++ fdbserver/include/fdbserver/IRKThroughputQuotaCache.h
@@ -9,14 +9,25 @@
 #include "flow/flow.h"
 #include "flow/Optional.h"
 
+// Responsible for maintaining a cache of per-tag throughput quotas
 class IRKThroughputQuotaCache {
 public:
 	virtual ~IRKThroughputQuotaCache() = default;
+
+	// Returns the cached value for the total throughput quota for
+	// the provided tag (in bytes/second)
 	virtual Optional<int64_t> getTotalQuota(TransactionTag) const = 0;
+
+	// Returns the cached value for the reserved throughput quota
+	// for the provided tag (in bytes/second)
 	virtual Optional<int64_t> getReservedQuota(TransactionTag) const = 0;
+
+	// Responsible for updating the quota cache. The returned future
+	// should never be ready, but can be used for propagating errors.
 	virtual Future<Void> run() = 0;
 };
 
+// Uses the system keyspace to populate a cache of per-tenant throughput quotas
 class RKThroughputQuotaCache : public IRKThroughputQuotaCache {
 	friend class RKThroughputQuotaCacheImpl;
 	TransactionTagMap<ThrottleApi::TagQuotaValue> quotas;
@@ -33,6 +44,7 @@ public:
 	Future<Void> run() override;
 };
 
+// Cache is updated by a test client that manually sets and removes quotas
 class MockRKThroughputQuotaCache : public IRKThroughputQuotaCache {
 	TransactionTagMap<ThrottleApi::TagQuotaValue> quotas;
 
