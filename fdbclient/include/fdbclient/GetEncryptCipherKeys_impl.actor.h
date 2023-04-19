8530b826802530518f6fbf4fb0c7c819912b4d78
Nim Wijetunga <nim.wijetunga@snowflake.com>
2023-04-19 10:21:12 -0700

    propogate encryption errors properly

--- fdbclient/include/fdbclient/GetEncryptCipherKeys_impl.actor.h
+++ fdbclient/include/fdbclient/GetEncryptCipherKeys_impl.actor.h
@@ -83,7 +83,7 @@ Future<EKPGetLatestBaseCipherKeysReply> _getUncachedLatestEncryptCipherKeys(Refe
 		EKPGetLatestBaseCipherKeysReply reply = wait(proxy.get().getLatestBaseCipherKeys.getReply(request));
 		if (reply.error.present()) {
 			TraceEvent(SevWarn, "GetLatestEncryptCipherKeysRequestFailed").error(reply.error.get());
-			throw encrypt_keys_fetch_failed();
+			throw reply.error.get();
 		}
 		return reply;
 	} catch (Error& e) {
@@ -186,8 +186,10 @@ Future<EKPGetBaseCipherKeysByIdsReply> _getUncachedEncryptCipherKeys(Reference<A
 		EKPGetBaseCipherKeysByIdsReply reply = wait(proxy.get().getBaseCipherKeysByIds.getReply(request));
 		if (reply.error.present()) {
 			TraceEvent(SevWarn, "GetEncryptCipherKeysRequestFailed").error(reply.error.get());
-			throw encrypt_keys_fetch_failed();
+			throw reply.error.get();
 		}
+		// The code below is used only during simulation to test backup/restore ability to handle encryption keys
+		// not being found for deleted tenants
 		if (g_network && g_network->isSimulated() && usageType == BlobCipherMetrics::RESTORE) {
 			std::unordered_set<int64_t> tenantIdsToDrop =
 			    parseStringToUnorderedSet<int64_t>(CLIENT_KNOBS->SIMULATION_EKP_TENANT_IDS_TO_DROP, ',');
@@ -195,7 +197,11 @@ Future<EKPGetBaseCipherKeysByIdsReply> _getUncachedEncryptCipherKeys(Reference<A
 				for (auto& baseCipherInfo : request.baseCipherInfos) {
 					if (tenantIdsToDrop.count(baseCipherInfo.domainId)) {
 						TraceEvent("GetEncryptCipherKeysSimulatedError").detail("DomainId", baseCipherInfo.domainId);
-						throw encrypt_keys_fetch_failed();
+						if (deterministicRandom()->coinflip()) {
+							throw encrypt_keys_fetch_failed();
+						} else {
+							throw encrypt_key_not_found();
+						}
 					}
 				}
 			}
