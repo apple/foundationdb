99b3ebaeea7bb0930abdb73e9551e8f08a3abb30
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-04-23 15:38:18 -0700

    Make test progress tracing in runWorkload consistent

--- fdbserver/tester.actor.cpp
+++ fdbserver/tester.actor.cpp
@@ -1105,7 +1105,7 @@ ACTOR Future<DistributedTestResults> runWorkload(Database cx,
 		}
 
 		state std::vector<Future<ErrorOr<CheckReply>>> checks;
-		TraceEvent("CheckingResults").log();
+		TraceEvent("TestCheckingResults").detail("WorkloadTitle", spec.title);
 
 		printf("checking test (%s)...\n", printable(spec.title).c_str());
 
@@ -1122,6 +1122,7 @@ ACTOR Future<DistributedTestResults> runWorkload(Database cx,
 			else
 				failure++;
 		}
+		TraceEvent("TestCheckComplete").detail("WorkloadTitle", spec.title);
 	}
 
 	if (spec.phases & TestWorkload::METRICS) {
