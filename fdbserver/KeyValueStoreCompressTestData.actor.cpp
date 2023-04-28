940430c48de15da6b7f7392028ec4bf7051a0095
neethuhaneesha <nbingi@apple.com>
2023-04-28 14:14:42 -0700

    Changing single key deletions to delete based on number of deletes instead of bytelimit. (#9884)

--- fdbserver/KeyValueStoreCompressTestData.actor.cpp
+++ fdbserver/KeyValueStoreCompressTestData.actor.cpp
@@ -53,11 +53,7 @@ struct KeyValueStoreCompressTestData final : IKeyValueStore {
 	void set(KeyValueRef keyValue, const Arena* arena = nullptr) override {
 		store->set(KeyValueRef(keyValue.key, pack(keyValue.value)), arena);
 	}
-	void clear(KeyRangeRef range,
-	           const StorageServerMetrics* storageMetrics = nullptr,
-	           const Arena* arena = nullptr) override {
-		store->clear(range, storageMetrics, arena);
-	}
+	void clear(KeyRangeRef range, const Arena* arena = nullptr) override { store->clear(range, arena); }
 	Future<Void> commit(bool sequential = false) override { return store->commit(sequential); }
 
 	Future<Optional<Value>> readValue(KeyRef key, Optional<ReadOptions> options) override {
