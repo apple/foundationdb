06762c770ea814176bd9fe15d53b6f5ad266a23f
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-04-29 22:13:03 -0700

    Rename count to cost in many places in TransactionTagCounter.actor.cpp

--- fdbserver/TransactionTagCounter.cpp
+++ fdbserver/TransactionTagCounter.cpp
@@ -27,18 +27,18 @@ namespace {
 
 class TopKTags {
 public:
-	struct TagAndCount {
+	struct TagAndCost {
 		TransactionTag tag;
-		double count;
-		bool operator<(TagAndCount const& other) const { return count < other.count; }
-		explicit TagAndCount(TransactionTag tag, double count) : tag(tag), count(count) {}
+		double cost;
+		bool operator<(TagAndCost const& other) const { return cost < other.cost; }
+		explicit TagAndCost(TransactionTag tag, double cost) : tag(tag), cost(cost) {}
 	};
 
 private:
 	// Because the number of tracked is expected to be small, they can be tracked
 	// in a simple vector. If the number of tracked tags increases, a more sophisticated
 	// data structure will be required.
-	std::vector<TagAndCount> topTags;
+	std::vector<TagAndCost> topTags;
 	int limit;
 
 public:
@@ -47,30 +47,30 @@ public:
 		topTags.reserve(limit);
 	}
 
-	void incrementCount(TransactionTag tag, double previousCount, double increase) {
+	void incrementCost(TransactionTag tag, double previousCost, double increase) {
 		auto iter = std::find_if(topTags.begin(), topTags.end(), [tag](const auto& tc) { return tc.tag == tag; });
 		if (iter != topTags.end()) {
-			ASSERT_EQ(previousCount, iter->count);
-			iter->count += increase;
+			ASSERT_EQ(previousCost, iter->cost);
+			iter->cost += increase;
 		} else if (topTags.size() < limit) {
-			ASSERT_EQ(previousCount, 0);
+			ASSERT_EQ(previousCost, 0);
 			topTags.emplace_back(tag, increase);
 		} else {
 			auto toReplace = std::min_element(topTags.begin(), topTags.end());
-			ASSERT_GE(toReplace->count, previousCount);
-			if (toReplace->count < previousCount + increase) {
+			ASSERT_GE(toReplace->cost, previousCost);
+			if (toReplace->cost < previousCost + increase) {
 				toReplace->tag = tag;
-				toReplace->count = previousCount + increase;
+				toReplace->cost = previousCost + increase;
 			}
 		}
 	}
 
-	std::vector<StorageQueuingMetricsReply::TagInfo> getBusiestTags(double elapsed, double totalCount) const {
+	std::vector<StorageQueuingMetricsReply::TagInfo> getBusiestTags(double elapsed, double totalCost) const {
 		std::vector<StorageQueuingMetricsReply::TagInfo> result;
-		for (auto const& tagAndCounter : topTags) {
-			auto rate = tagAndCounter.count / elapsed;
+		for (auto const& tagAndCost : topTags) {
+			auto rate = tagAndCost.cost / elapsed;
 			if (rate > SERVER_KNOBS->MIN_TAG_READ_PAGES_RATE * CLIENT_KNOBS->TAG_THROTTLING_PAGE_SIZE) {
-				result.emplace_back(tagAndCounter.tag, rate, tagAndCounter.count / totalCount);
+				result.emplace_back(tagAndCost.tag, rate, std::min(1.0, tagAndCost.cost / totalCost));
 			}
 		}
 		return result;
@@ -83,7 +83,7 @@ public:
 
 class TransactionTagCounterImpl {
 	UID thisServerID;
-	TransactionTagMap<double> intervalCounts;
+	TransactionTagMap<double> intervalCosts;
 	double intervalTotalCost = 0;
 	TopKTags topTags;
 	double intervalStart = 0;
@@ -97,15 +97,15 @@ public:
 	    busiestReadTagEventHolder(makeReference<EventCacheHolder>(thisServerID.toString() + "/BusiestReadTag")) {}
 
 	void addRequest(Optional<TagSet> const& tags, Optional<TenantGroupName> const& tenantGroup, int64_t bytes) {
+		auto const cost = getReadOperationCost(bytes);
+		intervalTotalCost += cost;
 		if (tags.present()) {
 			CODE_PROBE(true, "Tracking transaction tag in counter");
-			auto const cost = getReadOperationCost(bytes) / CLIENT_KNOBS->READ_TAG_SAMPLE_RATE;
 			for (auto const& tag : tags.get()) {
-				double& count = intervalCounts[TransactionTag(tag, tags.get().getArena())];
-				topTags.incrementCount(tag, count, cost);
-				count += cost;
+				double& tagCost = intervalCosts[TransactionTag(tag, tags.get().getArena())];
+				topTags.incrementCost(tag, tagCost, cost / CLIENT_KNOBS->READ_TAG_SAMPLE_RATE);
+				tagCost += cost / CLIENT_KNOBS->READ_TAG_SAMPLE_RATE;
 			}
-			intervalTotalCost += cost;
 		}
 	}
 
@@ -140,7 +140,7 @@ public:
 			}
 		}
 
-		intervalCounts.clear();
+		intervalCosts.clear();
 		intervalTotalCost = 0;
 		topTags.clear();
 		intervalStart = now();
@@ -176,7 +176,7 @@ TEST_CASE("/TransactionTagCounter/TopKTags") {
 	    std::max<double>(1.0, 2 * SERVER_KNOBS->MIN_TAG_READ_PAGES_RATE * CLIENT_KNOBS->TAG_THROTTLING_PAGE_SIZE);
 
 	ASSERT_EQ(topTags.getBusiestTags(1.0, 0).size(), 0);
-	topTags.incrementCount("a"_sr, 0, 1 * costMultiplier);
+	topTags.incrementCost("a"_sr, 0, 1 * costMultiplier);
 	{
 		auto const busiestTags = topTags.getBusiestTags(1.0, 1 * costMultiplier);
 		ASSERT_EQ(busiestTags.size(), 1);
@@ -185,8 +185,8 @@ TEST_CASE("/TransactionTagCounter/TopKTags") {
 		                        [](auto const& tagInfo) { return tagInfo.tag == "a"_sr; }),
 		          1);
 	}
-	topTags.incrementCount("b"_sr, 0, 2 * costMultiplier);
-	topTags.incrementCount("c"_sr, 0, 3 * costMultiplier);
+	topTags.incrementCost("b"_sr, 0, 2 * costMultiplier);
+	topTags.incrementCost("c"_sr, 0, 3 * costMultiplier);
 	{
 		auto busiestTags = topTags.getBusiestTags(1.0, 6 * costMultiplier);
 		ASSERT_EQ(busiestTags.size(), 2);
@@ -203,7 +203,7 @@ TEST_CASE("/TransactionTagCounter/TopKTags") {
 		                        [](auto const& tagInfo) { return tagInfo.tag == "c"_sr; }),
 		          1);
 	}
-	topTags.incrementCount("a"_sr, 1 * costMultiplier, 3 * costMultiplier);
+	topTags.incrementCost("a"_sr, 1 * costMultiplier, 3 * costMultiplier);
 	{
 		auto busiestTags = topTags.getBusiestTags(1.0, 9 * costMultiplier);
 		ASSERT_EQ(busiestTags.size(), 2);
