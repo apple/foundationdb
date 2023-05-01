cd05fc646db011a2ac17d9f87e4c09f991a0b13a
Dan Adkins <dan.adkins@snowflake.com>
2023-05-01 12:31:17 -0700

    Disable test failure entirely, since it will retry and fail again.
    Change the existing log message to a warning, so we at least have
    some indiciation in the logs that we hit the biased shard estimate
    issue.

--- fdbserver/ConsistencyScan.actor.cpp
+++ fdbserver/ConsistencyScan.actor.cpp
@@ -978,7 +978,7 @@ ACTOR Future<Void> checkDataConsistency(Database cx,
 			// normal distribution
 			if (sampledKeysWithProb > 30 && estimateError > failErrorNumStdDev * stdDev) {
 				double numStdDev = estimateError / sqrt(shardVariance);
-				TraceEvent("ConsistencyCheck_InaccurateShardEstimate")
+				TraceEvent(SevWarn, "ConsistencyCheck_InaccurateShardEstimate")
 				    .detail("Min", shardBounds.min.bytes)
 				    .detail("Max", shardBounds.max.bytes)
 				    .detail("Estimate", sampledBytes)
@@ -993,11 +993,12 @@ ACTOR Future<Void> checkDataConsistency(Database cx,
 				    .detail("NumSampledKeysWithProb", sampledKeysWithProb);
 
 				// NOTE: Shard sampling is known to be biased.
-				// Downgrade this to a warning until we have a proper solution.
-				testFailure(format("Shard size is more than %f std dev from estimate", failErrorNumStdDev),
-				            performQuiescentChecks,
-				            success,
-				            /*failureIsError*/ false);
+				// Disable this test failure until we have a proper solution.
+				//
+				// testFailure(format("Shard size is more than %f std dev from estimate", failErrorNumStdDev),
+				//             performQuiescentChecks,
+				//             success,
+				//             false);
 			}
 
 			// In a quiescent database, check that the (estimated) size of the shard is within permitted bounds
