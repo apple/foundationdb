6254525c8fb35fc6fd64611c0c923f307d61714b
Steve Atherton <steve.atherton@snowflake.com>
2023-05-03 10:39:13 -0700

    Move AsyncFileWriteChecker to right above SimpleFile in the file stack in simulation, which is analogous to where it is created in production and prevents false positive errors caused by stacking it on top of AsyncFileNonDurable multiple times for different users of the same file.

--- fdbrpc/include/fdbrpc/AsyncFileWriteChecker.h
+++ fdbrpc/include/fdbrpc/AsyncFileWriteChecker.h
@@ -35,12 +35,7 @@ public:
 		// Lambda must hold a reference to this to keep it alive until after the read
 		auto self = Reference<AsyncFileWriteChecker>::addRef(this);
 		return map(m_f->read(data, length, offset), [self, data, offset](int r) {
-			// Do not check the checksum if self is the sole owner of the reference because the end user has dropped the
-			// file handle and may already have re-opened the file and written to it before this read completed so our
-			// stored checksum can be wrong.
-			if (!self->isSoleOwner()) {
-				self->updateChecksumHistory(false, offset, r, (uint8_t*)data);
-			}
+			self->updateChecksumHistory(false, offset, r, (uint8_t*)data);
 			return r;
 		});
 	}
@@ -48,12 +43,7 @@ public:
 		// Lambda must hold a reference to this to keep it alive until after the read
 		auto self = Reference<AsyncFileWriteChecker>::addRef(this);
 		return map(m_f->readZeroCopy(data, length, offset), [self, data, length, offset](Void r) {
-			// Do not check the checksum if self is the sole owner of the reference because the end user has dropped the
-			// file handle and may already have re-opened the file and written to it before this read completed so our
-			// stored checksum can be wrong.
-			if (!self->isSoleOwner()) {
-				self->updateChecksumHistory(false, offset, *length, (uint8_t*)data);
-			}
+			self->updateChecksumHistory(false, offset, *length, (uint8_t*)data);
 			return r;
 		});
 	}
