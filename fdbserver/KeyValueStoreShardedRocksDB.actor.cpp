e2c2cc15fe8552563d31d749682166e77647e756
Yao Xiao <yxiao6@apple.com>
2023-05-03 13:20:38 -0700

    Log physical shard size in KVS

--- fdbserver/KeyValueStoreShardedRocksDB.actor.cpp
+++ fdbserver/KeyValueStoreShardedRocksDB.actor.cpp
@@ -866,7 +866,7 @@ public:
 		try {
 			wait(openFuture);
 			loop {
-				wait(delay(SERVER_KNOBS->ROCKSDB_METRICS_DELAY));
+				wait(delay(SERVER_KNOBS->ROCKSDB_CF_METRICS_DELAY));
 				if (rState->closing) {
 					break;
 				}
@@ -878,6 +878,13 @@ public:
 					if (!shard->initialized()) {
 						continue;
 					}
+					uint64_t liveDataSize = 0;
+					ASSERT(shard->db->GetIntProperty(
+					    shard->cf, rocksdb::DB::Properties::kEstimateLiveDataSize, &liveDataSize));
+					TraceEvent(SevInfo, "PhysicalShardCFSize")
+					    .detail("ShardId", id)
+					    .detail("LiveDataSize", liveDataSize);
+
 					std::string propValue = "";
 					ASSERT(shard->db->GetProperty(shard->cf, rocksdb::DB::Properties::kCFStats, &propValue));
 					TraceEvent(SevInfo, "PhysicalShardCFStats")
