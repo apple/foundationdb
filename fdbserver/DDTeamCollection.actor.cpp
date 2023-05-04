e139f5bf90d9f1d220a514e5daf8487ef7e9f33d
Xiaoxi Wang <xiaoxi.wang@snowflake.com>
2023-05-04 12:31:30 -0700

    Correctly handle buggify errors in MGSWaitStorageMetrics

--- fdbserver/DDTeamCollection.actor.cpp
+++ fdbserver/DDTeamCollection.actor.cpp
@@ -3784,7 +3784,7 @@ Future<Void> DDTeamCollection::readStorageWiggleMap() {
 }
 
 Future<Void> DDTeamCollection::updateStorageMetadata(TCServerInfo* server) {
-	if(db->isMocked())
+	if (db->isMocked())
 		return Never();
 	return DDTeamCollectionImpl::updateStorageMetadata(this, server);
 }
