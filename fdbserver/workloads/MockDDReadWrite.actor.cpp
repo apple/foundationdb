7b8463178398ba2d0612f37b8b237b77ef813e7b
Xiaoxi Wang <xiaoxi.wang@snowflake.com>
2023-05-05 13:59:14 -0700

    addStoragePerProcess method. Add testClass for mock dd test.

--- fdbserver/workloads/MockDDReadWrite.actor.cpp
+++ fdbserver/workloads/MockDDReadWrite.actor.cpp
@@ -44,6 +44,7 @@ public:
 		MockDDTestWorkload::setup(cx);
 		// populate sharedMgs before run DD
 		populateMgs();
+		sharedMgs->addStoragePerProcess();
 		mock = makeReference<DDMockTxnProcessor>(sharedMgs);
 		return Void();
 	}
