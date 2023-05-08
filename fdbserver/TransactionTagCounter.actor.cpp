d47f3e0dfc3cb8121ad003e3844fc63d53c987d4
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-05-07 19:35:40 -0700

    Modify TransactionTagCounter to only compute top k tags in startNewInterval

--- fdbserver/TransactionTagCounter.actor.cpp
+++ fdbserver/TransactionTagCounter.actor.cpp
@@ -24,89 +24,42 @@
 #include "flow/Trace.h"
 #include "flow/actorcompiler.h"
 
-namespace {
-
-class TopKTags {
-public:
-	struct TagAndCost {
-		TransactionTag tag;
-		double cost;
-		bool operator<(TagAndCost const& other) const { return cost < other.cost; }
-		explicit TagAndCost(TransactionTag tag, double cost) : tag(tag), cost(cost) {}
-	};
-
-private:
-	// Because the number of tracked is expected to be small, they can be tracked
-	// in a simple vector. If the number of tracked tags increases, a more sophisticated
-	// data structure will be required.
-	std::vector<TagAndCost> topTags;
-	int maxTagsTracked;
-	double minRateTracked;
-
-public:
-	explicit TopKTags(int maxTagsTracked, double minRateTracked)
-	  : maxTagsTracked(maxTagsTracked), minRateTracked(minRateTracked) {
-		ASSERT_GT(maxTagsTracked, 0);
-		topTags.reserve(maxTagsTracked);
-	}
-
-	void incrementCost(TransactionTag tag, double previousCost, double increase) {
-		TraceEvent("HERE")
-		    .detail("Tag", printable(tag))
-		    .detail("PreviousCost", previousCost)
-		    .detail("Increase", increase);
-		auto iter = std::find_if(topTags.begin(), topTags.end(), [tag](const auto& tc) { return tc.tag == tag; });
-		if (iter != topTags.end()) {
-			ASSERT_EQ(previousCost, iter->cost);
-			iter->cost += increase;
-		} else if (topTags.size() < maxTagsTracked) {
-			ASSERT_EQ(previousCost, 0);
-			topTags.emplace_back(tag, increase);
-		} else {
-			auto toReplace = std::min_element(topTags.begin(), topTags.end());
-			ASSERT_GE(toReplace->cost, previousCost);
-			if (toReplace->cost < previousCost + increase) {
-				toReplace->tag = tag;
-				toReplace->cost = previousCost + increase;
-			}
-		}
-	}
-
-	std::vector<BusyTagInfo> getBusiestTags(double elapsed, double totalCost) const {
-		std::vector<BusyTagInfo> result;
-		for (auto const& tagAndCost : topTags) {
-			auto rate = tagAndCost.cost / elapsed;
-			if (rate > minRateTracked) {
-				result.emplace_back(tagAndCost.tag, rate, std::min(1.0, tagAndCost.cost / totalCost));
-			}
-		}
-		return result;
-	}
-
-	void clear() { topTags.clear(); }
-};
-
-} // namespace
-
 class TransactionTagCounterImpl {
 	UID thisServerID;
 	TransactionTagMap<double> intervalCosts;
 	double intervalTotalCost = 0;
-	TopKTags topTags;
 	double intervalStart = 0;
+	int maxTagsTracked;
+	double minRateTracked;
 
 	std::vector<BusyTagInfo> previousBusiestTags;
 	Reference<EventCacheHolder> busiestReadTagEventHolder;
 
-	void updateTagCost(TransactionTag tag, double additionalCost) {
-		double& tagCost = intervalCosts[tag];
-		topTags.incrementCost(tag, tagCost, additionalCost);
-		tagCost += additionalCost;
+	std::vector<BusyTagInfo> getBusiestTagsFromLastInterval(double elapsed) const {
+		std::priority_queue<BusyTagInfo, std::vector<BusyTagInfo>, std::greater<BusyTagInfo>> topKTags;
+		for (auto const& [tag, cost] : intervalCosts) {
+			auto const rate = cost / elapsed;
+			auto const fractionalBusyness = std::min(1.0, cost / intervalTotalCost);
+			if (rate < minRateTracked) {
+				continue;
+			} else if (topKTags.size() < maxTagsTracked) {
+				topKTags.emplace(tag, rate, fractionalBusyness);
+			} else if (topKTags.top().rate < rate) {
+				topKTags.pop();
+				topKTags.emplace(tag, rate, fractionalBusyness);
+			}
+		}
+		std::vector<BusyTagInfo> result;
+		while (!topKTags.empty()) {
+			result.push_back(std::move(topKTags.top()));
+			topKTags.pop();
+		}
+		return result;
 	}
 
 public:
 	TransactionTagCounterImpl(UID thisServerID, int maxTagsTracked, double minRateTracked)
-	  : thisServerID(thisServerID), topTags(maxTagsTracked, minRateTracked),
+	  : thisServerID(thisServerID), maxTagsTracked(maxTagsTracked), minRateTracked(minRateTracked),
 	    busiestReadTagEventHolder(makeReference<EventCacheHolder>(thisServerID.toString() + "/BusiestReadTag")) {}
 
 	void addRequest(Optional<TagSet> const& tags, Optional<TenantGroupName> const& tenantGroup, int64_t bytes) {
@@ -115,12 +68,12 @@ public:
 		if (tags.present()) {
 			for (auto const& tag : tags.get()) {
 				CODE_PROBE(true, "Tracking transaction tag in TransactionTagCounter");
-				updateTagCost(TransactionTag(tag, tags.get().getArena()), cost / CLIENT_KNOBS->READ_TAG_SAMPLE_RATE);
+				intervalCosts[TransactionTag(tag, tags.get().getArena())] += cost / CLIENT_KNOBS->READ_TAG_SAMPLE_RATE;
 			}
 		}
 		if (tenantGroup.present()) {
 			CODE_PROBE(true, "Tracking tenant group in TransactionTagCounter");
-			updateTagCost(tenantGroup.get(), cost);
+			intervalCosts[tenantGroup.get()] += cost;
 		}
 	}
 
@@ -128,7 +81,7 @@ public:
 		double elapsed = now() - intervalStart;
 		previousBusiestTags.clear();
 		if (intervalStart > 0 && CLIENT_KNOBS->READ_TAG_SAMPLE_RATE > 0 && elapsed > 0) {
-			previousBusiestTags = topTags.getBusiestTags(elapsed, intervalTotalCost);
+			previousBusiestTags = getBusiestTagsFromLastInterval(elapsed);
 
 			// For status, report the busiest tag:
 			if (previousBusiestTags.empty()) {
@@ -157,7 +110,6 @@ public:
 
 		intervalCosts.clear();
 		intervalTotalCost = 0;
-		topTags.clear();
 		intervalStart = now();
 	}
 
@@ -191,38 +143,6 @@ bool containsTag(std::vector<BusyTagInfo> const& busyTags, TransactionTagRef tag
 }
 
 } // namespace
-
-TEST_CASE("/TransactionTagCounter/TopKTags") {
-	TopKTags topTags(2, 0.0);
-
-	ASSERT_EQ(topTags.getBusiestTags(1.0, 0).size(), 0);
-	topTags.incrementCost("a"_sr, 0, 2.0);
-	{
-		auto const busiestTags = topTags.getBusiestTags(1.0, 1.0);
-		ASSERT_EQ(busiestTags.size(), 1);
-		ASSERT(containsTag(busiestTags, "a"_sr));
-	}
-	topTags.incrementCost("b"_sr, 0, 2.0);
-	topTags.incrementCost("c"_sr, 0, 3.0);
-	{
-		auto busiestTags = topTags.getBusiestTags(1.0, 6.0);
-		ASSERT_EQ(busiestTags.size(), 2);
-		ASSERT(!containsTag(busiestTags, "a"_sr));
-		ASSERT(containsTag(busiestTags, "b"_sr));
-		ASSERT(containsTag(busiestTags, "c"_sr));
-	}
-	topTags.incrementCost("a"_sr, 1.0, 3.0);
-	{
-		auto busiestTags = topTags.getBusiestTags(1.0, 9.0);
-		ASSERT_EQ(busiestTags.size(), 2);
-		ASSERT(containsTag(busiestTags, "a"_sr));
-		ASSERT(!containsTag(busiestTags, "b"_sr));
-		ASSERT(containsTag(busiestTags, "c"_sr));
-	}
-	topTags.clear();
-	ASSERT_EQ(topTags.getBusiestTags(1.0, 0).size(), 0);
-	return Void();
-}
 
 TEST_CASE("/fdbserver/TransactionTagCounter/IgnoreBeyondMaxTags") {
 	state TransactionTagCounter counter(
