4a7df69a590004769a5678d2e65d449a2e0f34e8
Xiaoxi Wang <xiaoxi.wang@snowflake.com>
2023-05-14 20:49:28 -0700

    Fix notifyBytes bug: infinite vector item creation. (version 2)

--- fdbserver/storageserver.actor.cpp
+++ fdbserver/storageserver.actor.cpp
@@ -11773,8 +11773,8 @@ ACTOR Future<Void> waitMetrics(StorageServerMetrics* self, WaitMetricsRequest re
 	state Error error = success();
 	state bool timedout = false;
 
-	// state UID metricReqId = deterministicRandom()->randomUniqueID();
-	DisabledTraceEvent(SevDebug, "WaitMetrics", metricReqId)
+	state UID metricReqId = deterministicRandom()->randomUniqueID();
+	TraceEvent(SevDebug, "WaitMetrics", metricReqId)
 	    .detail("Keys", req.keys)
 	    .detail("Metrics", metrics.toString())
 	    .detail("ReqMin", req.min.toString())
@@ -11845,7 +11845,7 @@ ACTOR Future<Void> waitMetrics(StorageServerMetrics* self, WaitMetricsRequest re
 
 		wait(delay(0)); // prevent iterator invalidation of functions sending changes
 	}
-
+	// fmt::print("PopWaitMetricsMap {}\n", req.keys.toString());
 	auto rs = self->waitMetricsMap.modify(req.keys);
 	for (auto i = rs.begin(); i != rs.end(); ++i) {
 		auto& x = i->value();
