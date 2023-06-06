d4d3e562af25c29eeb9b16775578b498e5f55083
Josh Slocum <josh.slocum@snowflake.com>
2023-06-06 08:31:37 -0500

    this transaction could block if system priority not set, which would block BM startup

--- fdbserver/BlobManager.actor.cpp
+++ fdbserver/BlobManager.actor.cpp
@@ -5710,6 +5710,7 @@ ACTOR Future<UID> fetchClusterId(Database db) {
 	state Reference<ReadYourWritesTransaction> tr = makeReference<ReadYourWritesTransaction>(db);
 	loop {
 		try {
+			tr->setOption(FDBTransactionOptions::PRIORITY_SYSTEM_IMMEDIATE);
 			tr->setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
 			tr->setOption(FDBTransactionOptions::LOCK_AWARE);
 			Optional<Value> clusterIdVal = wait(tr->get(clusterIdKey));
