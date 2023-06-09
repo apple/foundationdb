25ab5770f4821f9fb343b487c3fd33ee0be5e3bb
A.J. Beamon <aj.beamon@snowflake.com>
2023-06-09 08:57:48 -0700

    Make some of the extra unused tenants in simulation locked. Add a key to some of them if we are using a default tenant.

--- fdbserver/tester.actor.cpp
+++ fdbserver/tester.actor.cpp
@@ -874,6 +874,24 @@ ACTOR Future<Void> testerServerCore(TesterInterface interf,
 	}
 }
 
+ACTOR Future<Void> clearTenantData(Database db, Reference<Tenant> tenant) {
+	state Transaction tr(db, tenant);
+	loop {
+		try {
+			tr.debugTransaction(debugRandom()->randomUniqueID());
+			tr.setOption(FDBTransactionOptions::LOCK_AWARE);
+			tr.clear(normalKeys);
+			wait(tr.commit());
+			return Void();
+		} catch (Error& e) {
+			TraceEvent(SevWarn, "TesterClearingTenantDataError", tr.trState->readOptions.get().debugID.get())
+			    .error(e)
+			    .detail("Tenant", tenant);
+			wait(tr.onError(e));
+		}
+	}
+}
+
 ACTOR Future<Void> clearData(Database cx, Optional<TenantName> defaultTenant) {
 	state Transaction tr(cx);
 
@@ -898,44 +916,62 @@ ACTOR Future<Void> clearData(Database cx, Optional<TenantName> defaultTenant) {
 		}
 	}
 
-	tr = Transaction(cx);
 	loop {
-		try {
-			tr.debugTransaction(debugRandom()->randomUniqueID());
-			ASSERT(tr.trState->readOptions.present() && tr.trState->readOptions.get().debugID.present());
-			TraceEvent("TesterClearingTenantsStart", tr.trState->readOptions.get().debugID.get());
-			state KeyBackedRangeResult<std::pair<int64_t, TenantMapEntry>> tenants =
-			    wait(TenantMetadata::tenantMap().getRange(&tr, {}, {}, 1000));
+		tr = Transaction(cx);
+		TraceEvent("TesterClearingTenantsStart");
+		state KeyBackedRangeResult<std::pair<int64_t, TenantMapEntry>> tenants =
+		    wait(TenantMetadata::tenantMap().getRange(cx.getReference(), {}, {}, 1000));
 
-			TraceEvent("TesterClearingTenantsDeletingBatch", tr.trState->readOptions.get().debugID.get())
-			    .detail("FirstTenant", tenants.results.empty() ? "<none>"_sr : tenants.results[0].second.tenantName)
-			    .detail("BatchSize", tenants.results.size());
+		TraceEvent("TesterClearingTenantsDeletingBatchData")
+		    .detail("FirstTenant", tenants.results.empty() ? "<none>"_sr : tenants.results[0].second.tenantName)
+		    .detail("BatchSize", tenants.results.size());
 
-			std::vector<Future<Void>> deleteFutures;
-			for (auto const& [id, entry] : tenants.results) {
-				if (entry.tenantName != defaultTenant) {
-					deleteFutures.push_back(TenantAPI::deleteTenantTransaction(&tr, id));
+		std::vector<Future<Void>> deleteDataFutures;
+		for (auto const& [id, entry] : tenants.results) {
+			if (entry.tenantName != defaultTenant) {
+				deleteDataFutures.push_back(clearTenantData(cx, makeReference<Tenant>(id)));
+			}
+		}
+
+		wait(waitForAll(deleteDataFutures));
+
+		loop {
+			try {
+				tr.setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
+				tr.debugTransaction(debugRandom()->randomUniqueID());
+				ASSERT(tr.trState->readOptions.present() && tr.trState->readOptions.get().debugID.present());
+
+				TraceEvent("TesterClearingTenantsDeletingBatch", tr.trState->readOptions.get().debugID.get())
+				    .detail("FirstTenant", tenants.results.empty() ? "<none>"_sr : tenants.results[0].second.tenantName)
+				    .detail("BatchSize", tenants.results.size());
+
+				std::vector<Future<Void>> deleteFutures;
+				for (auto const& [id, entry] : tenants.results) {
+					if (entry.tenantName != defaultTenant) {
+						deleteFutures.push_back(TenantAPI::deleteTenantTransaction(&tr, id));
+					}
 				}
-			}
 
-			CODE_PROBE(deleteFutures.size() > 0, "Clearing tenants after test");
+				CODE_PROBE(deleteFutures.size() > 0, "Clearing tenants after test");
 
-			wait(waitForAll(deleteFutures));
-			wait(tr.commit());
+				wait(waitForAll(deleteFutures));
+				wait(tr.commit());
 
-			TraceEvent("TesterClearingTenantsDeletedBatch", tr.trState->readOptions.get().debugID.get())
-			    .detail("FirstTenant", tenants.results.empty() ? "<none>"_sr : tenants.results[0].second.tenantName)
-			    .detail("BatchSize", tenants.results.size());
+				TraceEvent("TesterClearingTenantsDeletedBatch", tr.trState->readOptions.get().debugID.get())
+				    .detail("FirstTenant", tenants.results.empty() ? "<none>"_sr : tenants.results[0].second.tenantName)
+				    .detail("BatchSize", tenants.results.size());
 
-			if (!tenants.more) {
-				TraceEvent("TesterClearingTenantsComplete", tr.trState->readOptions.get().debugID.get())
-				    .detail("AtVersion", tr.getCommittedVersion());
 				break;
+			} catch (Error& e) {
+				TraceEvent(SevWarn, "TesterClearingTenantsError", tr.trState->readOptions.get().debugID.get()).error(e);
+				wait(tr.onError(e));
 			}
-			tr.reset();
-		} catch (Error& e) {
-			TraceEvent(SevWarn, "TesterClearingTenantsError", tr.trState->readOptions.get().debugID.get()).error(e);
-			wait(tr.onError(e));
+		}
+
+		if (!tenants.more) {
+			TraceEvent("TesterClearingTenantsComplete", tr.trState->readOptions.get().debugID.get())
+			    .detail("AtVersion", tr.getCommittedVersion());
+			break;
 		}
 	}
 
@@ -1904,6 +1940,20 @@ ACTOR Future<Void> disableConnectionFailuresAfter(double seconds, std::string co
 	return Void();
 }
 
+ACTOR Future<Void> writeTenantKey(Database db, Reference<Tenant> tenant) {
+	state ReadYourWritesTransaction tr(db, tenant);
+	loop {
+		try {
+			tr.setOption(FDBTransactionOptions::LOCK_AWARE);
+			tr.set("sampleKey"_sr, "sampleValue"_sr);
+			wait(tr.commit());
+			return Void();
+		} catch (Error& e) {
+			wait(tr.onError(e));
+		}
+	}
+}
+
 /**
  * \brief Test orchestrator: sends test specification to testers in the right order and collects the results.
  *
@@ -1929,7 +1979,7 @@ ACTOR Future<Void> runTests(Reference<AsyncVar<Optional<struct ClusterController
                             StringRef startingConfiguration,
                             LocalityData locality,
                             Optional<TenantName> defaultTenant,
-                            Standalone<VectorRef<TenantNameRef>> tenantsToCreate,
+                            Standalone<VectorRef<TenantNameRef>> extraTenants,
                             bool restartingTest) {
 	state Database cx;
 	state Reference<AsyncVar<ServerDBInfo>> dbInfo(new AsyncVar<ServerDBInfo>);
@@ -2066,17 +2116,55 @@ ACTOR Future<Void> runTests(Reference<AsyncVar<Optional<struct ClusterController
 	}
 
 	if (useDB) {
-		std::vector<Future<Void>> tenantFutures;
-		for (auto tenant : tenantsToCreate) {
+		if (defaultTenant.present() && !restartingTest) {
 			TenantMapEntry entry;
 			if (deterministicRandom()->coinflip()) {
 				entry.tenantGroup = "TestTenantGroup"_sr;
 			}
-			TraceEvent("CreatingTenant").detail("Tenant", tenant).detail("TenantGroup", entry.tenantGroup);
+
+			TraceEvent("CreatingDefaultTenant")
+			    .detail("Tenant", defaultTenant)
+			    .detail("TenantGroup", entry.tenantGroup);
+
+			wait(success(TenantAPI::createTenant(cx.getReference(), defaultTenant.get(), entry)));
+		}
+
+		std::vector<Future<Void>> tenantFutures;
+		for (auto tenant : extraTenants) {
+			TenantMapEntry entry;
+			if (deterministicRandom()->coinflip()) {
+				entry.tenantGroup = "TestTenantGroup"_sr;
+			}
+			if (deterministicRandom()->coinflip()) {
+				entry.tenantLockState = deterministicRandom()->coinflip() ? TenantAPI::TenantLockState::READ_ONLY
+				                                                          : TenantAPI::TenantLockState::LOCKED;
+				entry.tenantLockId = deterministicRandom()->randomUniqueID();
+			}
+
+			TraceEvent("CreatingExtraTenant")
+			    .detail("Tenant", tenant)
+			    .detail("TenantGroup", entry.tenantGroup)
+			    .detail("LockState", TenantAPI::tenantLockStateToString(entry.tenantLockState));
+
 			tenantFutures.push_back(success(TenantAPI::createTenant(cx.getReference(), tenant, entry)));
 		}
 
 		wait(waitForAll(tenantFutures));
+
+		// Only write keys to extra tenants if we have a default tenant. Otherwise, the extra keys could interfere with
+		// the existing workload.
+		if (defaultTenant.present()) {
+			std::vector<Future<Void>> tenantWriteFutures;
+			for (auto tenant : extraTenants) {
+				if (deterministicRandom()->coinflip()) {
+					TraceEvent("WritingKeyToExtraTenant").detail("Tenant", tenant);
+					tenantWriteFutures.push_back(writeTenantKey(cx, makeReference<Tenant>(cx, tenant)));
+				}
+			}
+
+			wait(waitForAll(tenantWriteFutures));
+		}
+
 		if (g_network->isSimulated()) {
 			wait(initializeSimConfig(cx));
 		}
@@ -2173,7 +2261,7 @@ ACTOR Future<Void> runTests(Reference<AsyncVar<Optional<struct ClusterController
                             StringRef startingConfiguration,
                             LocalityData locality,
                             Optional<TenantName> defaultTenant,
-                            Standalone<VectorRef<TenantNameRef>> tenantsToCreate,
+                            Standalone<VectorRef<TenantNameRef>> extraTenants,
                             bool restartingTest) {
 	state int flags = (at == TEST_ON_SERVERS ? 0 : GetWorkersRequest::TESTER_CLASS_ONLY) |
 	                  GetWorkersRequest::NON_EXCLUDED_PROCESSES_ONLY;
@@ -2205,7 +2293,7 @@ ACTOR Future<Void> runTests(Reference<AsyncVar<Optional<struct ClusterController
 	for (int i = 0; i < workers.size(); i++)
 		ts.push_back(workers[i].interf.testerInterface);
 
-	wait(runTests(cc, ci, ts, tests, startingConfiguration, locality, defaultTenant, tenantsToCreate, restartingTest));
+	wait(runTests(cc, ci, ts, tests, startingConfiguration, locality, defaultTenant, extraTenants, restartingTest));
 	return Void();
 }
 
@@ -2244,7 +2332,7 @@ ACTOR Future<Void> runTests(Reference<IClusterConnectionRecord> connRecord,
                             LocalityData locality,
                             UnitTestParameters testOptions,
                             Optional<TenantName> defaultTenant,
-                            Standalone<VectorRef<TenantNameRef>> tenantsToCreate,
+                            Standalone<VectorRef<TenantNameRef>> extraTenants,
                             bool restartingTest) {
 	state TestSet testSet;
 	state std::unique_ptr<KnobProtectiveGroup> knobProtectiveGroup(nullptr);
@@ -2331,7 +2419,7 @@ ACTOR Future<Void> runTests(Reference<IClusterConnectionRecord> connRecord,
 		                 startingConfiguration,
 		                 locality,
 		                 defaultTenant,
-		                 tenantsToCreate,
+		                 extraTenants,
 		                 restartingTest);
 	} else {
 		tests = reportErrors(runTests(cc,
@@ -2342,7 +2430,7 @@ ACTOR Future<Void> runTests(Reference<IClusterConnectionRecord> connRecord,
 		                              startingConfiguration,
 		                              locality,
 		                              defaultTenant,
-		                              tenantsToCreate,
+		                              extraTenants,
 		                              restartingTest),
 		                     "RunTests");
 	}
