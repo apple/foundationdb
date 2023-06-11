21205560a7aa2cc2281a8da30c39fe13404df972
Evan Tschannen <evan.tschannen@snowflake.com>
2023-06-11 15:27:57 -0700

    include server knobs because of CI failure

--- fdbserver/KeyValueStoreShardedRocksDB.actor.cpp
+++ fdbserver/KeyValueStoreShardedRocksDB.actor.cpp
@@ -41,6 +41,7 @@
 
 #endif // SSD_ROCKSDB_EXPERIMENTAL
 
+#include "fdbclient/ServerKnobs.h"
 #include "fdbclient/IKeyValueStore.h"
 #include "fdbserver/RocksDBCheckpointUtils.actor.h"
 #include "flow/actorcompiler.h" // has to be last include
