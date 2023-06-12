6cdba48d95ad0502ee9926e193ac289dc7521bf5
Hui Liu <hui.liu@snowflake.com>
2023-06-12 14:46:00 -0700

    Remove blobGranuleLockKeys after blob granule restore

--- fdbserver/BlobGranuleValidation.actor.cpp
+++ fdbserver/BlobGranuleValidation.actor.cpp
@@ -19,6 +19,7 @@
  */
 
 #include "fdbserver/BlobGranuleValidation.actor.h"
+#include "fdbclient/FDBOptions.h"
 #include "fdbserver/Knobs.h"
 #include "fdbclient/BlobGranuleRequest.actor.h"
 #include "fdbclient/DatabaseContext.h"
@@ -608,6 +609,51 @@ ACTOR Future<Void> checkFeedCleanup(Database cx, bool debug) {
 			wait(delay(2.0));
 			// reset transaction to get higher read version
 			tr.reset();
+		} catch (Error& e) {
+			wait(tr.onError(e));
+		}
+	}
+}
+
+ACTOR Future<Void> killBlobWorkers(Database cx) {
+	state Transaction tr(cx);
+	state std::set<UID> knownWorkers;
+	state bool first = true;
+	loop {
+		tr.setOption(FDBTransactionOptions::PRIORITY_SYSTEM_IMMEDIATE);
+		tr.setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
+		tr.setOption(FDBTransactionOptions::LOCK_AWARE);
+		tr.setOption(FDBTransactionOptions::RAW_ACCESS);
+		try {
+			RangeReadResult r = wait(tr.getRange(blobWorkerListKeys, CLIENT_KNOBS->TOO_MANY));
+
+			state std::vector<UID> haltIds;
+			state std::vector<Future<ErrorOr<Void>>> haltRequests;
+			for (auto& it : r) {
+				BlobWorkerInterface interf = decodeBlobWorkerListValue(it.value);
+				if (first) {
+					knownWorkers.insert(interf.id());
+				}
+				if (knownWorkers.count(interf.id())) {
+					haltIds.push_back(interf.id());
+					haltRequests.push_back(interf.haltBlobWorker.tryGetReply(HaltBlobWorkerRequest(1e6, UID())));
+				}
+			}
+			first = false;
+			wait(waitForAll(haltRequests));
+			bool allPresent = true;
+			for (int i = 0; i < haltRequests.size(); i++) {
+				if (haltRequests[i].get().present()) {
+					knownWorkers.erase(haltIds[i]);
+				} else {
+					allPresent = false;
+				}
+			}
+			if (allPresent) {
+				return Void();
+			} else {
+				wait(delay(1.0));
+			}
 		} catch (Error& e) {
 			wait(tr.onError(e));
 		}
