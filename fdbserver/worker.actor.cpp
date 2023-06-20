af7774d67c368ca1216639a9365527e2fed93000
Zhe Wu <halfprice@users.noreply.github.com>
2023-06-20 12:40:22 -0700

    Revert logic to test testing logic

--- fdbserver/worker.actor.cpp
+++ fdbserver/worker.actor.cpp
@@ -1167,7 +1167,6 @@ UpdateWorkerHealthRequest doPeerHealthCheck(const WorkerInterface& interf,
 
 	for (const auto& [address, peer] : allPeers) {
 		if (!shouldCheckPeer(peer)) {
-			req.recoveredPeers.push_back(address);
 			continue;
 		}
 
