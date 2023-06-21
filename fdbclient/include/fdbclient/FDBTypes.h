cc68320333bdb7d5f67f4c8ed930370b7d4fdb02
A.J. Beamon <aj.beamon@snowflake.com>
2023-06-21 08:59:22 -0700

    Add testing that a metacluster can be used after a restore. Fix some bugs found by this related to the restore ID and tenant tombstones.

--- fdbclient/include/fdbclient/FDBTypes.h
+++ fdbclient/include/fdbclient/FDBTypes.h
@@ -1761,6 +1761,16 @@ struct Versionstamp {
 	bool operator<=(const Versionstamp& r) const { return !(*this > r); }
 	bool operator>=(const Versionstamp& r) const { return !(*this < r); }
 
+	Versionstamp() {}
+	Versionstamp(Version version, uint16_t batchNumber) : version(version), batchNumber(batchNumber) {}
+	Versionstamp(Standalone<StringRef> str) {
+		ASSERT(str.size() == sizeof(Version) + sizeof(batchNumber));
+		version = bigEndian64(*reinterpret_cast<const Version*>(str.begin()));
+		batchNumber = bigEndian16(*reinterpret_cast<const uint16_t*>(str.begin() + sizeof(Version)));
+	}
+
+	std::string toString() const { return fmt::format("{}.{}", version, batchNumber); }
+
 	template <class Ar>
 	void serialize(Ar& ar) {
 		int64_t beVersion;
@@ -1789,5 +1799,10 @@ template <class Ar>
 inline void load(Ar& ar, Versionstamp& value) {
 	value.serialize(ar);
 }
+
+template <>
+struct Traceable<Versionstamp> : std::true_type {
+	static std::string toString(const Versionstamp& v) { return v.toString(); }
+};
 
 #endif
