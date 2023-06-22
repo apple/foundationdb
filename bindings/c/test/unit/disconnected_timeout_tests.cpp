9bd828d7f98cc024cb71977818202a22af217c63
Xi Cheng <xi.cheng@snowflake.com>
2023-06-22 11:23:32 -0700

    Finish up conversions

--- bindings/c/test/unit/disconnected_timeout_tests.cpp
+++ bindings/c/test/unit/disconnected_timeout_tests.cpp
@@ -42,7 +42,7 @@ void fdbCheck(const fdb::Error& err) {
 fdb::Database fdbOpenDatabase(const char* clusterFilePath) {
 	try {
 		return fdb::Database(clusterFilePath);
-	} catch (const fdb::Error &e) {
+	} catch (const fdb::Error& e) {
 		fdbCheck(e);
 	}
 	// Not reachable
@@ -57,29 +57,6 @@ fdb::Error waitFuture(fdb::Future& f) {
 	return f.error();
 }
 
-//void fdb_check(fdb_error_t e) {
-	//if (e) {
-		//std::cerr << fdb_get_error(e) << std::endl;
-		//std::abort();
-	//}
-//}
-//
-//FDBDatabase* fdb_open_database(const char* clusterFile) {
-	//FDBDatabase* db;
-	//fdb_check(fdb_create_database(clusterFile, &db));
-	//return db;
-//}
-//
-//static FDBDatabase* db = nullptr;
-//static FDBDatabase* timeoutDb = nullptr;
-//
-//// Blocks until the given future is ready, returning an error code if there was
-//// an issue.
-//fdb_error_t wait_future(fdb::Future& f) {
-	//fdb_check(f.block_until_ready());
-	//return f.get_error();
-//}
-//
 void validateTimeoutDuration(double expectedSeconds, std::chrono::time_point<std::chrono::steady_clock> start) {
 	std::chrono::duration<double> duration = std::chrono::steady_clock::now() - start;
 	double actualSeconds = duration.count();
@@ -225,46 +202,42 @@ TEST_CASE("transaction_reset_cancels_with_timeout") {
 	CHECK(err.code() == 1025);
 }
 
-//TEST_CASE("transaction_destruction_cancels_without_timeout") {
-	//FDBTransaction* tr;
-	//fdb_check(fdb_database_create_transaction(db, &tr));
-//
-	//FDBFuture* grvFuture = fdb_transaction_get_read_version(tr);
-	//fdb_transaction_destroy(tr);
-//
-	//fdb_check(fdb_future_block_until_ready(grvFuture));
-	//fdb_error_t err = fdb_future_get_error(grvFuture);
-	//CHECK(err == 1025);
-//
-	//fdb_future_destroy(grvFuture);
-//}
-//
-//TEST_CASE("transaction_destruction_cancels_with_timeout") {
-	//FDBTransaction* tr;
-	//fdb_check(fdb_database_create_transaction(db, &tr));
-//
-	//int64_t timeout = 500;
-	//fdb_check(fdb_transaction_set_option(
-	    //tr, FDB_TR_OPTION_TIMEOUT, reinterpret_cast<const uint8_t*>(&timeout), sizeof(timeout)));
-//
-	//FDBFuture* grvFuture = fdb_transaction_get_read_version(tr);
-	//fdb_transaction_destroy(tr);
-//
-	//fdb_check(fdb_future_block_until_ready(grvFuture));
-	//fdb_error_t err = fdb_future_get_error(grvFuture);
-	//CHECK(err == 1025);
-//
-	//fdb_future_destroy(grvFuture);
-//}
-//
-//TEST_CASE("transaction_set_timeout_and_destroy_repeatedly") {
-	//for (int i = 0; i < 1000; ++i) {
-		//fdb::Transaction tr(db);
-		//int64_t timeout = 500;
-		//fdb_check(tr.set_option(FDB_TR_OPTION_TIMEOUT, reinterpret_cast<const uint8_t*>(&timeout), sizeof(timeout)));
-	//}
-//}
-//
+TEST_CASE("transaction_destruction_cancels_without_timeout") {
+	fdb::Future grvFuture;
+	{
+		// This block forces the transaction to be destroyed when tr goes
+		// out of scope
+		auto tr = db.createTransaction();
+		grvFuture = tr.getReadVersion();
+	}
+
+	auto err = waitFuture(grvFuture);
+	CHECK(err.code() == 1025);
+}
+
+TEST_CASE("transaction_destruction_cancels_with_timeout") {
+	fdb::Future grvFuture;
+	{
+		// This block forces the transaction to be destroyed after tr
+		// goes out of scope
+		auto tr = db.createTransaction();
+		int64_t timeout = 500;
+		tr.setOptionNothrow(FDB_TR_OPTION_TIMEOUT, timeout);
+		grvFuture = tr.getReadVersion();
+	}
+
+	auto err = waitFuture(grvFuture);
+	CHECK(err.code() == 1025);
+}
+
+TEST_CASE("transaction_set_timeout_and_destroy_repeatedly") {
+	for (int i = 0; i < 1000; ++i) {
+		auto tr = db.createTransaction();
+		int64_t timeout = 500;
+		fdbCheck(tr.setOptionNothrow(FDB_TR_OPTION_TIMEOUT, timeout));
+	}
+}
+
 int main(int argc, char** argv) {
 	if (argc < 2) {
 		std::cout << "Disconnected timeout unit tests for the FoundationDB C API.\n"
@@ -276,11 +249,9 @@ int main(int argc, char** argv) {
 	if (argc >= 3) {
 		std::string externalClientLibrary = argv[2];
 		if (externalClientLibrary.substr(0, 2) != "--") {
-			fdbCheck(fdb::network::setOptionNothrow(
-			    FDBNetworkOption::FDB_NET_OPTION_DISABLE_LOCAL_CLIENT, ""));
-			fdbCheck(fdb::network::setOptionNothrow(
-			    FDBNetworkOption::FDB_NET_OPTION_EXTERNAL_CLIENT_LIBRARY,
-			    externalClientLibrary));
+			fdbCheck(fdb::network::setOptionNothrow(FDBNetworkOption::FDB_NET_OPTION_DISABLE_LOCAL_CLIENT, ""));
+			fdbCheck(fdb::network::setOptionNothrow(FDBNetworkOption::FDB_NET_OPTION_EXTERNAL_CLIENT_LIBRARY,
+			                                        externalClientLibrary));
 		}
 	}
 
