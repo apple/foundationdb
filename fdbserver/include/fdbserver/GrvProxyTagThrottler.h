fcd5463c589918ebb294f046b9e64a3d4de3eb8a
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-06-27 15:27:52 -0700

    Decouple token bucket knobs for different types of throttlers

--- fdbserver/include/fdbserver/GrvProxyTagThrottler.h
+++ fdbserver/include/fdbserver/GrvProxyTagThrottler.h
@@ -55,7 +55,10 @@ class GrvProxyTagThrottler {
 		Deque<DelayedRequest> requests;
 
 		TagQueue() = default;
-		explicit TagQueue(double rate) : rateInfo(rate) {}
+		explicit TagQueue(double rate)
+		  : rateInfo(GrvTransactionRateInfo(SERVER_KNOBS->TAG_THROTTLE_RATE_WINDOW,
+		                                    SERVER_KNOBS->TAG_THROTTLE_MAX_EMPTY_QUEUE_BUDGET,
+		                                    rate)) {}
 
 		void setRate(double rate);
 		bool isMaxThrottled(double maxThrottleDuration) const;
