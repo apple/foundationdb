757678a7bc2e393e14ef328e91a1cf95ad4c873b
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-06-27 18:22:57 -0700

    Add opsPerTransaction parameter to ExpectStableThroughputWorkload

--- fdbserver/workloads/ExpectStableThroughput.actor.cpp
+++ fdbserver/workloads/ExpectStableThroughput.actor.cpp
@@ -7,30 +7,49 @@
 #include "flow/actorcompiler.h" // This must be the last include
 
 class ExpectStableThroughputWorkload : public TestWorkload {
+	// Metrics:
 	uint64_t totalCost{ 0 };
 	double throttledDuration{ 0.0 };
 	int tagThrottledErrors{ 0 };
+
+	// Parameters:
 	double testDuration;
 	uint64_t expectedThroughputPagesRate;
 	TransactionTag throttlingTag;
 	double errorTolerance;
-	Key testKey;
+	Key keyPrefix;
 	int numActors;
-	double startTime;
+	double attemptedTransactionRatePerActor;
 	double warmupTime;
+	int opsPerTransaction;
+
+	double startTime;
+
+	void finishTransaction(Transaction const& tr) {
+		if (now() > startTime + warmupTime) {
+			totalCost += tr.getTotalCost();
+			throttledDuration += tr.getTagThrottledDuration();
+		}
+	}
+
+	Key getKey(int index) { return Key(format("%06d", index)).withPrefix(keyPrefix); }
 
 	ACTOR static Future<Void> runTransaction(ExpectStableThroughputWorkload* self, Database cx) {
 		state Transaction tr(cx);
+		state std::vector<Future<Void>> futures;
 		loop {
 			try {
 				tr.setOption(FDBTransactionOptions::AUTO_THROTTLE_TAG, self->throttlingTag);
-				wait(success(tr.get(self->testKey)));
-				if (now() > self->startTime + self->warmupTime) {
-					self->totalCost += tr.getTotalCost();
-					self->throttledDuration += tr.getTagThrottledDuration();
+				futures.clear();
+				futures.reserve(self->opsPerTransaction);
+				for (int i = 0; i < self->opsPerTransaction; ++i) {
+					futures.push_back(success(tr.get(self->getKey(i))));
 				}
+				wait(waitForAll(futures));
+				self->finishTransaction(tr);
 				return Void();
 			} catch (Error& e) {
+				self->finishTransaction(tr);
 				if (e.code() == error_code_proxy_tag_throttled) {
 					++self->tagThrottledErrors;
 				}
@@ -39,11 +58,11 @@ class ExpectStableThroughputWorkload : public TestWorkload {
 		}
 	}
 
-  ACTOR static Future<Void> runClient(ExpectStableThroughputWorkload* self, Database cx) {
-    loop {
-      wait(runTransaction(self, cx));
-    }
-  }
+	ACTOR static Future<Void> runClient(ExpectStableThroughputWorkload* self, Database cx) {
+		loop {
+			wait(delay(1 / self->attemptedTransactionRatePerActor) && runTransaction(self, cx));
+		}
+	}
 
 public:
 	static constexpr auto NAME = "ExpectStableThroughput";
@@ -52,28 +71,30 @@ public:
 		throttlingTag = getOption(options, "throttlingTag"_sr, "testTag"_sr);
 		expectedThroughputPagesRate = getOption(options, "expectedThroughputPagesRate"_sr, 1);
 		errorTolerance = getOption(options, "errorTolerance"_sr, 0.2);
-		testKey = getOption(options, "testKey"_sr, "testKey"_sr);
+		keyPrefix = getOption(options, "keyPrefix"_sr, "testKey"_sr);
 		numActors = getOption(options, "numActors"_sr, 100);
+		attemptedTransactionRatePerActor = getOption(options, "attemptedTransactionRatePerActor"_sr, 0.4);
 		warmupTime = getOption(options, "warmupTime"_sr, 10.0);
+		opsPerTransaction = getOption(options, "opsPerTransaction"_sr, 5);
 	}
 
 	Future<Void> setup(Database const& cx) override { return Void(); }
 
 	Future<Void> start(Database const& cx) override {
 		if (clientId != 0) {
-	  return Void();
+			return Void();
 		}
 		startTime = now();
 		std::vector<Future<Void>> clients;
 		for (int i = 0; i < numActors; ++i) {
-	  clients.push_back(runClient(this, cx));
+			clients.push_back(runClient(this, cx));
 		}
 		return success(timeout(waitForAll(clients), warmupTime + testDuration));
 	}
 
 	Future<bool> check(Database const& cx) override {
-		if (clientId) {
-	  return true;
+		if (clientId != 0) {
+			return true;
 		}
 		auto const expectedTotalCost =
 		    testDuration * expectedThroughputPagesRate * CLIENT_KNOBS->TAG_THROTTLING_PAGE_SIZE;
@@ -92,7 +113,9 @@ public:
 		m.emplace_back("Total Throttling Duration", throttledDuration, Averaged::False);
 	}
 
-	void disableFailureInjectionWorkloads(std::set<std::string>& out) const override { out.emplace("Attrition"); }
+	void disableFailureInjectionWorkloads(std::set<std::string>& out) const override {
+		out.emplace("Attrition", "RandomClogging");
+	}
 };
 
 WorkloadFactory<ExpectStableThroughputWorkload> ExpectStableThroughputWorkloadFactory;
