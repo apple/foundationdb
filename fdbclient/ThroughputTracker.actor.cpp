879463357a906b1cba3b3cb33ed370896b872387
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-07-01 12:49:23 -0700

    Lengthen timeout for ReportThroughputRequests, add logging

--- fdbclient/ThroughputTracker.actor.cpp
+++ fdbclient/ThroughputTracker.actor.cpp
@@ -10,19 +10,25 @@
 class ThroughputTrackerImpl {
 public:
 	ACTOR static Future<Void> run(ThroughputTracker* self, DatabaseContext* cx) {
+		state Future<Void> timer = Never();
 		loop {
 			try {
 				ReportThroughputRequest req(std::move(self->throughput));
 				self->throughput.clear();
-				wait(success(timeout(basicLoadBalance(cx->getGrvProxies(UseProvisionalProxies::False),
-				                                      &GrvProxyInterface::reportThroughput,
-				                                      std::move(req),
-				                                      TaskPriority::DefaultPromiseEndpoint,
-				                                      AtMostOnce::True),
-				                     0.1)));
-				wait(delayJittered(CLIENT_KNOBS->CLIENT_THROUGHPUT_REPORT_INTERVAL));
+				timer = delayJittered(CLIENT_KNOBS->CLIENT_THROUGHPUT_REPORT_INTERVAL);
+				wait(basicLoadBalance(cx->getGrvProxies(UseProvisionalProxies::False),
+				                      &GrvProxyInterface::reportThroughput,
+				                      std::move(req),
+				                      TaskPriority::DefaultPromiseEndpoint,
+				                      AtMostOnce::True) ||
+				     timer);
+				if (timer.isReady()) {
+					CODE_PROBE(true, "ReportThroughputRequest timed out");
+					TraceEvent(SevWarn, "ReportThroughputRequestTimedOut");
+				}
 			} catch (Error& e) {
 				if (e.code() == error_code_request_maybe_delivered) {
+					CODE_PROBE(true, "ReportThroughputRequest maybe delivered");
 					TraceEvent(SevWarn, "ReportThroughputRequestMaybeDelivered");
 				} else {
 					TraceEvent(SevWarnAlways, "ThroughputTrackerFailedWithError").error(e);
