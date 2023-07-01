aee6dd3c68599bba565dd3bd2d8451f5d04983a4
sfc-gh-tclinkenbeard <trevor.clinkenbeard@snowflake.com>
2023-07-01 12:18:36 -0700

    make ThoughputTrackerImpl::run swallow and log request_maybe_delivered errors

--- fdbclient/ThroughputTracker.actor.cpp
+++ fdbclient/ThroughputTracker.actor.cpp
@@ -11,14 +11,24 @@ class ThroughputTrackerImpl {
 public:
 	ACTOR static Future<Void> run(ThroughputTracker* self, DatabaseContext* cx) {
 		loop {
-			ReportThroughputRequest req(std::move(self->throughput));
-			self->throughput.clear();
-			wait(basicLoadBalance(cx->getGrvProxies(UseProvisionalProxies::False),
-			                      &GrvProxyInterface::reportThroughput,
-			                      std::move(req),
-			                      TaskPriority::DefaultPromiseEndpoint,
-			                      AtMostOnce::True));
-			wait(delayJittered(CLIENT_KNOBS->CLIENT_THROUGHPUT_REPORT_INTERVAL));
+			try {
+				ReportThroughputRequest req(std::move(self->throughput));
+				self->throughput.clear();
+				wait(success(timeout(basicLoadBalance(cx->getGrvProxies(UseProvisionalProxies::False),
+				                                      &GrvProxyInterface::reportThroughput,
+				                                      std::move(req),
+				                                      TaskPriority::DefaultPromiseEndpoint,
+				                                      AtMostOnce::True),
+				                     0.1)));
+				wait(delayJittered(CLIENT_KNOBS->CLIENT_THROUGHPUT_REPORT_INTERVAL));
+			} catch (Error& e) {
+				if (e.code() == error_code_request_maybe_delivered) {
+					TraceEvent(SevWarn, "ReportThroughputRequestMaybeDelivered");
+				} else {
+					TraceEvent(SevWarnAlways, "ThroughputTrackerFailedWithError").error(e);
+					throw e;
+				}
+			}
 		}
 	}
 }; // class ThroughputTrackersImpl
