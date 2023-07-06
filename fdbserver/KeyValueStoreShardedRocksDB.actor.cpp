1aacdfcabd2fc3550d593d54327ef2b6a533e9f3
Yao Xiao <87789492+yao-xiao-github@users.noreply.github.com>
2023-07-06 09:44:13 -0700

    Add knob for manifest file size and log rocksdb status. (#10567) (#10593)

--- fdbserver/KeyValueStoreShardedRocksDB.actor.cpp
+++ fdbserver/KeyValueStoreShardedRocksDB.actor.cpp
@@ -491,6 +491,7 @@ rocksdb::Options getOptions() {
 
 	options.skip_stats_update_on_db_open = SERVER_KNOBS->ROCKSDB_SKIP_STATS_UPDATE_ON_OPEN;
 	options.skip_checking_sst_file_sizes_on_db_open = SERVER_KNOBS->ROCKSDB_SKIP_FILE_SIZE_CHECK_ON_OPEN;
+	options.max_manifest_file_size = SERVER_KNOBS->ROCKSDB_MAX_MANIFEST_FILE_SIZE;
 	return options;
 }
 
@@ -2311,6 +2312,7 @@ struct ShardedRocksDBKeyValueStore : IKeyValueStore {
 		void action(AddShardAction& a) {
 			auto s = a.shard->init();
 			if (!s.ok()) {
+				TraceEvent(SevError, "AddShardError").detail("Status", s.ToString()).detail("ShardId", a.shard->id);
 				a.done.sendError(statusToError(s));
 				return;
 			}
@@ -2436,6 +2438,7 @@ struct ShardedRocksDBKeyValueStore : IKeyValueStore {
 			std::vector<std::pair<uint32_t, KeyRange>> deletes;
 			auto s = doCommit(a.writeBatch.get(), a.db, &deletes, a.getHistograms);
 			if (!s.ok()) {
+				TraceEvent(SevError, "CommitError").detail("Status", s.ToString());
 				a.done.sendError(statusToError(s));
 				return;
 			}
