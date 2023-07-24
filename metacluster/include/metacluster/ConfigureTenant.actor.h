9485191fc8c16d03d3ef180dad519fca59f8db19
Oleg Samarin <osamarin@openintegration.inc>
2023-07-24 16:49:25 +0300

    Some optimisation
    (cherry picked from commit 5c6cdae3ba3570e9e376117b9b82a3cbbe0442f2)

--- metacluster/include/metacluster/ConfigureTenant.actor.h
+++ metacluster/include/metacluster/ConfigureTenant.actor.h
@@ -233,7 +233,7 @@ struct ConfigureTenantImpl {
 			return Void();
 		}
 
-		if (tenantEntry.present() && self->updatedEntry.toTenantMapEntry() == tenantEntry.get()) {
+		if (self->updatedEntry.toTenantMapEntry() == tenantEntry.get()) {
 			// No update to write to data cluster, just return.
 			return Void();
 		}
