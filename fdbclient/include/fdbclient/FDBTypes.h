d599c7bcbd2aa9299d444bec2b75f795238a3a88
Zhe Wu <halfprice@users.noreply.github.com>
2023-08-31 11:06:38 -0700

    Use regex to validate wiggle locality

--- fdbclient/include/fdbclient/FDBTypes.h
+++ fdbclient/include/fdbclient/FDBTypes.h
@@ -24,6 +24,7 @@
 #include <algorithm>
 #include <array>
 #include <cinttypes>
+#include <regex>
 #include <set>
 #include <string>
 #include <variant>
@@ -1612,10 +1613,12 @@ struct DatabaseSharedState {
 	  : protocolVersion(currentProtocolVersion()), mutexLock(Mutex()), grvCacheSpace(GRVCacheSpace()), refCount(0) {}
 };
 
+const static std::regex wiggleLocalityValidation("\\w+:\\w+(;\\w:\\w)*");
 inline bool isValidPerpetualStorageWiggleLocality(std::string locality) {
-	int pos = locality.find(':');
-	// locality should be either 0 or in the format '<non_empty_string>:<non_empty_string>'
-	return ((pos > 0 && pos < locality.size() - 1) || locality == "0");
+	if (locality == "0") { 
+		return true;
+	}
+	return std::regex_match(locality, wiggleLocalityValidation);
 }
 
 // matches what's in fdb_c.h
