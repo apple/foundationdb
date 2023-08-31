4abd2edcad98eaf1893a1265f9d8b21babde1002
Zhe Wu <halfprice@users.noreply.github.com>
2023-08-31 13:32:06 -0700

    Parse wiggle locality as a list

--- fdbclient/include/fdbclient/FDBTypes.h
+++ fdbclient/include/fdbclient/FDBTypes.h
@@ -1613,7 +1613,7 @@ struct DatabaseSharedState {
 	  : protocolVersion(currentProtocolVersion()), mutexLock(Mutex()), grvCacheSpace(GRVCacheSpace()), refCount(0) {}
 };
 
-const static std::regex wiggleLocalityValidation("\\w+:\\w+(;\\w:\\w)*");
+const static std::regex wiggleLocalityValidation("(\\w+:\\w+)(;\\w+:\\w+)*");
 inline bool isValidPerpetualStorageWiggleLocality(std::string locality) {
 	if (locality == "0") { 
 		return true;
