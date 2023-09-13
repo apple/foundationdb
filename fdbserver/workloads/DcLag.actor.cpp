3c75b5d7de9967cf41d8dc6de5dd0ce9bfc3a799
Oleg Samarin <osamarin68@gmail.com>
2023-09-13 17:18:06 +0300

    Apply suggestions from code review
    Co-authored-by: A.J. Beamon <aj.beamon@snowflake.com>

--- fdbserver/workloads/DcLag.actor.cpp
+++ fdbserver/workloads/DcLag.actor.cpp
@@ -75,8 +75,8 @@ struct DcLagWorkload : TestWorkload {
 		std::vector<IPAddress> ips; // all remote process IPs
 		for (const auto& process : g_simulator->getAllProcesses()) {
 			const auto& ip = process->address.ip;
-			if (process->locality.dcId().present() && g_simulator->remoteDcId.present() &&
-			    process->locality.dcId().get() == g_simulator->remoteDcId.get()) {
+			if (process->locality.dcId().present() && 
+			    process->locality.dcId() == g_simulator->remoteDcId) {
 				ips.push_back(ip);
 			}
 		}
