c35a23d3f6b65698c3b888d76de2d93a725bff9c
Boris Korzun <drtr0jan@yandex.ru>
2023-09-26 18:16:57 +0300

    GetMsgpack: add 4+ versions of upstream

--- cmake/GetMsgpack.cmake
+++ cmake/GetMsgpack.cmake
@@ -1,9 +1,13 @@
 find_package(msgpack 3.3.0 EXACT QUIET CONFIG)
+find_package(msgpackc-cxx 4.0.0...<6 QUIET CONFIG)
+find_package(msgpack-cxx 6 QUIET CONFIG)
 
 add_library(msgpack INTERFACE)
 
-if(msgpack_FOUND)
+if(msgpack_FOUND OR msgpackc-cxx_FOUND)
   target_link_libraries(msgpack INTERFACE msgpackc-cxx)
+elseif(msgpack-cxx_FOUND)
+  target_link_libraries(msgpack INTERFACE msgpack-cxx)
 else()
   include(ExternalProject)
   ExternalProject_add(msgpackProject
