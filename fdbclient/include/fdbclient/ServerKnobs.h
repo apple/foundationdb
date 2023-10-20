adaf03b6f9f4b91d962cd9deec7a9c70e143751b
Zhe Wang <zhe.wang@wustl.edu>
2023-10-20 12:00:14 -0700

    Check write traffic when bypass shard split (#10974)
    * check write traffic when exiting shard split
    
    * address comments

--- fdbclient/include/fdbclient/ServerKnobs.h
+++ fdbclient/include/fdbclient/ServerKnobs.h
@@ -236,6 +236,7 @@ public:
 	// When the sampled read operations changes more than this threshold, the
 	// shard metrics will update immediately
 	int64_t SHARD_READ_OPS_CHANGE_THRESHOLD;
+	bool ENABLE_WRITE_BASED_SHARD_SPLIT; // experimental
 
 	double SHARD_MAX_READ_DENSITY_RATIO;
 	int64_t SHARD_READ_HOT_BANDWIDTH_MIN_PER_KSECONDS;
