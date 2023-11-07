967a546e15e81714e1dd763108b8afc3775416c5
He Liu <86634338+liquid-helium@users.noreply.github.com>
2023-11-07 14:19:18 -0800

    Optimize physical shard move (#10962)
    * Allow applyUpdates in multiple batches.
    
    * Persist lastAppliedVersion together with a batch of updates.
    
    * Fixed repeatedly applying the same fetched mutation.
    Apply updates at different versions.
    
    * Fixed race between removeDataShard and isInVersionedData.
    
    * Ignore mutations earlier than `lastAppliedVersion`.
    
    * Buggify the batch limit for applying updates.
    
    * Implemented async load of updates.
    
    * Fixed out-of-order version issue.
    
    * Cleanup.
    
    * Batch commit MoveInUpdates.
    
    * Avoid popping unpersisted updates.
    
    * Increment MoveInUpdates::lastAppliedVersion in MoveInUpdates::next().
    Fixed MoveInUpdates::hasNext().
    
    * Fixed loadUpdates start version.
    
    * cleanup.
    
    * Cleanup.
    
    * fmt
    
    * Commit MoveInUpdates regardless of MoveInShard status.
    
    * Enable move restore.
    
    * Disabled move restore and fallBackToAddingShard from ingestion failure.
    
    * Resolved comments.
    
    * Fixed leaks from other prs.

--- fdbclient/ServerKnobs.cpp
+++ fdbclient/ServerKnobs.cpp
@@ -533,7 +533,7 @@ void ServerKnobs::initialize(Randomize randomize, ClientKnobs* clientKnobs, IsSi
  	init( ROCKSDB_IMPORT_MOVE_FILES,                           false );
  	init( ROCKSDB_CHECKPOINT_REPLAY_MARKER,                    false );
  	init( ROCKSDB_VERIFY_CHECKSUM_BEFORE_RESTORE,               true );
- 	init( ROCKSDB_ENABLE_CHECKPOINT_VALIDATION,                false ); if( randomize && BUGGIFY )   ROCKSDB_ENABLE_CHECKPOINT_VALIDATION = deterministicRandom()->coinflip();
+ 	init( ROCKSDB_ENABLE_CHECKPOINT_VALIDATION,                false ); if ( randomize && BUGGIFY ) ROCKSDB_ENABLE_CHECKPOINT_VALIDATION = deterministicRandom()->coinflip();
 	init( ROCKSDB_RETURN_OVERLOADED_ON_TIMEOUT,                false ); if ( randomize && BUGGIFY ) ROCKSDB_RETURN_OVERLOADED_ON_TIMEOUT = true;
 	init( ROCKSDB_COMPACTION_PRI,                                  3 ); // kMinOverlappingRatio, RocksDB default. 
 	init( ROCKSDB_WAL_RECOVERY_MODE,                               2 ); // kPointInTimeRecovery, RocksDB default.
@@ -995,8 +995,9 @@ void ServerKnobs::initialize(Randomize randomize, ClientKnobs* clientKnobs, IsSi
 	// The enumeration is currently: eager, fetch, low, normal, high
 	init( STORAGESERVER_READTYPE_PRIORITY_MAP,           "0,1,2,3,4" );
 	init( SPLIT_METRICS_MAX_ROWS,                              10000 ); if( randomize && BUGGIFY ) SPLIT_METRICS_MAX_ROWS = 10;
-	init( PHYSICAL_SHARD_MOVE_LOG_SEVERITY,                         1);
+	init( PHYSICAL_SHARD_MOVE_LOG_SEVERITY,                        1 );
 	init( FETCH_SHARD_BUFFER_BYTE_LIMIT,                        20e6 ); if( randomize && BUGGIFY ) FETCH_SHARD_BUFFER_BYTE_LIMIT = 1;
+	init( FETCH_SHARD_UPDATES_BYTE_LIMIT,                    2500000 ); if( randomize && BUGGIFY ) FETCH_SHARD_UPDATES_BYTE_LIMIT = 1;
 
 	//Wait Failure
 	init( MAX_OUTSTANDING_WAIT_FAILURE_REQUESTS,                 250 ); if( randomize && BUGGIFY ) MAX_OUTSTANDING_WAIT_FAILURE_REQUESTS = 2;
