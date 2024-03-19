c99aff2ea23f78ac68cf56f973e12dce9a1f8812
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-03-19 10:15:51 -0400

    spelling: healthiness
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fdbserver/core/include/fdbserver/core/Knobs.h
+++ fdbserver/core/include/fdbserver/core/Knobs.h
@@ -393,7 +393,7 @@ public:
 
 	bool TR_FLAG_DISABLE_SERVER_TEAM_REMOVER; // disable the serverTeamRemover actor
 	double TR_REMOVE_SERVER_TEAM_DELAY; // wait for the specified time before try to remove next server team
-	double TR_REMOVE_SERVER_TEAM_EXTRA_DELAY; // serverTeamRemover waits for the delay and check DD healthyness again to
+	double TR_REMOVE_SERVER_TEAM_EXTRA_DELAY; // serverTeamRemover waits for the delay and check DD healthiness again to
 	                                          // ensure it runs after machineTeamRemover
 	double TR_REDUNDANT_TEAM_PERCENTAGE_THRESHOLD; // serverTeamRemover will only remove teams if existing team number
 	                                               // is p% more than the desired team number.
