8be8c7d2cb3f2570b42f945f83e0737c62e80430
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-03-19 09:26:00 -0400

    spelling: exclusion
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fdbserver/workloads/SpecialKeySpaceRobustness.cpp
+++ fdbserver/workloads/SpecialKeySpaceRobustness.cpp
@@ -130,7 +130,7 @@ struct SpecialKeySpaceRobustnessWorkload : TestWorkload {
 			}
 		}
 		tx->reset();
-		// "Exclude" same address multiple times, and only the first excluson should trigger a system metadata update.
+		// "Exclude" same address multiple times, and only the first exclusion should trigger a system metadata update.
 		{
 			Error err;
 			try {
