60abff0316c487012e14825bd83d6a15a59799ba
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-03-19 08:47:32 -0400

    spelling: before
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fdbserver/KeyValueStoreSQLite.actor.cpp
+++ fdbserver/KeyValueStoreSQLite.actor.cpp
@@ -1810,7 +1810,7 @@ private:
 
 			// If a wal file fails during the commit process before finishing a checkpoint, then it is possible that our
 			// wal file will be non-empty when we reload it.  We execute a checkpoint here to remedy that situation.
-			// This call must come before before creating a cursor because it will fail if there are any outstanding
+			// This call must come before creating a cursor because it will fail if there are any outstanding
 			// transactions.
 			fullCheckpoint();
 
