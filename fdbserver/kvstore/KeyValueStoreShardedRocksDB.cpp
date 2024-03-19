a2ebcec2ba16b68bb1ab57cdeb901627f4c35140
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-03-19 09:15:21 -0400

    spelling: column
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fdbserver/kvstore/KeyValueStoreShardedRocksDB.cpp
+++ fdbserver/kvstore/KeyValueStoreShardedRocksDB.cpp
@@ -55,7 +55,7 @@ static_assert((ROCKSDB_MAJOR == FDB_ROCKSDB_MAJOR && ROCKSDB_MINOR == FDB_ROCKSD
               "Unsupported rocksdb version.");
 
 const std::string METADATA_SHARD_ID = "kvs-metadata";
-const std::string DEFAULT_CF_NAME = "default"; // `specialKeys` is stored in this culoumn family.
+const std::string DEFAULT_CF_NAME = "default"; // `specialKeys` is stored in this column family.
 const std::string manifestFilePrefix = "MANIFEST-";
 const KeyRef shardMappingPrefix("\xff\xff/ShardMapping/"_sr);
 const KeyRef compactionTimestampPrefix("\xff\xff/CompactionTimestamp/"_sr);
