96e2c8411b3313f938fb540b252590c45ecb873e
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-03-20 08:40:16 -0400

    spelling: make
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fdbserver/workloads/SpecialKeySpaceRobustness.cpp
+++ fdbserver/workloads/SpecialKeySpaceRobustness.cpp
@@ -380,7 +380,7 @@ struct SpecialKeySpaceRobustnessWorkload : TestWorkload {
 			}
 		}
 		// test lock and unlock
-		// maske sure we lock the database
+		// make sure we lock the database
 		while (true) {
 			Error err;
 			try {
