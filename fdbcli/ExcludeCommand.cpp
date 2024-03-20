102d68fcf4ea0f2b13cff53949c33c40b43a4d18
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-03-20 08:38:15 -0400

    spelling: localityvalue
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- fdbcli/ExcludeCommand.cpp
+++ fdbcli/ExcludeCommand.cpp
@@ -405,7 +405,7 @@ CommandFactory excludeFactory(
     CommandHelp(
         "exclude [FORCE] [failed] [no_wait] [<ADDRESS...>] [locality_dcid:<excludedcid>]\n"
         "        [locality_zoneid:<excludezoneid>] [locality_machineid:<excludemachineid>]\n"
-        "        [locality_processid:<excludeprocessid>] [locality_<KEY>:<localtyvalue>]",
+        "        [locality_processid:<excludeprocessid>] [locality_<KEY>:<localityvalue>]",
         "exclude servers from the database by IP address or locality",
         "If no addresses or localities are specified, lists the set of excluded addresses and localities in addition "
         "to addresses for which exclusion is in progress.\n"
