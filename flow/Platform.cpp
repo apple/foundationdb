d1144a9509bcfaedd505a5abea0e1a19f8e2363f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2024-03-20 09:23:34 -0400

    spelling: sample
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- flow/Platform.cpp
+++ flow/Platform.cpp
@@ -3273,7 +3273,7 @@ void outOfMemory() {
 	TraceEvent("MemSample")
 	    .detail("Count", memSampleSize)
 	    .detail("TotalSize", memSampleSize * ((int)(sizeof(void*) + sizeof(uint32_t) + sizeof(size_t))))
-	    .detail("SapmleCount", memSampleSize)
+	    .detail("SampleCount", memSampleSize)
 	    .detail("Hash", "memSamples")
 	    .detail("Bt", "na");
 	TRACEALLOCATOR(16);
