146161a70be64b871d91dea6d8d4cca74bd21ff7
Matthew Newhook <matthew@customer.io>
2024-04-30 14:32:45 +0000

    Fix file headers.

--- fdbbackup/Decode.cpp
+++ fdbbackup/Decode.cpp
@@ -1,5 +1,5 @@
 /*
- * FileConverter.actor.cpp
+ * Decode.cpp
  *
  * This source file is part of the FoundationDB open source project
  *
--- fdbbackup/include/fdbbackup/Decode.h
+++ fdbbackup/include/fdbbackup/Decode.h
@@ -1,5 +1,5 @@
 /*
- * FileConverter.h
+ * Decode.h
  *
  * This source file is part of the FoundationDB open source project
  *
