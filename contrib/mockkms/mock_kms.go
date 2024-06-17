8ec542836fb4d7bfbb2cb05d5610d3503829110f
Simon Waldherr <git@simon.waldherr.eu>
2024-06-17 19:16:40 +0200

    fix two misspelled words

--- contrib/mockkms/mock_kms.go
+++ contrib/mockkms/mock_kms.go
@@ -26,7 +26,7 @@ package main
 
 // Importing module enable .note.gnu.build-id insertion in the ELF executable.
 // Few drawbacks of the scheme are:
-// 1. Potentialy slower builds
+// 1. Potentially slower builds
 // 2. No cross-compilation support
 // 3. Limited `go tools` availability.
 // TODO: Replace with a better scheme if possible.
