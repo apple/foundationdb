268875440b639bf91be63cdb6560a3a2e5cc397d
Johannes Scheuermann <johscheuer@users.noreply.github.com>
2024-06-24 17:01:23 +0200

    Add support for the isolate process group annotation to shutdown fdbserver processes (#11464)
    * Add support for the isolate process group annotation to shutdown fdbserver processes

--- fdbkubernetesmonitor/monitor_test.go
+++ fdbkubernetesmonitor/monitor_test.go
@@ -20,41 +20,46 @@
 package main
 
 import (
+	"os"
+	"path"
 	"time"
+
+	"github.com/apple/foundationdb/fdbkubernetesmonitor/api"
+	"k8s.io/apimachinery/pkg/util/json"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-var _ = Describe("Testing FDB Kubernetes Monitor", func() {
+var _ = Describe("Testing FDB Kubernetes monitor", func() {
 	When("updating the custom environment variables from the node metadata", func() {
-		var monitor *Monitor
+		var mon *monitor
 
 		BeforeEach(func() {
-			monitor = &Monitor{
-				Logger: GinkgoLogr,
-				CustomEnvironment: map[string]string{
+			mon = &monitor{
+				logger: GinkgoLogr,
+				customEnvironment: map[string]string{
 					"testing": "testing",
 				},
-				PodClient: &PodClient{},
+				podClient: &kubernetesClient{},
 			}
 		})
 
 		JustBeforeEach(func() {
-			monitor.updateCustomEnvironmentFromNodeMetadata()
+			mon.updateCustomEnvironmentFromNodeMetadata()
 		})
 
 		When("no node metadata is present", func() {
 			It("shouldn't add new entries", func() {
-				Expect(monitor.CustomEnvironment).To(HaveLen(1))
-				Expect(monitor.CustomEnvironment).To(HaveKeyWithValue("testing", "testing"))
+				Expect(mon.customEnvironment).To(HaveLen(1))
+				Expect(mon.customEnvironment).To(HaveKeyWithValue("testing", "testing"))
 			})
 		})
 
 		When("node metadata is present", func() {
 			BeforeEach(func() {
-				monitor.PodClient.nodeMetadata = &metav1.PartialObjectMetadata{
+				mon.podClient.nodeMetadata = &metav1.PartialObjectMetadata{
 					ObjectMeta: metav1.ObjectMeta{
 						Labels: map[string]string{
 							// Examples are taken from: https://kubernetes.io/docs/reference/labels-annotations-taints/
@@ -66,10 +71,10 @@ var _ = Describe("Testing FDB Kubernetes Monitor", func() {
 			})
 
 			It("should add the new entries", func() {
-				Expect(monitor.CustomEnvironment).To(HaveLen(3))
-				Expect(monitor.CustomEnvironment).To(HaveKeyWithValue("testing", "testing"))
-				Expect(monitor.CustomEnvironment).To(HaveKeyWithValue("NODE_LABEL_TOPOLOGY_KUBERNETES_IO_ZONE", "us-east-1c"))
-				Expect(monitor.CustomEnvironment).To(HaveKeyWithValue("NODE_LABEL_KUBERNETES_IO_HOSTNAME", "ip-172-20-114-199.ec2.internal"))
+				Expect(mon.customEnvironment).To(HaveLen(3))
+				Expect(mon.customEnvironment).To(HaveKeyWithValue("testing", "testing"))
+				Expect(mon.customEnvironment).To(HaveKeyWithValue("NODE_LABEL_TOPOLOGY_KUBERNETES_IO_ZONE", "us-east-1c"))
+				Expect(mon.customEnvironment).To(HaveKeyWithValue("NODE_LABEL_KUBERNETES_IO_HOSTNAME", "ip-172-20-114-199.ec2.internal"))
 			})
 		})
 	})
@@ -98,4 +103,90 @@ var _ = Describe("Testing FDB Kubernetes Monitor", func() {
 			60*time.Second,
 		),
 	)
+
+	When("reading the configuration file", func() {
+		var mon *monitor
+		var configurationFilePath string
+		var configuration *api.ProcessConfiguration
+		var configurationBytes []byte
+		defaultVersion := api.Version{Major: 7, Minor: 1, Patch: 51}
+
+		BeforeEach(func() {
+			tmpDir := GinkgoT().TempDir()
+			// Set the fdbserverPath to make sure the executable check works.
+			fdbserverPath = path.Join(tmpDir, "fdberver")
+			Expect(os.WriteFile(fdbserverPath, []byte(""), 0700)).NotTo(HaveOccurred())
+			configurationFilePath = path.Join(tmpDir, "config.json")
+			mon = &monitor{
+				logger:                  GinkgoLogr,
+				configFile:              configurationFilePath,
+				customEnvironment:       map[string]string{},
+				podClient:               &kubernetesClient{},
+				currentContainerVersion: defaultVersion,
+			}
+		})
+
+		JustBeforeEach(func() {
+			configuration, configurationBytes = mon.readConfiguration()
+		})
+
+		When("the configuration file is empty", func() {
+			BeforeEach(func() {
+				Expect(os.WriteFile(configurationFilePath, []byte("{}"), 0600)).NotTo(HaveOccurred())
+			})
+
+			It("should return an empty configuration", func() {
+				Expect(configuration).To(BeNil())
+				Expect(configurationBytes).To(BeNil())
+			})
+		})
+
+		When("the configuration is valid", func() {
+			var out []byte
+
+			BeforeEach(func() {
+				config := &api.ProcessConfiguration{
+					Version: &defaultVersion,
+				}
+
+				var err error
+				out, err = json.Marshal(config)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(os.WriteFile(configurationFilePath, out, 0600)).NotTo(HaveOccurred())
+			})
+
+			It("should return the configuration", func() {
+				Expect(configuration).NotTo(BeNil())
+				Expect(configuration.Version).To(Equal(&defaultVersion))
+				Expect(configuration.BinaryPath).To(Equal(fdbserverPath))
+				Expect(configuration.RunServers).To(BeNil())
+				Expect(configuration.Arguments).To(BeEmpty())
+				Expect(configurationBytes).To(Equal(out))
+			})
+
+			When("the pod has the isolate annotation set to true", func() {
+				BeforeEach(func() {
+					mon.podClient = &kubernetesClient{
+						podMetadata: &metav1.PartialObjectMetadata{
+							ObjectMeta: metav1.ObjectMeta{
+								Annotations: map[string]string{
+									api.IsolateProcessGroupAnnotation: "true",
+								},
+							},
+						},
+					}
+				})
+
+				It("should return the configuration with runServers set to false", func() {
+					Expect(configuration).NotTo(BeNil())
+					Expect(configuration.Version).To(Equal(&defaultVersion))
+					Expect(configuration.BinaryPath).To(Equal(fdbserverPath))
+					Expect(configuration.RunServers).NotTo(BeNil())
+					Expect(*configuration.RunServers).To(BeFalse())
+					Expect(configuration.Arguments).To(BeEmpty())
+					Expect(configurationBytes).To(Equal(out))
+				})
+			})
+		})
+	})
 })
