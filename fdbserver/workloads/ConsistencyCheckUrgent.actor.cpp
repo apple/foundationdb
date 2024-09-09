9f2c89c06a76e8f053ceb1cffd0ec7fae73a25a5
Jingyu Zhou <jingyuzhou@gmail.com>
2024-09-08 21:02:10 -0700

    Reduce retry count when SS interface is not found
    To speed up ConsistencyCheckUrgent workload that can take very long time in
    simulation.

--- fdbserver/workloads/ConsistencyCheckUrgent.actor.cpp
+++ fdbserver/workloads/ConsistencyCheckUrgent.actor.cpp
@@ -283,7 +283,8 @@ struct ConsistencyCheckUrgentWorkload : TestWorkload {
 					}
 					wait(delay(5.0));
 					retryCount++;
-					if (retryCount > 50) {
+					if (retryCount > 10) {
+						// SS could be removed from the cluster
 						throw timed_out();
 					}
 				}
@@ -324,6 +325,7 @@ struct ConsistencyCheckUrgentWorkload : TestWorkload {
 							valueAvailableToCheck = false;
 							TraceEvent e(SevInfo, "ConsistencyCheckUrgent_TesterGetRangeError");
 							e.detail("ResultPresent", rangeResult.present());
+							e.detail("StorageServer", storageServerInterfaces[j].uniqueID);
 							if (rangeResult.present()) {
 								e.detail("ErrorPresent", rangeResult.get().error.present());
 								if (rangeResult.get().error.present()) {
@@ -331,6 +333,10 @@ struct ConsistencyCheckUrgentWorkload : TestWorkload {
 								}
 							} else {
 								e.detail("ResultNotPresentWithError", rangeResult.getError().what());
+								if (g_network->isSimulated() &&
+								    g_simulator->getProcessByAddress(storageServerInterfaces[j].address())->failed) {
+									e.detail("MachineFailed", "True");
+								}
 							}
 							break;
 						}
