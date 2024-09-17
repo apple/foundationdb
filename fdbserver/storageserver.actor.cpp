f738426571fc48fab46f7dad6f4be7c0e33d48f8
He Liu <86634338+liquid-helium@users.noreply.github.com>
2024-09-16 17:30:50 -0700

    Remove storage engine type from DataLossRecovery test. (#11656)
    Co-authored-by: He Liu <heliu@Hes-MacBook-Pro-2.local>

--- fdbserver/storageserver.actor.cpp
+++ fdbserver/storageserver.actor.cpp
@@ -10119,6 +10119,9 @@ void changeServerKeysWithPhysicalShards(StorageServer* data,
 			    .detail("Version", cVer);
 			newEmptyRanges.push_back(range);
 			updatedShards.emplace_back(range, cVer, desiredId, desiredId, StorageServerShard::ReadWrite);
+			if (data->physicalShards.find(desiredId) == data->physicalShards.end()) {
+				data->pendingAddRanges[cVer].emplace_back(desiredId, range);
+			}
 		} else if (!nowAssigned) {
 			if (dataAvailable) {
 				ASSERT(data->newestAvailableVersion[range.begin] ==
