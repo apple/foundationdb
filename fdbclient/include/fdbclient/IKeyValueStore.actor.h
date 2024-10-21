488b6068cfd14efc5d80bcd7f96928c466fddfaf
Dan Lambright <hlambright@apple.com>
2024-10-21 15:09:24 -0400

    fix bug TxnStateStoreCycleTest for version vector

--- fdbclient/include/fdbclient/IKeyValueStore.actor.h
+++ fdbclient/include/fdbclient/IKeyValueStore.actor.h
@@ -60,6 +60,7 @@ public:
 	virtual KeyValueStoreType getType() const = 0;
 	// Returns true if the KV store supports shards, i.e., implements addRange(), removeRange(), and
 	// persistRangeMapping().
+	virtual bool getReplaceContent() const { return false; };
 	virtual bool shardAware() const { return false; }
 	virtual void set(KeyValueRef keyValue, const Arena* arena = nullptr) = 0;
 	virtual void clear(KeyRangeRef range, const Arena* arena = nullptr) = 0;
