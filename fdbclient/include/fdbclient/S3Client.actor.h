30cddcab8e7535705b375f3306cb58c58f4ec494
Zhe Wang <zhe.wang@wustl.edu>
2024-11-21 13:02:14 -0800

    init

---
+++ fdbclient/include/fdbclient/S3Client.actor.h
@@ -0,0 +1,42 @@
+/*
+ * S3Client.actor.h
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2013-2024 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#if defined(NO_INTELLISENSE) && !defined(FDBCLIENT_S3CLIENT_ACTOR_G_H)
+#define FDBCLIENT_S3CLIENT_ACTOR_G_H
+#include "fdbclient/S3Client.actor.g.h"
+#elif !defined(FDBCLIENT_S3CLIENT_ACTOR_H)
+#define FDBCLIENT_S3CLIENT_ACTOR_H
+
+#include "fdbclient/Knobs.h"
+#include "fdbclient/SystemData.h"
+#include "flow/actorcompiler.h" // This must be the last #include.
+
+ACTOR Future<Void> copyDownDirectory(std::string s3url, std::string dirpath);
+
+ACTOR Future<Void> copyDownFile(std::string s3url, std::string filepath);
+
+ACTOR Future<Void> copyUpDirectory(std::string dirpath, std::string s3url);
+
+ACTOR Future<Void> copyUpFile(std::string filepath, std::string s3url);
+
+#include "flow/unactorcompiler.h"
+#endif
