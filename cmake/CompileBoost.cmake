5e99dc5a9625cb6e35aac2a21e702a6557c0cc3d
Jingyu Zhou <jingyu_zhou@apple.com>
2025-01-08 08:22:40 -0800

    Fix boost URL (#11863)

--- cmake/CompileBoost.cmake
+++ cmake/CompileBoost.cmake
@@ -60,7 +60,7 @@ function(compile_boost)
 
   set(BOOST_INSTALL_DIR "${CMAKE_BINARY_DIR}/boost_install")
   ExternalProject_add("${COMPILE_BOOST_TARGET}Project"
-    URL                "https://boostorg.jfrog.io/artifactory/main/release/1.78.0/source/boost_1_78_0.tar.bz2"
+    URL                "https://archives.boost.io/release/1.78.0/source/boost_1_78_0.tar.bz2"
     URL_HASH           SHA256=8681f175d4bdb26c52222665793eef08490d7758529330f98d3b29dd0735bccc
     CONFIGURE_COMMAND  ${BOOTSTRAP_COMMAND}
                        ${BOOTSTRAP_ARGS}
