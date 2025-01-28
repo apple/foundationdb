a02f1078f0c7de2257bb9934d54a2e018b76fdf1
Zhe Wang <zhe.wang@wustl.edu>
2025-01-27 20:20:21 -0800

    Using knob to enable physical shard move in failure injection workloads #11914

--- fdbserver/workloads/DataLossRecovery.actor.cpp
+++ fdbserver/workloads/DataLossRecovery.actor.cpp
@@ -236,7 +236,10 @@ struct DataLossRecoveryWorkload : TestWorkload {
 				if (SERVER_KNOBS->SHARD_ENCODE_LOCATION_METADATA) {
 					UID dataMoveId = newDataMoveId(deterministicRandom()->randomUInt64(),
 					                               AssignEmptyRange(false),
-					                               DataMoveType::PHYSICAL,
+					                               deterministicRandom()->random01() <
+					                                       SERVER_KNOBS->DD_PHYSICAL_SHARD_MOVE_PROBABILITY
+					                                   ? DataMoveType::PHYSICAL
+					                                   : DataMoveType::LOGICAL,
 					                               DataMovementReason::TEAM_HEALTHY,
 					                               UnassignShard(false));
 					params = std::make_unique<MoveKeysParams>(dataMoveId,
