367e4cd299baab1cb0bee890b9a02a29b48668fd
gm42 <16498973+gm42@users.noreply.github.com>
2025-03-27 08:59:42 +0100

    Go: rename Advance() as NextBatch(), remove RangeIterator.Get()
    Other functions renamed:
    * GetSliceWithError() -> Get()
    * GetSliceOrPanic() -> MustGet()

--- bindings/go/src/fdb/database.go
+++ bindings/go/src/fdb/database.go
@@ -323,7 +323,7 @@ func (d Database) LocalityGetBoundaryKeys(er ExactRange, limit int, readVersion 
 		append(Key("\xFF/keyServers/"), ek.FDBKey()...),
 	}
 
-	kvs, err := tr.Snapshot().GetRange(ffer, RangeOptions{Limit: limit}).GetSliceWithError()
+	kvs, err := tr.Snapshot().GetRange(ffer, RangeOptions{Limit: limit}).Get()
 	if err != nil {
 		return nil, err
 	}
