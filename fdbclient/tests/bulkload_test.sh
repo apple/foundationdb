46368f15162ca70c4c2cd9e5d477d7dae1890903
michael stack <stack@duboce.com>
2025-04-24 15:09:41 -0700

    Fix this showing up in bulkload logs when we look for severity=40
    2025-04-24T21:13:48+00:00 Verify restore
    2025-04-24T21:14:06+00:00 Check for Severity=40 errors
    /tmp/weed.23581.FYr3/loopback_cluster/10/log/trace.127.0.0.1.1810.1745529123.Ubsec9.0.1.xml:<Event Severity="40" ErrorKind="BugDetected" Time="1745529133.001704" DateTime="2025-04-24T21:12:13Z" Type="InternalError" ID="0000000000000000" Error="internal_error" ErrorDescription="An internal error occurred" ErrorCode="4100" FailedAssertion="CLIENT_KNOBS-&gt;ENABLE_MUTATION_CHECKSUM &amp;&amp; CLIENT_KNOBS-&gt;ENABLE_ACCUMULATIVE_CHECKSUM" File="/codebuild/output/src1046262143/src/github.com/apple/foundationdb/fdbserver/AccumulativeChecksumUtil.cpp" Line="284" ThreadID="11337476354981857981" Backtrace="addr2line -e fdbserver.debug -p -C -f -i 0x643ae06 0x64392b1 0x643941d 0x633f1d8 0x40a08b 0x2f7b11c 0x5a873ef 0x5a8787e 0x63b7690 0x12c9150 0x7f21982e85d0" Machine="127.0.0.1:1810" LogGroup="default" Roles="SS" />
    /tmp/weed.23581.FYr3/loopback_cluster/10/log/trace.127.0.0.1.1810.1745529123.Ubsec9.0.1.xml:<Event Severity="40" ErrorKind="Unset" Time="1745529133.001704" DateTime="2025-04-24T21:12:13Z" Type="SystemError" ID="0000000000000000" Error="internal_error" ErrorDescription="An internal error occurred" ErrorCode="4100" ThreadID="11337476354981857981" Backtrace="addr2line -e fdbserver.debug -p -C -f -i 0x643ae06 0x64392b1 0x643941d 0x633c57b 0x633f1ea 0x40a08b 0x2f7b11c 0x5a873ef 0x5a8787e 0x63b7690 0x12c9150 0x7f21982e85d0" Machine="127.0.0.1:1810" LogGroup="default" Roles="SS" />
    /tmp/weed.23581.FYr3/loopback_cluster/10/log/trace.127.0.0.1.1810.1745529123.Ubsec9.0.1.xml:<Event Severity="40" ErrorKind="BugDetected" Time="1745529133.002836" DateTime="2025-04-24T21:12:13Z" Type="Crash" ID="0000000000000000" Signal="11" Name="Segmentation fault" Trace="addr2line -e fdbserver.debug -p -C -f -i 0x7f21982fd730 0x2fc7778 0x2fc78b0 0x63b7690 0x12c9150 0x7f21982e85d0" ThreadID="11337476354981857981" Backtrace="addr2line -e fdbserver.debug -p -C -f -i 0x643ae06 0x64392b1 0x643941d 0x63ed7c5 0x7f21982fd730 0x2fc7778 0x2fc78b0 0x63b7690 0x12c9150 0x7f21982e85d0" Machine="127.0.0.1:1810" LogGroup="default" Roles="SS" />

--- fdbclient/tests/bulkload_test.sh
+++ fdbclient/tests/bulkload_test.sh
@@ -242,7 +242,12 @@ TEST_SCRATCH_DIR=
 TLS_CA_FILE="${TLS_CA_FILE:-/etc/ssl/cert.pem}"
 readonly TLS_CA_FILE
 readonly HTTP_VERBOSE_LEVEL=2
-KNOBS=("--knob_blobstore_encryption_type=aws:kms" "--knob_http_verbose_level=${HTTP_VERBOSE_LEVEL}")
+KNOBS=(
+  "--knob_blobstore_encryption_type=aws:kms"
+  "--knob_http_verbose_level=${HTTP_VERBOSE_LEVEL}"
+  "--knob_enable_mutation_checksum=true"
+  "--knob_enable_accumulative_checksum=true"
+)
 readonly KNOBS
 # Should we use S3? If USE_S3 is not defined, then check if
 # OKTETO_NAMESPACE is defined (It is defined on the okteto
