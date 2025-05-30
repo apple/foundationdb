8b7ac8a56143e2d8a31018e20a67705ecb02c448
Zhe Wang <zhe.wang@wustl.edu>
2025-05-30 14:01:30 -0700

    Avoid Source Storage Server Being Overloaded by Data Movements with Replica Consistency Check (#12164)
    * add ss metrics for fetch key
    
    * bug fix
    
    * revert checkTimeSpanSec
    
    * fix adjustRelocationParallelismForSrc
    
    * code cleanup
    
    * fix replicaComparison
    
    * remove unnecessary counters
    
    * fix large storage server data structure
    
    * address comments
    
    * address comments
    
    * address comments
    
    * code cleanup
    
    * bug fix
    
    * fix bug

--- fdbserver/include/fdbserver/StorageMetrics.actor.h
+++ fdbserver/include/fdbserver/StorageMetrics.actor.h
@@ -206,6 +206,15 @@ struct CommonStorageCounters {
 	// The number of key-value pairs fetched by fetchKeys()
 	Counter kvFetched;
 
+	// Bytes replied for fetchKeys
+	Counter kvFetchBytesServed;
+
+	// The number of key-value pairs replied for fetchKeys
+	Counter kvFetchServed;
+
+	// The number of fetchKeys errors
+	Counter fetchKeyErrors;
+
 	// name and id are the inputs to CounterCollection initialization. If metrics provided, the caller should guarantee
 	// the lifetime of metrics is longer than this counter
 	CommonStorageCounters(const std::string& name,
