077c3fbb670f719ddaa829f5b6baaaecdd2721ce
Jingyu Zhou <jingyuzhou@gmail.com>
2025-07-11 11:02:51 -0700

    Fix go compiling errors
    fdb_test.go:409:1: ExampleGetClientStatus refers to unknown identifier: GetClientStatus

--- bindings/go/src/fdb/fdb_test.go
+++ bindings/go/src/fdb/fdb_test.go
@@ -537,7 +537,7 @@ func TestGetClientStatus(t *testing.T) {
 	}
 }
 
-func ExampleGetClientStatus() {
+func ExampleDatabase_GetClientStatus() {
 	fdb.MustAPIVersion(API_VERSION)
 	err := fdb.Options().SetDisableClientBypass()
 	if err != nil {
