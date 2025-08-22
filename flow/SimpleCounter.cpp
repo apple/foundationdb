2183366d832627103780ecd436b6d24d47a9f356
Gideon Glass <gxglassgithub@gmail.com>
2025-08-22 16:32:07 -0700

    Improve clarity on unit test

--- flow/SimpleCounter.cpp
+++ flow/SimpleCounter.cpp
@@ -28,37 +28,39 @@ TEST_CASE("/flow/simplecounter/int64") {
 	foo->increment(5);
 	foo->increment(1);
 
-	for (int i = 0; i < 100; i++) {
-		SimpleCounter<int64_t>* p = SimpleCounter<int64_t>::makeCounter(std::string("many") + std::to_string(i));
-		p->increment(i);
-	}
-
 	bar->increment(10);
 
 	ASSERT(foo->get() == 6);
 	ASSERT(bar->get() == 10);
 
+	for (int i = 0; i < 100; i++) {
+		SimpleCounter<int64_t>* p = SimpleCounter<int64_t>::makeCounter(std::string("many") + std::to_string(i));
+		p->increment(i);
+		ASSERT(p->get() == i);
+	}
+
 	SimpleCounter<int64_t>* conflict = SimpleCounter<int64_t>::makeCounter("lots_of_increments");
 
-	auto int_inclots = [conflict]() {
+	// Increment by all values in [1, 1000000] across 10 threads.
+	// Expected sum: 10 * (min + max) * (num entries in series)/2
+	// ==>  10 * ( 1 + 1M ) * 500K
+	int64_t expectedSum = int64_t{ 10 } * int64_t{ 1'000'000 + 1 } * uint64_t{ 500'000 };
+
+	auto inclots = [conflict]() {
 		for (int i = 1; i <= 1'000'000; i++) {
 			conflict->increment(i);
 		}
 	};
 
 	std::vector<std::thread> threads;
-
 	for (int i = 0; i < 10; i++) {
-		threads.emplace_back(int_inclots);
+		threads.emplace_back(inclots);
 	}
-
 	for (int i = 0; i < 10; i++) {
 		threads[i].join();
 	}
 
-	// 10 threads * sum of series ==>  10 threads * (min + max) * (num entries in series)/2
-	int64_t int_expected_count = int64_t{ 10 } * int64_t{ 1'000'000 + 1 } * uint64_t{ 500'000 };
-	ASSERT(conflict->get() == int_expected_count);
+	ASSERT(conflict->get() == expectedSum);
 
 	std::vector<SimpleCounter<int64_t>*> intCounters = SimpleCounter<int64_t>::getCounters();
 	ASSERT(intCounters.size() == 103);
@@ -73,6 +75,8 @@ TEST_CASE("/flow/simplecounter/double") {
 	// A way to do this is to only add values with exact representations.
 	// Integers and powers of two (within the limits of the number of mantissa
 	// and exponent bits) do have exact representations.  Hence the 0.5 increment.
+	double expectedSum = 10.0 * (0.5 + 1'000'000.0) * 1'000'000.0;
+
 	auto double_inclots = [baz]() {
 		for (double i = 0.5; i <= 1'000'000; i += 0.5) {
 			baz->increment(i);
@@ -80,20 +84,14 @@ TEST_CASE("/flow/simplecounter/double") {
 	};
 
 	std::vector<std::thread> threads;
-
 	for (int i = 0; i < 10; i++) {
 		threads.emplace_back(double_inclots);
 	}
-
 	for (int i = 0; i < 10; i++) {
 		threads[i].join();
 	}
 
-	double double_expected_count = 10.0 * (1'000'000 + 0.5) * 1'000'000.0;
-	// printf("double_expected_count: %.10f; baz: %.10f\n", double_expected_count, baz->get());
-	ASSERT(double_expected_count == baz->get());
-	// double ratio = baz->get()/double_expected_count;
-	// ASSERT(ratio >= 0.999 && ratio < 1.001);
+	ASSERT(baz->get() == expectedSum);
 
 	std::vector<SimpleCounter<double>*> doubleCounters = SimpleCounter<double>::getCounters();
 	ASSERT(doubleCounters.size() == 1);
