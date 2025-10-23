98d9dc76a4277610d20525b088b7ca7ddf0300bb
Zhe Wang <zhe.wang@wustl.edu>
2025-10-23 13:28:14 -0700

    address comments

--- contrib/mtlsbenchmark/client.sh
+++ contrib/mtlsbenchmark/client.sh
@@ -1,4 +1,15 @@
-taskset -c 0-2 /root/build_output/bin/fdbserver \
+#!/bin/bash
+# Run the mtlsbenchmark client
+# Usage: ./client.sh
+# This script assumes that the server is already running on localhost:4500
+# Adjust the parameters as needed
+# test_remoteAddresses is set to connect to the server
+# test_targetDuration is set to 10 seconds for the client duration
+# test_connectionsOut is set to 10 for the number of outgoing connections
+# knob_disable_mainthread_tls_handshake is enabled to use backgound threads for TLS handshakes only
+# knob_tls_handshake_flowlock_priority is set to 8900 for enabling TLS flowlock priority as high as the handshake priority. Default is 7000.
+
+taskset -c 0-0 /root/build_output/bin/fdbserver \
 	-r unittests \
 	-f :/network/p2ptest \
 	--test_remoteAddresses=127.0.0.1:4500:tls \
