c444da6b371465c7ac6cabb255695fe8fe02b9e6
Gideon Glass <gxglassgithub@gmail.com>
2025-10-24 16:51:39 -0700

    Remove more tenant or tenant-adjacent or blob-granule-adjacent stuff.  Or at least stuff that looks adjacent to that stuff.  Not compiled or tested.

--- fdbserver/tester.actor.cpp
+++ fdbserver/tester.actor.cpp
@@ -2147,6 +2147,8 @@ std::map<std::string, std::function<void(const std::string&)>> testSpecGlobalKey
 	  [](const std::string& value) { TraceEvent("TestParserTest").detail("ParsedDisableHostname", ""); } },
 	{ "disableRemoteKVS",
 	  [](const std::string& value) { TraceEvent("TestParserTest").detail("ParsedRemoteKVS", ""); } },
+	// TODO(gglass): remove this when it's no longer used.  For now it's a useful signal in a toml file
+	// for tests against functionality that might not actually be tenant related and hence need to keep.
 	{ "allowDefaultTenant",
 	  [](const std::string& value) { TraceEvent("TestParserTest").detail("ParsedDefaultTenant", ""); } }
 };
