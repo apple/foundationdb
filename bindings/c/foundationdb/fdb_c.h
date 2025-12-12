13472d28a4a75753fe715ef8479aef78f4d17c17
Gideon Glass <gxglassgithub@gmail.com>
2025-12-11 16:29:43 -0800

    Use C-style comments because of insane compile options.  THE IMPLEMENTATION IS C++.  WHO ARE WE KIDDING.

--- bindings/c/foundationdb/fdb_c.h
+++ bindings/c/foundationdb/fdb_c.h
@@ -376,11 +376,13 @@ DLLEXPORT WARN_UNUSED_RESULT fdb_error_t fdb_database_set_option(FDBDatabase* d,
 DLLEXPORT WARN_UNUSED_RESULT fdb_error_t fdb_database_create_transaction(FDBDatabase* d,
                                                                          FDBTransaction** out_transaction);
 
-// Dummy versions of tenant-related functions are needed in the FDB C library
-// because 7.x python bindings always load these functions on startup.
-// (In 8.0+, users should not be invoking this deleted experimental
-// functionality directly.)
-typedef struct FDB_tenant FDBTenant;
+/*
+ * Dummy versions of tenant-related functions are needed in the FDB C library
+ * because 7.x python bindings always load these functions on startup.
+ * (In 8.0+, users should not be invoking this deleted experimental
+ * functionality directly.)
+ * typedef struct FDB_tenant FDBTenant;
+ */
 DLLEXPORT WARN_UNUSED_RESULT fdb_error_t fdb_database_open_tenant(FDBDatabase* d,
                                                                   uint8_t const* tenant_name,
                                                                   int tenant_name_length,
