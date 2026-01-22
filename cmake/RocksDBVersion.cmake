0490f756a3562d0dc7642fef4fd094077c7eeefa
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2026-01-21 23:51:20 -0800

    fdb release 7.4: use rocksdb v8.11.5 (#12651)

--- cmake/RocksDBVersion.cmake
+++ cmake/RocksDBVersion.cmake
@@ -16,8 +16,8 @@
 # OPTION 1: RocksDB Release Number
 # If you use this option, make sure Option 2 below is commented out.
 ###############################################################################
-# set(ROCKSDB_VERSION "9.7.3")
-# set(ROCKSDB_VERSION_SHA256 "acfabb989cbfb5b5c4d23214819b059638193ec33dad2d88373c46448d16d38b")
+set(ROCKSDB_VERSION "8.11.5")
+set(ROCKSDB_VERSION_SHA256 "2641eb63ee2d691c0e96de0a55edfcc4cab181d9b6c31fa4a33c478423062196")
 
 ###############################################################################
 # OPTION 2: RocksDB Git Commit Hash
@@ -26,5 +26,5 @@
 # Note: CMake will auto-fetch the version from GitHub at configure time.
 # This requires network access during cmake configure.
 ###############################################################################
-set(ROCKSDB_GIT_HASH "2732f118497ab75cd2e44bc327746be180b42dcf")
-set(ROCKSDB_GIT_HASH_SHA256 "5f0dd06680c0bf302abb9bc70b4698fdcd0d5623c7264c8b3af7a1fe4f8b3078")
+# set(ROCKSDB_GIT_HASH "2732f118497ab75cd2e44bc327746be180b42dcf")
+# set(ROCKSDB_GIT_HASH_SHA256 "5f0dd06680c0bf302abb9bc70b4698fdcd0d5623c7264c8b3af7a1fe4f8b3078")
