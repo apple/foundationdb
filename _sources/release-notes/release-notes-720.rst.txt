76f68eae4830a1878f2e921451887d7259b36e5e
FoundationDB CI <foundationdb_ci@apple.com>
2026-02-18 23:25:39 +0000

    Updated FoundationDB Git Docs to version: 7.3.73

--- _sources/release-notes/release-notes-720.rst.txt
+++ _sources/release-notes/release-notes-720.rst.txt
@@ -1,5 +1,3 @@
-.. _release-notes:
-
 #############
 Release Notes
 #############
--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '7.4.6',
+    VERSION: 'ON',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
