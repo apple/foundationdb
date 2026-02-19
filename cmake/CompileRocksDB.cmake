ce08c22c1b2de036d7d66d35f18c10552429c312
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2026-02-18 22:15:07 -0800

    Use rocksdb 8.11.5 (#12690)

--- cmake/CompileRocksDB.cmake
+++ cmake/CompileRocksDB.cmake
@@ -1,6 +1,6 @@
 # FindRocksDB
 
-find_package(RocksDB 8.11.4)
+find_package(RocksDB 8.11.5)
 
 include(ExternalProject)
 
@@ -49,8 +49,8 @@ if(ROCKSDB_FOUND)
       ${BINARY_DIR}/librocksdb.a)
 else()
   ExternalProject_Add(rocksdb
-    URL https://github.com/facebook/rocksdb/archive/refs/tags/v8.11.4.tar.gz
-    URL_HASH SHA256=1b84c7d7214360fd536349917c57ebd5030d5b4fc214a343ba628b0c6e3d2711
+    URL https://github.com/facebook/rocksdb/archive/refs/tags/v8.11.5.tar.gz
+    URL_HASH SHA256=2641eb63ee2d691c0e96de0a55edfcc4cab181d9b6c31fa4a33c478423062196
     CMAKE_ARGS ${RocksDB_CMAKE_ARGS}
     BUILD_BYPRODUCTS <BINARY_DIR>/librocksdb.a
     INSTALL_COMMAND ""
