2ee6443962264b6c3e6cc69e5c39a2225012df4b
Pierce Lopez <pierce.lopez@apple.com>
2026-04-21 14:44:31 -0400

    build: update fmt to version 11.1.4 to fix compat with new toolchains
    latest macOS toolchain has c++ standards errors that are hard to avoid
    with fmt 11.0.2

--- cmake/GetFmt.cmake
+++ cmake/GetFmt.cmake
@@ -1,11 +1,11 @@
-find_package(fmt 11.0.2 EXACT QUIET CONFIG)
+find_package(fmt 11.1.4 EXACT QUIET CONFIG)
 
 if(NOT fmt_FOUND)
   include(FetchContent)
   FetchContent_Declare(
     fmt
     GIT_REPOSITORY https://github.com/fmtlib/fmt
-    GIT_TAG 11.0.2
+    GIT_TAG 11.1.4
   )
   FetchContent_MakeAvailable(fmt)
 endif()
