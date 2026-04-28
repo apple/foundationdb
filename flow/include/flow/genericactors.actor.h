77b0adb792f56507005f1bc9bd166b56ddc2acfc
Trevor Clinkenbeard <tclinkenbeard@openai.com>
2026-04-28 06:54:10 -0700

    Remove disabled genericactors tuple helper

--- flow/include/flow/genericactors.actor.h
+++ flow/include/flow/genericactors.actor.h
@@ -339,19 +339,6 @@ template <class T, class X>
 	});
 }
 
-#if false
-// NOTE: Think twice whether create a new struct for a complex return type is better before using tuple.
-// If we just use the return type once, is it worth to create a new struct?
-// And enable the unit test in genericactors.actor.cpp
-template <class A, class... Bs>
-Future<Void> storeTuple(Future<std::tuple<A, Bs...>> what, A& a, Bs&... b) {
-	return map(what, [&](std::tuple<A, Bs...> const& v) {
-		std::tie(a, b...) = v;
-		return Void();
-	});
-}
-#endif
-
 template <class T>
 Future<Void> storeOrThrow(T& out, Future<Optional<T>> what, Error e = key_not_found()) {
 	return map(what, [&out, e](Optional<T> const& o) {
