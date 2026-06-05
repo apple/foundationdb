apple/foundationdb

commit 49bd7f489c2accc066698a8feb447c2036407457

64-bit DeterministicRandom() seed and getRandomSeed() (#13136)
DeterministicRandom() can use a boost::random::mt19937_64
to take a 64-bit seed and return 64-bit random values,
and this simplifies things because fdb always wants 64 bits.
Performance should be slightly better because the 32-bit
mt19937 was always called twice to get 64 bits.

flow/Platform.cpp
@@ -171,6 +171,8 @@ static_assert(std::is_same<boost::asio::ip::address_v6::bytes_type, std::array<u
 #include <netinet/in.h>
 #include <sys/mount.h>
 #include <sys/param.h>
+/* Needed for getentropy() */
+#include <sys/random.h>
 #include <sys/sysctl.h>
 #include <sys/syslimits.h>
 #include <sys/uio.h>
@@ -2202,29 +2204,22 @@ void setAffinity(int proc) {
 
 namespace platform {
 
-int getRandomSeed() {
+uint64_t getRandomSeed() {
 	INJECT_FAULT(platform_error, "getRandomSeed"); // getting a random seed failed
-	int randomSeed;
-
+	uint64_t randomSeed;
 #ifdef _WIN32
-	if (rand_s((unsigned int*)&randomSeed) != 0) {
+	uint32_t high, low;
+	if (rand_s(&low) != 0 || rand_s(&high) != 0 {
 		TraceEvent(SevError, "WindowsRandomSeedError").log();
 		throw platform_error();
 	}
+	randomSeed = (uint64_t(high) << 32) | uint64_t(low);
 #else
-	int devRandom = open("/dev/urandom", O_RDONLY | O_CLOEXEC);
-	if (devRandom == -1) {
-		TraceEvent(SevError, "OpenURandom").GetLastError();
-		throw platform_error();
-	}
-	int nbytes = read(devRandom, &randomSeed, sizeof(randomSeed));
-	close(devRandom);
-	if (nbytes != sizeof(randomSeed)) {
-		TraceEvent(SevError, "ReadURandom").GetLastError();
+	if (getentropy(&randomSeed, sizeof(randomSeed)) != 0) {
+		TraceEvent(SevError, "GetEntropyError").GetLastError();
 		throw platform_error();
 	}
 #endif
-
 	return randomSeed;
 }
 
@@ -2256,6 +2251,7 @@ void getRandomBytes(void* buf, size_t len) {
 	}
 #endif
 }
+
 } // namespace platform
 
 std::string joinPath(std::string const& directory, std::string const& filename) {

fdbclient/MultiVersionTransaction.cpp
@@ -2237,7 +2237,7 @@ void MultiVersionApi::setupNetwork() {
 			}
 
 			if (!bypassMultiClientApi) {
-				platform::getRandomBytes(&transportId, sizeof(transportId));
+				transportId = platform::getRandomSeed();
 				if (transportId <= 1)
 					transportId += 2;
 				localClient->api->setNetworkOption(FDBNetworkOptions::EXTERNAL_CLIENT_TRANSPORT_ID,

fdbrpc/bench/BenchIONet2.actor.cpp
@@ -49,7 +49,7 @@ static inline TaskPriority getRandomTaskPriority(DeterministicRandom& rand) {
 ACTOR static Future<Void> benchIONet2Actor(benchmark::State* benchState) {
 	state size_t actorCount = benchState->range(0);
 	state uint32_t sum;
-	state int seed = platform::getRandomSeed();
+	state uint64_t seed = platform::getRandomSeed();
 	state std::unique_ptr<char[]> data(new char[4096]);
 	memset(data.get(), 0, 4096);
 	while (benchState->KeepRunning()) {

fdbserver/SimulatedCluster.cpp
@@ -2687,7 +2687,7 @@ void setupSimulatedSystem(std::vector<Future<Void>>* systemActors,
 using namespace std::literals;
 
 // Actor that waits for a specified simulation time and then resets the random seed
-Future<Void> reseedRandomAtTime(double waitTime, uint32_t newSeed) {
+Future<Void> reseedRandomAtTime(double waitTime, uint64_t newSeed) {
 	co_await delay(waitTime);
 	TraceEvent("ResettingRandomSeed").detail("WaitTime", waitTime).detail("NewSeed", newSeed);
 	deterministicRandom()->resetSeed(newSeed);
@@ -2842,7 +2842,7 @@ static Future<Void> simulationSetupAndRunImpl(std::string dataFolder,
 		// If reseedTime is set, schedule a random seed reset at a random time between [0, reseedTime]
 		if (reseedTime >= 0.0) {
 			double actualReseedTime = nondeterministicRandom()->random01() * reseedTime;
-			uint32_t newSeed = platform::getRandomSeed();
+			uint64_t newSeed = platform::getRandomSeed();
 			TraceEvent("SchedulingRandomSeedReset")
 			    .detail("ReseedTimeMax", reseedTime)
 			    .detail("ActualReseedTime", actualReseedTime)

fdbserver/fdbserver.actor.cpp
@@ -1068,7 +1068,7 @@ struct CLIOptions {
 	bool maxLogsSet = false;
 
 	ServerRole role = ServerRole::FDBD;
-	uint32_t randomSeed = platform::getRandomSeed();
+	uint64_t randomSeed = platform::getRandomSeed();
 	double reseedTime = -1.0; // Time in seconds when to reset random seed in simulation (-1 = disabled)
 
 	const char* testFile = "tests/default.txt";
@@ -1539,7 +1539,7 @@ private:
 				break;
 			case OPT_RANDOMSEED: {
 				char* end;
-				randomSeed = (uint32_t)strtoul(args.OptionArg(), &end, 0);
+				randomSeed = strtoull(args.OptionArg(), &end, 0);
 				if (*end) {
 					fprintf(stderr, "ERROR: Could not parse random seed `%s'\n", args.OptionArg());
 					printHelpTeaser(argv[0]);
@@ -1975,7 +1975,7 @@ int main(int argc, char* argv[]) {
 		const auto role = opts.role;
 
 		if (role == ServerRole::Simulation) {
-			printf("Random seed is %u...\n", opts.randomSeed);
+			printf("Random seed is %llu...\n", opts.randomSeed);
 			bindDeterministicRandomToOpenssl();
 		}
 

flow/DeterministicRandom.cpp
@@ -27,25 +27,14 @@
 
 uint64_t DeterministicRandom::gen64() {
 	uint64_t curr = next;
-
-	// RE: the previous implementation of this function: order of
-	// evaluation of arguments to the ^ operator is not specified, so
-	// two rng() calls in the same ^ expression may not produce a
-	// consistent 64-bit value across compilations, because we don't
-	// know if the first call was used for the higher order bits and
-	// the second call for the low order bits, or vice-versa.
-	// See https://en.cppreference.com/w/cpp/language/eval_order.html
-	next = (uint64_t(rng()) << 32);
-	next ^= rng();
+	next = rng();
 	if (TRACE_SAMPLE())
 		TraceEvent(SevSample, "Random").log();
 	return curr;
 }
 
-DeterministicRandom::DeterministicRandom(uint32_t seed, bool useRandLog)
-  : rng((unsigned long)seed), next(0), useRandLog(useRandLog) {
-	next = (uint64_t(rng()) << 32);
-	next ^= rng();
+DeterministicRandom::DeterministicRandom(uint64_t seed, bool useRandLog) : rng(seed), next(0), useRandLog(useRandLog) {
+	next = rng();
 }
 
 double DeterministicRandom::random01() {
@@ -164,10 +153,9 @@ uint64_t DeterministicRandom::peek() const {
 	return next;
 }
 
-void DeterministicRandom::resetSeed(uint32_t seed) {
-	rng.seed((unsigned long)seed);
-	next = (uint64_t(rng()) << 32);
-	next ^= rng();
+void DeterministicRandom::resetSeed(uint64_t seed) {
+	rng.seed(seed);
+	next = rng();
 }
 
 void DeterministicRandom::addref() {

flow/FlowTest.cpp
@@ -48,7 +48,7 @@ struct FlowTestOptions {
 	std::string testPattern;
 	std::vector<std::string> testsIgnored;
 	std::string dataDir = "flow_test_data";
-	uint32_t randomSeed = 0;
+	uint64_t randomSeed = 0;
 	int maxTestCases = -1;
 	bool cleanupAfterTests = true;
 	bool listTests = false;
@@ -112,13 +112,13 @@ bool parseInt(const char* text, int* value) {
 	return true;
 }
 
-bool parseUInt32(const char* text, uint32_t* value) {
+bool parseUInt64(const char* text, uint64_t* value) {
 	char* end = nullptr;
-	unsigned long parsed = strtoul(text, &end, 10);
-	if (*text == '\0' || *end != '\0' || parsed > UINT32_MAX) {
+	uint64_t parsed = strtoull(text, &end, 10);
+	if (*text == '\0' || *end != '\0') {
 		return false;
 	}
-	*value = static_cast<uint32_t>(parsed);
+	*value = parsed;
 	return true;
 }
 
@@ -161,7 +161,7 @@ bool parseArgs(int argc, char** argv, FlowTestOptions* options) {
 			options->dataDir = args.OptionArg();
 			break;
 		case OPT_SEED:
-			if (!parseUInt32(args.OptionArg(), &options->randomSeed)) {
+			if (!parseUInt64(args.OptionArg(), &options->randomSeed)) {
 				fmt::print(stderr, "ERROR: --seed requires a uint32 value\n");
 				return false;
 			}
@@ -332,7 +332,7 @@ int main(int argc, char** argv) {
 	fmt::print(stdout, "Random seed is {}\n", options.randomSeed);
 
 	std::string originalWorkingDirectory = platform::getWorkingDirectory();
-	std::string runDirectory = joinPath("/tmp", format("flow_test.%d.%u", ::getpid(), options.randomSeed));
+	std::string runDirectory = joinPath("/tmp", format("flow_test.%d.%llu", ::getpid(), options.randomSeed));
 	platform::createDirectory(runDirectory);
 	if (::chdir(runDirectory.c_str()) != 0) {
 		fmt::print(stderr, "ERROR: Could not chdir to {}\n", runDirectory);

flow/bench/BenchNet2.cpp
@@ -41,7 +41,7 @@ static inline TaskPriority getRandomTaskPriority(DeterministicRandom& rand) {
 static Future<Void> benchNet2Actor(benchmark::State* benchState) {
 	size_t actorCount = benchState->range(0);
 	uint32_t sum;
-	int seed = platform::getRandomSeed();
+	uint64_t seed = platform::getRandomSeed();
 	while (benchState->KeepRunning()) {
 		sum = 0;
 		std::vector<Future<Void>> futures;

commit 6aa687e1b5467f4a86cec7ae402450c58fa1af32

fix: platform::getRandomSeed() win32 code missing parenthesis
bug introduced in 49bd7f489c - apple/foundationdb#13136

flow/Platform.cpp
@@ -2209,7 +2209,7 @@ uint64_t getRandomSeed() {
 	uint64_t randomSeed;
 #ifdef _WIN32
 	uint32_t high, low;
-	if (rand_s(&low) != 0 || rand_s(&high) != 0 {
+	if (rand_s(&low) != 0 || rand_s(&high) != 0) {
 		TraceEvent(SevError, "WindowsRandomSeedError").log();
 		throw platform_error();
 	}