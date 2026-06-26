231f3262e544ab9f9d4dc3086d2b918ec6a48bd3
Syed Paymaan Raza <1238752+spraza@users.noreply.github.com>
2026-06-26 15:33:11 -0700

    review feedback

--- fdbserver/SimulatedCluster.actor.cpp
+++ fdbserver/SimulatedCluster.actor.cpp
@@ -2515,7 +2515,7 @@ void setupSimulatedSystem(std::vector<Future<Void>>* systemActors,
 
 		// FIXME: we hardcode some machines to specifically test storage cache and blob workers
 		// TODO: caching disabled for this merge
-		int storageCacheMachines = testConfig.storageCacheMachinesInDC0.orDefault(dc == 0 ? 1 : 0);
+		int storageCacheMachines = dc == 0 ? testConfig.storageCacheMachinesInDC0.orDefault(1) : 0;
 		int blobWorkerMachines = 0;
 		int simHTTPMachines = 0;
 		if (testConfig.blobGranulesEnabled) {
