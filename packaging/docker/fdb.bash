522ed4c0e6aa5135cf9f088efdb43583ed88e09c
Elly Dameki <elly.dameki@tuta.io>
2026-08-06 16:07:11 -0400

    fdb.bash enhancements
    - Now passes arguments from docker/podman run to the spawned fdbserver process.
    - Now allows the usage of an mounted fdb.cluster file (such as one stored as a docker/podman secret!)
    - Lukewarm improvements to error messages in some fail states (i.e. cluster file contents defined but target read-only).
    - IPv6 addresses from `hostname -i` are now properly wrapped in brackets.
    - Public IP is now exported directly instead of creating a temporary .fdbenv file.

--- packaging/docker/fdb.bash
+++ packaging/docker/fdb.bash
@@ -26,34 +26,43 @@ function create_cluster_file() {
 
     if [[ -n "$FDB_CLUSTER_FILE_CONTENTS" ]]; then
         echo "$FDB_CLUSTER_FILE_CONTENTS" > "$FDB_CLUSTER_FILE"
+        if [[ $? != 0 ]]; then
+            echo "FDB_CLUSTER_FILE_CONTENTS is defined, but cannot write to FDB_CLUSTER_FILE ($FDB_CLUSTER_FILE). Is something mounted read-only there?"
+            exit 1
+        fi
     elif [[ -n $FDB_COORDINATOR ]]; then
         coordinator_ip=$(dig +short "$FDB_COORDINATOR")
         if [[ -z "$coordinator_ip" ]]; then
             echo "Failed to look up coordinator address for $FDB_COORDINATOR" 1>&2
             exit 1
         fi
-        coordinator_port=${FDB_COORDINATOR_PORT:-4500}
-        echo "docker:docker@$coordinator_ip:$coordinator_port" > "$FDB_CLUSTER_FILE"
-    else
-        echo "FDB_COORDINATOR environment variable not defined" 1>&2
+        echo "docker:docker@$coordinator_ip:$FDB_COORDINATOR_PORT" > "$FDB_CLUSTER_FILE"
+    elif [[ ! -r "$FDB_CLUSTER_FILE" ]]; then
+        echo "Neither FDB_CLUSTER_FILE_CONTENTS nor FDB_COORDINATOR are set, but no readable cluster file is at FDB_CLUSTER_FILE ($FDB_CLUSTER_FILE)."
         exit 1
+    else
+        echo "Using existing FDB_CLUSTER_FILE at $FDB_CLUSTER_FILE"
     fi
 }
 
 function create_server_environment() {
-    env_file=/var/fdb/.fdbenv
-
     if [[ "$FDB_NETWORKING_MODE" == "host" ]]; then
         public_ip=127.0.0.1
     elif [[ "$FDB_NETWORKING_MODE" == "container" ]]; then
         public_ip=$(hostname -i | awk '{print $1}')
+        
+        # IPv6 addresses need to be enclosed in brackets
+        if [[ $public_ip == *":"* ]]; then
+            public_ip="[$public_ip]"
+        fi
     else
         echo "Unknown FDB Networking mode \"$FDB_NETWORKING_MODE\"" 1>&2
         exit 1
     fi
 
-    echo "export PUBLIC_IP=$public_ip" > $env_file
-    if [[ -z $FDB_COORDINATOR && -z "$FDB_CLUSTER_FILE_CONTENTS" ]]; then
+    export PUBLIC_IP=$public_ip
+    # Set default cluster file contents only if no configuration exists already.
+    if [[ (! -s "$FDB_CLUSTER_FILE") && -z $FDB_COORDINATOR && -z "$FDB_CLUSTER_FILE_CONTENTS" ]]; then
         FDB_CLUSTER_FILE_CONTENTS="docker:docker@$public_ip:$FDB_PORT"
     fi
 
@@ -61,8 +70,8 @@ function create_server_environment() {
 }
 
 create_server_environment
-source /var/fdb/.fdbenv
 echo "Starting FDB server on $PUBLIC_IP:$FDB_PORT"
 fdbserver --listen-address 0.0.0.0:"$FDB_PORT" --public-address "$PUBLIC_IP:$FDB_PORT" \
     --datadir /var/fdb/data --logdir /var/fdb/logs \
-    --locality-zoneid="$(hostname)" --locality-machineid="$(hostname)" --class "$FDB_PROCESS_CLASS" --knob_disable_posix_kernel_aio=1
+    --locality-zoneid="$(hostname)" --locality-machineid="$(hostname)" --class "$FDB_PROCESS_CLASS" --knob_disable_posix_kernel_aio=1 \
+    $@
