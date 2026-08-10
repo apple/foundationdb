3965b0a3933d409c1fbe049937ef83b8e6d03bd4
Elly Dameki <elly.dameki@tuta.io>
2026-08-10 16:31:11 -0400

    Read-only clusterfile error message more terse
    May as well sneak this in before the workflow is approved

--- packaging/docker/fdb.bash
+++ packaging/docker/fdb.bash
@@ -27,7 +27,7 @@ function create_cluster_file() {
 
     if [[ -n "$FDB_CLUSTER_FILE_CONTENTS" || -n "$FDB_COORDINATOR" ]]; then
         if [[ -f "$FDB_CLUSTER_FILE" && (! -w  "$FDB_CLUSTER_FILE") ]]; then
-            echo "Clusterfile to be overwritten at \"$FDB_CLUSTER_FILE\" already exists but is read-only." 1>&2
+            echo "Clusterfile to be overwritten at \"$FDB_CLUSTER_FILE\" is read-only." 1>&2
             exit 1
         elif [[ -r "$FDB_CLUSTER_FILE" ]]; then
             echo "Overwriting existing clusterfile." 1>&2
