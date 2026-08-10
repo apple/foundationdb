d3c924c46b048633576306b8f795ac7ed0c79ef9
Elly Dameki <elly.dameki@tuta.io>
2026-08-10 18:14:19 -0400

    create_cluster_file simplifications
    If FDB_COORDINATOR is set, then instead of writing to file directly, store changes in FDB_CLUSTER_FILE_CONTENTS so we can check for overwrites all at once.
    
    Also reworded generic error message, as per suggestion.

--- packaging/docker/fdb.bash
+++ packaging/docker/fdb.bash
@@ -24,26 +24,21 @@ function create_cluster_file() {
     FDB_CLUSTER_FILE=${FDB_CLUSTER_FILE:-/etc/foundationdb/fdb.cluster}
     mkdir -p "$(dirname $FDB_CLUSTER_FILE)"
 
-
-    if [[ -n "$FDB_CLUSTER_FILE_CONTENTS" || -n "$FDB_COORDINATOR" ]]; then
-        if [[ -f "$FDB_CLUSTER_FILE" && (! -w  "$FDB_CLUSTER_FILE") ]]; then
-            echo "Clusterfile to be overwritten at \"$FDB_CLUSTER_FILE\" is read-only." 1>&2
-            exit 1
-        elif [[ -r "$FDB_CLUSTER_FILE" ]]; then
-            echo "Overwriting existing clusterfile." 1>&2
-        fi
-    fi
-
-    if [[ -n "$FDB_CLUSTER_FILE_CONTENTS" ]]; then
-        echo "$FDB_CLUSTER_FILE_CONTENTS" > "$FDB_CLUSTER_FILE"
-    elif [[ -n $FDB_COORDINATOR ]]; then
+    if [[ -n $FDB_COORDINATOR ]]; then
         coordinator_ip=$(dig +short "$FDB_COORDINATOR")
         if [[ -z "$coordinator_ip" ]]; then
             echo "Failed to look up coordinator address for $FDB_COORDINATOR" 1>&2
             exit 1
         fi
         coordinator_port=${FDB_COORDINATOR_PORT:-4500}
-        echo "docker:docker@$coordinator_ip:$coordinator_port" > "$FDB_CLUSTER_FILE"
+        FDB_CLUSTER_FILE_CONTENTS="docker:docker@$coordinator_ip:$coordinator_port"
+    fi
+
+    if [[ -n "$FDB_CLUSTER_FILE_CONTENTS" ]]; then
+        if [[ -w "$FDB_CLUSTER_FILE" ]]; then
+            echo "Overwriting existing clusterfile." 1>&2
+        fi
+        echo "$FDB_CLUSTER_FILE_CONTENTS" > "$FDB_CLUSTER_FILE"
     elif [[ ! -r "$FDB_CLUSTER_FILE" ]]; then
         echo "FDB_CLUSTER_FILE_CONTENTS and FDB_COORDINATOR are not set, and no clusterfile at \"$FDB_CLUSTER_FILE\"." 1>&2
         exit 1
@@ -52,7 +47,7 @@ function create_cluster_file() {
     fi
 
     if (( $? != 0 )); then
-        echo "An unexpected error occurred while setting configuration." 1>&2
+        echo "Unable to write to FDB_CLUSTER_FILE." 1>&2
         exit 1
     fi
 }
