8df70aa06b0ad2eae2b263f7b637d6e17c229716
Trevor Clinkenbeard <tclinkenbeard@openai.com>
2026-09-06 11:17:55 -0700

    Consume incompatible peer reports through transport API

--- fdbrpc/include/fdbrpc/FlowTransport.h
+++ fdbrpc/include/fdbrpc/FlowTransport.h
@@ -26,6 +26,7 @@
 #include <map>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 
 #include "fdbrpc/DDSketch.h"
 #include "flow/genericactors.h"
@@ -237,10 +238,11 @@ public:
 	// Returns all peers that the FlowTransport is monitoring.
 	const std::unordered_map<NetworkAddress, Reference<Peer>>& getAllPeers() const;
 
-	// Returns the set of all peers that have attempted to connect, but have incompatible protocol versions
-	std::map<NetworkAddress, std::pair<uint64_t, double>>* getIncompatiblePeers();
+	// Returns and removes peers whose incompatible protocol versions have persisted long enough to report.
+	// Peers later recognized as multi-version connections are discarded without reporting.
+	std::vector<NetworkAddress> consumeReportableIncompatiblePeers();
 
-	// Returns when getIncompatiblePeers has at least one peer which is incompatible.
+	// Returns when an incompatible peer has persisted long enough to report.
 	Future<Void> onIncompatibleChanged();
 
 	// Signal that a peer connection is being used, even if no messages are currently being sent to the peer
