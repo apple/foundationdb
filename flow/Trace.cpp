4bbbdad66ed03fa8f6c61734ef529d98a8e0c080
Trevor Clinkenbeard <tclinkenbeard@openai.com>
2026-09-09 09:21:07 -0700

    Retire orphaned AcAC actor stack mode

--- flow/Trace.cpp
+++ flow/Trace.cpp
@@ -18,7 +18,6 @@
  * limitations under the License.
  */
 
-#include "flow/ActorContext.h"
 #include "flow/Trace.h"
 #include "FileTraceLogWriter.h"
 #include "flow/Knobs.h"
@@ -1353,9 +1352,6 @@ void BaseTraceEvent::writeEvent() {
 				if (this->severity == SevError) {
 					severity = SevInfo;
 					backtrace();
-#ifdef WITH_ACAC
-					detail("ActorStack", encodeActorContext(ActorContextDumpType::CURRENT_CALL_BACKTRACE));
-#endif // WITH_ACAC
 					severity = SevError;
 					if (errorKindIndex != -1) {
 						fields.mutate(errorKindIndex).second = toString(errorKind);
