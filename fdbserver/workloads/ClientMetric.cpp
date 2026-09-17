7764cf0497155c4419316e07852d3dccf119c1d5
Trevor Clinkenbeard <tclinkenbeard@openai.com>
2026-09-16 23:51:07 -0700

    Reset ClientMetric retry state between independent writes

--- fdbserver/workloads/ClientMetric.cpp
+++ fdbserver/workloads/ClientMetric.cpp
@@ -145,12 +145,19 @@ struct ClientMetricWorkload : TestWorkload {
 	Future<Void> writeRandomKeys(Database cx, int total) {
 		int cnt = 0;
 		Transaction tr(cx);
+		bool startNewTransaction = false;
 		try {
 			while (true) {
 				Error err;
 				try {
 					co_await delay(0.001);
-					tr.reset();
+					if (startNewTransaction) {
+						// Independent writes must not inherit the previous transaction's retry backoff.
+						tr.fullReset();
+						startNewTransaction = false;
+					} else {
+						tr.reset();
+					}
 					tr.set(Key(deterministicRandom()->randomAlphaNumeric(10)),
 					       Value(Key(deterministicRandom()->randomAlphaNumeric(10))));
 					co_await tr.commit();
@@ -158,6 +165,7 @@ struct ClientMetricWorkload : TestWorkload {
 						break;
 					}
 					++cnt;
+					startNewTransaction = true;
 				} catch (Error& e) {
 					err = e;
 				}
