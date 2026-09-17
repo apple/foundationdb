776f3d95b48538ead625f309974fa51685b2985c
Pierce Lopez <pierce.lopez@apple.com>
2026-09-17 04:08:28 -0400

    build: always compress debug section, cleanup debug level  (#13904)
    Debug section compression "-gz" was disabled for FDB_RELEASE
    because of compatibility with cpack RPM gen. This was due
    to "debugedit" being used, but the version in rhel9 has
    compressed debuginfo support. Compression helps a lot, we want it.
    
    The "ggdb" flag meant basically "g3" with the highest level of dwarf supported.
    Instead, use gdwarf-4 or gdwarf-5 with level g3 or g2 or g1, explicitly.
    Use g3 only for FULL_DEBUG_SYMBOLS, g2 only for Debug / RelWithDebInfo.
    
    For FDB_RELEASE, use just g1 for the smallest reasonable debuginfo,
    so it is more practical to deploy for more regular use-cases.
    
    Also:
     * move ENABLE_LONG_RUNNING_TESTS to tests/
     * move USE_SCCACHE to ConfigureCompiler.cmake
     * move FDB_RELEASE(_CANDIDATE) to main CMakeLists.txt
     * limit parallel link jobs to 4 to avoid OOM

--- cmake/InstallLayout.cmake
+++ cmake/InstallLayout.cmake
@@ -44,7 +44,7 @@ set(CPACK_PROJECT_CONFIG_FILE "${CMAKE_BINARY_DIR}/packaging/CPackConfig.cmake")
 # User config
 ################################################################################
 
-set(GENERATE_DEBUG_PACKAGES "${FDB_RELEASE}" CACHE BOOL "Build debug rpm/deb packages (default: only ON for FDB_RELEASE)")
+set(GENERATE_DEBUG_PACKAGES ON CACHE BOOL "Build debug rpm/deb packages")
 
 ################################################################################
 # Alternatives config
@@ -142,6 +142,7 @@ string(REPLACE "-" "_" FDB_PACKAGE_VERSION ${FDB_VERSION})
 set(CPACK_RPM_PACKAGE_GROUP                                ${CURRENT_GIT_VERSION})
 set(CPACK_RPM_PACKAGE_LICENSE                              "Apache 2.0")
 set(CPACK_RPM_PACKAGE_NAME                                 "foundationdb")
+
 set(CPACK_RPM_CLIENTS-EL9_PACKAGE_NAME                     "${CPACK_RPM_PACKAGE_NAME}-clients")
 set(CPACK_RPM_CLIENTS-EL9_FILE_NAME                        "${CPACK_RPM_CLIENTS-EL9_PACKAGE_NAME}-${FDB_PACKAGE_VERSION}${package_version_postfix}.el9.${CMAKE_SYSTEM_PROCESSOR}.rpm")
 set(CPACK_RPM_CLIENTS-EL9_DEBUGINFO_FILE_NAME              "${CPACK_RPM_CLIENTS-EL9_PACKAGE_NAME}-${FDB_PACKAGE_VERSION}${package_version_postfix}.el9-debuginfo.${CMAKE_SYSTEM_PROCESSOR}.rpm")
@@ -173,11 +174,13 @@ set(CPACK_RPM_SERVER-VERSIONED_PACKAGE_REQUIRES            "${CPACK_COMPONENT_CL
 set(CPACK_RPM_SERVER-VERSIONED_POST_INSTALL_SCRIPT_FILE    ${CMAKE_BINARY_DIR}/packaging/multiversion/server/postinst-rpm)
 set(CPACK_RPM_SERVER-VERSIONED_PRE_UNINSTALL_SCRIPT_FILE   ${CMAKE_BINARY_DIR}/packaging/multiversion/server/prerm)
 
-# Versioned packages must not own RPM's global build-id links. Two otherwise
-# independent client packages can contain identical binaries, and those links
-# would make the packages conflict outside their versioned install tree.
-set(CPACK_RPM_SPEC_MORE_DEFINE
-    "%if \\\"%{name}\\\" == \\\"${CPACK_RPM_CLIENTS-VERSIONED_PACKAGE_NAME}\\\"\n%define _build_id_links none\n%endif")
+# Avoid VERSIONED client package conflicting with main client package of same exact version,
+# due to the build-id links in /usr/lib/.build-id/
+set(CPACK_RPM_SPEC_MORE_DEFINE "
+%if \\\"%{name}\\\" == \\\"${CPACK_RPM_CLIENTS-VERSIONED_PACKAGE_NAME}\\\"
+%define _build_id_links none
+%endif
+")
 
 file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/packaging/emptydir")
 fdb_install(DIRECTORY "${CMAKE_BINARY_DIR}/packaging/emptydir/" DESTINATION data COMPONENT server)
@@ -191,8 +194,6 @@ set(CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST_ADDITION
   "/usr/lib64/cmake"
   "/etc/foundationdb"
   "/usr/lib64/pkgconfig"
-  "/usr/lib64/python2.7"
-  "/usr/lib64/python2.7/site-packages"
   "/var"
   "/var/log"
   "/var/lib"
@@ -204,10 +205,9 @@ set(CPACK_RPM_EXCLUDE_FROM_AUTO_FILELIST_ADDITION
   "/usr/lib/foundationdb"
   "/usr/lib/cmake"
   "/usr/lib/foundationdb-${FDB_VERSION}${FDB_BUILDTIME_STRING}/etc/foundationdb"
-  )
+)
 set(CPACK_RPM_BUILD_SOURCE_DIRS_PREFIX "/usr/src")
 set(CPACK_RPM_DEBUGINFO_PACKAGE ${GENERATE_DEBUG_PACKAGES})
-#set(CPACK_RPM_BUILD_SOURCE_FDB_INSTALL_DIRS_PREFIX /usr/src)
 set(CPACK_RPM_COMPONENT_INSTALL ON)
 
 ################################################################################
