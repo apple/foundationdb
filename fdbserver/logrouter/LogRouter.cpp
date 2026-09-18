daa00c062d39a873a1e89669834a1389bf1e0025
stack <stack@duboce.com>
2026-09-17 18:57:51 -0700

    Stop the LogRouter warning that fires on successful peeks

--- fdbserver/logrouter/LogRouter.cpp
+++ fdbserver/logrouter/LogRouter.cpp
@@ -427,8 +427,10 @@ Future<Reference<IReplayPeekCursor>> LogRouterData::getPeekCursorData(Reference<
 			              .trackLatest(eventCacheHolder->trackingKey);
 			          // If no primary peek location after many tries, flag an error for manual intervention.
 			          // The LR may become a bottleneck on the system and need to be excluded.
-			          noPrimaryPeekLocation = primaryPeekLocation.present() ? 0 : ++noPrimaryPeekLocation;
-			          if (!(noPrimaryPeekLocation % 4)) {
+			          // Only consecutive absences count.
+			          if (primaryPeekLocation.present()) {
+				          noPrimaryPeekLocation = 0;
+			          } else if (!(++noPrimaryPeekLocation % 4)) {
 				          TraceEvent(SevWarnAlways, "NoPrimaryPeekLocationForLR", dbgid);
 			          }
 		          })
