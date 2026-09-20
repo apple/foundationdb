a98362a2fc286d653d03d7a4e10ff0f874ffab15
Trevor Clinkenbeard <tclinkenbeard@openai.com>
2026-09-19 22:58:21 -0700

    Remove redundant ErrorName trace details

--- fdbserver/mocks3/MockS3ServerChaos.cpp
+++ fdbserver/mocks3/MockS3ServerChaos.cpp
@@ -342,10 +342,7 @@ Future<Void> registerMockS3ChaosServer(std::string ip, std::string port) {
 		    .detail("TotalRegistered", registeredMockS3ChaosServers().size());
 
 	} catch (Error& e) {
-		TraceEvent(SevError, "MockS3ChaosServerRegistrationFailed")
-		    .error(e)
-		    .detail("Address", serverKey)
-		    .detail("ErrorName", e.name());
+		TraceEvent(SevError, "MockS3ChaosServerRegistrationFailed").error(e).detail("Address", serverKey);
 		throw;
 	}
 }
