3aa37282067abaded587419e5cc2a29cf6b9bd29
Jingyu Zhou <jingyuzhou@gmail.com>
2026-09-21 13:45:28 -0700

    Fix CP admission control (#14090)
    * Fix CP admission control
    
    getKeyLocation_internal() constructs the request with a hardcoded limit of 100,
    which is exactly STORAGE_METRICS_SHARD_LIMIT. This means the admission control
    is never used.
    
    The problem is after a bounce, all clients will send this request to CP. So if
    the cluster is large enough and has a lot of traffic, key location request can
    saturate CP CPU, leading to a metastable failure state.
    
    * Format
    
    * Fix format

--- fdbserver/commitproxy/CommitProxyServer.cpp
+++ fdbserver/commitproxy/CommitProxyServer.cpp
@@ -2303,10 +2303,9 @@ static Future<Void> readRequestServer(CommitProxyInterface proxy,
 	while (true) {
 		GetKeyServerLocationsRequest req = co_await proxy.getKeyServersLocations.getFuture();
 		// WARNING: this code is run at a high priority, so it needs to do as little work as possible
-		if (req.limit != CLIENT_KNOBS->STORAGE_METRICS_SHARD_LIMIT && // Always do data distribution requests
-		    (commitData->stats.keyServerLocationIn.getValue() - commitData->stats.keyServerLocationOut.getValue() >
-		         SERVER_KNOBS->KEY_LOCATION_MAX_QUEUE_SIZE ||
-		     (g_network->isSimulated() && buggify(0.001)))) {
+		if (commitData->stats.keyServerLocationIn.getValue() - commitData->stats.keyServerLocationOut.getValue() >
+		        SERVER_KNOBS->KEY_LOCATION_MAX_QUEUE_SIZE ||
+		    (g_network->isSimulated() && buggify(0.001))) {
 			++commitData->stats.keyServerLocationErrors;
 			req.reply.sendError(commit_proxy_memory_limit_exceeded());
 			TraceEvent(SevWarnAlways, "ProxyLocationRequestThresholdExceeded").suppressFor(60);
